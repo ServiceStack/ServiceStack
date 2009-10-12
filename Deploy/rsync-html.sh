8b1ca7ecc1589fc5f67e1f0f095c2d46026574f7
Demis Bellot <demis.bellot@gmail.com>
2009-10-12 23:05:05 +0000

    commit deploy scripts

--- Deploy/rsync-html.sh
+++ Deploy/rsync-html.sh
@@ -9,3 +9,8 @@ rsync --verbose  --progress --stats --compress \
       --recursive --times --perms --links \
       $BASE_PATH/html/ $HOST_LOGIN:html/servicestack.net/
 
+rsync --verbose  --progress --stats --compress \
+      -e "ssh -i $SERVER_KEY" \
+      --recursive --times --perms --links --delete \
+      --exclude ".svn" \
+      $BASE_PATH/MonoTouch.Examples/html/ $HOST_LOGIN:html/servicestack.net/monotouch
