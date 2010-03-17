0c6979ae53792821cbe451abc664027d6e1632ee
Demis Bellot <demis.bellot@gmail.com>
2010-03-17 22:09:19 +0000

    Added new project to test Standalone.Redis.ConsoleApp, new servicestack release dlls

---
+++ Common/Release.Tests/Standalone.Redis.ConsoleApp/Standalone.Redis.ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.Redis;
+
+namespace Standalone.Redis.ConsoleApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var redisClient = new RedisClient("chi-dev-mem1"))
+			{
+				redisClient.Set("release-test", "works");
+				var result = redisClient.Get<string>("release-test");
+				Console.WriteLine("Result: " + result);
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
--- Common/ServiceStack.Redis/Build/ilmerge-all.bat
+++ Common/ServiceStack.Redis/Build/ilmerge-all.bat
@@ -8,8 +8,11 @@ REM SET BUILD=Debug
 SET BUILD=Release
 
 SET PROJ_LIBS=
+SET PROJ_LIBS=%PROJ_LIBS% ..\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Interfaces.dll
 SET PROJ_LIBS=%PROJ_LIBS% ..\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Client.dll
 SET PROJ_LIBS=%PROJ_LIBS% ..\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Common.dll
+SET PROJ_LIBS=%PROJ_LIBS% ..\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Text.dll
+SET PROJ_LIBS=%PROJ_LIBS% ..\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Messaging.dll
 SET PROJ_LIBS=%PROJ_LIBS% ..\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.dll
 
 %ILMERGE_UTIL% /ndebug /t:library /out:ServiceStack.Redis.dll %PROJ_LIBS%
