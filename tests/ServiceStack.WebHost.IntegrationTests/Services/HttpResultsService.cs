9ebde329048c8cf62549df625b9dc28f3c8afd76
mythz <demis.bellot@gmail.com>
2011-10-21 13:46:50 -0400

    Test HttpResults behaviour

---
+++ tests/ServiceStack.WebHost.IntegrationTests/Services/HttpResultsService.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Net;
+using System.Runtime.Serialization;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceInterface.ServiceModel;
+
+namespace ServiceStack.WebHost.IntegrationTests.Services
+{
+    [RestService("/httpresults")]
+	[DataContract]
+	public class HttpResults
+	{
+		[DataMember]
+		public string Name { get; set; }
+	}
+
+	[DataContract]
+	public class HttpResultsResponse
+		: IHasResponseStatus
+	{
+        public HttpResultsResponse()
+		{
+			this.ResponseStatus = new ResponseStatus();
+		}
+
+		[DataMember]
+		public string Result { get; set; }
+
+		[DataMember]
+		public ResponseStatus ResponseStatus { get; set; }
+	}
+
+    public class HttpResultsService
+        : ServiceBase<HttpResults>
+	{
+        protected override object Run(HttpResults request)
+		{
+            if (request.Name == "Error")
+                throw new HttpError(HttpStatusCode.NotFound, "Error NotFound");
+
+            return new HttpResult(HttpStatusCode.NotFound, "Returned NotFound");
+		}
+	}
+}
