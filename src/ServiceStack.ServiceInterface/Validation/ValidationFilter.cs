57917434669a8e36d19f82b7c63e123f310a5022
Steffen M <arxisos@googlemail.com>
2011-12-23 11:16:00 +0100

    Added request dto validation mechanism with fluent validation

---
+++ src/ServiceStack.ServiceInterface/Validation/ValidationFilter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.ServiceHost;
+using FluentValidation;
+
+namespace ServiceStack.ServiceInterface.Validation
+{
+    public class ValidationFilter
+    {
+        public void ValidateRequest<T>(IHttpRequest req, IHttpResponse res, T requestDto)
+        {
+            IValidator<T> validator = req.TryResolve<IValidator<T>>();
+            if (validator != null)
+            {
+                var validationResult = validator.Validate(requestDto, ruleSet: req.HttpMethod);
+                if (!validationResult.IsValid)
+                    validationResult.Throw();
+            }
+        }
+    }
+}
---
+++ src/ServiceStack.ServiceInterface/Validation/ValidationResultExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ServiceStack.Validation;
+using FluentValidation.Results;
+
+namespace ServiceStack.ServiceInterface.Validation
+{
+    public static class ValidationResultExtensions
+    {
+        public static void Throw(this ValidationResult result)
+        {
+            SerializableValidationResult validationResult = new SerializableValidationResult();
+            foreach (var error in result.Errors)
+                validationResult.Errors.Add(new SerializableValidationError(error.ErrorMessage, error.PropertyName));
+
+            throw new SerializableValidationException(validationResult);
+        }
+    }
+}
