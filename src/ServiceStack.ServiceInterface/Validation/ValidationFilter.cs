86ca0b5606900c1a2df1d4f30c50b1e5f60e1802
Steffen M <arxisos@googlemail.com>
2011-12-23 14:18:02 +0100

    Improvements for validation mechanism

--- src/ServiceStack.ServiceInterface/Validation/ValidationFilter.cs
+++ src/ServiceStack.ServiceInterface/Validation/ValidationFilter.cs
@@ -4,19 +4,32 @@ using System.Linq;
 using System.Text;
 using ServiceStack.ServiceHost;
 using FluentValidation;
+using System.Reflection;
+using FluentValidation.Internal;
+using ServiceStack.Text;
+using ServiceStack.ServiceInterface.ServiceModel;
+using ServiceStack.Common.Utils;
+using ServiceStack.WebHost.Endpoints.Extensions;
 
 namespace ServiceStack.ServiceInterface.Validation
 {
     public class ValidationFilter
     {
-        public void ValidateRequest<T>(IHttpRequest req, IHttpResponse res, T requestDto)
+        public void ValidateRequest(IHttpRequest req, IHttpResponse res, object requestDto)
         {
-            IValidator<T> validator = req.TryResolve<IValidator<T>>();
+            Type validatorType = typeof(IValidator<>).MakeGenericType(requestDto.GetType());
+            MethodInfo resolver = typeof(IHttpRequest).GetMethod("TryResolve")
+                .MakeGenericMethod(validatorType);
+
+            IValidator validator = (IValidator)resolver.Invoke(req, null);
             if (validator != null)
             {
-                var validationResult = validator.Validate(requestDto, ruleSet: req.HttpMethod);
-                if (!validationResult.IsValid)
-                    validationResult.Throw();
+                string ruleSet = req.HttpMethod;
+                var validationResult = validator.Validate(new ValidationContext(requestDto, null, new MultiRuleSetValidatorSelector(ruleSet)));
+
+                ResponseStatusTranslator translator = new ResponseStatusTranslator();
+                ResponseStatus responseStatus = translator.Parse(validationResult.AsSerializable());
+                res.WriteToResponse(req, responseStatus);
             }
         }
     }
--- src/ServiceStack.ServiceInterface/Validation/ValidationResultExtensions.cs
+++ src/ServiceStack.ServiceInterface/Validation/ValidationResultExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using ServiceStack.Validation;
 using FluentValidation.Results;
+using ServiceStack.ServiceInterface.ServiceModel;
 
 namespace ServiceStack.ServiceInterface.Validation
 {
@@ -11,11 +12,17 @@ namespace ServiceStack.ServiceInterface.Validation
     {
         public static void Throw(this ValidationResult result)
         {
+            SerializableValidationResult validationResult = result.AsSerializable();
+            throw new SerializableValidationException(validationResult);
+        }
+
+        public static SerializableValidationResult AsSerializable(this ValidationResult result)
+        {
             SerializableValidationResult validationResult = new SerializableValidationResult();
             foreach (var error in result.Errors)
                 validationResult.Errors.Add(new SerializableValidationError(error.ErrorMessage, error.PropertyName));
 
-            throw new SerializableValidationException(validationResult);
+            return validationResult;
         }
     }
 }
