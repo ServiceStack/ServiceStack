2a40aec8e08b6f003689ca908cc24bbcc45652e0
Steffen M <arxisos@googlemail.com>
2012-01-01 11:37:17 +0100

    Refactored authenticate attribute and created filter attribute base classes

--- src/ServiceStack.ServiceInterface/AuthenticateAttribute.cs
+++ src/ServiceStack.ServiceInterface/AuthenticateAttribute.cs
@@ -1,4 +1,13 @@
 ﻿using System;
+using System.Linq;
+using System.Net;
+using ServiceStack.CacheAccess;
+using ServiceStack.Common;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface.Auth;
+using ServiceStack.Text;
+using ServiceStack.WebHost.Endpoints.Extensions;
 
 namespace ServiceStack.ServiceInterface
 {
@@ -7,7 +16,7 @@ namespace ServiceStack.ServiceInterface
     /// requires authentication.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
-	public class AuthenticateAttribute : Attribute
+	public class AuthenticateAttribute : RequestFilterAttribute
 	{
 		public string Provider { get; set; }
         public ApplyTo ApplyTo { get; set; }
@@ -32,5 +41,34 @@ namespace ServiceStack.ServiceInterface
             this.Provider = provider;
             this.ApplyTo = applyTo;
         }
-	}
+
+        public override void Execute(IHttpRequest req, IHttpResponse res, object requestDto)
+        {
+            var matchingOAuthConfigs = AuthService.AuthConfigs.Where(x =>
+                            this.Provider.IsNullOrEmpty()
+                            || x.Provider == this.Provider).ToList();
+
+            if (matchingOAuthConfigs.Count == 0)
+            {
+                res.WriteError(req, requestDto, "No OAuth Configs found matching {0} provider"
+                    .Fmt(this.Provider ?? "any"));
+                res.Close();
+                return;
+            }
+
+            using (var cache = req.GetCacheClient())
+            {
+                var sessionId = req.GetPermanentSessionId();
+                var session = sessionId != null ? cache.GetSession(sessionId) : null;
+
+                if (session == null || !matchingOAuthConfigs.Any(x => session.IsAuthorized(x.Provider)))
+                {
+                    res.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    res.AddHeader(HttpHeaders.WwwAuthenticate, "OAuth realm=\"{0}\"".Fmt(matchingOAuthConfigs[0].AuthRealm));
+                    res.Close();
+                    return;
+                }
+            }
+        }
+    }
 }
