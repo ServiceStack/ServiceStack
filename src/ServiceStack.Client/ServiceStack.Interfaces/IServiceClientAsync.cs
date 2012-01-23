d6f2365d11d51e25e20f5c40d5c7b8674781ccde
Demis Bellot <demis.bellot@gmail.com>
2012-01-23 02:36:43 -0500

    Moved ServiceStack.Common and ServiceStack.MonoTouch out of core trunk into ServiceStack.Contrib

--- src/ServiceStack.Client/ServiceStack.Interfaces/IServiceClientAsync.cs
+++
@@ -1,9 +0,0 @@
-using System;
-
-namespace ServiceStack.Service
-{
-	public interface IServiceClientAsync : IRestClientAsync
-	{
-		void SendAsync<TResponse>(object request, Action<TResponse> onSuccess, Action<TResponse, Exception> onError);
-	}
-}
