4b197436afc3418eb9f54393153a22bcd314c2e6
mythz <demis.bellot@gmail.com>
2012-02-20 20:45:01 -0500

    check in ProtoBuf format classes + ServiceClient

---
+++ tests/ServiceStack.WebHost.Endpoints.Tests/ProtoBufTests/ProtoBufServiceClient.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceClient.Web;
+using ServiceStack.ServiceHost;
+
+namespace ServiceStack.WebHost.Endpoints.Tests.ProtoBufTests
+{
+    public static class ProtoBufFormat
+    {
+        public static void Register(IAppHost appHost)
+        {
+            appHost.ContentTypeFilters.Register(ContentType.ProtoBuf, SerializeProtoBuf, DeserializeProtoBuf);
+        }
+
+        //AppHost
+        
+        public static void SerializeProtoBuf(IRequestContext requestContext, object response, Stream stream)
+        {
+            ProtoBuf.Serializer.NonGeneric.Serialize(stream, response);
+        }
+
+        public static object DeserializeProtoBuf(Type type, Stream stream)
+        {
+            return ProtoBuf.Serializer.NonGeneric.Deserialize(type, stream);
+        }
+
+        //ServiceClient
+
+        public static void SerializeToStream(IRequestContext requestContext, object request, Stream stream)
+        {
+            ProtoBuf.Serializer.NonGeneric.Serialize(stream, request);
+        }
+
+        public static T DeserializeFromStream<T>(Stream stream)
+        {
+            return ProtoBuf.Serializer.Deserialize<T>(stream);
+        }
+
+        public static StreamDeserializerDelegate StreamDeserializer
+        {
+            get { return ProtoBuf.Serializer.NonGeneric.Deserialize; }
+        }
+    }
+
+    public class ProtoBufServiceClient : ServiceClientBase
+    {
+
+        public ProtoBufServiceClient(string baseUri)
+        {
+            SetBaseUri(baseUri, "x-protobuf");
+        }
+
+        public ProtoBufServiceClient(string syncReplyBaseUri, string asyncOneWayBaseUri)
+            : base(syncReplyBaseUri, asyncOneWayBaseUri)
+        {
+        }
+
+        public override void SerializeToStream(IRequestContext requestContext, object request, Stream stream)
+        {
+            ProtoBufFormat.SerializeToStream(requestContext, request, stream);
+        }
+
+        public override T DeserializeFromStream<T>(Stream stream)
+        {
+            return ProtoBufFormat.DeserializeFromStream<T>(stream);
+        }
+
+        public override string ContentType
+        {
+            get { return ServiceStack.Common.Web.ContentType.ProtoBuf; }
+        }
+
+        public override StreamDeserializerDelegate StreamDeserializer
+        {
+            get { return ProtoBufFormat.StreamDeserializer; }
+        }
+    }
+}
