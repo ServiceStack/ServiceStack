e983f1e291884f0f31b4b256d40e96bd3710fb56
Demis Bellot <demis.bellot@gmail.com>
2012-03-31 17:42:27 -0400

    Start of InMemoryRequest Logger whilst checkin INCR to v3.65

---
+++ src/ServiceStack.ServiceInterface/Providers/InMemoryRequestLogger.cs
@@ -0,0 +1,24 @@
+﻿using ServiceStack.ServiceHost;
+
+namespace ServiceStack.ServiceInterface.Providers
+{
+	public class RequestLog
+	{
+		public string IpAddress { get; set; }
+		public string HttpMethod { get; set; }
+		public string[] Headers { get; set; }
+		public object RequestDto { get; set; }
+	}
+
+	public class InMemoryRequestLogger : IRequestLogger
+	{
+		public void Log(IRequestContext requestContext, object requestDto)
+		{
+			var httpReq = requestContext.Get<IHttpRequest>();
+			var entry = new RequestLog {
+				HttpMethod = httpReq.HttpMethod,
+				RequestDto = requestDto,
+			};
+		}
+	}
+}
