ef539acf6cbdd81f9a2efa3423e4cbdcdf2752fd
Demis Bellot <demis.bellot@gmail.com>
2012-04-16 09:43:10 -0400

    Add JSONP cache test

--- tests/ServiceStack.WebHost.IntegrationTests/Tests/CachedServiceTests.cs
+++ tests/ServiceStack.WebHost.IntegrationTests/Tests/CachedServiceTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using ServiceStack.Common;
 using ServiceStack.Service;
 using ServiceStack.ServiceClient.Web;
 using ServiceStack.WebHost.IntegrationTests.Services;
@@ -15,12 +16,20 @@ namespace ServiceStack.WebHost.IntegrationTests.Tests
 
 		[Test]
 		public void Can_call_Cached_WebService()
-        {
+		{
 			var client = CreateNewServiceClient();
 
 			var response = client.Get<MoviesResponse>("/cached/movies");
 
 			Assert.That(response.Movies.Count, Is.EqualTo(ResetMoviesService.Top5Movies.Count));
-        }
+		}
+
+		[Test]
+		public void Can_call_Cached_WebService_with_JSONP()
+		{
+			var url = Config.ServiceStackBaseUri.CombineWith("/cached/movies?callback=cb");
+			var jsonp = url.DownloadJsonFromUrl();
+			Assert.That(jsonp.StartsWith("cb("));
+		}
 	}
 }
