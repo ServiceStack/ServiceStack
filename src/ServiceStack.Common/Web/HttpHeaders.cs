2e7a00fc6ff87fcb51450083c94633d45a715142
mythz <demis.bellot@gmail.com>
2012-06-06 16:08:56 -0400

    Added back missing Referer HttpHeader constant

--- src/ServiceStack.Common/Web/HttpHeaders.cs
+++ src/ServiceStack.Common/Web/HttpHeaders.cs
@@ -1,43 +1,45 @@
 namespace ServiceStack.Common.Web
 {
-	public static class HttpHeaders
-	{
-		public const string XParamOverridePrefix = "X-Param-Override-";
+    public static class HttpHeaders
+    {
+        public const string XParamOverridePrefix = "X-Param-Override-";
 
-		public const string XHttpMethodOverride = "X-Http-Method-Override";
+        public const string XHttpMethodOverride = "X-Http-Method-Override";
 
-		public const string XUserAuthId = "X-UAId";
+        public const string XUserAuthId = "X-UAId";
 
-        	public const string XForwardedFor = "X-Forwarded-For";
+        public const string XForwardedFor = "X-Forwarded-For";
 
-        	public const string XRealIp = "X-Real-IP";
+        public const string XRealIp = "X-Real-IP";
 
-		public const string CacheControl = "Cache-Control";
+        public const string Referer = "Referer";
 
-		public const string IfModifiedSince = "If-Modified-Since";
+        public const string CacheControl = "Cache-Control";
 
-		public const string LastModified = "Last-Modified";
+        public const string IfModifiedSince = "If-Modified-Since";
 
-		public const string Accept = "Accept";
+        public const string LastModified = "Last-Modified";
 
-		public const string AcceptEncoding = "Accept-Encoding";
+        public const string Accept = "Accept";
 
-		public const string ContentType = "Content-Type";
+        public const string AcceptEncoding = "Accept-Encoding";
 
-		public const string ContentEncoding = "Content-Encoding";
+        public const string ContentType = "Content-Type";
 
-		public const string ContentLength = "Content-Length";
+        public const string ContentEncoding = "Content-Encoding";
 
-		public const string ContentDisposition = "Content-Disposition";
+        public const string ContentLength = "Content-Length";
 
-		public const string Location = "Location";
+        public const string ContentDisposition = "Content-Disposition";
 
-		public const string SetCookie = "Set-Cookie";
+        public const string Location = "Location";
 
-		public const string ETag = "ETag";
+        public const string SetCookie = "Set-Cookie";
 
-		public const string Authorization = "Authorization";
+        public const string ETag = "ETag";
 
-		public const string WwwAuthenticate = "WWW-Authenticate";
-	}
+        public const string Authorization = "Authorization";
+
+        public const string WwwAuthenticate = "WWW-Authenticate";
+    }
 }
