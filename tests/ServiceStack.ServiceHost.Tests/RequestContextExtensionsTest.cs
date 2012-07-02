218cee7ddca97f683ab4550439679f4417ee4972
mythz <demis.bellot@gmail.com>
2012-07-02 12:27:31 -0400

    Added new SS.Text dll to fix CsvSerializer compression bug.
    Changed RequestContextExtensionsTest to test different formats

--- tests/ServiceStack.ServiceHost.Tests/RequestContextExtensionsTest.cs
+++ tests/ServiceStack.ServiceHost.Tests/RequestContextExtensionsTest.cs
@@ -1,5 +1,4 @@
 ﻿using NUnit.Framework;
-using ServiceStack.CacheAccess.Providers;
 using ServiceStack.Common.Web;
 using ServiceStack.ServiceHost.Tests.Formats;
 using ServiceStack.ServiceInterface.Testing;
@@ -12,22 +11,51 @@ namespace ServiceStack.ServiceHost.Tests
     public class RequestContextExtensionsTest
     {
         [Test]
-        public void Can_optimize_result_with_ToOptimizedResult()
+        public void Can_optimize_html_result_with_ToOptimizedResult()
+        {
+            CanOptimizeResult("text/html", new HtmlFormat());
+        }
+
+        [Test]
+        public void Can_optimize_csv_result_with_ToOptimizedResult()
+        {
+            CanOptimizeResult("text/csv", new CsvFormat());
+        }
+
+        [Test]
+        public void Can_optimize_json_result_with_ToOptimizedResult()
+        {
+            CanOptimizeResult(ContentType.Json, null);
+        }
+
+        [Test]
+        public void Can_optimize_xml_result_with_ToOptimizedResult()
+        {
+            CanOptimizeResult(ContentType.Xml, null);
+        }
+
+        [Test]
+        public void Can_optimize_jsv_result_with_ToOptimizedResult()
+        {
+            CanOptimizeResult(ContentType.Jsv, null);
+        }
+
+        private static void CanOptimizeResult(string contentType, IPlugin pluginFormat)
         {
             var dto = new TestDto {Name = "test"};
-            
+
             var httpReq = new MockHttpRequest();
             httpReq.Headers.Add(HttpHeaders.AcceptEncoding, "gzip,deflate,sdch");
-            httpReq.ResponseContentType = "text/html";
+            httpReq.ResponseContentType = contentType;
             var httpRes = new ViewTests.MockHttpResponse();
 
             var httpRequestContext = new HttpRequestContext(httpReq, httpRes, dto);
 
             var appHost = new TestAppHost();
-			new HtmlFormat().Register(appHost);
+            if (pluginFormat != null) pluginFormat.Register(appHost);
 
-			EndpointHost.ContentTypeFilter = appHost.ContentTypeFilters;  
-            
+            EndpointHost.ContentTypeFilter = appHost.ContentTypeFilters;
+
             object result = httpRequestContext.ToOptimizedResult(dto);
             Assert.IsNotNull(result);
             Assert.IsTrue(result is CompressedResult);
