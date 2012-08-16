87a51116957ebf12c5e51be443a3776c25894463
mythz <demis.bellot@gmail.com>
2012-08-16 15:02:55 -0400

    Add test to parse random IP

---
+++ tests/ServiceStack.Common.Tests/EndpointHandlerBaseTests.cs
@@ -0,0 +1,34 @@
+﻿using System.Web;
+using NUnit.Framework;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceHost;
+using ServiceStack.ServiceInterface.Testing;
+using ServiceStack.Text;
+using ServiceStack.WebHost.Endpoints;
+
+namespace ServiceStack.Common.Tests
+{
+    [TestFixture]
+    public class EndpointHandlerBaseTests
+    {
+        public IHttpRequest CreateRequest(string userHostAddress)
+        {
+            var httpReq = new MockHttpRequest("test", HttpMethods.Get, ContentType.Json, "/", null, null, null)
+            {                
+                UserHostAddress = userHostAddress
+            };
+            return httpReq;
+        }
+
+        [Test]
+        public void Can_parse_Ips()
+        {
+            var handler = new RestHandler();
+            var result = handler.GetEndpointAttributes(CreateRequest("204.2.145.235"));
+
+            Assert.That(result.Has(EndpointAttributes.External));
+            Assert.That(result.Has(EndpointAttributes.HttpGet));
+            Assert.That(result.Has(EndpointAttributes.InSecure));
+        }
+    }
+}
