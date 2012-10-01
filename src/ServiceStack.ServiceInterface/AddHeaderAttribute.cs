a3c0d3821a40e7c057b31d2d3eaabbc93ca11740
Demis Bellot <demis.bellot@gmail.com>
2012-10-01 04:37:59 -0400

    Add strong-typed response headers on [AddHeader] attribute

--- src/ServiceStack.ServiceInterface/AddHeaderAttribute.cs
+++ src/ServiceStack.ServiceInterface/AddHeaderAttribute.cs
@@ -9,15 +9,8 @@ namespace ServiceStack.ServiceInterface
     {
         public string Name { get; set; }
         public string Value { get; set; }
-
-        public string ContentType
-        {
-            set 
-            { 
-                Name = HttpHeaders.ContentType;
-                Value = value;
-            }
-        }
+        
+        public AddHeaderAttribute() { }
 
         public AddHeaderAttribute(string name, string value)
         {
@@ -28,15 +21,106 @@ namespace ServiceStack.ServiceInterface
         public override void Execute(IHttpRequest req, IHttpResponse res, object requestDto)
         {
             if (string.IsNullOrEmpty(Name) || string.IsNullOrEmpty(Value)) return;
-            
+
             if (Name.Equals(HttpHeaders.ContentType, StringComparison.InvariantCultureIgnoreCase))
             {
                 res.ContentType = Value;
             }
             else
             {
-                    res.AddHeader(Name, Value);
+                res.AddHeader(Name, Value);
             }
         }
+
+        public string ContentType
+        {
+            get { return Name == HttpHeaders.ContentType ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.ContentType;
+                Value = value;
+            }
+        }
+
+        public string ContentEncoding
+        {
+            get { return Name == HttpHeaders.ContentEncoding ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.ContentEncoding;
+                Value = value;
+            }
+        }
+
+        public string ContentLength
+        {
+            get { return Name == HttpHeaders.ContentLength ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.ContentLength;
+                Value = value;
+            }
+        }
+
+        public string ContentDisposition
+        {
+            get { return Name == HttpHeaders.ContentDisposition ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.ContentDisposition;
+                Value = value;
+            }
+        }
+
+        public string Location
+        {
+            get { return Name == HttpHeaders.Location ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.Location;
+                Value = value;
+            }
+        }
+
+        public string SetCookie
+        {
+            get { return Name == HttpHeaders.SetCookie ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.SetCookie;
+                Value = value;
+            }
+        }
+
+        public string ETag
+        {
+            get { return Name == HttpHeaders.ETag ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.ETag;
+                Value = value;
+            }
+        }
+
+        public string CacheControl
+        {
+            get { return Name == HttpHeaders.CacheControl ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.CacheControl;
+                Value = value;
+            }
+        }
+
+        public string LastModified
+        {
+            get { return Name == HttpHeaders.LastModified ? Value : null; }
+            set
+            {
+                Name = HttpHeaders.LastModified;
+                Value = value;
+            }
+        }
+
     }
 }
