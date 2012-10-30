95f6c640162eaaf178d2e4ec018e7177517b8181
Sergey Bogdanov <sergey.bogdanov@gmail.com>
2012-10-30 11:27:07 +0200

    added NuGetPack for: StackExpress, StackExpress.Mvc, StackExpress.Common, StackExpress.Razor, StackExpress.Authentication, StackExpress.MsgPack and StackExpress.ProtoBuf

--- build.cmd
+++ build.cmd
@@ -5,4 +5,11 @@ if "%target%" == "" (
    set target=UnitTests
 )
 
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\build.msbuild /target:%target% /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
+if "%target%" == "NuGetPack" (
+	if "%BUILD_NUMBER%" == "" (
+	 	echo BUILD_NUMBER environment variable is not set.
+		exit;
+	)
+)
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild Build\Build.msbuild /target:%target% /v:M /fl /flp:LogFile=msbuild.log;Verbosity=Normal /nr:false
