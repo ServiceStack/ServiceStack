b8ed594dd89e392179094dd95b405701795d70d3
Demis Bellot <demis.bellot@gmail.com>
2012-11-14 09:17:51 -0500

    Add non-matching Razor + DTO Models (e.g. exceptions) to Razor ViewPage.ModelError and leave Model as null

--- src/ServiceStack.Razor/ViewPageBase.cs
+++ src/ServiceStack.Razor/ViewPageBase.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using ServiceStack.CacheAccess;
+using ServiceStack.Common.Web;
 using ServiceStack.Html;
 using ServiceStack.Messaging;
 using ServiceStack.OrmLite;
@@ -10,6 +11,7 @@ using ServiceStack.Redis;
 using ServiceStack.ServiceHost;
 using ServiceStack.ServiceInterface;
 using ServiceStack.ServiceInterface.Auth;
+using ServiceStack.ServiceInterface.ServiceModel;
 using ServiceStack.WebHost.Endpoints;
 
 namespace ServiceStack.Razor
@@ -41,6 +43,13 @@ namespace ServiceStack.Razor
         public IHttpRequest Request { get; set; }
 
         public IHttpResponse Response { get; set; }
+
+        public object ModelError { get; set; } 
+
+        public ResponseStatus ResponseStatus
+        {
+            get { return ModelError.ToResponseStatus(); }
+        }
 
         private ICacheClient cache;
         public ICacheClient Cache
