33b176962b3ff0920e487390ca5e118691d3b364
Demis Bellot <demis.bellot@gmail.com>
2012-11-22 03:03:35 -0500

    Revert "Merge pull request #340 from ShamsulAmry/master"
    This reverts commit 269b0808a75a1c195755572a9161a583226568d8, reversing
    changes made to d12954746256a6d7c9f6b970ece46b2a5ef88675.

--- src/ServiceStack.Interfaces/Service/IServiceClientAsync.cs
+++ src/ServiceStack.Interfaces/Service/IServiceClientAsync.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ServiceStack.Service
 {
-	public interface IServiceClientAsync
+	public interface IServiceClientAsync : IRestClientAsync
 	{
 		void SendAsync<TResponse>(object request, Action<TResponse> onSuccess, Action<TResponse, Exception> onError);
 	}
