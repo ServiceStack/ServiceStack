c836307c5eb057a9797310424e83719e8213479f
Shamsul Amry <shamsulamry@gmail.com>
2012-11-22 08:32:42 +0800

    Dropping IRestClientAsync from IServiceClientAsync
    Related to #338

--- src/ServiceStack.Interfaces/Service/IServiceClientAsync.cs
+++ src/ServiceStack.Interfaces/Service/IServiceClientAsync.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ServiceStack.Service
 {
-	public interface IServiceClientAsync : IRestClientAsync
+	public interface IServiceClientAsync
 	{
 		void SendAsync<TResponse>(object request, Action<TResponse> onSuccess, Action<TResponse, Exception> onError);
 	}
