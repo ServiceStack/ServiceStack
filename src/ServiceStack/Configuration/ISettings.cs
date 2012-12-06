62c9a62fc853050a542854eafcd331148b25f8dd
David Stone <david@gixug.com>
2012-12-05 16:33:19 -0800

    Extract common functionality into AppSettingsBase
    This also adds some nice error checking around settings values
    and provides a common interface for objects that provide settings
    (e.g. App.config, Azure's RoleEnvironment, etc.)

---
+++ src/ServiceStack/Configuration/ISettings.cs
@@ -0,0 +1,16 @@
+namespace ServiceStack.Configuration
+{
+    public interface ISettings
+    {
+        /// <summary>
+        /// Provides a common interface for Settings providers such as
+        /// ConfigurationManager or Azure's RoleEnvironment. The only
+        /// requirement is that if the implementation cannot find the
+        /// specified key, the return value must be null
+        /// </summary>
+        /// <param name="key">The key for the setting</param>
+        /// <returns>The string value of the specified key, or null if the key
+        /// was invalid</returns>
+        string Get(string key);
+    }
+}
