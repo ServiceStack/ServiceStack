768b3cc3514ae6030ec3031276ebd9298a780b50
Demis Bellot <demis.bellot@gmail.com>
2012-12-24 18:38:15 +0800

    fix whitespace

--- src/ServiceStack/WebHost.Endpoints/Extensions/HttpRequestWrapper.cs
+++ src/ServiceStack/WebHost.Endpoints/Extensions/HttpRequestWrapper.cs
@@ -11,165 +11,167 @@ using ServiceStack.ServiceHost;
 
 namespace ServiceStack.WebHost.Endpoints.Extensions
 {
-	public class HttpRequestWrapper
-		: IHttpRequest
-	{
-		private static readonly string physicalFilePath;
-		public Container Container { get; set; }
-		private readonly HttpRequest request;
+    public class HttpRequestWrapper
+        : IHttpRequest
+    {
+        private static readonly string physicalFilePath;
+        public Container Container { get; set; }
+        private readonly HttpRequest request;
 
-		static HttpRequestWrapper()
-		{
-			physicalFilePath = "~".MapHostAbsolutePath();
-		}
+        static HttpRequestWrapper()
+        {
+            physicalFilePath = "~".MapHostAbsolutePath();
+        }
 
-		public HttpRequest Request
-		{
-			get { return request; }
-		}
+        public HttpRequest Request
+        {
+            get { return request; }
+        }
 
-		public object OriginalRequest
-		{
-			get { return request; }
-		}
+        public object OriginalRequest
+        {
+            get { return request; }
+        }
 
-		public HttpRequestWrapper(HttpRequest request)
-			: this(null, request)
-		{
-		}
+        public HttpRequestWrapper(HttpRequest request)
+            : this(null, request)
+        {
+        }
 
-		public HttpRequestWrapper(string operationName, HttpRequest request)
-		{
-			this.OperationName = operationName;
-			this.request = request;
-			this.Container = Container;
-		}
+        public HttpRequestWrapper(string operationName, HttpRequest request)
+        {
+            this.OperationName = operationName;
+            this.request = request;
+            this.Container = Container;
+        }
 
-		public T TryResolve<T>()
-		{
-			return Container == null 
-				? EndpointHost.AppHost.TryResolve<T>()
-				: Container.TryResolve<T>();
-		}
+        public T TryResolve<T>()
+        {
+            return Container == null
+                ? EndpointHost.AppHost.TryResolve<T>()
+                : Container.TryResolve<T>();
+        }
 
-		public string OperationName { get; set; }
+        public string OperationName { get; set; }
 
-		public string ContentType
-		{
-			get { return request.ContentType; }
-		}
+        public string ContentType
+        {
+            get { return request.ContentType; }
+        }
 
-		private string httpMethod;
-		public string HttpMethod
-		{
-			get { return httpMethod
-				?? (httpMethod = request.Headers[HttpHeaders.XHttpMethodOverride] ?? request.HttpMethod);
-			}
-		}
+        private string httpMethod;
+        public string HttpMethod
+        {
+            get
+            {
+                return httpMethod
+                    ?? (httpMethod = request.Headers[HttpHeaders.XHttpMethodOverride] ?? request.HttpMethod);
+            }
+        }
 
-		public string UserAgent
-		{
-			get { return request.UserAgent; }
-		}
+        public string UserAgent
+        {
+            get { return request.UserAgent; }
+        }
 
-		private Dictionary<string, object> items;
-		public Dictionary<string, object> Items
-		{
-			get
-			{
-				if (items == null)
-				{
-					items = new Dictionary<string, object>();
-				}
-				return items;
-			}
-		}
+        private Dictionary<string, object> items;
+        public Dictionary<string, object> Items
+        {
+            get
+            {
+                if (items == null)
+                {
+                    items = new Dictionary<string, object>();
+                }
+                return items;
+            }
+        }
 
-		private string responseContentType;
-		public string ResponseContentType
-		{
-			get
-			{
-				if (responseContentType == null)
-				{
-					responseContentType = this.GetResponseContentType();
-				}
-				return responseContentType;
-			}
-			set
-			{
-				this.responseContentType = value;
-			}
-		}
+        private string responseContentType;
+        public string ResponseContentType
+        {
+            get
+            {
+                if (responseContentType == null)
+                {
+                    responseContentType = this.GetResponseContentType();
+                }
+                return responseContentType;
+            }
+            set
+            {
+                this.responseContentType = value;
+            }
+        }
 
-		private Dictionary<string, Cookie> cookies;
-		public IDictionary<string, Cookie> Cookies
-		{
-			get
-			{
-				if (cookies == null)
-				{
-					cookies = new Dictionary<string, Cookie>();
-					for (var i = 0; i < this.request.Cookies.Count; i++)
-					{
-						var httpCookie = this.request.Cookies[i];
-						var cookie = new Cookie(
-							httpCookie.Name, httpCookie.Value, httpCookie.Path, httpCookie.Domain)
-							{
-								HttpOnly = httpCookie.HttpOnly,
-								Secure = httpCookie.Secure,
-								Expires = httpCookie.Expires,
-							};
-						cookies[httpCookie.Name] = cookie;
-					}
-				}
-				return cookies;
-			}
-		}
+        private Dictionary<string, Cookie> cookies;
+        public IDictionary<string, Cookie> Cookies
+        {
+            get
+            {
+                if (cookies == null)
+                {
+                    cookies = new Dictionary<string, Cookie>();
+                    for (var i = 0; i < this.request.Cookies.Count; i++)
+                    {
+                        var httpCookie = this.request.Cookies[i];
+                        var cookie = new Cookie(
+                            httpCookie.Name, httpCookie.Value, httpCookie.Path, httpCookie.Domain)
+                            {
+                                HttpOnly = httpCookie.HttpOnly,
+                                Secure = httpCookie.Secure,
+                                Expires = httpCookie.Expires,
+                            };
+                        cookies[httpCookie.Name] = cookie;
+                    }
+                }
+                return cookies;
+            }
+        }
 
-		public NameValueCollection Headers
-		{
-			get { return request.Headers; }
-		}
+        public NameValueCollection Headers
+        {
+            get { return request.Headers; }
+        }
 
-		public NameValueCollection QueryString
-		{
-			get { return request.QueryString; }
-		}
+        public NameValueCollection QueryString
+        {
+            get { return request.QueryString; }
+        }
 
-		public NameValueCollection FormData
-		{
-			get { return request.Form; }
-		}
+        public NameValueCollection FormData
+        {
+            get { return request.Form; }
+        }
 
-		public string GetRawBody()
-		{
-			using (var reader = new StreamReader(request.InputStream))
-			{
-				return reader.ReadToEnd();
-			}
-		}
+        public string GetRawBody()
+        {
+            using (var reader = new StreamReader(request.InputStream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
 
-		public string RawUrl
-		{
-			get { return request.RawUrl; }
-		}
+        public string RawUrl
+        {
+            get { return request.RawUrl; }
+        }
 
-		public string AbsoluteUri
-		{
-			get
-			{
-				try
-				{
-					return request.Url.AbsoluteUri.TrimEnd('/');
-				}
-				catch (Exception)
-				{
-					//fastcgi mono, do a 2nd rounds best efforts
-					return "http://" + request.UserHostName + request.RawUrl;
-				}
-			}
-		}
+        public string AbsoluteUri
+        {
+            get
+            {
+                try
+                {
+                    return request.Url.AbsoluteUri.TrimEnd('/');
+                }
+                catch (Exception)
+                {
+                    //fastcgi mono, do a 2nd rounds best efforts
+                    return "http://" + request.UserHostName + request.RawUrl;
+                }
+            }
+        }
 
         public string UserHostAddress
         {
@@ -185,65 +187,65 @@ namespace ServiceStack.WebHost.Endpoints.Extensions
             }
         }
 
-	    public bool IsSecureConnection
-		{
-			get { return request.IsSecureConnection; }
-		}
+        public bool IsSecureConnection
+        {
+            get { return request.IsSecureConnection; }
+        }
 
-		public string[] AcceptTypes
-		{
-			get { return request.AcceptTypes; }
-		}
+        public string[] AcceptTypes
+        {
+            get { return request.AcceptTypes; }
+        }
 
-		public string PathInfo
-		{
-			get { return request.GetPathInfo(); }
-		}
+        public string PathInfo
+        {
+            get { return request.GetPathInfo(); }
+        }
 
-		public string UrlHostName
-		{
-			get { return request.GetUrlHostName(); }
-		}
+        public string UrlHostName
+        {
+            get { return request.GetUrlHostName(); }
+        }
 
-		public Stream InputStream
-		{
-			get { return request.InputStream; }
-		}
+        public Stream InputStream
+        {
+            get { return request.InputStream; }
+        }
 
-		public long ContentLength
-		{
-			get { return request.ContentLength; }
-		}
+        public long ContentLength
+        {
+            get { return request.ContentLength; }
+        }
 
-		private IFile[] files;
-		public IFile[] Files
-		{
-			get
-			{
-				if (files == null)
-				{
-					files = new IFile[request.Files.Count];
-					for (var i = 0; i < request.Files.Count; i++)
-					{
-						var reqFile = request.Files[i];
+        private IFile[] files;
+        public IFile[] Files
+        {
+            get
+            {
+                if (files == null)
+                {
+                    files = new IFile[request.Files.Count];
+                    for (var i = 0; i < request.Files.Count; i++)
+                    {
+                        var reqFile = request.Files[i];
 
-						files[i] = new HttpFile
-						{
-							ContentType = reqFile.ContentType,
-							ContentLength = reqFile.ContentLength,
-							FileName = reqFile.FileName,
-							InputStream = reqFile.InputStream,
-						};
-					}
-				}
-				return files;
-			}
-		}
+                        files[i] = new HttpFile
+                        {
+                            ContentType = reqFile.ContentType,
+                            ContentLength = reqFile.ContentLength,
+                            FileName = reqFile.FileName,
+                            InputStream = reqFile.InputStream,
+                        };
+                    }
+                }
+                return files;
+            }
+        }
 
-		public string ApplicationFilePath
-		{
-			get { return physicalFilePath; }
-		}
-	}
+        public string ApplicationFilePath
+        {
+            get { return physicalFilePath; }
+        }
+    }
 
 }
