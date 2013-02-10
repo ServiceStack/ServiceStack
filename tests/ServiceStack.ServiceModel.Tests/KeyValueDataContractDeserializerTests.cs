2589405f000865bbe76369c946834d48290e8f92
Demis Bellot <demis.bellot@gmail.com>
2013-02-09 23:51:18 -0500

    Use JsConfig.With in public fields tests

--- tests/ServiceStack.ServiceModel.Tests/KeyValueDataContractDeserializerTests.cs
+++ tests/ServiceStack.ServiceModel.Tests/KeyValueDataContractDeserializerTests.cs
@@ -103,10 +103,13 @@ namespace ServiceStack.ServiceModel.Tests
         [Test]
         public void KVP_Serializer_fills_public_fields()
         {
-            var valueMap = new Dictionary<string, string> { { "FirstName", "james" }, { "LastName", "bond" }, { "FullName", "james bond" } };
-            var result = (CustomerWithFields)KeyValueDataContractDeserializer.Instance.Parse(valueMap, typeof(CustomerWithFields));
-            Assert.That(result.FirstName, Is.EqualTo("james"));
-            Assert.That(result.LastName, Is.EqualTo("bond"));
+            using (JsConfig.With(includePublicFields:true))
+            {
+                var valueMap = new Dictionary<string, string> { { "FirstName", "james" }, { "LastName", "bond" }, { "FullName", "james bond" } };
+                var result = (CustomerWithFields)KeyValueDataContractDeserializer.Instance.Parse(valueMap, typeof(CustomerWithFields));
+                Assert.That(result.FirstName, Is.EqualTo("james"));
+                Assert.That(result.LastName, Is.EqualTo("bond"));
+            }
         }
     }
 }
