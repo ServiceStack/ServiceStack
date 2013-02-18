79dcc045022cc9effad8ebfff7d0577dfcb65fd9
Demis Bellot <demis.bellot@gmail.com>
2013-02-18 05:31:04 -0500

    INCR to v3.9.37

--- src/ServiceStack.Api.Swagger/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Api.Swagger/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.35.0")]
-[assembly: AssemblyFileVersion("3.9.35.0")]
+[assembly: AssemblyVersion("3.9.37.0")]
+[assembly: AssemblyFileVersion("3.9.37.0")]
--- src/ServiceStack.Razor/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Razor/Properties/AssemblyInfo.cs
@@ -37,4 +37,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("Release")]
 #endif
 
-[assembly: AssemblyVersion("3.9.36.0")]
+[assembly: AssemblyVersion("3.9.37.0")]
