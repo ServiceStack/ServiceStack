5802bcd3a9dc9970ca3c58d86ba6ced60b73c9ca
Demis Bellot <demis.bellot@gmail.com>
2013-03-17 18:08:14 -0400

    Merge static AutoWireHelpers into Container partial extensions

---
+++ tests/ServiceStack.Common.Tests/FunqTests.cs
@@ -0,0 +1,34 @@
+﻿using Funq;
+using NUnit.Framework;
+
+namespace ServiceStack.Common.Tests
+{
+    [TestFixture]
+    public class FunqTests
+    {
+        interface IBar { }
+        class Bar : IBar { }
+        class TestFoo { public IBar Bar { get; set; } }
+
+        [Test]
+        public void Test1()
+        {
+            var container = new Container();
+            var m = new TestFoo();
+            container.Register<IBar>(new Bar());
+            Assert.NotNull(container.Resolve<IBar>(), "Resolve");
+            container.AutoWire(m);
+            Assert.NotNull(m.Bar, "Autowire");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            var container = new Container();
+            var m = new TestFoo();
+            container.AutoWire(m);
+            Assert.Throws<ResolutionException>(() => container.Resolve<IBar>());
+            Assert.IsNull(m.Bar); // FAILS HERE
+        }         
+    }
+}
