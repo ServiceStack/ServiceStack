29cdd0a4dfde5477b9c304906667fce0627ac6b2
Demis Bellot <demis.bellot@gmail.com>
2013-04-01 04:23:25 -0400

    INCR to v3.9.43

--- src/ServiceStack.Api.Swagger/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Api.Swagger/Properties/AssemblyInfo.cs
@@ -33,8 +33,8 @@ using System.Runtime.Serialization;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.42.0")]
-[assembly: AssemblyFileVersion("3.9.42.0")]
+[assembly: AssemblyVersion("3.9.43.0")]
+[assembly: AssemblyFileVersion("3.9.43.0")]
 
 [assembly: ContractNamespace("http://schemas.servicestack.net/types",
     ClrNamespace = "ServiceStack.Api.Swagger")]
