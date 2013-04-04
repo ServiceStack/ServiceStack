863a67f1927ec27d214afb0ef944c6cea0430f8f
Nick Jones <medianick@gmail.com>
2013-04-04 16:53:25 -0400

    Switched to GlobalAssemblyInfo approach for AssemblyVersion.
    All projects in the ServiceStack.sln solution now pull their
    AssemblyVersion attribute value from a single GlobalAssemblyInfo.cs file
    at the src root, allowing this to be updated centrally. I did not adjust
    the Android, SL, or WP projects.

--- src/ServiceStack/Properties/AssemblyInfo.cs
+++ src/ServiceStack/Properties/AssemblyInfo.cs
@@ -20,15 +20,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("06704d66-af8e-411f-8260-8d05de5ce6ad")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.9.43.0")]
