a85797e2719182673ead0ddb21804d7c57ca7f2a
danbarua <dbarua@businesssystemsuk.com>
2013-04-20 12:08:14 +0100

    Tidy up and unit tests

---
+++ src/ServiceStack.ServiceInterface/PartialContentResult.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Web;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceHost;
+using ServiceStack.Text;
+
+namespace ServiceStack.ServiceInterface
+{
+    public class PartialContentResult : IPartialContentResult, IDisposable
+    {
+        private readonly FileInfo file;
+        private readonly Stream stream;
+        private PartialContentResult(string contentType)
+        {
+            if (contentType == null) throw new ArgumentNullException("contentType");
+            ContentType = contentType;
+
+            Headers = new Dictionary<string, string>
+                {
+                    {"Accept-Ranges", "bytes"}
+                };
+        }
+
+        public PartialContentResult(FileInfo file, string contentType = null) : this(contentType ?? MimeTypes.GetMimeType(file.Name))
+        {
+            this.file = file;
+        }
+
+        public PartialContentResult(Stream stream, string contentType)
+            : this(contentType)
+        {
+            if (contentType == null) throw new ArgumentNullException("contentType");
+            this.stream = stream;
+        }
+
+        public IDictionary<string, string> Options
+        {
+            get { return Headers; }
+        }
+
+        public int Status { get; set; }
+        public HttpStatusCode StatusCode { get; set; }
+        public string StatusDescription { get; set; }
+        public string ContentType { get; set; }
+
+        public Dictionary<string, string> Headers { get; set; }
+
+        public object Response { get; set; }
+        public IContentTypeWriter ResponseFilter { get; set; }
+        public IRequestContext RequestContext { get; set; }
+
+        public long Start { get; set; }
+        public long End { get; set; }
+
+        public long GetContentLength()
+        {
+            if (file != null)
+                return file.Length;
+            if (stream != null)
+                return stream.Length;
+
+            throw new InvalidOperationException("Neither file nor stream were set when attempting to establish Content-Length.");
+        }
+
+        public void WriteTo(Stream responseStream)
+        {
+            if (file != null)
+            {
+                using (FileStream fs = file.OpenRead())
+                {
+                    WriteTo(fs, responseStream);
+                }
+                return;
+            }
+
+            if (stream != null)
+            {
+                WriteTo(stream, responseStream);
+                return;
+            }
+                
+
+            throw new InvalidOperationException("Neither file nor stream were set when attempting to write to the Response Stream.");
+        }
+
+        private void WriteTo(Stream inputStream, Stream responseStream)
+        {
+            if (!inputStream.CanSeek)
+                throw new InvalidOperationException(
+                    "Sending Range Responses requires a seekable stream eg. FileStream or MemoryStream");
+
+
+            long totalBytesToSend = End - Start + 1;
+            var buffer = new byte[0x1000]; //new byte[BufferSize];
+            long bytesRemaining = totalBytesToSend;
+
+            inputStream.Seek(Start, SeekOrigin.Begin);
+
+            while (bytesRemaining > 0)
+            {
+                int count;
+                if (bytesRemaining <= buffer.Length)
+                    count = inputStream.Read(buffer, 0,
+                                             (bytesRemaining <= int.MaxValue) ? (int) bytesRemaining : int.MaxValue);
+                else
+                    count = inputStream.Read(buffer, 0, buffer.Length);
+
+                /* Would be nice if we could do this */
+                //if (!response.IsClientConnected)
+                //{
+                //    
+                //    break;
+                //}
+
+                try
+                {
+                    responseStream.Write(buffer, 0, count);
+                    responseStream.Flush();
+                    bytesRemaining -= count;
+                }
+                catch (HttpException httpException)
+                {
+                    /* in Asp.Net we can call HttpResponseBase.IsClientConnected
+                        * to see if the client broke off the connection
+                        * and avoid trying to flush the response stream.
+                        * I'm not quite I can do the same here without some invasive changes,
+                        * so instead I'll swallow the exception that IIS throws in this situation.*/
+
+                    if (httpException.Message ==
+                        "An error occurred while communicating with the remote host. The error code is 0x80070057.")
+                        break;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            if (stream != null)
+            {
+                stream.Dispose();
+            }
+        }
+    }
+}
---
+++ tests/ServiceStack.WebHost.Endpoints.Tests/PartialContentResultTests.cs
@@ -0,0 +1,153 @@
+﻿using System.IO;
+using Funq;
+using NUnit.Framework;
+using ServiceStack.Common.Web;
+using ServiceStack.ServiceInterface;
+using ServiceStack.ServiceInterface.Testing;
+using ServiceStack.Text;
+using ServiceStack.WebHost.Endpoints.Extensions;
+using ServiceStack.WebHost.Endpoints.Support.Mocks;
+using ServiceStack.WebHost.Endpoints.Tests.Mocks;
+
+namespace ServiceStack.WebHost.Endpoints.Tests
+{
+    [TestFixture]
+    public class PartialContentResultTests : TestBase
+    {
+        protected override void Configure(Container container)
+        {
+        }
+
+        [Test]
+        public void Can_seek_from_beginning_to_middle()
+        {
+            var mockRequest = new HttpRequestMock();
+            mockRequest.Headers.Add("Range","bytes=0-2");
+            var mockResponse = new HttpResponseMock();
+
+            var customText = "1234567890";
+            var customTextBytes = customText.ToUtf8Bytes();
+            var ms = new MemoryStream();
+            ms.Write(customTextBytes, 0, customTextBytes.Length);
+
+
+            var httpResult = new PartialContentResult(ms, ContentType.PlainText);
+
+            var reponseWasAutoHandled = mockResponse.WriteToResponse(mockRequest,httpResult);
+            Assert.That(reponseWasAutoHandled, Is.True);
+
+            var writtenString = mockResponse.GetOutputStreamAsString();
+            Assert.That(writtenString, Is.EqualTo("123"));
+
+            Assert.That(mockResponse.Headers["Content-Range"], Is.EqualTo("bytes 0-2/10"));
+            Assert.That(mockResponse.Headers["Content-Length"], Is.EqualTo(customTextBytes.Length.ToString()));
+            Assert.That(mockResponse.Headers["Accept-Ranges"], Is.EqualTo("bytes"));
+            Assert.That(mockResponse.StatusCode, Is.EqualTo(206));
+        }
+
+        [Test]
+        public void Can_seek_from_beginning_to_end()
+        {
+            var mockRequest = new HttpRequestMock();
+            mockRequest.Headers.Add("Range", "bytes=0");
+            var mockResponse = new HttpResponseMock();
+
+            var customText = "1234567890";
+            var customTextBytes = customText.ToUtf8Bytes();
+            var ms = new MemoryStream();
+            ms.Write(customTextBytes, 0, customTextBytes.Length);
+
+
+            var httpResult = new PartialContentResult(ms, ContentType.PlainText);
+
+            var reponseWasAutoHandled = mockResponse.WriteToResponse(mockRequest, httpResult);
+            Assert.That(reponseWasAutoHandled, Is.True);
+
+            var writtenString = mockResponse.GetOutputStreamAsString();
+            Assert.That(writtenString, Is.EqualTo(customText));
+
+            Assert.That(mockResponse.Headers["Content-Range"], Is.EqualTo("bytes 0-9/10"));
+            Assert.That(mockResponse.Headers["Content-Length"], Is.EqualTo(customTextBytes.Length.ToString()));
+            Assert.That(mockResponse.Headers["Accept-Ranges"], Is.EqualTo("bytes"));
+            Assert.That(mockResponse.StatusCode, Is.EqualTo(206));
+        }
+
+        [Test] public void Can_seek_from_middle_to_end()
+        {
+            var mockRequest = new HttpRequestMock();
+            mockRequest.Headers.Add("Range", "bytes=4-");
+            var mockResponse = new HttpResponseMock();
+
+            var customText = "1234567890";
+            var customTextBytes = customText.ToUtf8Bytes();
+            var ms = new MemoryStream();
+            ms.Write(customTextBytes, 0, customTextBytes.Length);
+
+
+            var httpResult = new PartialContentResult(ms, ContentType.PlainText);
+
+            var reponseWasAutoHandled = mockResponse.WriteToResponse(mockRequest, httpResult);
+            Assert.That(reponseWasAutoHandled, Is.True);
+
+            var writtenString = mockResponse.GetOutputStreamAsString();
+            Assert.That(writtenString, Is.EqualTo("567890"));
+
+            Assert.That(mockResponse.Headers["Content-Range"], Is.EqualTo("bytes 4-9/10"));
+            Assert.That(mockResponse.Headers["Content-Length"], Is.EqualTo(customTextBytes.Length.ToString()));
+            Assert.That(mockResponse.Headers["Accept-Ranges"], Is.EqualTo("bytes"));
+            Assert.That(mockResponse.StatusCode, Is.EqualTo(206));
+        }
+
+        [Test]
+        public void Can_seek_from_middle_to_middle()
+        {
+            var mockRequest = new HttpRequestMock();
+            mockRequest.Headers.Add("Range", "bytes=3-5");
+            var mockResponse = new HttpResponseMock();
+
+            var customText = "1234567890";
+            var customTextBytes = customText.ToUtf8Bytes();
+            var ms = new MemoryStream();
+            ms.Write(customTextBytes, 0, customTextBytes.Length);
+
+
+            var httpResult = new PartialContentResult(ms, ContentType.PlainText);
+
+            var reponseWasAutoHandled = mockResponse.WriteToResponse(mockRequest, httpResult);
+            Assert.That(reponseWasAutoHandled, Is.True);
+
+            var writtenString = mockResponse.GetOutputStreamAsString();
+            Assert.That(writtenString, Is.EqualTo("456"));
+
+            Assert.That(mockResponse.Headers["Content-Range"], Is.EqualTo("bytes 3-5/10"));
+            Assert.That(mockResponse.Headers["Content-Length"], Is.EqualTo(customTextBytes.Length.ToString()));
+            Assert.That(mockResponse.Headers["Accept-Ranges"], Is.EqualTo("bytes"));
+            Assert.That(mockResponse.StatusCode, Is.EqualTo(206));
+        }
+
+        [Test]
+        public void Can_respond_to_non_range_requests_with_200_OK_response()
+        {
+            var mockRequest = new HttpRequestMock();
+            var mockResponse = new HttpResponseMock();
+
+            var customText = "1234567890";
+            var customTextBytes = customText.ToUtf8Bytes();
+            var ms = new MemoryStream();
+            ms.Write(customTextBytes, 0, customTextBytes.Length);
+
+            var httpResult = new PartialContentResult(ms, ContentType.PlainText);
+
+            var reponseWasAutoHandled = mockResponse.WriteToResponse(mockRequest, httpResult);
+            Assert.That(reponseWasAutoHandled, Is.True);
+
+            var writtenString = mockResponse.GetOutputStreamAsString();
+            Assert.That(writtenString, Is.EqualTo(customText));
+
+            Assert.That(mockResponse.Headers["Content-Range"], Is.Null);
+            Assert.That(mockResponse.Headers["Content-Length"], Is.EqualTo(customTextBytes.Length.ToString()));
+            Assert.That(mockResponse.Headers["Accept-Ranges"], Is.EqualTo("bytes"));
+            Assert.That(mockResponse.StatusCode, Is.EqualTo(200));
+        }
+    }
+}
