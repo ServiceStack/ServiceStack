5e257fdd2c79c516bf6647d816cc01d4e813b371
Demis Bellot <demis.bellot@gmail.com>
2013-05-05 17:11:27 -0400

    run tests with new dlls

--- tests/ServiceStack.Common.Tests/QueryStringSerializerTests.cs
+++ tests/ServiceStack.Common.Tests/QueryStringSerializerTests.cs
@@ -17,25 +17,7 @@ namespace ServiceStack.Common.Tests
     public class QueryStringSerializerTests
     {
         [Test]
-        public void Can_deserialize_TestRequest_current_QueryStringSerializer_output()
-        {
-            // Setup
-            var testAppHost = new TestAppHost(new Container(), typeof(TestService).Assembly);
-            var restPath = new RestPath(typeof(TestRequest), "/service", "GET");
-            var restHandler = new RestHandler { RestPath = restPath };
-            
-            var requestString = "ListOfA={ListOfB:{Property:prop1},{Property:prop2}}";
-            NameValueCollection queryString = HttpUtility.ParseQueryString(requestString);
-            var httpReq = new HttpRequestMock("service", "GET", "application/json", "service", queryString, new MemoryStream(), new NameValueCollection());
-
-            var request2 = (TestRequest)restHandler.CreateRequest(httpReq, "service");
-
-            Assert.That(request2.ListOfA.Count, Is.EqualTo(1));
-            Assert.That(request2.ListOfA.First().ListOfB.Count, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Can_deserialize_TestRequest_old_QueryStringSerializer_output()
+        public void Can_deserialize_TestRequest_QueryStringSerializer_output()
         {
             // Setup
             var testAppHost = new TestAppHost(new Container(), typeof(TestService).Assembly);
@@ -53,15 +35,7 @@ namespace ServiceStack.Common.Tests
         }
 
         [Test]
-        public void QueryStringSerializer_TestRequest_current_output()
-        {
-            var testRequest = new TestRequest { ListOfA = new List<A> { new A { ListOfB = new List<B> { new B { Property = "prop1" }, new B { Property = "prop2" } } } } };
-            var str = QueryStringSerializer.SerializeToString(testRequest);
-            Assert.That(str, Is.EqualTo("ListOfA={ListOfB:{Property:prop1},{Property:prop2}}"));
-        }
-
-        [Test]
-        public void QueryStringSerializer_TestRequest_old_output()
+        public void QueryStringSerializer_TestRequest_output()
         {
             var testRequest = new TestRequest { ListOfA = new List<A> { new A { ListOfB = new List<B> { new B { Property = "prop1" }, new B { Property = "prop2" } } } } };
             var str = QueryStringSerializer.SerializeToString(testRequest);
