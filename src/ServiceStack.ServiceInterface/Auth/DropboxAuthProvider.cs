d571d37ff1a3bc124917c0afe9ebdb43e881b97b
Dave Hawes <dave@davehawes.com>
2013-05-08 10:14:11 +0100

    * added a DropboxAuthProvider.cs to enable easy use of the dropbox oauth api

---
+++ src/ServiceStack.ServiceInterface/Auth/DropboxAuthProvider.cs
@@ -0,0 +1,17 @@
+﻿namespace ServiceStack.ServiceInterface.Auth
+{
+    using ServiceStack.Configuration;
+
+    public class DropboxAuthProvider : OAuthProvider
+    {
+        public const string Name = "dropbox";
+
+        public static string Realm = "https://api.dropbox.com/1/";
+
+        public DropboxAuthProvider(IResourceManager appSettings)
+            : base(appSettings, Realm, Name)
+        {
+            this.OAuthUtils = new DropboxOAuthAuthorizer(this);
+        }
+    }
+}
