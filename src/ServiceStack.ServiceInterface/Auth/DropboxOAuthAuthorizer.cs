3731640f09d3060b97c7d3b71613ea00a6f7c04e
Dave Hawes <dave@davehawes.com>
2013-05-08 10:08:31 +0100

    * added a new DropboxOAuthAuthorizer.cs class to allow users to login via the dropbox oauth api.

---
+++ src/ServiceStack.ServiceInterface/Auth/DropboxOAuthAuthorizer.cs
@@ -0,0 +1,25 @@
+﻿namespace ServiceStack.ServiceInterface.Auth
+{
+    using System.Collections.Specialized;
+
+    public class DropboxOAuthAuthorizer : OAuthAuthorizer
+    {
+        public DropboxOAuthAuthorizer(OAuthProvider provider)
+            : base(provider)
+        {
+        }
+
+        protected override bool AcquireRequestToken(NameValueCollection requestTokenResult)
+        {
+            if (requestTokenResult["oauth_token"] != null)
+            {
+                this.RequestToken = requestTokenResult["oauth_token"];
+                this.RequestTokenSecret = requestTokenResult["oauth_token_secret"];
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
