44266d41195a08bd1c068f55de747002dd60faf6
Nick Jones <medianick@gmail.com>
2013-05-09 16:21:07 -0400

    Added fix and test for issue #656 (with BasicAppHost.Dispose)
    When BasicAppHost.Dispose() is called without BasicAppHost.Init() having
    been called, it throws a NullReferenceException. I modified it to
    dispose EndpointHost.Config.ServiceManager only if EndpointHost.Config
    is not null as well, and provided a unit test (in a new project) to
    verify.

---
+++ tests/ServiceStack.ServiceInterface.Tests/BasicAppHostTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceStack.ServiceInterface.Testing;
+using NUnit.Framework;
+
+namespace ServiceStack.ServiceInterface.Tests
+{
+    [TestFixture]
+    public class BasicAppHostTests
+    {
+        [Test]
+        public void Can_dispose_without_init()
+        {
+            BasicAppHost appHost = new BasicAppHost();
+            appHost.Dispose();
+        }
+    }
+}
