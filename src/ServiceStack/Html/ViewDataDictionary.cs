5b65e08083ea2f13e19607fa932bce73936812e6
Damir Simunic <damir.simunic@wa-research.ch>
2013-05-22 01:32:03 +0200

    A few unit tests for Razor testing access to Html helper and accessing ViewBag from Layout Remove unused using statements

--- src/ServiceStack/Html/ViewDataDictionary.cs
+++ src/ServiceStack/Html/ViewDataDictionary.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Globalization;
-using ServiceStack.Markdown;
 using ServiceStack.Text;
 
 namespace ServiceStack.Html
