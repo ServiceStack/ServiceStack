780ebd768145af175e295462e1c4e87f601624ff
Demis Bellot <demis.bellot@gmail.com>
2013-06-18 00:08:45 -0400

    Add swagger-ui and SwaggerTestService to Integration ASP.NET project

---
+++ tests/ServiceStack.WebHost.IntegrationTests/Services/SwaggerTestService.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+using System.Net;
+using System.Runtime.Serialization;
+using ServiceStack.Api.Swagger;
+using ServiceStack.ServiceHost;
+
+namespace ServiceStack.WebHost.IntegrationTests.Services
+{
+    [Api("SwaggerTest Service Description")]
+    [ApiResponse(HttpStatusCode.BadRequest, "Your request was not understood")]
+    [ApiResponse(HttpStatusCode.InternalServerError, "Oops, something broke")]
+    [Route("/swagger", "GET", Summary = @"GET / Summary", Notes = "GET / Notes")]
+    [Route("/swagger/{Name}", "GET", Summary = @"GET Summary", Notes = "GET /Name Notes")]
+    [Route("/swagger/{Name}", "POST", Summary = @"POST Summary", Notes = "POST /Name Notes")]
+    [DataContract]
+    public class SwaggerTest
+    {
+        [ApiMember(Description = "Color Description",
+                   ParameterType = "path", DataType = "string", IsRequired = true)]
+        [ApiAllowableValues("Color", typeof(Color))] //Enum
+        public string Color { get; set; }
+
+        [ApiMember(Description = "Aliased Description",
+                   ParameterType = "path", DataType = "string", IsRequired = true)]
+        [DataMember(Name = "Aliased")]
+        public string Name { get; set; }
+
+        [ApiMember(Description = "Not Aliased Description",
+                   ParameterType = "path", DataType = "string", IsRequired = true)]
+        public string NotAliased { get; set; }
+    }
+
+    public class SwaggerTestService : ServiceInterface.Service
+    {
+         public object Get(SwaggerTest request)
+         {
+             return request;
+         }
+    }
+}
