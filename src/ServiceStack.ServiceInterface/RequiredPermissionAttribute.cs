337c9211ecd52ef68a64e3011d5d8bfd36b9e35b
Greg Pakes <greg.pakes@isams.co.uk>
2013-06-22 14:33:59 +0100

    Code tidy up from pull request #737.
    Tidied up the code from pull request #737.  Removed duplicate code in
    the 5 attributes that dealt with the HTML Redirects.

--- src/ServiceStack.ServiceInterface/RequiredPermissionAttribute.cs
+++ src/ServiceStack.ServiceInterface/RequiredPermissionAttribute.cs
@@ -34,14 +34,8 @@ namespace ServiceStack.ServiceInterface
 
             var session = req.GetSession();
             if (HasAllPermissions(req, session)) return;
-            
-            var htmlRedirect = HtmlRedirect ?? AuthService.HtmlRedirect;
-            if (htmlRedirect != null && req.ResponseContentType.MatchesContentType(ContentType.Html))
-            {
-                var url = req.ResolveAbsoluteUrl(htmlRedirect);
-                res.RedirectToUrl(url);
-                return;
-            }
+
+            if (DoHtmlRedirectIfConfigured(req, res)) return;
 
             res.StatusCode = (int)HttpStatusCode.Forbidden;
             res.StatusDescription = "Invalid Permission";
