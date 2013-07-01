9d956bf03336a9a7bf8979024d4f41e2d98ae9bf
Robert Beal <robertbeal@gmail.com>
2013-07-01 18:07:30 +0100

    Add NotAcceptableException and implement it in the StatusCode extension

---
+++ src/ServiceStack/NotAcceptableException.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace ServiceStack
+{
+	public class NotAcceptableException : HttpException
+	{
+		public IEnumerable<string> Allowed { get; private set; }
+
+		public NotAcceptableException() : this(new List<string>())
+		{
+		}
+
+		public NotAcceptableException(IEnumerable<string> allowed)
+		{
+			Allowed = allowed;
+		}
+	}
+}
