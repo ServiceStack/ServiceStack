5a6da3b274eb2af3d2c8ee115b53abd100955d65
mythz <demis.bellot@gmail.com>
2013-08-06 19:22:53 -0400

    Add ReturnsVoidService

---
+++ tests/ServiceStack.WebHost.IntegrationTests/Services/ReturnsVoidService.cs
@@ -0,0 +1,16 @@
+using ServiceStack.ServiceHost;
+
+namespace ServiceStack.WebHost.IntegrationTests.Services
+{
+	[Route("/returnsvoid")]
+	public class ReturnsVoid : IReturnVoid
+	{
+		public string Name { get; set; }
+	}
+
+    public class ReturnsVoidService : IService
+	{
+        public void Any(ReturnsVoid request) {}
+	}
+
+}
