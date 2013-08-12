9bc912fcea0bbe46e8b4b273f07a8799e1e1aea9
Pavel Savara <pavel.savara@gmail.com>
2013-08-12 17:31:21 +0200

    wsld improvements
    missing files

--- src/ServiceStack/WebHost.Endpoints/Support/Templates/Soap12WsdlTemplate.cs
+++ src/ServiceStack/WebHost.Endpoints/Support/Templates/Soap12WsdlTemplate.cs
@@ -43,7 +43,7 @@ namespace ServiceStack.WebHost.Endpoints.Support.Templates
 			get
 			{
 				return
-	@"<wsdl:binding name=""WSHttpBinding_ISyncReply"" type=""svc:ISyncReply"">
+    @"<wsdl:binding name=""WSHttpBinding_I{1}"" type=""svc:I{1}"">
 		<soap12:binding transport=""http://schemas.xmlsoap.org/soap/http"" />
 		{0}
 	</wsdl:binding>";
@@ -55,7 +55,7 @@ namespace ServiceStack.WebHost.Endpoints.Support.Templates
 			get
 			{
 				return
-	@"<wsdl:binding name=""WSHttpBinding_IOneWay"" type=""svc:IOneWay"">
+    @"<wsdl:binding name=""WSHttpBinding_I{1}OneWay"" type=""svc:I{1}OneWay"">
 		<soap12:binding transport=""http://schemas.xmlsoap.org/soap/http"" />
 		{0}
 	</wsdl:binding>";
@@ -67,8 +67,8 @@ namespace ServiceStack.WebHost.Endpoints.Support.Templates
 			get
 			{
 				return
-	@"<wsdl:service name=""{0}SyncReply"">
-		<wsdl:port name=""WSHttpBinding_ISyncReply"" binding=""svc:WSHttpBinding_ISyncReply"">
+    @"<wsdl:service name=""{0}SyncReply"">
+		<wsdl:port name=""WSHttpBinding_I{2}"" binding=""svc:WSHttpBinding_I{2}"">
 			<soap:address location=""{1}"" />
 		</wsdl:port>
 	</wsdl:service>";
@@ -80,8 +80,8 @@ namespace ServiceStack.WebHost.Endpoints.Support.Templates
 			get
 			{
 				return
-	@"<wsdl:service name=""{0}AsyncOneWay"">
-		<wsdl:port name=""WSHttpBinding_IOneWay"" binding=""svc:WSHttpBinding_IOneWay"">
+    @"<wsdl:service name=""{0}AsyncOneWay"">
+		<wsdl:port name=""WSHttpBinding_I{2}OneWay"" binding=""svc:WSHttpBinding_I{2}OneWay"">
 			<soap:address location=""{1}"" />
 		</wsdl:port>
 	</wsdl:service>";
