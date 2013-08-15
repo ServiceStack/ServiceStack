af4e95e0cd0be35e32a033043dc69a0d9ece31b9
mythz <demis.bellot@gmail.com>
2013-08-15 12:36:51 -0400

    fix broken test

--- tests/ServiceStack.WebHost.Endpoints.Tests/WsdlMetadataTests.cs
+++ tests/ServiceStack.WebHost.Endpoints.Tests/WsdlMetadataTests.cs
@@ -16,7 +16,7 @@ namespace ServiceStack.WebHost.Endpoints.Tests
 		{
 			var wsdlGenerator = new Soap11WsdlMetadataHandler();
 		    var xsdMetadata = new XsdMetadata(Metadata);
-		    var wsdlTemplate = wsdlGenerator.GetWsdlTemplate(xsdMetadata, "http://w3c.org/types", false, "http://w3c.org/types");
+		    var wsdlTemplate = wsdlGenerator.GetWsdlTemplate(xsdMetadata, "http://w3c.org/types", false, "http://w3c.org/types", "Service Name");
 
             Assert.That(wsdlTemplate.ReplyOperationNames, Is.EquivalentTo(xsdMetadata.GetReplyOperationNames(Format.Soap12)));
             Assert.That(wsdlTemplate.OneWayOperationNames, Is.EquivalentTo(xsdMetadata.GetOneWayOperationNames(Format.Soap12)));
