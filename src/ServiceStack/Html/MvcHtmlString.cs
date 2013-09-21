c7503e96608882f7ac7c9acff80b8dae377ba8f2
Demis Bellot <demis.bellot@gmail.com>
2013-09-21 04:15:23 -0400

    re-sync MvcHtmlString impl after runtime errors dynamically generating a class

--- src/ServiceStack/Html/MvcHtmlString.cs
+++ src/ServiceStack/Html/MvcHtmlString.cs
@@ -1,67 +1,27 @@
 ﻿using System;
-using System.Linq.Expressions;
+using System.Diagnostics.CodeAnalysis;
 using System.Web;
 
-namespace ServiceStack.Html
+public sealed class MvcHtmlString : HtmlString
 {
-	public class MvcHtmlString
-	{
-		private delegate MvcHtmlString MvcHtmlStringCreator(string value);
-		private static readonly MvcHtmlStringCreator _creator = GetCreator();
+    private readonly string _value;
 
-		// imporant: this declaration must occur after the _creator declaration
-		public static readonly MvcHtmlString Empty = Create(String.Empty);
+    public MvcHtmlString(string value)
+        : base(value ?? String.Empty)
+    {
+        _value = value ?? String.Empty;
+    }
 
-		private readonly string _value;
+    [SuppressMessage("Microsoft.Security", "CA2104:DoNotDeclareReadOnlyMutableReferenceTypes", Justification = "MvcHtmlString is immutable")]
+    public static readonly MvcHtmlString Empty = Create(String.Empty);
 
-		protected MvcHtmlString(string value)
-		{
-			_value = value ?? String.Empty;
-		}
+    public static MvcHtmlString Create(string value)
+    {
+        return new MvcHtmlString(value);
+    }
 
-		public static MvcHtmlString Create(string value)
-		{
-			return _creator(value);
-		}
-
-		// in .NET 4, we dynamically create a type that subclasses MvcHtmlString and implements IHtmlString
-		private static MvcHtmlStringCreator GetCreator()
-		{
-			var iHtmlStringType = typeof(HttpContext).Assembly.GetType("System.Web.IHtmlString");
-			if (iHtmlStringType != null)
-			{
-				// first, create the dynamic type
-				var dynamicType = DynamicTypeGenerator.GenerateType("DynamicMvcHtmlString", typeof(MvcHtmlString), new[] { iHtmlStringType });
-
-				// then, create the delegate to instantiate the dynamic type
-				var valueParamExpr = Expression.Parameter(typeof(string), "value");
-				var newObjExpr = Expression.New(dynamicType.GetConstructor(new[] { typeof(string) }), valueParamExpr);
-				var lambdaExpr = Expression.Lambda<MvcHtmlStringCreator>(newObjExpr, valueParamExpr);
-				return lambdaExpr.Compile();
-			}
-			else
-			{
-				// disabling 0618 allows us to call the MvcHtmlString() constructor
-#pragma warning disable 0618
-				return value => new MvcHtmlString(value);
-#pragma warning restore 0618
-			}
-		}
-
-		public static bool IsNullOrEmpty(MvcHtmlString value)
-		{
-			return (value == null || value._value.Length == 0);
-		}
-
-		// IHtmlString.ToHtmlString()
-		public string ToHtmlString()
-		{
-			return _value;
-		}
-
-		public override string ToString()
-		{
-			return _value;
-		}
-	}
+    public static bool IsNullOrEmpty(MvcHtmlString value)
+    {
+        return (value == null || value._value.Length == 0);
+    }
 }
