ed472131bfaf32806bb7d39ec17e44ee126e86b6
Demis Bellot <demis.bellot@gmail.com>
2013-09-22 03:14:18 -0400

    Move ServiceStack.FluentValidation.Mvc3 to ServiceStack.Mvc

--- src/ServiceStack.Mvc/ExecuteServiceStackFiltersAttribute.cs
+++ src/ServiceStack.Mvc/ExecuteServiceStackFiltersAttribute.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Web.Routing;
 using ServiceStack.ServiceInterface;
 using ServiceStack.ServiceInterface.Auth;
-using ServiceStack.Text;
 
 namespace ServiceStack.Mvc
 {
--- src/ServiceStack.Mvc/FunqControllerFactory.cs
+++ src/ServiceStack.Mvc/FunqControllerFactory.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Net;
 using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
