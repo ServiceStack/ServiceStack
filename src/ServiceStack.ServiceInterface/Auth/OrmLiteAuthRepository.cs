d8e5e8da367ee16cccbe730ae1410cd480c2a3f4
Demis Bellot <demis.bellot@gmail.com>
2013-10-10 17:14:31 -0400

    Rename DigestHA1Hash to DigestHa1Hash to be keep consistent Pascal naming of acronyms

--- src/ServiceStack.ServiceInterface/Auth/OrmLiteAuthRepository.cs
+++ src/ServiceStack.ServiceInterface/Auth/OrmLiteAuthRepository.cs
@@ -71,7 +71,7 @@ namespace ServiceStack.ServiceInterface.Auth
                 string hash;
                 passwordHasher.GetHashAndSaltString(password, out hash, out salt);
                 var digestHelper = new DigestAuthFunctions();
-                newUser.DigestHA1Hash = digestHelper.CreateHa1(newUser.UserName, DigestAuthProvider.Realm, password);
+                newUser.DigestHa1Hash = digestHelper.CreateHa1(newUser.UserName, DigestAuthProvider.Realm, password);
                 newUser.PasswordHash = hash;
                 newUser.Salt = salt;
                 newUser.CreatedDate = DateTime.UtcNow;
@@ -116,7 +116,7 @@ namespace ServiceStack.ServiceInterface.Auth
                     passwordHasher.GetHashAndSaltString(password, out hash, out salt);
                 }
                 // If either one changes the digest hash has to be recalculated
-                var digestHash = existingUser.DigestHA1Hash;
+                var digestHash = existingUser.DigestHa1Hash;
                 if (password != null || existingUser.UserName != newUser.UserName)
                 {
                     var digestHelper = new DigestAuthFunctions();
@@ -125,7 +125,7 @@ namespace ServiceStack.ServiceInterface.Auth
                 newUser.Id = existingUser.Id;
                 newUser.PasswordHash = hash;
                 newUser.Salt = salt;
-                newUser.DigestHA1Hash = digestHash;
+                newUser.DigestHa1Hash = digestHash;
                 newUser.CreatedDate = existingUser.CreatedDate;
                 newUser.ModifiedDate = DateTime.UtcNow;
 
@@ -173,7 +173,7 @@ namespace ServiceStack.ServiceInterface.Auth
             if (userAuth == null) return false;
 
             var digestHelper = new DigestAuthFunctions();
-            if (digestHelper.ValidateResponse(digestHeaders,PrivateKey,NonceTimeOut,userAuth.DigestHA1Hash,sequence))
+            if (digestHelper.ValidateResponse(digestHeaders,PrivateKey,NonceTimeOut,userAuth.DigestHa1Hash,sequence))
             {
                 //userId = userAuth.Id.ToString(CultureInfo.InvariantCulture);
                 return true;
