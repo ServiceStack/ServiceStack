af415b8be81fc378855ef7fdcf48e0773ece26f2
Demis Bellot <demis.bellot@gmail.com>
2013-10-19 03:03:25 -0400

    Add ConvertHtmlCodes extension method

---
+++ tests/ServiceStack.Common.Tests/StringUtilsTests.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Service Stack LLC. All Rights Reserved.
+// License: https://raw.github.com/ServiceStack/ServiceStack/master/license.txt
+
+
+using System;
+using System.Globalization;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ServiceStack.Common.Tests
+{
+    [TestFixture]
+    public class StringUtilsTests
+    {
+        [Test]
+        public void HtmlStrip_unescapes_all_html_character_codes_correctly()
+        {
+            foreach (var value in Enumerable.Range(ushort.MinValue, ushort.MaxValue).Select(i => (ushort)i))
+            {
+                var expected = ((char)value).ToString(CultureInfo.InvariantCulture);
+
+                var decimalNotation = String.Format("&#{0};", value);
+                var decimalActual = decimalNotation.StripHtml().ConvertHtmlCodes();
+                Assert.AreEqual(expected, decimalActual);
+
+                var hexNotation = String.Format("&#x{0:X};", value);
+                var hexActual = hexNotation.StripHtml().ConvertHtmlCodes();
+                Assert.AreEqual(expected, hexActual);
+            }
+
+            foreach (var htmlNotation in StringUtils.HtmlCharacterCodes)
+            {
+                var actual = htmlNotation.Key.StripHtml().ConvertHtmlCodes();
+                var expected = htmlNotation.Value;
+                Assert.AreEqual(expected, actual);
+            }
+        }
+    }
+}
