d19b8b5d8d3df5eeb14ce265621b0bded57ab539
Tyson Stolarski <tyson.stolarski+git@gmail.com>
2013-10-24 16:12:31 +0800

    Added test that reproduces malformed cookie name exception.

---
+++ tests/ServiceStack.WebHost.IntegrationTests/Services/CookiesService.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ServiceStack.ServiceHost;
+
+namespace ServiceStack.WebHost.IntegrationTests.Services
+{
+    [Route("/cookies")]
+    public class Cookies : IReturn<CookiesResponse>
+    { }
+
+    public class CookiesResponse
+    {
+        public List<string> RequestCookieNames { get; set; }
+    }
+
+    public class CookiesService : ServiceInterface.Service
+    {
+        public CookiesResponse Any(Cookies c)
+        {
+            var response = new CookiesResponse
+            {
+                RequestCookieNames = Request.Cookies.Keys.ToList(),
+            };
+            return response;
+        }
+    }
+}
