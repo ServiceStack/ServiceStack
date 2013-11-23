29d60dfa22424fe20ba35c8603686c05f88a6c25
Demis Bellot <demis.bellot@gmail.com>
2013-11-23 00:52:40 -0500

    Add new IRestGateway interface for 3rd Party typed APIs, with MockRestGateway and tests

---
+++ src/ServiceStack.Interfaces/IRestGateway.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) Service Stack LLC. All Rights Reserved.
+// License: https://raw.github.com/ServiceStack/ServiceStack/master/license.txt
+
+namespace ServiceStack
+{
+    public interface IRestGateway
+    {
+        T Send<T>(IReturn<T> request);
+        T Get<T>(IReturn<T> request);
+        T Post<T>(IReturn<T> request);
+        T Put<T>(IReturn<T> request);
+        T Delete<T>(IReturn<T> request);
+    }
+}
---
+++ src/ServiceStack/Testing/MockRestGateway.cs
@@ -0,0 +1,57 @@
+﻿// Copyright (c) Service Stack LLC. All Rights Reserved.
+// License: https://raw.github.com/ServiceStack/ServiceStack/master/license.txt
+
+using System;
+
+namespace ServiceStack.Testing
+{
+    public delegate object RestGatewayDelegate(string httpVerb, Type responseType, object requestDto);
+
+    public class MockRestGateway : IRestGateway
+    {
+        public RestGatewayDelegate ResultsFilter { get; set; }
+
+        public T Send<T>(IReturn<T> request)
+        {
+            var method = request is IPost ?
+                  HttpMethods.Post
+                : request is IPut ?
+                  HttpMethods.Put
+                : request is IDelete ?
+                  HttpMethods.Delete :
+                  HttpMethods.Get;
+
+            return ResultsFilter != null
+                ? (T)ResultsFilter(method, typeof(T), request)
+                : default(T);
+        }
+
+        public T Get<T>(IReturn<T> request)
+        {
+            return ResultsFilter != null
+                ? (T)ResultsFilter(HttpMethods.Get, typeof(T), request)
+                : default(T);
+        }
+
+        public T Post<T>(IReturn<T> request)
+        {
+            return ResultsFilter != null
+                ? (T)ResultsFilter(HttpMethods.Post, typeof(T), request)
+                : default(T);
+        }
+
+        public T Put<T>(IReturn<T> request)
+        {
+            return ResultsFilter != null
+                ? (T)ResultsFilter(HttpMethods.Put, typeof(T), request)
+                : default(T);
+        }
+
+        public T Delete<T>(IReturn<T> request)
+        {
+            return ResultsFilter != null
+                ? (T)ResultsFilter(HttpMethods.Delete, typeof(T), request)
+                : default(T);
+        }
+    }
+}
