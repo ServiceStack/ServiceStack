89b060920c48553196df21a8e3698e5e88abf7a8
Demis Bellot <demis.bellot@gmail.com>
2014-01-08 03:04:08 +0800

    Add Configure to PclExportClient.SL5

--- src/ServiceStack.Client/PclExportClient.SL5.cs
+++ src/ServiceStack.Client/PclExportClient.SL5.cs
@@ -4,6 +4,7 @@
 #if SL5
 using System;
 using System.Collections.Specialized;
+using System.Threading;
 using System.Web;
 using ServiceStack.Pcl;
 using ServiceStack.Web;
@@ -12,6 +13,8 @@ namespace ServiceStack
 {
     public class Sl5PclExportClient : PclExportClient
     {
+        public static Sl5PclExportClient Provider = new Sl5PclExportClient();
+
         public override INameValueCollection NewNameValueCollection()
         {
             return new NameValueCollectionWrapper(new NameValueCollection());
@@ -25,6 +28,37 @@ namespace ServiceStack
         public override void RunOnUiThread(Action fn)
         {
             System.Windows.Deployment.Current.Dispatcher.BeginInvoke(fn);
+        }
+
+        public static void Configure()
+        {
+            Configure(Provider);
+            Sl5PclExport.Configure();
+        }
+    }
+
+
+    public class AsyncTimer : ITimer
+    {
+        public System.Threading.Timer Timer;
+
+        public AsyncTimer(System.Threading.Timer timer)
+        {
+            Timer = timer;
+        }
+
+        public void Cancel()
+        {
+            this.Timer.Change(Timeout.Infinite, Timeout.Infinite);
+            this.Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (Timer == null) return;
+
+            this.Timer.Dispose();
+            this.Timer = null;
         }
     }
 }
