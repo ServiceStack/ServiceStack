ff9b38b1f40edfb597d3e2fc0a985016d5d72b3e
Demis Bellot <demis.bellot@gmail.com>
2014-02-27 21:03:34 -0500

    Add OrmLiteCacheClient

---
+++ src/ServiceStack.Server/Caching/OrmLiteCacheClient.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using ServiceStack.Auth;
+using ServiceStack.OrmLite;
+
+namespace ServiceStack.Caching
+{
+    public class OrmLiteCacheClient : RepositoryBase, ICacheClient, IRequiresSchema
+    {
+        CacheEntry CreateEntry(string id, string data=null, 
+            DateTime? created=null, DateTime? expires=null)
+        {
+            var createdDate = created ?? DateTime.UtcNow;
+            return new CacheEntry {
+                Id = id,
+                Data = data,
+                ExpiryDate = expires,
+                CreatedDate = createdDate,
+                ModifiedDate = createdDate,
+            };
+        }
+
+        public bool Remove(string key)
+        {
+            return Db.DeleteById<CacheEntry>(key) > 0;
+        }
+
+        public void RemoveAll(IEnumerable<string> keys)
+        {
+            Db.DeleteByIds<CacheEntry>(keys);
+        }
+
+        public T Get<T>(string key)
+        {
+            var cache = Verify(Db.SingleById<CacheEntry>(key));
+            return cache == null 
+                ? default(T) 
+                : Db.Deserialize<T>(cache.Data);
+        }
+
+        public long Increment(string key, uint amount)
+        {
+            long nextVal;
+            using (var dbTrans = Db.OpenTransaction(IsolationLevel.ReadCommitted))
+            {
+                var cache = Verify(Db.SingleById<CacheEntry>(key));
+
+                if (cache == null)
+                {
+                    nextVal = amount;
+                    Db.Insert(CreateEntry(key, nextVal.ToString()));
+                }
+                else
+                {
+                    nextVal = long.Parse(cache.Data) + amount;
+                    cache.Data = nextVal.ToString();
+
+                    Db.Update(cache);
+                }
+
+                dbTrans.Commit();
+            }
+
+            return nextVal;
+        }
+
+        public long Decrement(string key, uint amount)
+        {
+            long nextVal;
+            using (var dbTrans = Db.OpenTransaction(IsolationLevel.ReadCommitted))
+            {
+                var cache = Verify(Db.SingleById<CacheEntry>(key));
+
+                if (cache == null)
+                {
+                    nextVal = -amount;
+                    Db.Insert(CreateEntry(key, nextVal.ToString()));
+                }
+                else
+                {
+                    nextVal = long.Parse(cache.Data) - amount;
+                    cache.Data = nextVal.ToString();
+
+                    Db.Update(cache);
+                }
+
+                dbTrans.Commit();
+            }
+
+            return nextVal;
+        }
+
+        public bool Add<T>(string key, T value)
+        {
+            try
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value)));
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Set<T>(string key, T value)
+        {
+            var exists = Db.UpdateOnly(new CacheEntry
+                {
+                    Id = key,
+                    Data = Db.Serialize(value),
+                    ModifiedDate = DateTime.UtcNow,
+                },
+                q => new { q.Data, q.ModifiedDate },
+                q => q.Id == key) == 1;
+
+            if (!exists)
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value)));
+            }
+
+            return true;
+        }
+
+        public bool Replace<T>(string key, T value)
+        {
+            var exists = Db.UpdateOnly(new CacheEntry
+                {
+                    Id = key,
+                    Data = Db.Serialize(value),
+                    ModifiedDate = DateTime.UtcNow,
+                },
+                q => new { q.Data, q.ModifiedDate },
+                q => q.Id == key) == 1;
+
+            if (!exists)
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value)));
+            }
+
+            return true;
+        }
+
+        public bool Add<T>(string key, T value, DateTime expiresAt)
+        {
+            try
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value), expires:expiresAt));
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Set<T>(string key, T value, DateTime expiresAt)
+        {
+            var exists = Db.UpdateOnly(new CacheEntry
+                {
+                    Id = key,
+                    Data = Db.Serialize(value),
+                    ExpiryDate = expiresAt,
+                    ModifiedDate = DateTime.UtcNow,
+                },
+                q => new { q.Data, ExpiredDate = q.ExpiryDate, q.ModifiedDate },
+                q => q.Id == key) == 1;
+
+            if (!exists)
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value), expires:expiresAt));
+            }
+
+            return true;
+        }
+
+        public bool Replace<T>(string key, T value, DateTime expiresAt)
+        {
+            var exists = Db.UpdateOnly(new CacheEntry
+                {
+                    Id = key,
+                    Data = Db.Serialize(value),
+                    ExpiryDate = expiresAt,
+                    ModifiedDate = DateTime.UtcNow,
+                },
+                q => new { q.Data, ExpiredDate = q.ExpiryDate, q.ModifiedDate },
+                q => q.Id == key) == 1;
+
+            if (!exists)
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value), expires: expiresAt));
+            }
+
+            return true;
+        }
+
+        public bool Add<T>(string key, T value, TimeSpan expiresIn)
+        {
+            try
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value), 
+                    expires: DateTime.UtcNow.Add(expiresIn)));
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public bool Set<T>(string key, T value, TimeSpan expiresIn)
+        {
+            var exists = Db.UpdateOnly(new CacheEntry
+                {
+                    Id = key,
+                    Data = Db.Serialize(value),
+                    ExpiryDate = DateTime.UtcNow.Add(expiresIn),
+                    ModifiedDate = DateTime.UtcNow,
+                },
+                q => new { q.Data, ExpiredDate = q.ExpiryDate, q.ModifiedDate },
+                q => q.Id == key) == 1;
+
+            if (!exists)
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value), expires: DateTime.UtcNow.Add(expiresIn)));
+            }
+
+            return true;
+        }
+
+        public bool Replace<T>(string key, T value, TimeSpan expiresIn)
+        {
+            var exists = Db.UpdateOnly(new CacheEntry
+            {
+                Id = key,
+                Data = Db.Serialize(value),
+                ExpiryDate = DateTime.UtcNow.Add(expiresIn),
+                ModifiedDate = DateTime.UtcNow,
+            },
+                q => new { q.Data, ExpiredDate = q.ExpiryDate, q.ModifiedDate },
+                q => q.Id == key) == 1;
+
+            if (!exists)
+            {
+                Db.Insert(CreateEntry(key, Db.Serialize(value), expires: DateTime.UtcNow.Add(expiresIn)));
+            }
+
+            return true;
+        }
+
+        public void FlushAll()
+        {
+            Db.DeleteAll<CacheEntry>();
+        }
+
+        public IDictionary<string, T> GetAll<T>(IEnumerable<string> keys)
+        {
+            var results = Verify(Db.SelectByIds<CacheEntry>(keys));
+            var map = new Dictionary<string, T>();
+
+            results.Each(x => 
+                map[x.Id] = Db.Deserialize<T>(x.Data));
+
+            foreach (var key in keys)
+            {
+                if (!map.ContainsKey(key))
+                    map[key] = default(T);
+            }
+
+            return map;
+        }
+
+        public void SetAll<T>(IDictionary<string, T> values)
+        {
+            var rows = values.Select(entry => 
+                CreateEntry(entry.Key, Db.Serialize(entry.Value)))
+                .ToList();
+
+            Db.InsertAll(rows);
+        }
+
+        public void InitSchema()
+        {
+            Db.CreateTableIfNotExists<CacheEntry>();
+        }
+
+        public List<CacheEntry> Verify(IEnumerable<CacheEntry> entries)
+        {
+            var results = entries.ToList();
+            var expired = results.RemoveAll(x => x.ExpiryDate != null && DateTime.UtcNow > x.ExpiryDate);
+            if (expired > 0)
+            {
+                Db.Delete<CacheEntry>(q => q.ExpiryDate > DateTime.UtcNow);
+            }
+
+            return results;
+        }
+
+        public CacheEntry Verify(CacheEntry entry)
+        {
+            if (entry != null &&
+                entry.ExpiryDate != null && DateTime.UtcNow > entry.ExpiryDate)
+            {
+                Db.Delete<CacheEntry>(q => DateTime.UtcNow > q.ExpiryDate);
+                return null;
+            }
+            return entry;
+        }
+    }
+
+    public class CacheEntry
+    {
+        public string Id { get; set; }
+        public string Data { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+
+    public static class CacheExtensions
+    {
+        public static void InitSchema(this ICacheClient cache)
+        {
+            var requiresSchema = cache as IRequiresSchema;
+            if (requiresSchema != null)
+            {
+                requiresSchema.InitSchema();
+            }
+        }
+
+        public static string Serialize<T>(this IDbConnection db, T value)
+        {
+            return db.GetDialectProvider().StringSerializer.SerializeToString(value);
+        }
+
+        public static T Deserialize<T>(this IDbConnection db, string text)
+        {
+            return text == null 
+                ? default(T) 
+                : db.GetDialectProvider().StringSerializer.DeserializeFromString<T>(text);
+        }
+    }
+}
---
+++ tests/ServiceStack.Server.Tests/Properties/CacheClientTests.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using ServiceStack.Caching;
+using ServiceStack.OrmLite;
+using ServiceStack.Redis;
+
+namespace ServiceStack.Server.Tests.Properties
+{
+    public class Config
+    {
+        public static string SqlServerBuildDb = "Server={0};Database=test;User Id=test;Password=test;".Fmt(Environment.GetEnvironmentVariable("CI_HOST"));
+    }
+
+    public class Item
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        protected bool Equals(Item other)
+        {
+            return Id == other.Id && string.Equals(Name, other.Name);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Item) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Id*397) ^ (Name != null ? Name.GetHashCode() : 0);
+            }
+        }
+    }
+
+    public class OrmLiteCacheClientTests : CacheClientTestsBase
+    {
+        public override ICacheClient CreateClient()
+        {
+            var cache = new OrmLiteCacheClient
+            {
+                DbFactory = new OrmLiteConnectionFactory(
+                    Config.SqlServerBuildDb, SqlServerDialect.Provider)
+            };
+            
+            //using (var db = cache.DbFactory.Open())
+            //{
+            //    db.DropTable<CacheEntry>();
+            //}
+
+            cache.InitSchema();
+
+            return cache;
+        }
+    }
+
+    public class MemoryCacheClientTests : CacheClientTestsBase
+    {
+        public override ICacheClient CreateClient()
+        {
+            return new MemoryCacheClient();
+        }
+    }
+
+    [TestFixture]
+    public abstract class CacheClientTestsBase
+    {
+        private readonly ICacheClient Cache;
+
+        public abstract ICacheClient CreateClient();
+
+        protected CacheClientTestsBase()
+        {
+            Cache = CreateClient();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            Cache.Dispose();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            Cache.FlushAll();
+        }
+
+        [Test]
+        public void Does_flush_all()
+        {
+            3.Times(i => 
+                Cache.Set(i.ToUrn<Item>(), new Item { Id = i, Name = "Name" + i }));
+
+            Assert.That(Cache.Get<Item>(1.ToUrn<Item>()), Is.Not.Null);
+
+            Cache.FlushAll();
+            
+            Assert.That(Cache.Get<Item>(1.ToUrn<Item>()), Is.Null);
+        }
+
+        [Test]
+        public void Can_set_and_remove_entry()
+        {
+            var key = 1.ToUrn<Item>();
+
+            var item = Cache.Get<Item>(key);
+            Assert.That(item, Is.Null);
+
+            var whenNotExists = Cache.Set(key, new Item { Id = 1, Name = "Foo" });
+            Assert.That(whenNotExists, Is.True);
+            var whenExists = Cache.Set(key, new Item { Id = 1, Name = "Foo" });
+            Assert.That(whenExists, Is.True);
+
+            item = Cache.Get<Item>(key);
+            Assert.That(item, Is.Not.Null);
+            Assert.That(item.Name, Is.EqualTo("Foo"));
+
+            whenExists = Cache.Remove(key);
+            Assert.That(whenExists, Is.True);
+
+            whenNotExists = Cache.Remove(key);
+            Assert.That(whenNotExists, Is.False);
+        }
+
+        [Test]
+        public void Can_update_existing_entry()
+        {
+            var key = 1.ToUrn<Item>();
+
+            Cache.Set(key, new Item { Id = 1, Name = "Foo" });
+            Cache.Set(key, new Item { Id = 2, Name = "Updated" });
+
+            var item = Cache.Get<Item>(key);
+
+            Assert.That(item.Id, Is.EqualTo(2));
+            Assert.That(item.Name, Is.EqualTo("Updated"));
+        }
+
+        [Test]
+        public void Does_SetAll_and_GetAll()
+        {
+            var map = 3.Times(i => new Item { Id = i, Name = "Name" + i })
+                .ToSafeDictionary(x => x.ToUrn());
+
+            Cache.SetAll(map);
+
+            var cacheMap = Cache.GetAll<Item>(map.Keys);
+
+            Assert.That(cacheMap, Is.EquivalentTo(map));
+        }
+
+        [Test]
+        public void Does_not_return_expired_items()
+        {
+            var key = 1.ToUrn<Item>();
+
+            Cache.Set(key, new Item { Id = 1, Name = "Foo" }, DateTime.UtcNow.AddSeconds(-1));
+
+            Assert.That(Cache.Get<Item>(key), Is.Null);
+
+            Cache.Set(key, new Item { Id = 1, Name = "Foo" }, TimeSpan.FromMilliseconds(100));
+            Assert.That(Cache.Get<Item>(key), Is.Not.Null);
+            Thread.Sleep(200);
+
+            Assert.That(Cache.Get<Item>(key), Is.Null);
+
+            Cache.Set(key, new Item { Id = 1, Name = "Foo" }, DateTime.UtcNow.AddMilliseconds(100));
+            Assert.That(Cache.Get<Item>(key), Is.Not.Null);
+            Thread.Sleep(200);
+
+            Assert.That(Cache.Get<Item>(key), Is.Null);
+        }
+
+        [Test]
+        public void Can_increment_and_decrement_values()
+        {
+            Assert.That(Cache.Increment("incr:a", 2), Is.EqualTo(2));
+            Assert.That(Cache.Increment("incr:a", 3), Is.EqualTo(5));
+
+            Assert.That(Cache.Decrement("decr:a", 2), Is.EqualTo(-2));
+            Assert.That(Cache.Decrement("decr:a", 3), Is.EqualTo(-5));
+        }
+
+        [Test]
+        public void Can_remove_multiple_items()
+        {
+            var map = 5.Times(i => new Item { Id = i, Name = "Name" + i })
+                .ToSafeDictionary(x => x.ToUrn());
+
+            Cache.SetAll(map);
+
+            Cache.RemoveAll(map.Keys);
+
+            var cacheMap = Cache.GetAll<Item>(map.Keys);
+
+            Assert.That(cacheMap.Count, Is.EqualTo(5));
+            Assert.That(cacheMap.Values.All(x => x == null));
+        }
+    }
+}
