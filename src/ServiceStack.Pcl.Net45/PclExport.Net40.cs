ae2af5ee5b0fe29eb4c3cdd362be0166dcd7f4b8
Demis Bellot <demis.bellot@gmail.com>
2014-03-05 13:46:53 -0500

    Update PCL classes

--- src/ServiceStack.Pcl.Net45/PclExport.Net40.cs
+++ src/ServiceStack.Pcl.Net45/PclExport.Net40.cs
@@ -461,9 +461,9 @@ namespace ServiceStack
             return key;
         }
 
-        public override void VerifyInAssembly(Type accessType, string assemblyName)
+        public override void VerifyInAssembly(Type accessType, ICollection<string> assemblyNames)
         {
-            if (!assemblyName.EqualsIgnoreCase(accessType.Assembly.ManifestModule.Name)) //might get merged/mangled on alt platforms
+            if (!assemblyNames.Contains(accessType.Assembly.ManifestModule.Name)) //might get merged/mangled on alt platforms
                 throw new LicenseException(LicenseUtils.ErrorMessages.UnauthorizedAccessRequest);
         }
 
@@ -689,14 +689,14 @@ namespace ServiceStack
         }
 
         [MonoTouch.Foundation.Preserve]
-        static void RegisterQueryStringWriter()
+        public static void RegisterQueryStringWriter()
         {
             var i = 0;
             if (QueryStringWriter<Poco>.WriteFn() != null) i++;
         }
 
         [MonoTouch.Foundation.Preserve]
-        internal static int RegisterElement<T, TElement>()
+        public static int RegisterElement<T, TElement>()
         {
             var i = 0;
             i += AotConfig.RegisterSerializers<TElement>();
