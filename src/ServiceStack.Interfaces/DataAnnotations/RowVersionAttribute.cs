e1c63e80837770c6be9ed397190ae4f05a0ad55a
Bruce Cowan <bruce.cowan@aquaticinformatics.com>
2014-03-17 12:39:47 -0700

    Add RowVersionAttribute.

---
+++ src/ServiceStack.Interfaces/DataAnnotations/RowVersionAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceStack.DataAnnotations
+{
+    /// <summary>
+    /// Used to indicate that property is a row version incremented automatically by the database
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class RowVersionAttribute : AttributeBase
+    {
+    }
+}
