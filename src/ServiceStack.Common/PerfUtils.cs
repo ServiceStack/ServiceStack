95732d863d3b5f49c00775cad0beb4dff34146d9
Demis Bellot <demis.bellot@gmail.com>
2014-03-23 19:10:47 -0400

    Add Dart's benchmark approach to PerfUtils

--- src/ServiceStack.Common/PerfUtils.cs
+++ src/ServiceStack.Common/PerfUtils.cs
@@ -12,19 +12,67 @@ namespace ServiceStack
             return TimeSpan.FromSeconds(fromTicks * 1d / Stopwatch.Frequency);
         }
 
-        public static long Measure(long iterations, Action action)
+        /// <summary>
+        /// Runs an action for a minimum of runForMs
+        /// </summary>
+        /// <param name="fn">What to run</param>
+        /// <param name="runForMs">Minimum ms to run for</param>
+        /// <returns>time elapsed in micro seconds</returns>
+        public static double MeasureFor(Action fn, int runForMs)
         {
-            GC.Collect();
-            var begin = Stopwatch.GetTimestamp();
-
-            for (var i = 0; i < iterations; i++)
+            int iter = 0;
+            var watch = new Stopwatch();
+            watch.Start();
+            long elapsed = 0;
+            while (elapsed < runForMs)
             {
-                action();
+                fn();
+                elapsed = watch.ElapsedMilliseconds;
+                iter++;
             }
 
-            var end = Stopwatch.GetTimestamp();
+            return 1000.0 * elapsed / iter;
+        }
 
-            return (end - begin);
+        /// <summary>
+        /// Returns average microseconds an action takes when run for the specified runForMs
+        /// </summary>
+        /// <param name="fn">What to run</param>
+        /// <param name="times">How many times to run for each iteration</param>
+        /// <param name="runForMs">Minimum ms to run for</param>
+        /// <param name="setup"></param>
+        /// <param name="warmup"></param>
+        /// <param name="teardown"></param>
+        /// <returns></returns>
+        public static double Measure(Action fn,
+            int times = 1,
+            int runForMs = 2000,
+            Action setup = null,
+            Action warmup = null,
+            Action teardown = null)
+        {
+            if (setup != null)
+                setup();
+
+            // Warmup for at least 100ms. Discard result.
+            if (warmup == null)
+                warmup = fn;
+
+            GC.Collect();
+
+            MeasureFor(() => warmup(), 100);
+
+            // Run the benchmark for at least 2000ms.
+            double result = MeasureFor(() =>
+            {
+                for (var i = 0; i < times; i++)
+                    fn();
+            }, runForMs);
+
+            if (teardown != null)
+                teardown();
+
+            return result;
         }
     }
 }
