020fc54e5c3709fd092dff22b52323a0293a38d0
Demis Bellot <demis.bellot@gmail.com>
2014-03-24 18:06:17 -0400

    Update year to 2014

--- src/ServiceStack.Common/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Common/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.Serialization;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Service Stack LLC")]
 [assembly: AssemblyProduct("ServiceStack")]
-[assembly: AssemblyCopyright("Copyright (c) ServiceStack 2013")]
+[assembly: AssemblyCopyright("Copyright (c) ServiceStack 2014")]
 [assembly: AssemblyTrademark("Service Stack")]
 [assembly: AssemblyCulture("")]
 
