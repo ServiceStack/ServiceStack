7e6c730a1d87d2dfb36a08b202137dc25461dd70
Carl Healy <carl@connectassist.com.au>
2014-04-16 02:41:54 +1000

    Add MSBuild Task to generate Razor View source

---
+++ src/ServiceStack.Razor.BuildTask/Support/HelperResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+
+namespace ServiceStack.MiniProfiler
+{
+    // Dummy class to satisfy linked files from SS.Razor project
+    public class HelperResult : IHtmlString
+    {
+        public string ToHtmlString()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ src/ServiceStack.Razor.BuildTask/Support/ViewPage.cs
@@ -0,0 +1,12 @@
+﻿namespace ServiceStack.Razor
+{
+    // Dummy class to satisfy linked files from SS.Razor project
+    public abstract class ViewPage : ViewPageBase<dynamic>
+    {
+    }
+
+    // Dummy class to satisfy linked files from SS.Razor project
+    public abstract class ViewPageBase<T>
+    {
+    }
+}
