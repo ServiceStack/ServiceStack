5496f1b6f4e6407820e29d03915e77f452dcb94d
Carl Healy <carl@connectassist.com.au>
2014-04-17 13:43:52 +1000

    Create NuGet package for Razor Build Task

---
+++ NuGet/ServiceStack.Razor.BuildTask/tools/install.ps1
@@ -0,0 +1,26 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$targetsFileName = 'ServiceStack.Razor.BuildTask.targets';
+$targetsPath = [System.IO.Path]::Combine($toolsPath, $targetsFileName)
+
+# Need to load MSBuild assembly if it's not loaded yet.
+Add-Type -AssemblyName 'Microsoft.Build, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a'
+
+# Grab the loaded MSBuild project for the project
+$msbuild = [Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName) | Select-Object -First 1
+
+# Make the path to the targets file relative.
+$projectUri = new-object Uri('file://' + $project.FullName)
+$targetUri = new-object Uri('file://' + $targetsPath)
+$relativePath = $projectUri.MakeRelativeUri($targetUri).ToString().Replace([System.IO.Path]::AltDirectorySeparatorChar, [System.IO.Path]::DirectorySeparatorChar)
+
+# Remove previous imports to .targets
+$msbuild.Xml.Imports | Where-Object {$_.Project.ToLowerInvariant().EndsWith($targetsFileName.ToLowerInvariant()) } | Foreach { 
+	$_.Parent.RemoveChild( $_ ) 
+	[string]::Format( "Removed import of '{0}'" , $_.Project )
+}
+
+# Add the import and save the project
+$import = $msbuild.Xml.AddImport($relativePath)
+$import.set_Condition( "Exists('$relativePath')" ) | Out-Null
+[string]::Format("Added import of '{0}'.", $relativePath )
