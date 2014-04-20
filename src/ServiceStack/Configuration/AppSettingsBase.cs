a6b6f6dc7b87be1c3729aef6c0eef31f577c27a3
Demis Bellot <demis.bellot@gmail.com>
2014-04-20 19:43:05 -0400

    Don't attempt to deserialize a null value

--- src/ServiceStack/Configuration/AppSettingsBase.cs
+++ src/ServiceStack/Configuration/AppSettingsBase.cs
@@ -78,10 +78,13 @@ namespace ServiceStack.Configuration
         {
             var stringValue = GetNullableString(name);
 
-            T deserializedValue;
+            T ret = defaultValue;
             try
             {
-                deserializedValue = TypeSerializer.DeserializeFromString<T>(stringValue);
+                if (stringValue != null)
+                {
+                    ret = TypeSerializer.DeserializeFromString<T>(stringValue);
+                }
             }
             catch (Exception ex)
             {
@@ -92,9 +95,7 @@ namespace ServiceStack.Configuration
                 throw new ConfigurationErrorsException(message, ex);
             }
 
-            return stringValue != null
-                       ? deserializedValue
-                       : defaultValue;
+            return ret;
         }
     }
 
