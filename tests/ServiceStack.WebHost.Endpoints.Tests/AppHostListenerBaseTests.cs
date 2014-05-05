a14acddf69fac60136c92faa250a87d431105ba2
angel ignacio colmenares laguado <angel.ignacio.colmenares@gmail.com>
2014-05-05 17:12:56 -0500

    Update AppHostListenerBaseTests.cs
    change port (82) to a registered port (1337), that can be used by ordinary users.

--- tests/ServiceStack.WebHost.Endpoints.Tests/AppHostListenerBaseTests.cs
+++ tests/ServiceStack.WebHost.Endpoints.Tests/AppHostListenerBaseTests.cs
@@ -15,7 +15,7 @@ namespace ServiceStack.WebHost.Endpoints.Tests
     [TestFixture]
     public class AppHostListenerBaseTests
     {
-        private const string ListeningOn = "http://localhost:1338/";
+        private const string ListeningOn = "http://localhost:1337/";
         ServiceStackHost appHost;
 
         static AppHostListenerBaseTests()
