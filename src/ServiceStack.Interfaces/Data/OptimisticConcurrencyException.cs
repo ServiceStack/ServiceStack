6c93c11f53363a4c4cac3f29215758d873791c52
Demis Bellot <demis.bellot@gmail.com>
2014-06-02 01:37:09 -0400

    Add OptimisticConcurrencyException to SS.Interfaces

---
+++ src/ServiceStack.Interfaces/Data/OptimisticConcurrencyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ServiceStack.Data
+{
+    public class OptimisticConcurrencyException : DataException
+    {
+        public OptimisticConcurrencyException() {}
+
+        public OptimisticConcurrencyException(string message) : base(message) {}
+
+        public OptimisticConcurrencyException(string message, Exception innerException) 
+            : base(message, innerException) {}
+    }
+}
