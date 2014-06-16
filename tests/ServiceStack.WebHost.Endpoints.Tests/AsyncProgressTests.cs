8ae7e8459180255bcccec0c57bdcbee11f82d828
Demis Bellot <demis.bellot@gmail.com>
2014-06-16 14:42:55 -0400

    Add Async progress example using POST

--- tests/ServiceStack.WebHost.Endpoints.Tests/AsyncProgressTests.cs
+++ tests/ServiceStack.WebHost.Endpoints.Tests/AsyncProgressTests.cs
@@ -62,6 +62,39 @@ namespace ServiceStack.WebHost.Endpoints.Tests
             {
                 AsyncServiceClient.BufferSize = hold;
             }
+        }
+
+        [Test]
+        public async Task Can_report_progress_when_downloading_async_with_Post()
+        {
+            var hold = AsyncServiceClient.BufferSize;
+            AsyncServiceClient.BufferSize = 100;
+
+            try
+            {
+                var asyncClient = new JsonServiceClient(ListeningOn);
+
+                var progress = new List<string>();
+
+                //Note: total = -1 when 'Transfer-Encoding: chunked'
+                //Available in ASP.NET or in HttpListener when downloading responses with known lengths: 
+                //E.g: Strings, Files, etc.
+                asyncClient.OnDownloadProgress = (done, total) =>
+                    progress.Add("{0}/{1} bytes downloaded".Fmt(done, total));
+
+                var response = await asyncClient.PostAsync(new TestProgress());
+
+                progress.Each(x => x.Print());
+
+                Assert.That(response.Length, Is.GreaterThan(0));
+                Assert.That(progress.Count, Is.GreaterThan(0));
+                Assert.That(progress.First(), Is.EqualTo("100/1160 bytes downloaded"));
+                Assert.That(progress.Last(), Is.EqualTo("1160/1160 bytes downloaded"));
+            }
+            finally
+            {
+                AsyncServiceClient.BufferSize = hold;
+            }
         }         
     }
 }
