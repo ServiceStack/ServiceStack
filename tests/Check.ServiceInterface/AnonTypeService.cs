5cb8965184b6c9c70851ad2c65c97bf14ff82321
Demis Bellot <demis.bellot@gmail.com>
2014-07-09 15:12:59 -0400

    Add new AnonTypeService

---
+++ tests/Check.ServiceInterface/AnonTypeService.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack;
+
+namespace Check.ServiceInterface
+{
+    [Route("/anontype")]
+    public class AnonType { }
+
+    public class AnonTypeService : Service
+    {
+        public object Any(AnonType request)
+        {
+            return new { PrimaryKeyId = 1 };
+        }
+    }
+}
