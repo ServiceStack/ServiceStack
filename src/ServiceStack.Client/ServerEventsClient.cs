5a60ab416c7dd6c25949b84abbb4e0f83a29b35b
Demis Bellot <demis.bellot@gmail.com>
2014-08-21 03:05:20 -0400

    Add initial cut of ServerEventsClient with Tests

---
+++ src/ServiceStack.Client/ServerEventsClient.cs
@@ -0,0 +1,368 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ServiceStack.Logging;
+using ServiceStack.Text;
+
+namespace ServiceStack
+{
+    public class ServerEventConnect : ServerEventJoin
+    {
+        public string Id { get; set; }
+        public string UnRegisterUrl { get; set; }
+        public string HeartbeatUrl { get; set; }
+        public long HeartbeatIntervalMs { get; set; }
+    }
+
+    public class ServerEventJoin : ServerEventCommand
+    {
+        public string UserId { get; set; }
+        public string DisplayName { get; set; }
+        public string ProfileUrl { get; set; }
+    }
+
+    public class ServerEventLeave : ServerEventCommand {}
+
+    public class ServerEventCommand : ServerEventMessage { }
+
+    public class ServerEventMessage : IMeta
+    {
+        public long EventId { get; set; }
+        public string Data { get; set; }
+        public string Selector { get; set; }
+        public string Json { get; set; }
+        public string Op { get; set; }
+        public string CssSelector { get; set; }
+
+        public Dictionary<string, string> Meta { get; set; }
+    }
+
+    public class ServerEventsClient : IDisposable
+    {
+        private static ILog log = LogManager.GetLogger(typeof(ServerEventsClient));
+
+        public static int BufferSize = 1024 * 64;
+        static int DefaultHeartbeatMs = 10 * 1000;
+
+        byte[] buffer;
+        Encoding encoding = new UTF8Encoding();
+
+        HttpWebRequest httpReq;
+        CancellationTokenSource cancel;
+
+        public ServerEventConnect ConnectionInfo { get; private set; }
+
+        public string SubscriptionId
+        {
+            get { return ConnectionInfo != null ? ConnectionInfo.Id : null; }
+        }
+
+        public string EventStreamUri { get; set; }
+        public IServiceClient ServiceClient { get; set; }
+
+        public Action<ServerEventConnect> OnConnect;
+        public Action<ServerEventMessage> OnCommand;
+        public Action<ServerEventMessage> OnMessage;
+        public Action<Exception> OnException;
+
+        public ServerEventsClient(string baseUri, string channel=null)
+        {
+            this.EventStreamUri = baseUri.CombineWith("event-stream");
+            if (channel != null)
+                this.EventStreamUri = this.EventStreamUri.AddQueryParam("channel", channel);
+
+            this.ServiceClient = new JsonServiceClient(baseUri);
+        }
+
+        public ServerEventsClient Start()
+        {
+            httpReq = (HttpWebRequest)WebRequest.Create(EventStreamUri);
+            //httpReq.AllowReadStreamBuffering = false; //.NET v4.5
+
+            var response = httpReq.GetResponse();
+            var stream = response.GetResponseStream();
+
+            buffer = new byte[BufferSize];
+            cancel = new CancellationTokenSource();
+            connectTcs = new TaskCompletionSource<ServerEventConnect>();
+            commandTcs = new TaskCompletionSource<ServerEventCommand>();
+            messageTcs = new TaskCompletionSource<ServerEventMessage>();
+
+            ProcessResponse(stream);
+
+            return this;
+        }
+
+        private TaskCompletionSource<ServerEventConnect> connectTcs;
+        public Task<ServerEventConnect> Connect()
+        {
+            if (httpReq == null)
+                Start();
+
+            return connectTcs.Task;
+        }
+
+        private TaskCompletionSource<ServerEventCommand> commandTcs;
+        public Task<ServerEventCommand> WaitForNextCommand()
+        {
+            return commandTcs.Task;
+        }
+
+        private TaskCompletionSource<ServerEventMessage> messageTcs;
+        public Task<ServerEventMessage> WaitForNextMessage()
+        {
+            if (messageTcs.Task.IsCompleted)
+            {
+                log.WarnFormat("WaitForNextMessage Already Completed: {0}", messageTcs.Task.Result.EventId);
+            }
+
+            return messageTcs.Task;
+        }
+
+        protected void OnConnectReceived()
+        {
+            if (log.IsDebugEnabled)
+                log.DebugFormat("OnConnectReceived: {0} on #{1}", ConnectionInfo.EventId, ConnectionInfo.DisplayName);
+
+            var hold = connectTcs;
+            connectTcs = new TaskCompletionSource<ServerEventConnect>();
+
+            if (OnConnect != null)
+                OnConnect(ConnectionInfo);
+
+            hold.SetResult(ConnectionInfo);
+        }
+
+        protected void OnCommandReceived(ServerEventCommand e)
+        {
+            if (log.IsDebugEnabled)
+                log.DebugFormat("OnCommandReceived: {0} on #{1}", e.EventId, ConnectionInfo.DisplayName);
+
+            var hold = commandTcs;
+            commandTcs = new TaskCompletionSource<ServerEventCommand>();
+
+            if (OnCommand != null)
+                OnCommand(e);
+
+            hold.SetResult(e);
+        }
+
+        protected void OnMessageReceived(ServerEventMessage e)
+        {
+            if (log.IsDebugEnabled)
+                log.DebugFormat("OnMessageReceived: {0} on #{1}", e.EventId, ConnectionInfo.DisplayName);
+
+            var hold = messageTcs;
+            messageTcs = new TaskCompletionSource<ServerEventMessage>();
+
+            if (OnMessage != null)
+                OnMessage(e);
+
+            hold.SetResult(e);
+        }
+
+        protected void OnExceptionReceived(Exception ex)
+        {
+            ex = ex.UnwrapIfSingleException();
+
+            if (OnException != null)
+                OnException(ex);
+
+            if (ConnectionInfo == null)
+                connectTcs.SetException(ex);
+
+            messageTcs.SetException(ex);
+        }
+
+        private string overflowText = "";
+        public void ProcessResponse(Stream stream)
+        {
+            if (!stream.CanRead) return;
+
+            var task = stream.ReadAsync(buffer, 0, 2048, cancel.Token);
+            task.ContinueWith(t =>
+            {
+                if (cancel.IsCancellationRequested || t.IsCanceled)
+                {
+                    httpReq = null;
+                    return;
+                }
+
+                if (t.IsFaulted)
+                {
+                    OnExceptionReceived(t.Exception);
+                    httpReq = null;
+                    return;
+                }
+
+                int len = task.Result;
+                if (len > 0)
+                {
+                    var text = overflowText + encoding.GetString(buffer, 0, len);
+                    int pos;
+                    while ((pos = text.IndexOf('\n')) >= 0)
+                    {
+                        if (text == "\n")
+                        {
+                            ProcessEventMessage(currentMsg);
+                            text = "";
+                            break;
+                        }
+
+                        var line = text.Substring(0, pos);
+                        ProcessLine(line);
+                        if (text.Length > pos + 1)
+                            text = text.Substring(pos + 1);
+                    }
+
+                    overflowText = text;
+
+                    ProcessResponse(stream);
+                }
+            });
+        }
+
+        private ServerEventMessage currentMsg;
+        void ProcessLine(string line)
+        {
+            if (line == null) return;
+
+            if (currentMsg == null)
+                currentMsg = new ServerEventMessage();
+
+            var parts = line.SplitOnFirst(':');
+            var label = parts[0];
+            var data = parts[1];
+            if (data.Length > 0 && data[0] == ' ')
+                data = data.Substring(1);
+
+            switch (label)
+            {
+                case "id":
+                    currentMsg.EventId = long.Parse(data);
+                    break;
+                case "data":
+                    currentMsg.Data = data;
+                    break;
+            }
+        }
+
+        void ProcessEventMessage(ServerEventMessage e)
+        {
+            var parts = e.Data.SplitOnFirst(' ');
+            e.Selector = parts[0];
+            e.Json = parts[1];
+
+            //"Message Received: ".Fmt(e.Selector);
+
+            if (!string.IsNullOrEmpty(e.Selector))
+            {
+                parts = e.Selector.SplitOnFirst('.');
+                e.Op = parts[0];
+                var target = parts[1].Replace("%20", " ");
+
+                var tokens = target.SplitOnFirst('$');
+                var cmd = tokens[0];
+                if (tokens.Length > 1)
+                    e.CssSelector = tokens[1];
+
+                if (e.Op == "cmd")
+                {
+                    switch (cmd)
+                    {
+                        case "onConnect":
+                            ProcessOnConnectMessage(e);
+                            return;
+                        case "onJoin":
+                            ProcessOnJoinMessage(e);
+                            return;
+                        case "onLeave":
+                            ProcessOnLeaveMessage(e);
+                            return;
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            OnMessageReceived(e);
+        }
+
+        private void ProcessOnConnectMessage(ServerEventMessage e)
+        {
+            var msg = JsonObject.Parse(e.Json);
+            ConnectionInfo = new ServerEventConnect {
+                HeartbeatIntervalMs = DefaultHeartbeatMs,
+            }.Populate(e, msg);
+
+            ConnectionInfo.Id = msg.Get("id");
+            ConnectionInfo.HeartbeatUrl = msg.Get("heartbeatUrl");
+            ConnectionInfo.HeartbeatIntervalMs = msg.Get<long>("heartbeatIntervalMs");
+            ConnectionInfo.UnRegisterUrl = msg.Get("unRegisterUrl");
+            ConnectionInfo.UserId = msg.Get("userId");
+            ConnectionInfo.DisplayName = msg.Get("displayName");
+            ConnectionInfo.ProfileUrl = msg.Get("profileUrl");
+
+            OnConnectReceived();
+        }
+
+        private void ProcessOnJoinMessage(ServerEventMessage e)
+        {
+            var msg = JsonObject.Parse(e.Json);
+            var joinMsg = new ServerEventJoin().Populate(e, msg);
+            joinMsg.UserId = msg.Get("userId");
+            joinMsg.DisplayName = msg.Get("displayName");
+            joinMsg.ProfileUrl = msg.Get("profileUrl");
+
+            OnCommandReceived(joinMsg);
+        }
+
+        private void ProcessOnLeaveMessage(ServerEventMessage e)
+        {
+            var msg = JsonObject.Parse(e.Json);
+            var leaveMsg = new ServerEventLeave().Populate(e, msg);
+
+            OnCommandReceived(leaveMsg);
+        }
+
+        public void Dispose()
+        {
+            if (cancel != null)
+                cancel.Cancel();
+
+            if (ConnectionInfo != null && ConnectionInfo.UnRegisterUrl != null)
+            {
+                ConnectionInfo.UnRegisterUrl.GetStringFromUrlAsync();
+            }
+   
+            cancel = null;
+            httpReq = null;
+        }
+    }
+
+    public static class ServerEventClientExtensions
+    {
+        public static T Populate<T>(this T dst, ServerEventMessage src, JsonObject msg) where T : ServerEventMessage
+        {
+            dst.EventId = src.EventId;
+            dst.Data = src.Data;
+            dst.Selector = src.Selector;
+            dst.Json = src.Json;
+            dst.Op = src.Op;
+
+            if (dst.Meta == null)
+                dst.Meta = new Dictionary<string, string>();
+
+            foreach (var entry in msg)
+            {
+                dst.Meta[entry.Key] = entry.Value;
+            }
+
+            return dst;
+        }
+    }
+}
---
+++ tests/ServiceStack.WebHost.Endpoints.Tests/ServerEventTests.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Funq;
+using NUnit.Framework;
+using ServiceStack.Logging;
+using ServiceStack.Text;
+
+namespace ServiceStack.WebHost.Endpoints.Tests
+{
+    [Route("/channels/{Channel}/chat")]
+    public class PostChatToChannel : IReturn<ChatMessage>
+    {
+        public string From { get; set; }
+        public string ToUserId { get; set; }
+        public string Channel { get; set; }
+        public string Message { get; set; }
+        public string Selector { get; set; }
+    }
+
+    public class ChatMessage
+    {
+        public long Id { get; set; }
+        public string FromUserId { get; set; }
+        public string FromName { get; set; }
+        public string DisplayName { get; set; }
+        public string Message { get; set; }
+        public string UserAuthId { get; set; }
+        public bool Private { get; set; }
+    }
+
+    [Route("/channels/{Channel}/raw")]
+    public class PostRawToChannel : IReturnVoid
+    {
+        public string From { get; set; }
+        public string ToUserId { get; set; }
+        public string Channel { get; set; }
+        public string Message { get; set; }
+        public string Selector { get; set; }
+    }
+
+    public class ServerEventsService : Service
+    {
+        private static long msgId;
+
+        public IServerEvents ServerEvents { get; set; }
+
+        public object Any(PostChatToChannel request)
+        {
+            var sub = ServerEvents.GetSubscription(request.From);
+            if (sub == null)
+                throw HttpError.NotFound("Subscription {0} does not exist".Fmt(request.From));
+
+            var msg = new ChatMessage
+            {
+                Id = Interlocked.Increment(ref msgId),
+                FromUserId = sub.UserId,
+                FromName = sub.DisplayName,
+                Message = request.Message,
+            };
+
+            if (request.ToUserId != null)
+            {
+                msg.Private = true;
+                ServerEvents.NotifyUserId(request.ToUserId, request.Selector, msg);
+                var toSubs = ServerEvents.GetSubscriptionsByUserId(request.ToUserId);
+                foreach (var toSub in toSubs)
+                {
+                    msg.Message = "@{0}: {1}".Fmt(toSub.DisplayName, msg.Message);
+                    ServerEvents.NotifySubscription(request.From, request.Selector, msg);
+                }
+            }
+            else
+            {
+                ServerEvents.NotifyChannel(request.Channel, request.Selector, msg);
+            }
+
+            return msg;
+        }
+
+        public void Any(PostRawToChannel request)
+        {
+            var sub = ServerEvents.GetSubscription(request.From);
+            if (sub == null)
+                throw HttpError.NotFound("Subscription {0} does not exist".Fmt(request.From));
+
+            if (request.ToUserId != null)
+            {
+                ServerEvents.NotifyUserId(request.ToUserId, request.Selector, request.Message);
+            }
+            else
+            {
+                ServerEvents.NotifyChannel(request.Channel, request.Selector, request.Message);
+            }
+        }
+    }
+
+    public class ServerEventsAppHost : AppSelfHostBase
+    {
+        public ServerEventsAppHost()
+            : base(typeof(ServerEventsAppHost).Name, typeof(ServerEventsAppHost).Assembly) {}
+
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new ServerEventsFeature());
+        }
+    }
+
+
+    [TestFixture]
+    public class ServerEventsTests
+    {
+        private ServiceStackHost appHost;
+
+        public ServerEventsTests()
+        {
+            //LogManager.LogFactory = new ConsoleLogFactory();
+
+            appHost = new ServerEventsAppHost()
+                .Init()
+                .Start(Config.AbsoluteBaseUri);
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            appHost.Dispose();
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            var serverEvents = appHost.TryResolve<IServerEvents>();
+            serverEvents.Reset();
+        }
+
+        private static ServerEventsClient CreateServerEventsClient()
+        {
+            return new ServerEventsClient(Config.AbsoluteBaseUri);
+        }
+
+        [Test]
+        public async void Can_connect_to_ServerEventsStream()
+        {
+            var client = CreateServerEventsClient()
+                .Start();
+
+            var task = client.Connect();
+            if (task != await Task.WhenAny(task, Task.Delay(2000)))
+                throw new TimeoutException();
+            
+            var connectMsg = await task;
+            Assert.That(connectMsg.HeartbeatUrl, Is.StringStarting(Config.AbsoluteBaseUri));
+            Assert.That(connectMsg.UnRegisterUrl, Is.StringStarting(Config.AbsoluteBaseUri));
+            Assert.That(connectMsg.HeartbeatIntervalMs, Is.GreaterThan(0));
+        }
+
+        [Test]
+        public async void Does_fire_onJoin_events()
+        {
+            var client = CreateServerEventsClient()
+                .Start();
+
+            var taskConnect = client.Connect();
+            var taskMsg = client.WaitForNextCommand();
+
+            if (taskConnect != await Task.WhenAny(taskConnect, Task.Delay(2000)))
+                throw new TimeoutException();
+
+            if (taskMsg != await Task.WhenAny(taskMsg, Task.Delay(2000)))
+                throw new TimeoutException();
+
+            var connectMsg = await taskConnect;
+            Assert.That(connectMsg.HeartbeatUrl, Is.StringStarting(Config.AbsoluteBaseUri));
+
+            var joinMsg = (ServerEventJoin)await taskMsg;
+            Assert.That(joinMsg.DisplayName, Is.EqualTo(client.ConnectionInfo.DisplayName));
+        }
+
+        [Test]
+        public async void Does_fire_all_callbacks()
+        {
+            ServerEventConnect connectMsg = null;
+
+            var client1 = CreateServerEventsClient();
+
+            var msgs = new List<ServerEventMessage>();
+            var commands = new List<ServerEventMessage>();
+            var errors = new List<Exception>();
+
+            client1.OnConnect = e => connectMsg = e;
+            client1.OnCommand = commands.Add;
+            client1.OnMessage = msgs.Add;
+            client1.OnException = errors.Add;
+
+            //Pop Connect + onJoin messages off
+            var taskConnect = client1.Connect();
+            var taskCmd = client1.WaitForNextCommand();
+
+            if (taskConnect != await Task.WhenAny(taskConnect, Task.Delay(2000)))
+                throw new TimeoutException();
+            if (taskCmd != await Task.WhenAny(taskCmd, Task.Delay(2000)))
+                throw new TimeoutException();
+
+            var joinMsg = commands.OfType<ServerEventJoin>().FirstOrDefault();
+
+            Assert.That(connectMsg, Is.Not.Null, "connectMsg == null");
+            Assert.That(joinMsg, Is.Not.Null, "joinMsg == null");
+
+            Assert.That(msgs.Count, Is.EqualTo(0));
+            Assert.That(errors.Count, Is.EqualTo(0));
+            Assert.That(commands.Count, Is.EqualTo(1)); //join
+
+            commands.Clear();
+
+            "New Client....".Print();
+            taskCmd = client1.WaitForNextCommand();
+
+            var client2 = CreateServerEventsClient();
+            var connectMsg2 = await client2.Connect();
+
+            if (taskCmd != await Task.WhenAny(taskCmd, Task.Delay(2000)))
+                throw new TimeoutException();
+
+            joinMsg = commands.OfType<ServerEventJoin>().FirstOrDefault();
+
+            taskCmd = client1.WaitForNextCommand();
+
+            connectMsg2.UnRegisterUrl.GetStringFromUrl(); //unsubscribe 2nd client
+
+            if (taskCmd != await Task.WhenAny(taskCmd, Task.Delay(2000)))
+                throw new TimeoutException();
+
+            var leaveMsg = commands.OfType<ServerEventLeave>().FirstOrDefault();
+
+            Assert.That(joinMsg, Is.Not.Null, "joinMsg == null");  //2nd connection
+            Assert.That(leaveMsg, Is.Not.Null, "leaveMsg == null");
+            Assert.That(commands.Count, Is.EqualTo(2)); //join + leave
+            Assert.That(errors.Count, Is.EqualTo(0)); 
+        }
+
+        [Test]
+        public async Task Does_receive_messages()
+        {
+            var msgs1 = new List<ServerEventMessage>();
+            var msgs2 = new List<ServerEventMessage>();
+
+            var client1 = CreateServerEventsClient();
+            var client2 = CreateServerEventsClient();
+            client1.OnMessage = msgs1.Add;
+            client2.OnMessage = msgs2.Add;
+
+            await Task.WhenAll(client1.Connect(), client1.WaitForNextCommand()); //connect1 + join1
+
+            "client2.Connect()...".Print();
+            await Task.WhenAll(
+                client2.Connect(), client2.WaitForNextCommand(), //connect2 + join2
+                client1.WaitForNextCommand()); //join2
+
+            "Waiting for Msg1...".Print();
+            var taskMsg1 = client1.WaitForNextMessage();
+            var taskMsg2 = client2.WaitForNextMessage();
+
+            var info1 = client1.ConnectionInfo;
+            client1.ServiceClient.Post(new PostChatToChannel {
+                From = client1.SubscriptionId,
+                Message = "hello from client1",
+                Channel = EventSubscription.UnknownChannel,
+                Selector = "cmd.chat",
+            });
+
+            if (taskMsg1 != await Task.WhenAny(taskMsg1, Task.Delay(2000)))
+                throw new TimeoutException();
+            if (taskMsg2 != await Task.WhenAny(taskMsg2, Task.Delay(2000)))
+                throw new TimeoutException();
+
+            var msg1 = await taskMsg1;
+            var msg2 = await taskMsg2;
+
+            Assert.That(msg1.EventId, Is.GreaterThan(0));
+            Assert.That(msg2.EventId, Is.GreaterThan(0));
+            Assert.That(msg1.Selector, Is.EqualTo("cmd.chat"));
+            Assert.That(msg2.Selector, Is.EqualTo("cmd.chat"));
+
+            var chatMsg1 = msg1.Json.FromJson<ChatMessage>();
+            Assert.That(chatMsg1.Id, Is.GreaterThan(0));
+            Assert.That(chatMsg1.FromUserId, Is.EqualTo(info1.UserId)); //-1 / anon user
+            Assert.That(chatMsg1.FromName, Is.EqualTo(info1.DisplayName)); //user1 / anon user
+            Assert.That(chatMsg1.Message, Is.EqualTo("hello from client1"));
+
+            var chatMsg2 = msg2.Json.FromJson<ChatMessage>();
+            Assert.That(chatMsg2.Id, Is.GreaterThan(0));
+            Assert.That(chatMsg2.FromUserId, Is.EqualTo(info1.UserId));
+            Assert.That(chatMsg2.FromName, Is.EqualTo(info1.DisplayName));
+            Assert.That(chatMsg2.Message, Is.EqualTo("hello from client1"));
+
+            Assert.That(msgs1.Count, Is.EqualTo(1));
+            Assert.That(msgs2.Count, Is.EqualTo(1));
+
+            "Waiting for Msg2...".Print();
+            taskMsg1 = client1.WaitForNextMessage();
+            taskMsg2 = client2.WaitForNextMessage();
+
+            var info2 = client2.ConnectionInfo;
+            client2.ServiceClient.Post(new PostChatToChannel
+            {
+                From = client2.SubscriptionId,
+                Message = "hello from client2",
+                Channel = EventSubscription.UnknownChannel,
+                Selector = "cmd.chat",
+            });
+
+            if (taskMsg1 != await Task.WhenAny(taskMsg1, Task.Delay(2000)))
+                throw new TimeoutException();
+            if (taskMsg2 != await Task.WhenAny(taskMsg2, Task.Delay(2000)))
+                throw new TimeoutException();
+
+            msg1 = await taskMsg1;
+            msg2 = await taskMsg2;
+
+            chatMsg1 = msg1.Json.FromJson<ChatMessage>();
+            Assert.That(chatMsg1.FromUserId, Is.EqualTo(info2.UserId));
+            Assert.That(chatMsg1.FromName, Is.EqualTo(info2.DisplayName));
+            Assert.That(chatMsg1.Message, Is.EqualTo("hello from client2"));
+
+            chatMsg2 = msg2.Json.FromJson<ChatMessage>();
+            Assert.That(chatMsg2.FromUserId, Is.EqualTo(info2.UserId));
+            Assert.That(chatMsg2.FromName, Is.EqualTo(info2.DisplayName));
+            Assert.That(chatMsg2.Message, Is.EqualTo("hello from client2"));
+
+            Assert.That(msgs1.Count, Is.EqualTo(2));
+            Assert.That(msgs2.Count, Is.EqualTo(2));
+        }
+    }
+}
