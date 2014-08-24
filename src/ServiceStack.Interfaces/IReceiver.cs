f5f6d2b5a1feb6b36a01f6f0e1cffa5f3c201b90
Demis Bellot <demis.bellot@gmail.com>
2014-08-24 01:45:21 -0400

    Add new IReceiver marker API for client handlers like ServerEvent Receivers

---
+++ src/ServiceStack.Interfaces/IReceiver.cs
@@ -0,0 +1,8 @@
+﻿namespace ServiceStack
+{
+    //Marker interface for Receiver classes, akin to IService
+    public interface IReceiver
+    {
+        void NoSuchMethod(string selector, object message);
+    }
+}
