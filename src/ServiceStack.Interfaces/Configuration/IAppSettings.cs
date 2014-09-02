c13cec6fbede26596800b47153617d535f01783d
Demis Bellot <demis.bellot@gmail.com>
2014-09-02 15:40:46 -0400

    reformat IAppSettings

--- src/ServiceStack.Interfaces/Configuration/IAppSettings.cs
+++ src/ServiceStack.Interfaces/Configuration/IAppSettings.cs
@@ -2,18 +2,18 @@ using System.Collections.Generic;
 
 namespace ServiceStack.Configuration
 {
-	public interface IAppSettings
-	{
+    public interface IAppSettings
+    {
         bool Exists(string key);
 
-	    void Set<T>(string key, T value);
-        
+        void Set<T>(string key, T value);
+
         string GetString(string name);
 
-		IList<string> GetList(string key);
+        IList<string> GetList(string key);
 
-		IDictionary<string, string> GetDictionary(string key);
+        IDictionary<string, string> GetDictionary(string key);
 
-		T Get<T>(string name, T defaultValue);
-	}
+        T Get<T>(string name, T defaultValue);
+    }
 }
