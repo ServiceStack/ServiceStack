b9eb34eb80ff64fa1171d2f7f29ef359c3580eed
Demis Bellot <demis.bellot@gmail.com>
2014-09-02 02:05:05 -0400

    remove unnecessary comment

--- tests/ServiceStack.WebHost.Endpoints.Tests/ServerEventTests.cs
+++ tests/ServiceStack.WebHost.Endpoints.Tests/ServerEventTests.cs
@@ -177,7 +177,6 @@ namespace ServiceStack.WebHost.Endpoints.Tests
         }
     }
 
-    //[Explicit("Remove from autorunning in CI for now")]
     [TestFixture]
     public class RedisServerEventsTests : ServerEventsTests
     {
