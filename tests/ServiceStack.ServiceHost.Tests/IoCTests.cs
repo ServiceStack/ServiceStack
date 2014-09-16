b46b4c76d7e7ec80b3731e8d500380800d07efec
Demis Bellot <demis.bellot@gmail.com>
2014-09-16 09:05:20 -0400

    fix whitespace

--- tests/ServiceStack.ServiceHost.Tests/IoCTests.cs
+++ tests/ServiceStack.ServiceHost.Tests/IoCTests.cs
@@ -9,61 +9,61 @@ using Funq;
 
 namespace ServiceStack.ServiceHost.Tests
 {
-	[TestFixture]
-	public class IoCTests
-	{
-		[Test]
-		public void Can_AutoWire_types_dynamically_with_expressions()
-		{
-			var serviceType = typeof(AutoWireService);
+    [TestFixture]
+    public class IoCTests
+    {
+        [Test]
+        public void Can_AutoWire_types_dynamically_with_expressions()
+        {
+            var serviceType = typeof(AutoWireService);
 
-			var container = new Container();
-			container.Register<IFoo>(c => new Foo());
-			container.Register<IBar>(c => new Bar());
-			container.Register<int>(c => 100);
+            var container = new Container();
+            container.Register<IFoo>(c => new Foo());
+            container.Register<IBar>(c => new Bar());
+            container.Register<int>(c => 100);
 
-			container.RegisterAutoWiredType(serviceType);
+            container.RegisterAutoWiredType(serviceType);
 
-			var service = container.Resolve<AutoWireService>();
+            var service = container.Resolve<AutoWireService>();
 
-			Assert.That(service.Foo, Is.Not.Null);
-			Assert.That(service.Bar, Is.Not.Null);
-			Assert.That(service.Count, Is.EqualTo(0));
-		}
+            Assert.That(service.Foo, Is.Not.Null);
+            Assert.That(service.Bar, Is.Not.Null);
+            Assert.That(service.Count, Is.EqualTo(0));
+        }
 
-		public class Test
-		{
-			public IFoo Foo { get; set; }
-			public IBar Bar { get; set; }
-			public IFoo Foo2 { get; set; }
-			public IEnumerable<string> Names { get; set; }
-			public int Age { get; set; }
+        public class Test
+        {
+            public IFoo Foo { get; set; }
+            public IBar Bar { get; set; }
+            public IFoo Foo2 { get; set; }
+            public IEnumerable<string> Names { get; set; }
+            public int Age { get; set; }
 
-			public Test()
-			{
-				this.Foo2 = new Foo2();
-				this.Names = new List<string>() { "Steffen", "Demis" };
-			}
-		}
+            public Test()
+            {
+                this.Foo2 = new Foo2();
+                this.Names = new List<string>() { "Steffen", "Demis" };
+            }
+        }
 
-		[Test]
-		public void Can_AutoWire_Existing_Instance()
-		{
-			var test = new Test();
+        [Test]
+        public void Can_AutoWire_Existing_Instance()
+        {
+            var test = new Test();
 
-			var container = new Container();
-			container.Register<IFoo>(c => new Foo());
-			container.Register<IBar>(c => new Bar());
-			container.Register<int>(c => 10);
+            var container = new Container();
+            container.Register<IFoo>(c => new Foo());
+            container.Register<IBar>(c => new Bar());
+            container.Register<int>(c => 10);
 
-			container.AutoWire(test);
+            container.AutoWire(test);
 
-			Assert.That(test.Foo, Is.Not.Null);
-			Assert.That(test.Bar, Is.Not.Null);
-			Assert.That(test.Foo2 as Foo, Is.Null);
-			Assert.That(test.Names, Is.Not.Null);
-			Assert.That(test.Age, Is.EqualTo(0));
-		}
+            Assert.That(test.Foo, Is.Not.Null);
+            Assert.That(test.Bar, Is.Not.Null);
+            Assert.That(test.Foo2 as Foo, Is.Null);
+            Assert.That(test.Names, Is.Not.Null);
+            Assert.That(test.Age, Is.EqualTo(0));
+        }
 
         public class DependencyWithBuiltInTypes
         {
@@ -76,12 +76,12 @@ namespace ServiceStack.ServiceHost.Tests
             public IFoo Foo { get; set; }
             public IBar Bar { get; set; }
             public string String { get; set; }
-            public int Age { get; set; }            
+            public int Age { get; set; }
         }
 
-	    [Test]
-	    public void Does_not_AutoWire_BuiltIn_BCL_and_ValueTypes()
-	    {
+        [Test]
+        public void Does_not_AutoWire_BuiltIn_BCL_and_ValueTypes()
+        {
             var container = new Container();
             container.Register<IFoo>(c => new Foo());
             container.Register<IBar>(c => new Bar());
@@ -92,12 +92,12 @@ namespace ServiceStack.ServiceHost.Tests
 
             container.RegisterAutoWired<DependencyWithBuiltInTypes>();
 
-	        var test = container.Resolve<DependencyWithBuiltInTypes>();
+            var test = container.Resolve<DependencyWithBuiltInTypes>();
             Assert.That(test.Foo, Is.Not.Null);
             Assert.That(test.Bar, Is.Not.Null);
             Assert.That(test.String, Is.EqualTo("A String"));
             Assert.That(test.Age, Is.EqualTo(27));
-	    }
+        }
 
         public class FunqTest
         {
@@ -207,20 +207,20 @@ namespace ServiceStack.ServiceHost.Tests
             }
         }
 
-	    [Test]
-	    public void Can_autowire_generic_type_definitions()
-	    {
+        [Test]
+        public void Can_autowire_generic_type_definitions()
+        {
             var container = new Container();
-	        container.Register(c => new Foo());
-	        container.Register(c => new Bar());
+            container.Register(c => new Foo());
+            container.Register(c => new Bar());
 
-	        GetType().Assembly.GetTypes()
+            GetType().Assembly.GetTypes()
                 .Where(x => x.IsOrHasGenericInterfaceTypeOf(typeof(ICommand<>)))
                 .Each(x => container.RegisterAutoWiredType(x));
 
-	        var fooCmd = container.Resolve<FooCommand>();
+            var fooCmd = container.Resolve<FooCommand>();
             Assert.That(fooCmd.Execute(), Is.EqualTo(container.Resolve<Foo>()));
-	        var barCmd = container.Resolve<BarCommand>();
+            var barCmd = container.Resolve<BarCommand>();
             Assert.That(barCmd.Execute(), Is.EqualTo(container.Resolve<Bar>()));
         }
     }
