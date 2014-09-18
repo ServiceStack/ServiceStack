fe7509683988c9c60223efeb0500866d4ded4c49
Thomas James <thomasvjames@gmail.com>
2014-09-18 10:02:31 +1000

    Ensure expected base behaviour if request scope is turned on

--- tests/ServiceStack.Server.Tests/Messaging/MqServerIntroTests.cs
+++ tests/ServiceStack.Server.Tests/Messaging/MqServerIntroTests.cs
@@ -18,11 +18,27 @@ namespace ServiceStack.Server.Tests.Messaging
         }
     }
 
+    public class RabbitMqServerIntroWithRequestScopeTests : MqServerIntroTests
+    {
+        public override IMessageService CreateMqServer(int retryCount = 1)
+        {
+            return new RabbitMqServer { RetryCount = retryCount, ProcessEachMessageInRequestScope = true};
+        }
+    }
+
     public class RedisMqServerIntroTests : MqServerIntroTests
     {
         public override IMessageService CreateMqServer(int retryCount = 1)
         {
             return new RedisMqServer(new BasicRedisClientManager()) { RetryCount = retryCount };
+        }
+    }
+
+    public class RedisMqServerIntroWithRequestScopeTests : MqServerIntroTests
+    {
+        public override IMessageService CreateMqServer(int retryCount = 1)
+        {
+            return new RedisMqServer(new BasicRedisClientManager()) { RetryCount = retryCount, ProcessEachMessageInRequestScope = true};
         }
     }
 
