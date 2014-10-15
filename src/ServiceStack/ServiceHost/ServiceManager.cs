235bba52c083dd106d6a9bbdf0a9f7e55ccb8a7b
Kyle Peter Fritz <kyle.fritz@zocdoc.com>
2014-10-15 10:55:25 -0400

    Provide methods to register services during operation.

--- src/ServiceStack/ServiceHost/ServiceManager.cs
+++ src/ServiceStack/ServiceHost/ServiceManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using ServiceStack.Logging;
 using Funq;
@@ -128,6 +129,33 @@ namespace ServiceStack.ServiceHost
 			    return genericServiceType;
 			}
 		}
+
+        /// <summary>
+        /// Registers the given service instance to be reused for each request that would route to 
+        /// its service type.
+        /// </summary>
+        /// <remarks>
+        /// This method should not be used in production.  Because it reuses a single instance for 
+        /// each request, it can't really do dependency injection safely.  This method is only 
+        /// useful by the functional testing harness, which self-hosts a ServiceStack with mock 
+        /// implementations.
+        /// </remarks>
+        /// <param name="serviceImplementation">The singleton instance of a service to use.</param>
+	    public void RegisterService(IService serviceImplementation)
+        {
+            var serviceType = serviceImplementation.GetType();
+
+            this.ServiceController.RegisterNService(typeFactory, serviceType);
+            
+            //Container.Register<TService>(TService instance)
+            var containerType = typeof(Container);
+            var registrationMethodGeneric = containerType.GetMethods()
+                .Where(x => x.Name == "Register")
+                .Select(y => new { Method = y, Parameters = y.GetParameters() })
+                .Single(z => z.Parameters.Length == 1 && !z.Parameters[0].ParameterType.IsGenericType);
+            var registrationMethod = registrationMethodGeneric.Method.MakeGenericMethod(serviceType);
+            registrationMethod.Invoke(this.Container, new object[] { serviceImplementation });
+        }
 
 		public object Execute(object dto)
 		{
