c7b80018ba45161f4e21fc77679e3c8b0548fdb4
Demis Bellot <demis.bellot@gmail.com>
2014-10-20 20:48:41 -0400

    Add commented example  of using GlobalResponseFilter to prefix JSON responses

--- tests/CheckWeb/Global.asax.cs
+++ tests/CheckWeb/Global.asax.cs
@@ -14,6 +14,7 @@ using ServiceStack.OrmLite;
 using ServiceStack.Razor;
 using ServiceStack.Text;
 using ServiceStack.Validation;
+using ServiceStack.Web;
 
 namespace CheckWeb
 {
@@ -82,6 +83,14 @@ namespace CheckWeb
                 db.DropAndCreateTable<Rockstar>();
                 db.InsertAll(SeedRockstars);
             }
+
+            //this.GlobalResponseFilters.Add((req, res, dto) =>
+            //{
+            //    if (req.ResponseContentType.Matches(MimeTypes.Json) && !(dto is IHttpResult))
+            //    {
+            //        res.Write(")]}',\n");
+            //    }
+            //});
         }
 
         public static Rockstar[] SeedRockstars = new[] {
