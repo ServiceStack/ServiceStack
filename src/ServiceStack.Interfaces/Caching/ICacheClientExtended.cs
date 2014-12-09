4b2eef272599f20330f44a4d6a6510be389ee019
Demis Bellot <demis.bellot@gmail.com>
2014-12-09 14:49:34 -0500

    Add new ICacheClientExtended for adding enhanced API's on CacheClient

---
+++ src/ServiceStack.Interfaces/Caching/ICacheClientExtended.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceStack.Caching
+{
+    /// <summary>
+    /// Extend ICacheClient API with shared, non-core features
+    /// </summary>
+    public interface ICacheClientExtended : ICacheClient
+    {
+        TimeSpan? GetTimeToLive(string key);
+    }
+}
