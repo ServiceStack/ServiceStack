b9bd0a66de9d5669763495f0b81166645853e11e
Demis Bellot <demis.bellot@gmail.com>
2015-01-22 15:01:32 -0500

    Add DataContract Namespace for ServiceStack.Client

--- src/ServiceStack.Client/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Client/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -34,3 +35,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("4.0.0.0")]
 [assembly: AssemblyFileVersion("4.0.0.0")]
+
+[assembly: ContractNamespace("http://schemas.servicestack.net/types",
+    ClrNamespace = "ServiceStack")]
