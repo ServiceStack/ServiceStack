4552cff169458cdf16fce0af5770c4185e53377d
Demis Bellot <demis.bellot@gmail.com>
2015-02-20 19:29:05 -0500

    Expand CheckWeb to include static .html, .css, .js for testing minification with

---
+++ tests/Check.ServiceInterface/TestMinifierServices.cs
@@ -0,0 +1,14 @@
+﻿using ServiceStack;
+
+namespace Check.ServiceInterface
+{
+    public class TestMiniverView {}
+
+    public class TestMinifierServices : Service
+    {
+         public object Any(TestMiniverView request)
+         {
+             return request;
+         }
+    }
+}
