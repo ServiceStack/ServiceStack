da112619749eadd4dd8b11923cff203941417f12
Demis Bellot <demis.bellot@gmail.com>
2015-03-09 01:55:54 -0400

    Extend Android PCL Test with SSE example

--- tests/PclTest.Android/Activity1.cs
+++ tests/PclTest.Android/Activity1.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using Android.App;
 using Android.Widget;
 using Android.OS;
 using PclTest.ServiceModel;
 using PclTest.SharedLogic;
 using ServiceStack;
+using ServiceStack.Logging;
 using ServiceStack.Text;
 
 namespace PclTest.Android
@@ -38,6 +40,8 @@ namespace PclTest.Android
     [Activity(Label = "PclTest.Android", MainLauncher = true, Icon = "@drawable/icon")]
     public class Activity1 : Activity
     {
+        TextView lblResults = null;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
@@ -51,12 +55,13 @@ namespace PclTest.Android
             var btnGoShared = FindViewById<Button>(Resource.Id.btnGoShared);
             var btnTest = FindViewById<Button>(Resource.Id.btnTest);
             var txtName = FindViewById<EditText>(Resource.Id.txtName);
-            var lblResults = FindViewById<TextView>(Resource.Id.lblResults);
+            lblResults = FindViewById<TextView>(Resource.Id.lblResults);
 
             //10.0.2.2 = loopback
             //http://developer.android.com/tools/devices/emulator.html
             var client = new JsonServiceClient("http://10.0.2.2:81/");
             var gateway = new SharedGateway("http://10.0.2.2:81/");
+            LogManager.LogFactory = new LogFactory(AddMessage);
 
             btnGoSync.Click += delegate
             {
@@ -78,26 +83,6 @@ namespace PclTest.Android
                     .Error(ex => lblResults.Text = ex.ToString());
             };
 
-            btnTest.Click += delegate
-            {
-                try
-                {
-                    var dto = new Question
-                    {
-                        Id = Guid.NewGuid(),
-                        Title = "Title",
-                    };
-
-                    var json = dto.ToJson();
-                    var q = json.FromJson<Question>();
-                    lblResults.Text = "{0}:{1}".Fmt(q.Id, q.Title);
-                }
-                catch (Exception ex)
-                {
-                    lblResults.Text = ex.ToString();
-                }
-            };
-
             btnGoShared.Click += async delegate
             {
                 try
@@ -113,8 +98,118 @@ namespace PclTest.Android
                     lblResults.Text = ex.ToString();
                 }
             };
+
+            btnTest.Click += delegate
+            {
+                try
+                {
+                    //var dto = new Question
+                    //{
+                    //    Id = Guid.NewGuid(),
+                    //    Title = "Title",
+                    //};
+
+                    //var json = dto.ToJson();
+                    //var q = json.FromJson<Question>();
+                    //lblResults.Text = "{0}:{1}".Fmt(q.Id, q.Title);
+
+                    ConnectServerEvents();
+                }
+                catch (Exception ex)
+                {
+                    lblResults.Text = ex.ToString();
+                }
+            };
         }
 
+        void AddMessage(string message)
+        {
+            RunOnUiThread(() => {
+                lblResults.Text = "{0}  {1}\n".Fmt(DateTime.Now.ToLongTimeString(), message) + lblResults.Text;
+            });
+        }
+
+        private static ServerEventsClient serverEventsClient = null;
+        static ServerEventConnect connectMsg = null;
+        static List<ServerEventMessage> msgs = new List<ServerEventMessage>();
+        static List<ServerEventMessage> commands = new List<ServerEventMessage>();
+        static List<System.Exception> errors = new List<System.Exception>();
+        static string lastText = "";
+
+        class LogFactory : ILogFactory 
+        {
+            public Action<string> OnMessage;
+
+            public LogFactory(Action<string> onMessage)
+            {
+                OnMessage = onMessage;
+            }
+
+            public ILog GetLogger(Type type)
+            {
+                return new GenericLog(type) { OnMessage = OnMessage };
+            }
+
+            public ILog GetLogger(string typeName)
+            {
+                return new GenericLog(typeName) { OnMessage = OnMessage };
+            }
+        }
+
+        private void ConnectServerEvents()
+        {
+            if (serverEventsClient == null)
+            {
+                // var client = new ServerEventsClient("http://chat.servicestack.net", channels: "home")
+
+                // bla.ybookz.com is a copy of the SS Chat sample, that sends 'HAHAHAHA' every second to all listeners
+                //serverEventsClient = new ServerEventsClient("http://bla.ybookz.com/", channels: "home")
+                //serverEventsClient = new ServerEventsClient("http://10.0.2.2:1337/", channels: "home")
+                serverEventsClient = new ServerEventsClient("http://chat.servicestack.net", channels: "home")
+                {
+                    OnConnect = e => {
+                        connectMsg = e;
+                    },
+                    OnCommand = a => {
+                        commands.Add(a);
+                    },
+                    OnHeartbeat = () => {
+
+                        RunOnUiThread(() => {
+                            try
+                            {
+                                Toast.MakeText(this, "Heartbeat", ToastLength.Short).Show();
+                            }
+                            catch {}
+                        });
+                    },
+                    OnMessage = a => {
+                        msgs.Add(a);
+                        if (lastText != a.Data)
+                        {
+                            lastText = a.Data ?? "";
+                            RunOnUiThread(() => {
+                                try
+                                {
+                                    Toast.MakeText(this, lastText, ToastLength.Short).Show();
+                                }
+                                catch {}
+                            });
+                        }
+                    },
+                    OnException = ex => {
+                        AddMessage("OnException: " + ex.Message);
+                        errors.Add(ex);
+                    },
+                    HeartbeatRequestFilter = x => {
+                        AddMessage("HeartbeatRequestFilter");
+                    },
+                };
+
+                AddMessage("Started Listening...");
+                serverEventsClient.Start();
+            }
+        }
     }
 }
 
---
+++ tests/PclTest.Android/GenericLog.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Text;
+using ServiceStack.Logging;
+
+namespace PclTest.Android
+{
+    public class GenericLog : ILog
+    {
+        const string DEBUG = "DEBUG: ";
+        const string ERROR = "ERROR: ";
+        const string FATAL = "FATAL: ";
+        const string INFO = "INFO: ";
+        const string WARN = "WARN: ";
+
+        public Action<string> OnMessage;
+
+        public StringBuilder Logs = new StringBuilder();
+
+        public void LogMessage(string message)
+        {
+            Logs.AppendLine(message);
+
+            if (OnMessage != null)
+                OnMessage(message);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceStack.Logging.DebugLogger"/> class.
+        /// </summary>
+        public GenericLog(Type type) : this(type.Name) { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ServiceStack.Logging.DebugLogger"/> class.
+        /// </summary>
+        public GenericLog(string type)
+        {
+            IsDebugEnabled = true;
+        }
+
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        private void Log(object message, Exception exception)
+        {
+            string msg = message == null ? string.Empty : message.ToString();
+            if (exception != null)
+            {
+                msg += ", Exception: " + exception.Message;
+            }
+            LogMessage(msg);
+        }
+
+        /// <summary>
+        /// Logs the format.
+        /// </summary>
+        private void LogFormat(object message, params object[] args)
+        {
+            string msg = message == null ? string.Empty : message.ToString();
+            LogMessage(string.Format(msg, args));
+        }
+
+        /// <summary>
+        /// Logs the specified message.
+        /// </summary>
+        private void Log(object message)
+        {
+            string msg = message == null ? string.Empty : message.ToString();
+            LogMessage(msg);
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            Log(DEBUG + message, exception);
+        }
+
+        public bool IsDebugEnabled { get; set; }
+
+        public void Debug(object message)
+        {
+            Log(DEBUG + message);
+        }
+
+        public void DebugFormat(string format, params object[] args)
+        {
+            LogFormat(DEBUG + format, args);
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            Log(ERROR + message, exception);
+        }
+
+        public void Error(object message)
+        {
+            Log(ERROR + message);
+        }
+
+        public void ErrorFormat(string format, params object[] args)
+        {
+            LogFormat(ERROR + format, args);
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            Log(FATAL + message, exception);
+        }
+
+        public void Fatal(object message)
+        {
+            Log(FATAL + message);
+        }
+
+        public void FatalFormat(string format, params object[] args)
+        {
+            LogFormat(FATAL + format, args);
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            Log(INFO + message, exception);
+        }
+
+        public void Info(object message)
+        {
+            Log(INFO + message);
+        }
+
+        public void InfoFormat(string format, params object[] args)
+        {
+            LogFormat(INFO + format, args);
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            Log(WARN + message, exception);
+        }
+
+        public void Warn(object message)
+        {
+            Log(WARN + message);
+        }
+
+        public void WarnFormat(string format, params object[] args)
+        {
+            LogFormat(WARN + format, args);
+        }
+    }
+}
