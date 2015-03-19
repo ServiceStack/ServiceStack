58a3758467615a8c0b4048e0ad09c8bf1e670175
Demis Bellot <demis.bellot@gmail.com>
2015-03-19 16:24:53 -0400

    Don't run adhoc CheckWebTests by default

--- tests/ServiceStack.Common.Tests/CheckWebTests.cs
+++ tests/ServiceStack.Common.Tests/CheckWebTests.cs
@@ -40,6 +40,7 @@ namespace ServiceStack.Common.Tests
         public string Sentence { get; set; }
     }
 
+    [Explicit]
     public class CheckWebTests
     {
         private const string BaseUri = "http://localhost:55799/";
