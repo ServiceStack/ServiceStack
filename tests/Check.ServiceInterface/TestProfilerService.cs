5fa911a907a64892040820af994e084d49c52b03
Demis Bellot <demis.bellot@gmail.com>
2015-04-04 22:01:48 -0400

    Tweak SP param output in MiniProfiler

---
+++ tests/Check.ServiceInterface/TestProfilerService.cs
@@ -0,0 +1,26 @@
+﻿using ServiceStack;
+using ServiceStack.OrmLite;
+
+namespace Check.ServiceInterface
+{
+    public class DummyTable
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    [Route("/testexecproc")]
+    public class TestExecProc {}
+
+    public class TestProfilerService : Service
+    {
+         public object Any(TestExecProc request)
+         {
+             using (var sqlServer = DbFactory.OpenDbConnection("SqlServer"))
+             {
+                 var results = sqlServer.SqlList<DummyTable>("EXEC DummyTable @Times", new { Times = 10 });
+                 return results;
+             }
+         }
+    }
+}
