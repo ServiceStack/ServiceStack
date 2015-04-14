a145149f710b24ef28f97ebc68d82472b70f81c2
Demis Bellot <demis.bellot@gmail.com>
2015-04-14 04:12:49 -0400

    Add new GenericLogFactory

---
+++ src/ServiceStack.Interfaces/Logging/GenericLogFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ServiceStack.Logging
+{
+    public class GenericLogFactory : ILogFactory
+    {
+        public Action<string> OnMessage;
+
+        public GenericLogFactory(Action<string> onMessage)
+        {
+            OnMessage = onMessage;
+        }
+
+        public ILog GetLogger(Type type)
+        {
+            return new GenericLogger(type) { OnMessage = OnMessage };
+        }
+
+        public ILog GetLogger(string typeName)
+        {
+            return new GenericLogger(typeName) { OnMessage = OnMessage };
+        }
+    }
+}
