4dde1ce673d46e2871c6fd6b1820c7f2d61a4c36
Demis Bellot <demis.bellot@gmail.com>
2015-06-17 11:13:34 +0800

    Allow EmptyArray in GET requests

---
+++ tests/ServiceStack.WebHost.Endpoints.Tests/Issues/EmptyDtoIssue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Funq;
+using NUnit.Framework;
+
+namespace ServiceStack.WebHost.Endpoints.Tests.Issues
+{
+    public class PostEmptyArray : IReturnVoid
+    {
+        public int[] Ids { get; set; }
+    }
+
+    public class GetEmptyArray : IReturn<List<int>>
+    {
+        public int[] Ids { get; set; }
+    }
+
+    public class TestService : Service
+    {
+        public void Post(PostEmptyArray e)
+        {
+            if (e.Ids == null)
+                throw new Exception();
+        }
+
+        public List<int> Get(GetEmptyArray e)
+        {
+            if (e.Ids == null)
+                throw new Exception();
+
+            return e.Ids.ToList();
+        }
+    }
+
+    public class EmptyDtoIssue
+    {
+        public class EmptyArrayDtoTest
+        {
+            public class AppHost : AppHostHttpListenerBase
+            {
+                public AppHost() : base(typeof(EmptyArrayDtoTest).Name, typeof(GetEmptyArray).Assembly) { }
+
+                public override void Configure(Container container) { }
+            }
+
+            ServiceStackHost appHost;
+
+            [TestFixtureSetUp]
+            public void TestFixtureSetUp()
+            {
+                appHost = new AppHost()
+                    .Init()
+                    .Start(Config.ListeningOn);
+            }
+
+            [TestFixtureTearDown]
+            public void TestFixtureTearDown()
+            {
+                appHost.Dispose();
+            }
+
+            [Test]
+            public void Can_POST_empty_array()
+            {
+                var client = new JsonServiceClient(Config.AbsoluteBaseUri);
+                client.Post(new PostEmptyArray { Ids = new int[] { } });
+            }
+
+            [Test]
+            public void Can_GET_empty_array()
+            {
+
+                var client = new JsonServiceClient(Config.AbsoluteBaseUri);
+                client.Get(new GetEmptyArray { Ids = new int[] { } });
+            }
+        }
+    }
+}
