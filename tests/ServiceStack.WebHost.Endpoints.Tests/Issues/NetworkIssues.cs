88e5a8d2b81ce691aecc7e2f55c475d950da8b47
Demis Bellot <demis.bellot@gmail.com>
2015-06-25 13:18:24 +0800

    Add integration test to manually check disconnected networks

---
+++ tests/ServiceStack.WebHost.Endpoints.Tests/Issues/NetworkIssues.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+using ServiceStack.Text;
+
+namespace ServiceStack.WebHost.Endpoints.Tests.Issues
+{
+    [Route("/wait/{ForMs}")]
+    public class Wait : IReturn<Wait>
+    {
+        public int ForMs { get; set; }
+    }
+
+    [TestFixture, Ignore("Requires external Services")]
+    public class NetworkIssues
+    {
+        [Test]
+        public async Task Simulate_broken_Network()
+        {
+            var client = new JsonServiceClient("http://test.servicestack.net");
+
+            var response = await client.GetAsync(new Wait { ForMs = 50000 });
+
+            response.PrintDump();
+        }
+    }
+}
