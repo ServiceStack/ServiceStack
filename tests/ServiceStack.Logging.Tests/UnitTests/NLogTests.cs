180fc5d6d504fc6ac57ed0ae2e4304645f7fd17e
Demis Bellot <demis.bellot@gmail.com>
2015-07-10 23:19:30 +0800

    Switch to use NLog LogEntry to preserve callsite

---
+++ tests/ServiceStack.Logging.Tests/UnitTests/NLogTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ServiceStack.Logging.Tests.UnitTests
+{
+    [TestFixture]
+    public class NLogTests
+    {
+        [Test]
+        public void Does_maintain_callsite()
+        {
+            ServiceStack.Logging.LogManager.LogFactory = new ServiceStack.Logging.NLogger.NLogFactory();
+
+            var log = ServiceStack.Logging.LogManager.LogFactory.GetLogger(GetType());
+            log.InfoFormat("Message");
+            log.InfoFormat("Message with Args {0}", "Foo");
+            log.Info("Message with Exception", new Exception("Foo Exception"));
+        }
+    }
+}
