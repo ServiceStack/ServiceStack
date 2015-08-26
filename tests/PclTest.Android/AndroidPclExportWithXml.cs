667f7721a7f86c0f651fa73d97d4b5bd4b811128
Demis Bellot <demis.bellot@gmail.com>
2015-08-26 17:13:50 -0400

    Add custom PclExport implementations that restore XmlConvert DateTime behavior

---
+++ tests/PclTest.Android/AndroidPclExportWithXml.cs
@@ -0,0 +1,38 @@
+using System;
+using ServiceStack;
+using ServiceStack.Text;
+using ServiceStack.Text.Common;
+
+namespace PclTest.Android
+{
+    public class AndroidPclExportWithXml : AndroidPclExport
+    {
+        public static new AndroidPclExportWithXml Provider = new AndroidPclExportWithXml();
+
+        public new static void Configure()
+        {
+            AndroidPclExportClient.Configure();
+            Configure(Provider);
+        }
+
+        public override string ToXsdDateTimeString(DateTime dateTime)
+        {
+            return System.Xml.XmlConvert.ToString(dateTime.ToStableUniversalTime(), System.Xml.XmlDateTimeSerializationMode.Utc);
+        }
+
+        public override string ToLocalXsdDateTimeString(DateTime dateTime)
+        {
+            return System.Xml.XmlConvert.ToString(dateTime, System.Xml.XmlDateTimeSerializationMode.Local);
+        }
+
+        public override DateTime ParseXsdDateTime(string dateTimeStr)
+        {
+            return System.Xml.XmlConvert.ToDateTime(dateTimeStr, System.Xml.XmlDateTimeSerializationMode.Utc);
+        }
+
+        public override DateTime ParseXsdDateTimeAsUtc(string dateTimeStr)
+        {
+            return System.Xml.XmlConvert.ToDateTime(dateTimeStr, System.Xml.XmlDateTimeSerializationMode.Utc).Prepare(parsedAsUtc: true);
+        }
+    }
+}
---
+++ tests/PclTest.Ios/IosPclExportWithXml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using ServiceStack;
+using ServiceStack.Text;
+using ServiceStack.Text.Common;
+
+namespace PclTest.Ios
+{
+    public class IosPclExportWithXml : IosPclExport
+    {
+        public static new IosPclExportWithXml Provider = new IosPclExportWithXml();
+
+        public new static void Configure()
+        {
+            IosPclExportClient.Configure(); 
+            Configure(Provider);
+        }
+
+        public override string ToXsdDateTimeString(DateTime dateTime)
+        {
+            return System.Xml.XmlConvert.ToString(dateTime.ToStableUniversalTime(), System.Xml.XmlDateTimeSerializationMode.Utc);
+        }
+
+        public override string ToLocalXsdDateTimeString(DateTime dateTime)
+        {
+            return System.Xml.XmlConvert.ToString(dateTime, System.Xml.XmlDateTimeSerializationMode.Local);
+        }
+
+        public override DateTime ParseXsdDateTime(string dateTimeStr)
+        {
+            return System.Xml.XmlConvert.ToDateTime(dateTimeStr, System.Xml.XmlDateTimeSerializationMode.Utc);
+        }
+
+        public override DateTime ParseXsdDateTimeAsUtc(string dateTimeStr)
+        {
+            return System.Xml.XmlConvert.ToDateTime(dateTimeStr, System.Xml.XmlDateTimeSerializationMode.Utc).Prepare(parsedAsUtc: true);
+        }
+    }
+}
