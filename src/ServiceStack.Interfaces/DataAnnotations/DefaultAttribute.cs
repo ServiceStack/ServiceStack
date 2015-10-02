73ce7e9e2ac6ee7e73c89509ce0f83d8074ce631
Demis Bellot <demis.bellot@gmail.com>
2015-10-01 17:00:30 -0700

    Add overload for [Default(string)] and specify DefaultType/DefaultValue for all overloads

--- src/ServiceStack.Interfaces/DataAnnotations/DefaultAttribute.cs
+++ src/ServiceStack.Interfaces/DataAnnotations/DefaultAttribute.cs
@@ -14,11 +14,21 @@ namespace ServiceStack.DataAnnotations
         public DefaultAttribute(int intValue)
         {
             this.IntValue = intValue;
+            this.DefaultType = typeof(int);
+            this.DefaultValue = this.IntValue.ToString();
         }
 
         public DefaultAttribute(double doubleValue)
         {
             this.DoubleValue = doubleValue;
+            this.DefaultType = typeof(double);
+            this.DefaultValue = doubleValue.ToString();
+        }
+
+        public DefaultAttribute(string defaultValue)
+        {
+            this.DefaultType = typeof(string);
+            this.DefaultValue = defaultValue;
         }
 
         public DefaultAttribute(Type defaultType, string defaultValue)
