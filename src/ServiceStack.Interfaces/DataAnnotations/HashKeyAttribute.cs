68ec053f1652b9efa1de50187748d78b8faa2d7b
Demis Bellot <demis.bellot@gmail.com>
2015-10-11 16:14:11 -0700

    Add the [HashKey] attribute as well

---
+++ src/ServiceStack.Interfaces/DataAnnotations/HashKeyAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServiceStack.DataAnnotations
+{
+    /// <summary>
+    /// Hash Key Attribute used to specify which property is the HashKey, e.g. in DynamoDb.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property)]
+    public class HashKeyAttribute : AttributeBase
+    {
+    }
+}
