6949f3f045c56bf141cd87e9ae4870314ac193bc
Demis Bellot <demis.bellot@gmail.com>
2015-10-18 15:57:50 -0700

    Add new CompositeKey Attribute

---
+++ src/ServiceStack.Interfaces/DataAnnotations/CompositeKeyAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ServiceStack.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false)]
+    public class CompositeKeyAttribute : AttributeBase
+    {
+        public CompositeKeyAttribute()
+        {
+            this.FieldNames = new List<string>();
+        }
+
+        public CompositeKeyAttribute(params string[] fieldNames)
+        {
+            this.FieldNames = new List<string>(fieldNames);
+        }
+
+        public List<string> FieldNames { get; set; }
+    }
+}
