0ba50588aef362e124e4daf837ff73e68b43c44c
Demis Bellot <demis.bellot@gmail.com>
2015-10-30 17:38:13 -0700

    Add more VPP extensions

--- src/ServiceStack/VirtualPath/VirtualPathProviderExtensions.cs
+++ src/ServiceStack/VirtualPath/VirtualPathProviderExtensions.cs
@@ -12,12 +12,12 @@ namespace ServiceStack.VirtualPath
 
         public static bool IsFile(this IVirtualPathProvider pathProvider, string filePath)
         {
-            return pathProvider.GetFile(filePath) != null;
+            return pathProvider.FileExists(filePath);
         }
 
         public static bool IsDirectory(this IVirtualPathProvider pathProvider, string filePath)
         {
-            return pathProvider.GetDirectory(filePath) != null;
+            return pathProvider.DirectoryExists(filePath);
         }
 
         [Obsolete("Renamed to WriteFile")]
@@ -56,6 +56,18 @@ namespace ServiceStack.VirtualPath
             }
         }
 
+        public static void WriteFile(this IVirtualPathProvider pathProvider, IVirtualFile file, string filePath = null)
+        {
+            var writableFs = pathProvider as IVirtualFileSystem;
+            if (writableFs == null)
+                throw new InvalidOperationException(ErrorNotWritable.Fmt(pathProvider.GetType().Name));
+
+            using (var stream = file.OpenRead())
+            {
+                writableFs.WriteFile(filePath ?? file.VirtualPath, stream);
+            }
+        }
+
         public static void DeleteFile(this IVirtualPathProvider pathProvider, string filePath)
         {
             var writableFs = pathProvider as IVirtualFileSystem;
@@ -63,6 +75,11 @@ namespace ServiceStack.VirtualPath
                 throw new InvalidOperationException(ErrorNotWritable.Fmt(pathProvider.GetType().Name));
 
             writableFs.DeleteFile(filePath);
+        }
+
+        public static void DeleteFile(this IVirtualPathProvider pathProvider, IVirtualFile file)
+        {
+            pathProvider.DeleteFile(file.VirtualPath);
         }
 
         public static void DeleteFiles(this IVirtualPathProvider pathProvider, IEnumerable<string> filePaths)
@@ -114,6 +131,19 @@ namespace ServiceStack.VirtualPath
                     pathProvider.WriteFile(dstPath, stream);
                 }
             }
+        }
+    }
+
+    public static class VirtualDirectoryExtensions
+    {
+        public static IEnumerable<IVirtualFile> GetFiles(this IVirtualDirectory dir)
+        {
+            return dir.Files;
+        }
+
+        public static IEnumerable<IVirtualDirectory> GetDirectories(this IVirtualDirectory dir)
+        {
+            return dir.Directories;
         }
     }
 }
