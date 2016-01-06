ccdf02856de7eedf4ef3bf18c6b264d2adbad5c2
Demis Bellot <demis.bellot@gmail.com>
2016-01-05 16:48:16 -0800

    Update Pcl files w/ updated RegisterForAot list

--- src/ServiceStack.Pcl.WinStore/PclExport.WinStore.cs
+++ src/ServiceStack.Pcl.WinStore/PclExport.WinStore.cs
@@ -7,6 +7,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using ServiceStack.Text;
+using ServiceStack.Text.Common;
+using ServiceStack.Text.Json;
 
 namespace ServiceStack
 {
@@ -31,7 +34,7 @@ namespace ServiceStack
             task.AsTask().Wait();
 
             var file = task.GetResults();
-            
+
             var streamTask = file.OpenStreamForReadAsync();
             streamTask.Wait();
 
@@ -74,21 +77,21 @@ namespace ServiceStack
         {
             public static AppDomain CurrentDomain { get; private set; }
             public static Assembly[] cacheObj = null;
- 
+
             static AppDomain()
             {
                 CurrentDomain = new AppDomain();
             }
- 
+
             public Assembly[] GetAssemblies()
             {
                 return cacheObj ?? GetAssemblyListAsync().Result.ToArray();
             }
- 
+
             private async System.Threading.Tasks.Task<IEnumerable<Assembly>> GetAssemblyListAsync()
             {
                 var folder = Windows.ApplicationModel.Package.Current.InstalledLocation;
- 
+
                 var assemblies = new List<Assembly>();
                 foreach (Windows.Storage.StorageFile file in await folder.GetFilesAsync())
                 {
@@ -97,7 +100,7 @@ namespace ServiceStack
                         try
                         {
                             var filename = file.Name.Substring(0, file.Name.Length - file.FileType.Length);
-                            AssemblyName name = new AssemblyName() { Name = filename };
+                            AssemblyName name = new AssemblyName() {Name = filename};
                             Assembly asm = Assembly.Load(name);
                             assemblies.Add(asm);
                         }
@@ -109,7 +112,7 @@ namespace ServiceStack
                 }
 
                 cacheObj = assemblies.ToArray();
- 
+
                 return cacheObj;
             }
         }
@@ -125,6 +128,204 @@ namespace ServiceStack
         //    // .Net 4.0+ does this under the hood anyway.
         //    return TimeZoneInfo.ConvertTimeToUtc(dateTime);
         //}
+
+        public override void VerifyInAssembly(Type accessType, ICollection<string> assemblyNames)
+        {
+        }
+
+        public static void InitForAot()
+        {
+        }
+
+        internal class Poco
+        {
+            public string Dummy { get; set; }
+        }
+
+        public override void RegisterForAot()
+        {
+            RegisterTypeForAot<Poco>();
+
+            RegisterElement<Poco, string>();
+
+            RegisterElement<Poco, bool>();
+            RegisterElement<Poco, char>();
+            RegisterElement<Poco, byte>();
+            RegisterElement<Poco, sbyte>();
+            RegisterElement<Poco, short>();
+            RegisterElement<Poco, ushort>();
+            RegisterElement<Poco, int>();
+            RegisterElement<Poco, uint>();
+
+            RegisterElement<Poco, long>();
+            RegisterElement<Poco, ulong>();
+            RegisterElement<Poco, float>();
+            RegisterElement<Poco, double>();
+            RegisterElement<Poco, decimal>();
+
+            RegisterElement<Poco, bool?>();
+            RegisterElement<Poco, char?>();
+            RegisterElement<Poco, byte?>();
+            RegisterElement<Poco, sbyte?>();
+            RegisterElement<Poco, short?>();
+            RegisterElement<Poco, ushort?>();
+            RegisterElement<Poco, int?>();
+            RegisterElement<Poco, uint?>();
+            RegisterElement<Poco, long?>();
+            RegisterElement<Poco, ulong?>();
+            RegisterElement<Poco, float?>();
+            RegisterElement<Poco, double?>();
+            RegisterElement<Poco, decimal?>();
+
+            //RegisterElement<Poco, JsonValue>();
+
+            RegisterTypeForAot<DayOfWeek>(); // used by DateTime
+
+            // register built in structs
+            RegisterTypeForAot<Guid>();
+            RegisterTypeForAot<TimeSpan>();
+            RegisterTypeForAot<DateTime>();
+            RegisterTypeForAot<DateTimeOffset>();
+
+            RegisterTypeForAot<Guid?>();
+            RegisterTypeForAot<TimeSpan?>();
+            RegisterTypeForAot<DateTime?>();
+            RegisterTypeForAot<DateTimeOffset?>();
+        }
+
+        public static void RegisterTypeForAot<T>()
+        {
+            AotConfig.RegisterSerializers<T>();
+        }
+
+        public static void RegisterQueryStringWriter()
+        {
+            var i = 0;
+            if (QueryStringWriter<Poco>.WriteFn() != null) i++;
+        }
+
+        public static int RegisterElement<T, TElement>()
+        {
+            var i = 0;
+            i += AotConfig.RegisterSerializers<TElement>();
+            AotConfig.RegisterElement<T, TElement, JsonTypeSerializer>();
+            AotConfig.RegisterElement<T, TElement, Text.Jsv.JsvTypeSerializer>();
+            return i;
+        }
+
+        internal class AotConfig
+        {
+            internal static JsReader<JsonTypeSerializer> jsonReader;
+            internal static JsWriter<JsonTypeSerializer> jsonWriter;
+            internal static JsReader<Text.Jsv.JsvTypeSerializer> jsvReader;
+            internal static JsWriter<Text.Jsv.JsvTypeSerializer> jsvWriter;
+            internal static JsonTypeSerializer jsonSerializer;
+            internal static Text.Jsv.JsvTypeSerializer jsvSerializer;
+
+            static AotConfig()
+            {
+                jsonSerializer = new JsonTypeSerializer();
+                jsvSerializer = new Text.Jsv.JsvTypeSerializer();
+                jsonReader = new JsReader<JsonTypeSerializer>();
+                jsonWriter = new JsWriter<JsonTypeSerializer>();
+                jsvReader = new JsReader<Text.Jsv.JsvTypeSerializer>();
+                jsvWriter = new JsWriter<Text.Jsv.JsvTypeSerializer>();
+            }
+
+            internal static int RegisterSerializers<T>()
+            {
+                var i = 0;
+                i += Register<T, JsonTypeSerializer>();
+                if (jsonSerializer.GetParseFn<T>() != null) i++;
+                if (jsonSerializer.GetWriteFn<T>() != null) i++;
+                if (jsonReader.GetParseFn<T>() != null) i++;
+                if (jsonWriter.GetWriteFn<T>() != null) i++;
+
+                i += Register<T, Text.Jsv.JsvTypeSerializer>();
+                if (jsvSerializer.GetParseFn<T>() != null) i++;
+                if (jsvSerializer.GetWriteFn<T>() != null) i++;
+                if (jsvReader.GetParseFn<T>() != null) i++;
+                if (jsvWriter.GetWriteFn<T>() != null) i++;
+
+                //RegisterCsvSerializer<T>();
+                RegisterQueryStringWriter();
+                return i;
+            }
+
+            internal static void RegisterCsvSerializer<T>()
+            {
+                CsvSerializer<T>.WriteFn();
+                CsvSerializer<T>.WriteObject(null, null);
+                CsvWriter<T>.Write(null, default(IEnumerable<T>));
+                CsvWriter<T>.WriteRow(null, default(T));
+            }
+
+            public static ParseStringDelegate GetParseFn(Type type)
+            {
+                var parseFn = JsonTypeSerializer.Instance.GetParseFn(type);
+                return parseFn;
+            }
+
+            internal static int Register<T, TSerializer>() where TSerializer : ITypeSerializer
+            {
+                var i = 0;
+
+                if (JsonWriter<T>.WriteFn() != null) i++;
+                if (JsonWriter.Instance.GetWriteFn<T>() != null) i++;
+                if (JsonReader.Instance.GetParseFn<T>() != null) i++;
+                if (JsonReader<T>.Parse(null) != null) i++;
+                if (JsonReader<T>.GetParseFn() != null) i++;
+                //if (JsWriter.GetTypeSerializer<JsonTypeSerializer>().GetWriteFn<T>() != null) i++;
+                if (new List<T>() != null) i++;
+                if (new T[0] != null) i++;
+
+                JsConfig<T>.ExcludeTypeInfo = false;
+
+                if (JsConfig<T>.OnDeserializedFn != null) i++;
+                if (JsConfig<T>.HasDeserializeFn) i++;
+                if (JsConfig<T>.SerializeFn != null) i++;
+                if (JsConfig<T>.DeSerializeFn != null) i++;
+                //JsConfig<T>.SerializeFn = arg => "";
+                //JsConfig<T>.DeSerializeFn = arg => default(T);
+                if (TypeConfig<T>.Properties != null) i++;
+
+                WriteListsOfElements<T, TSerializer>.WriteList(null, null);
+                WriteListsOfElements<T, TSerializer>.WriteIList(null, null);
+                WriteListsOfElements<T, TSerializer>.WriteEnumerable(null, null);
+                WriteListsOfElements<T, TSerializer>.WriteListValueType(null, null);
+                WriteListsOfElements<T, TSerializer>.WriteIListValueType(null, null);
+                WriteListsOfElements<T, TSerializer>.WriteGenericArrayValueType(null, null);
+                WriteListsOfElements<T, TSerializer>.WriteArray(null, null);
+
+                TranslateListWithElements<T>.LateBoundTranslateToGenericICollection(null, null);
+                TranslateListWithConvertibleElements<T, T>.LateBoundTranslateToGenericICollection(null, null);
+
+                QueryStringWriter<T>.WriteObject(null, null);
+                return i;
+            }
+
+            internal static void RegisterElement<T, TElement, TSerializer>() where TSerializer : ITypeSerializer
+            {
+                DeserializeDictionary<TSerializer>.ParseDictionary<T, TElement>(null, null, null, null);
+                DeserializeDictionary<TSerializer>.ParseDictionary<TElement, T>(null, null, null, null);
+
+                ToStringDictionaryMethods<T, TElement, TSerializer>.WriteIDictionary(null, null, null, null);
+                ToStringDictionaryMethods<TElement, T, TSerializer>.WriteIDictionary(null, null, null, null);
+
+                // Include List deserialisations from the Register<> method above.  This solves issue where List<Guid> properties on responses deserialise to null.
+                // No idea why this is happening because there is no visible exception raised.  Suspect IOS is swallowing an AOT exception somewhere.
+                DeserializeArrayWithElements<TElement, TSerializer>.ParseGenericArray(null, null);
+                DeserializeListWithElements<TElement, TSerializer>.ParseGenericList(null, null, null);
+
+                // Cannot use the line below for some unknown reason - when trying to compile to run on device, mtouch bombs during native code compile.
+                // Something about this line or its inner workings is offensive to mtouch. Luckily this was not needed for my List<Guide> issue.
+                // DeserializeCollection<JsonTypeSerializer>.ParseCollection<TElement>(null, null, null);
+
+                TranslateListWithElements<TElement>.LateBoundTranslateToGenericICollection(null, typeof(List<TElement>));
+                TranslateListWithConvertibleElements<TElement, TElement>.LateBoundTranslateToGenericICollection(null, typeof(List<TElement>));
+            }
+        }
+
     }
 }
 
