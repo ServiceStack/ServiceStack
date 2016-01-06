efabc351522815a6a88522487811fe570fdb24a9
Demis Bellot <demis.bellot@gmail.com>
2016-01-05 16:48:45 -0800

    Add WinStore81 project so it builds in VS2015 to test UWP

---
+++ src/ServiceStack.Pcl.WinStore81/PclExportClient.WinStore.cs
@@ -0,0 +1,67 @@
+﻿//Copyright (c) Service Stack LLC. All Rights Reserved.
+//License: https://raw.github.com/ServiceStack/ServiceStack/master/license.txt
+
+#if NETFX_CORE
+using System;
+using System.Threading;
+using ServiceStack.Pcl;
+using ServiceStack.Web;
+using Windows.System.Threading;
+
+namespace ServiceStack
+{
+    public class WinStorePclExportClient : PclExportClient
+    {
+        public static WinStorePclExportClient Provider = new WinStorePclExportClient();
+
+        public static PclExportClient Configure()
+        {
+            Configure(Provider ?? (Provider = new WinStorePclExportClient()));
+            WinStorePclExport.Configure();
+            return Provider;
+        }
+
+        public override INameValueCollection NewNameValueCollection()
+        {
+            return new NameValueCollectionWrapper(new NameValueCollection());
+        }
+
+        public override INameValueCollection ParseQueryString(string query)
+        {
+            return HttpUtility.ParseQueryString(query).InWrapper();
+        }
+
+        public override ITimer CreateTimer(TimerCallback cb, TimeSpan timeOut, object state)
+        {
+            return new WinStoreAsyncTimer(ThreadPoolTimer.CreateTimer(s => cb(s), timeOut));
+        }
+
+        public override void RunOnUiThread(Action fn)
+        {
+            Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(
+                Windows.UI.Core.CoreDispatcherPriority.Normal, () => fn());
+        }
+    }
+
+    public class WinStoreAsyncTimer : ITimer
+    {
+        public ThreadPoolTimer Timer;
+
+        public WinStoreAsyncTimer(ThreadPoolTimer timer)
+        {
+            Timer = timer;
+        }
+
+        public void Cancel()
+        {
+            this.Timer.Cancel();
+        }
+
+        public void Dispose()
+        {
+            this.Timer = null;
+        }
+    }
+
+}
+#endif
