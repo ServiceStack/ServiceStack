15ff5f35249d105692fd45a11173cc9077e7ea57
Demis Bellot <demis.bellot@gmail.com>
2016-02-01 17:37:35 -0500

    Swapout existing CheckHttpListener impl with latest basic auth test

--- tests/CheckHttpListener/Program.cs
+++ tests/CheckHttpListener/Program.cs
@@ -1,226 +1,217 @@
 ﻿using System;
-using System.Diagnostics;
-using System.Net;
-using Autofac;
-using Check.ServiceInterface;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Threading;
 using Funq;
 using ServiceStack;
 using ServiceStack.Auth;
+using ServiceStack.Caching;
 using ServiceStack.Configuration;
-using ServiceStack.Host.Handlers;
-using ServiceStack.Text;
+using ServiceStack.Web;
 
 namespace CheckHttpListener
 {
-    [Route("/request")]
-    public class Request : IReturn<Response> { }
-    public class Response { }
-
-    public class MyServices : Service
+    [DataContract]
+    public class CustomUserSession : AuthUserSession
     {
-        public object Any(Request request)
+        private int organisationId_;
+
+        [DataMember]
+        public int UtcOffset { get; set; }
+
+        [DataMember]
+        public Int32 CreatedDateUnix { get; set; }
+
+        [DataMember]
+        public DateTime CreatedDate { get; set; }
+
+        public override void OnAuthenticated(IServiceBase authService, IAuthSession session, IAuthTokens tokens,
+            Dictionary<string, string> authInfo)
         {
-            return new Response();
+            base.OnAuthenticated(authService, session, tokens, authInfo);
+
+            CreatedDate = DateTime.Now;
+
+            CreatedDateUnix = (Int32)(CreatedDate.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+
+            authService.SaveSession(this);
+
         }
     }
 
-    public class AutofacIocAdapter : IContainerAdapter
+    public class CustomBasicAuthProvider : BasicAuthProvider
     {
-        private readonly IContainer _container;
+        public CustomBasicAuthProvider() {}
 
-        public AutofacIocAdapter(IContainer container)
+        public CustomBasicAuthProvider(IAppSettings appSettings)
+            : base(appSettings) {}
+
+        public override object Authenticate(IServiceBase authService, IAuthSession session, Authenticate request)
         {
-            _container = container;
+            return base.Authenticate(authService, session, request);
         }
 
-        public T Resolve<T>()
+        /// <summary>
+        ///     This method will only send the WWW-Authenticate response header when the User-Agent request
+        ///     header contains servicestack i.e. it's the servicestack client and not a browser
+        /// </summary>
+        public override void OnFailedAuthentication(IAuthSession session, IRequest httpReq, IResponse httpRes)
         {
-            return _container.Resolve<T>();
-        }
-
-        public T TryResolve<T>()
-        {
-            T result;
-
-            if (_container.TryResolve<T>(out result))
+            //Only return digest header if User-Agent is the ServiceStack .NET Client
+            //expected user agent value is: "ServiceStack .NET Client {Version}"
+            if (httpReq.Headers["User-Agent"].ToLower().Contains("servicestack"))
             {
-                return result;
+                httpRes.AddHeader("WWW-Authenticate",
+                    "{0} realm=\"{1}\"".Fmt(Provider, AuthRealm));
             }
 
-            return default(T);
+            httpRes.StatusCode = 401;
+            httpRes.EndRequest(false);
         }
     }
 
-    public class AppHost : AppHostHttpListenerBase
+    public static class AppHostConfiguration
     {
-        public AppHost() : base("Check HttpListener Tests", 
-            typeof(ErrorsService).Assembly,
-            typeof(UserService).Assembly) { }
-
-        public override void Configure(Container container)
+        public static void Configure(ServiceStackHost appHost, Container container)
         {
-            //RawHttpHandlers.Add(_ => new CustomActionHandler((req, res) =>
-            //{
-            //    var bytes = req.InputStream.ReadFully();
-            //    res.OutputStream.Write(bytes, 0, bytes.Length);
-            //}));
+            var appSettings = new AppSettings();
 
-            this.CustomErrorHttpHandlers[HttpStatusCode.NotFound] = null;
-
-            SetConfig(new HostConfig
+            var auth = new AuthFeature(() => new CustomUserSession(),
+                new IAuthProvider[]
+                {
+                    new CustomBasicAuthProvider(),
+                    new CredentialsAuthProvider(appSettings) {SessionExpiry = TimeSpan.FromMinutes(30)}
+                },
+                "/login")
             {
+                GenerateNewSessionCookiesOnAuthentication = false,
+            };
+
+            appHost.Plugins.Add(auth);
+
+            IUserAuthRepository authRepository = new InMemoryAuthRepository();
+            ICacheClient cacheClient = new MemoryCacheClient();
+
+            var testUser = authRepository.CreateUserAuth(new UserAuth { Email = "a@b.com" }, "a");
+
+
+            //IoC registrations
+            container.Register(cacheClient);
+            container.Register(authRepository);
+
+
+            var hostConfig = new HostConfig
+            {
+#if DEBUG || STAGING || UAT
                 DebugMode = true,
-                SkipFormDataInCreatingRequest = true,
-            });
+#endif
+                AppendUtf8CharsetOnContentTypes = new HashSet<string> { MimeTypes.Csv },                
+            };
 
-            //Plugins.Add(new NativeTypesFeature());
 
-            //var builder = new ContainerBuilder();
-            //var autofac = builder.Build();
-            //container.Adapter = new AutofacIocAdapter(autofac);
-
-            //Plugins.Add(new AuthFeature(() => new AuthUserSession(), 
-            //    new IAuthProvider[] {
-            //        new BasicAuthProvider(AppSettings), 
-            //    }));
-
-            container.Register<IConnectionString>(c => 
-                new FoundationConnectionString("My Connection"));
+            appHost.SetConfig(hostConfig);
         }
 
-        //public override void OnAfterInit()
-        //{
-        //    base.OnAfterInit();
+        public static void Start() {}
 
-        //    using (var authService = Container.Resolve<AuthenticateService>())
-        //    {
-        //        authService.Authenticate(new Authenticate());
-        //    }
-        //}
+        public static void Stop(bool immediate) {}
     }
 
-    class Program
+    [Route("/test2")]
+    public class TestNoAuthRequest : IReturn<string>
     {
-        static void Main(string[] args)
-        {
-            //Licensing.RegisterLicenseFromFileIfExists(@"c:\src\appsettings.license.txt");
-
-            new AppHost()
-                .Init()
-                .Start("http://*:2020/");
-
-            //TestService();
-
-            Process.Start("http://localhost:2020/upload.html");
-
-            //var response = "http://localhost:2020".PostToUrl(new { a = "foo", b = "bar" });
-            //"Response: {0}".Print(response);
-
-            
-            Console.ReadLine();
-        }
-
-        private async static void TestService()
-        {
-            var client = new JsonServiceClient("http://localhost:2020/");
-
-            try
-            {
-                var request = new UpdateAddressVoid();
-                request.ToGetUrl().Print();
-                request.ToPostUrl().Print();
-                await client.PostAsync(request);
-                //var response = client.Post(request);
-            }
-            catch (WebServiceException ex)
-            {
-                ex.StatusCode.ToString().Print();
-                ex.StatusDescription.Print();
-                ex.ResponseBody.Print();
-            }
-        }
-    }
-
-    [Route("/user/{UserId}/Address")]
-    public class UpdateAddress : IReturn<AddressResponse>
-    {
-        public int UserId { get; set; }
-
-        public string Address { get; set; }
-    }
-
-    [Route("/user/{UserId}/AddressVoid")]
-    public class UpdateAddressVoid : IReturnVoid
-    {
-        public int UserId { get; set; }
-
-        public string Address { get; set; }
-    }
-
-    public class AddressResponse
-    {
-        public string Address { get; set; }
-    }
-
-    public class UserService : Service
-    {
-        public object Post(UpdateAddress request)
-        {
-            if (request.UserId > 0)
-            {
-                return new HttpResult(request.Address, HttpStatusCode.OK);
-            }
-
-            //throw new UnauthorizedAccessException("Unauthorized UserId");
-            //throw HttpError.Unauthorized(request.Address ?? "Unauthorized UserId");
-            return HttpError.Unauthorized(request.Address ?? "Unauthorized UserId");
-        }
-
-        public void Post(UpdateAddressVoid request)
-        {
-            //throw new UnauthorizedAccessException("Unauthorized UserId");
-            throw HttpError.Unauthorized(request.Address ?? "Unauthorized UserId");
-            //return HttpError.Unauthorized(request.Address ?? "Unauthorized UserId");
-        }
-    }
-
-    [Route("/upload", "POST")]
-    public class UploadFileRequest { }
-
-    public class TestController : Service
-    {
-        public void Any(UploadFileRequest request)
-        {
-            int filesCount = Request.Files.Length;
-            Console.WriteLine(filesCount); // Always 0
-        }
-    }
-
-    public interface IConnectionString
-    {
-        string ConnectionString { get; }
-    }
-
-    class FoundationConnectionString : IConnectionString
-    {
-        public FoundationConnectionString(string connectionString)
-        {
-            ConnectionString = connectionString;
-        }
-
-        public string ConnectionString { get; set; }
     }
 
     [Route("/test")]
-    public class Test : IReturn<string> { }
+    public class TestRequest : IReturn<string>
+    {
+    }
 
     public class TestService : Service
     {
-        public IConnectionString Config { get; set; }
+        private CustomUserSession session_;
 
-        public object Any(Test request)
+        protected CustomUserSession Session
         {
-            return Config.ConnectionString;
+            get
+            {
+                return session_ ?? (session_ = SessionAs<CustomUserSession>());
+            }
+        }
+
+        [Authenticate]
+        public string Any(TestRequest request)
+        {
+            return Session.UserAuthId;
+        }
+
+        public string Any(TestNoAuthRequest request)
+        {
+            return "Hi";
+        }
+    }
+
+    public class AppSelfHost : AppSelfHostBase
+    {
+        public AppSelfHost()
+            : base("DocuRec Services", typeof(TestService).Assembly)
+        { }
+
+        public override void Configure(Container container)
+        {
+            AppHostConfiguration.Configure(this, container);
+        }
+
+        /// <summary>
+        ///     Starts the ServiceStackHost and Schedules jobs (if it's the first time being called)
+        /// </summary>
+        public override ServiceStackHost Start(string urlBase)
+        {
+            return Start(new List<string> { urlBase });
+        }
+
+        /// <summary>
+        ///     Starts the ServiceStackHost and Schedules jobs (if it's the first time being called)
+        /// </summary>
+        public override ServiceStackHost Start(IEnumerable<string> urlBases)
+        {
+            AppHostConfiguration.Start();
+
+            return base.Start(urlBases);
+        }
+
+        public override void Stop()
+        {
+            AppHostConfiguration.Stop(true);
+
+            base.Stop();
+        }
+    }
+
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var appHost = new AppSelfHost();
+            appHost.Init();
+            appHost.Start("http://127.0.0.1:1234/");
+
+            Thread.Sleep(2500);
+
+            var client = new JsonServiceClient("http://127.0.0.1:1234/")
+            {
+                AlwaysSendBasicAuthHeader = true,
+                UserName = "a@b.com",
+                Password = "a"
+            };
+
+            var post1 = client.Post(new TestRequest());
+            var post2 = client.Post(new TestRequest());
+            var response = "First response: {0}, Second Response: {1}".Fmt(post1, post2);
+
+            Console.Out.WriteLine(response);
+            Console.Read();
         }
     }
 }
