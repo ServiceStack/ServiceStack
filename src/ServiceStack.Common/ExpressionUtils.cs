961c9998174625b08b34146a0af37dc5d98c343c
Demis Bellot <demis.bellot@gmail.com>
2016-03-13 22:41:36 -0400

    Add ExpressionUtils

---
+++ src/ServiceStack.Common/ExpressionUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ServiceStack
+{
+    public static class ExpressionUtils
+    {
+        public static PropertyInfo ToPropertyInfo(this Expression fieldExpr)
+        {
+            return ToPropertyInfo(fieldExpr as LambdaExpression)
+                ?? ToPropertyInfo(fieldExpr as MemberExpression);
+        }
+
+        public static PropertyInfo ToPropertyInfo(LambdaExpression lambda)
+        {
+            if (lambda == null)
+                return null;
+
+            return lambda.Body.NodeType == ExpressionType.MemberAccess 
+                ? ToPropertyInfo(lambda.Body as MemberExpression) 
+                : null;
+        }
+
+        public static PropertyInfo ToPropertyInfo(MemberExpression m)
+        {
+            if (m == null)
+                return null;
+
+            var pi = m.Member as PropertyInfo;
+            return pi;
+        }
+    }
+}
