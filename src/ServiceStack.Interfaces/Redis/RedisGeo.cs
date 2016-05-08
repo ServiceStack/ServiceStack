703e559625c964f846b274b3cc5bb13340d114c2
Demis Bellot <demis.bellot@gmail.com>
2016-05-08 01:01:00 -0400

    Add RedisGeoUnit

--- src/ServiceStack.Interfaces/Redis/RedisGeo.cs
+++ src/ServiceStack.Interfaces/Redis/RedisGeo.cs
@@ -24,8 +24,16 @@ namespace ServiceStack.Redis
         public string Member { get; set; }
         public double Longitude { get; set; }
         public double Latitude { get; set; }
-        public string Hash { get; set; }
+        public long Hash { get; set; }
         public string Unit { get; set; }
         public double Distance { get; set; }
+    }
+
+    public static class RedisGeoUnit
+    {
+        public const string Meters = "m";
+        public const string Kilometers = "km";
+        public const string Miles = "mo";
+        public const string Feet = "ft";
     }
 }
