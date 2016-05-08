7e18bfecb34da1c27b5b57b83bf931056d3e9a83
Demis Bellot <demis.bellot@gmail.com>
2016-05-07 23:25:15 -0400

    Add convenience ctor for RedisGeo

--- src/ServiceStack.Interfaces/Redis/RedisGeo.cs
+++ src/ServiceStack.Interfaces/Redis/RedisGeo.cs
@@ -5,6 +5,15 @@ namespace ServiceStack.Redis
 {
     public class RedisGeo
     {
+        public RedisGeo() { }
+
+        public RedisGeo(double longitude, double latitude, string member)
+        {
+            Longitude = longitude;
+            Latitude = latitude;
+            Member = member;
+        }
+
         public double Longitude { get; set; }
         public double Latitude { get; set; }
         public string Member { get; set; }
@@ -13,10 +22,10 @@ namespace ServiceStack.Redis
     public class RedisGeoResult
     {
         public string Member { get; set; }
-        public double? Longitude { get; set; }
-        public double? Latitude { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
         public string Hash { get; set; }
         public string Unit { get; set; }
-        public double? Distance { get; set; }
+        public double Distance { get; set; }
     }
 }
