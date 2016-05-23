c8fd77498fc755de97a72b442180eb824ff7e90e
Demis Bellot <demis.bellot@gmail.com>
2016-05-23 01:23:07 -0400

    Add Assignment Expression tests that use CachedExpressionCompiler

--- tests/ServiceStack.Common.Tests/ExpressionUtilsTests.cs
+++ tests/ServiceStack.Common.Tests/ExpressionUtilsTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
 using NUnit.Framework;
 using ServiceStack.Common.Tests.Models;
 
@@ -14,6 +16,32 @@ namespace ServiceStack.Common.Tests
 
             Assert.That(ExpressionUtils.GetMemberName((ModelWithFieldsOfNullableTypes x) => x.NId),
                 Is.EqualTo("NId"));
+        }
+
+        public Expression<Func<T>> GetAssignmentExpression<T>(Expression<Func<T>> expr)
+        {
+            return expr;
+        }
+
+        [Test]
+        public void Can_get_assigned_constants()
+        {
+            Assert.That(GetAssignmentExpression(() => new Poco { Name = "Foo" }).AssignedValues(),
+                Is.EquivalentTo(new Dictionary<string, object> {
+                    {"Name", "Foo"}
+                }));
+        }
+
+        [Test]
+        public void Can_get_assigned_expressions()
+        {
+            2.Times(i =>
+            {
+                Assert.That(GetAssignmentExpression(() => new Poco { Name = i % 2 == 0 ? "Foo" : "Bar" }).AssignedValues(),
+                    Is.EquivalentTo(new Dictionary<string, object> {
+                        { "Name", i % 2 == 0 ? "Foo" : "Bar" }
+                    }));
+            });
         }
     }
 }
