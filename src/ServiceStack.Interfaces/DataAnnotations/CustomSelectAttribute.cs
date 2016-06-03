e284fb2a91bb0eb242762eec39f42fc42805673d
Demis Bellot <demis.bellot@gmail.com>
2016-06-02 20:21:05 -0400

    Add new [CustomSelect] attribute for allowing columns to be selected from an expression

---
+++ src/ServiceStack.Interfaces/DataAnnotations/CustomSelectAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ServiceStack.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CustomSelectAttribute : AttributeBase
+    {
+        public string Sql { get; set; }
+
+        public CustomSelectAttribute(string sql)
+        {
+            Sql = sql;
+        }
+    }
+}
