1971ed91c88566e2e47a50d0360e7b4c23a9ca84
Demis Bellot <demis.bellot@gmail.com>
2016-06-13 10:25:21 -0400

    Update ObjectId DTO classes

--- tests/Check.ServiceInterface/IssueServices.cs
+++ tests/Check.ServiceInterface/IssueServices.cs
@@ -64,12 +64,46 @@ namespace Check.ServiceInterface
         }
     }
 
-    [Route("/code/objectid", "get", Summary = "Objenin", Notes = "")]
-    public class ObjectId : IReturn<IntegerResponse>
+    [Route("/code", "post", Summary = @"Intellisense desteği sunar",
+                    Notes = "")]
+    public class AutoComplete : IReturn<StringListResponse>
+    {
+        public int objectId { get; set; }
+        public int line { get; set; }
+        public int col { get; set; }
+        public string code { get; set; }
+    }
+    [Route("/code/object", "GET", Summary = @"Objenin adından objeyi döner",
+                Notes = "")]
+    public class ObjectId : IReturn<ObjectDesignResponse>
     {
         public string objectName { get; set; }
     }
+    [Route("/code/execute", "get", Summary = @"Objeyi çalıştırır",
+                Notes = "")]
+    public class Execute : IReturn<StringListResponse>
+    {
+        public string objectName { get; set; }
+        public string args { get; set; }
+    }
 
+    public class StringListResponse
+    {
+        public List<string> data { get; set; }
+    }
+    public class StringResponse
+    {
+        public string data { get; set; }
+    }
+    public class ObjectDesignResponse
+    {
+        public ObjectDesign data { get; set; }
+    }
+
+    public class ObjectDesign
+    {
+        public int Id { get; set; }
+    }
     public class IntegerResponse
     {
         public int data { get; set; }
