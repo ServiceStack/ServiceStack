42a66b741139f72849cb7364d1e0ff7dcb60bcc7
xplicit <svg@ngs.ru>
2016-09-02 20:49:41 +0700

    Remove unrelated comment

--- src/ServiceStack.Common/SocketExtensions.cs
+++ src/ServiceStack.Common/SocketExtensions.cs
@@ -3,11 +3,6 @@ using System.Net.Sockets;
 
 namespace ServiceStack
 {
-    /// <summary>
-    /// Useful IPAddressExtensions from: 
-    /// http://blogs.msdn.com/knom/archive/2008/12/31/ip-address-calculations-with-c-subnetmasks-networks.aspx
-    /// 
-    /// </summary>
     public static class SocketExtensions
     {
         public static void Close(this Socket socket)
