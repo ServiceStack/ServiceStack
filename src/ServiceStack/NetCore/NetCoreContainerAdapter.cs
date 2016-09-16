6ed127420dccb54ca6c6f3d97e404457241147a8
Demis Bellot <demis.bellot@gmail.com>
2016-09-16 05:08:08 -0400

    Add NetCoreContainerAdapter

---
+++ src/ServiceStack/NetCore/NetCoreContainerAdapter.cs
@@ -0,0 +1,30 @@
+﻿#if NETSTANDARD1_6
+
+using System;
+using ServiceStack.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceStack.NetCore
+{
+    public class NetCoreContainerAdapter : IContainerAdapter
+    {
+        private IServiceProvider appServices;
+
+        public NetCoreContainerAdapter(IServiceProvider appServices)
+        {
+            this.appServices = appServices;
+        }
+
+        public T TryResolve<T>()
+        {
+            return appServices.GetService<T>();
+        }
+
+        public T Resolve<T>()
+        {
+            return appServices.GetRequiredService<T>();
+        }
+    }
+}
+
+#endif
