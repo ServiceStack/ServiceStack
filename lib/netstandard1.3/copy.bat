7c0bcdba4f470816f5f63ea0dd335fee43e77dab
Demis Bellot <demis.bellot@gmail.com>
2016-09-21 00:56:38 -0400

    Add copy dll script for SS.Redis .NET Standard 1.3

--- lib/netstandard1.3/copy.bat
+++ lib/netstandard1.3/copy.bat
@@ -2,4 +2,5 @@ SET BUILD=Debug
 REM SET BUILD=Release
 
 COPY ..\..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\netstandard1.3\ServiceStack.Text.* .\
+COPY ..\..\..\ServiceStack.Redis\src\ServiceStack.Redis\bin\%BUILD%\netstandard1.3\ServiceStack.Redis.* .\
 
