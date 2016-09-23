e4692576d152b5629e52565174c9e7dddb5b9666
Sergey Zhukov <svg@ngs.ru>
2016-09-23 21:46:56 +0600

    Add ServiceStack.Kestrel to publishing script (#1069)

--- build/publish-core.sh
+++ build/publish-core.sh
@@ -2,25 +2,26 @@
 
 declare -A projects=( \
  ["ServiceStack.Kestrel"]="1.6" \
- ["ServiceStack"]="1.6" \
- ["ServiceStack.Client"]="1.1 1.6" \
- ["ServiceStack.Common"]="1.3" \
- ["ServiceStack.Interfaces"]="1.1" \
+ ["ServiceStack.Core"]="1.6" \
+ ["ServiceStack.Client.Core"]="1.1 1.6" \
+ ["ServiceStack.Common.Core"]="1.3" \
+ ["ServiceStack.Interfaces.Core"]="1.1" \
 )
 
 #for each project copy files to Nuget.Core/$project/lib folder
 #and build nuget package
 for proj in "${!projects[@]}"; do
+  projname=$(basename ${proj} .Core)
   echo "$proj - ${projects[$proj]}";
-  rm -r NuGet.Core/$proj.Core/lib/*
+  rm -r NuGet.Core/$projname/lib/*
 
   for ver in ${projects[$proj]}; do
-    mkdir -p NuGet.Core/$proj.Core/lib/netstandard$ver
-    cp src/$proj/bin/Release/netstandard$ver/$proj.dll NuGet.Core/$proj.Core/lib/netstandard$ver
-    cp src/$proj/bin/Release/netstandard$ver/$proj.pdb NuGet.Core/$proj.Core/lib/netstandard$ver
-    cp src/$proj/bin/Release/netstandard$ver/$proj.deps.json NuGet.Core/$proj.Core/lib/netstandard$ver
+    mkdir -p NuGet.Core/$projname/lib/netstandard$ver
+    cp src/$projname/bin/Release/netstandard$ver/$projname.dll NuGet.Core/$proj/lib/netstandard$ver
+    cp src/$projname/bin/Release/netstandard$ver/$projname.pdb NuGet.Core/$proj/lib/netstandard$ver
+    cp src/$projname/bin/Release/netstandard$ver/$projname.deps.json NuGet.Core/$proj/lib/netstandard$ver
   done
 
-  (cd ./NuGet.Core && mono ./nuget.exe pack $proj.Core/$proj.Core.nuspec -symbols)
+  (cd ./NuGet.Core && mono ./nuget.exe pack $proj/$proj.nuspec -symbols)
 
 done
