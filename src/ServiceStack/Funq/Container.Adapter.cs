7239a23c28eaccb4a4c774293b4b3e0c52c4f0a0
Demis Bellot <demis.bellot@gmail.com>
2016-10-06 02:25:07 -0400

    Make TryResolve(Type) fast

--- src/ServiceStack/Funq/Container.Adapter.cs
+++ src/ServiceStack/Funq/Container.Adapter.cs
@@ -215,10 +215,11 @@ namespace Funq
                 do
                 {
                     snapshot = autoWireCache;
-                    newCache = new Dictionary<Type, Action<object>[]>(autoWireCache);
-                    newCache[instanceType] = setters;
+                    newCache = new Dictionary<Type, Action<object>[]>(autoWireCache) {
+                        [instanceType] = setters
+                    };
                 } while (!ReferenceEquals(
-                Interlocked.CompareExchange(ref autoWireCache, newCache, snapshot), snapshot));
+                    Interlocked.CompareExchange(ref autoWireCache, newCache, snapshot), snapshot));
             }
 
             foreach (var setter in setters)
@@ -275,16 +276,36 @@ namespace Funq
             return Expression.Call(containerParam, method);
         }
 
+        private static Dictionary<Type, Func<object>> tryResolveCache = new Dictionary<Type, Func<object>>();
+
         public object TryResolve(Type type)
         {
+            Func<object> fn;
+            if (tryResolveCache.TryGetValue(type, out fn))
+                return fn();
+
             var mi = typeof(Container).GetMethods(BindingFlags.Public | BindingFlags.Instance)
                 .First(x => x.Name == "TryResolve" &&
                        x.GetGenericArguments().Length == 1 &&
                        x.GetParameters().Length == 0);
 
             var genericMi = mi.MakeGenericMethod(type);
-            var instance = genericMi.Invoke(this, TypeConstants.EmptyObjectArray);
-            return instance;
+
+            fn = Expression.Lambda<Func<object>>(
+                    Expression.Call(Expression.Constant(this), genericMi)
+                ).Compile();
+
+            Dictionary<Type, Func<object>> snapshot, newCache;
+            do
+            {
+                snapshot = tryResolveCache;
+                newCache = new Dictionary<Type, Func<object>>(tryResolveCache) {
+                    [type] = fn
+                };
+            } while (!ReferenceEquals(
+                Interlocked.CompareExchange(ref tryResolveCache, newCache, snapshot), snapshot));
+
+            return fn();
         }
     }
 }
