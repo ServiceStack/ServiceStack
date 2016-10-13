11f36bc67b59fc48ff024838ab9ba66cee7b412e
Demis Bellot <demis.bellot@gmail.com>
2016-10-13 16:53:46 -0400

    Add MailRuAuthProvider

---
+++ src/ServiceStack.Authentication.OAuth2/MailRuAuthProvider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using ServiceStack.Auth;
+using ServiceStack.Configuration;
+using ServiceStack.Text;
+
+namespace ServiceStack.Authentication.OAuth2
+{
+    public class MailRuAuthProvider : OAuth2Provider
+    {
+        public const string Name = "MailRuAuth";
+        public const string Realm = "https://connect.mail.ru/oauth/authorize";
+
+        public MailRuAuthProvider(IAppSettings appSettings, string realm, string provider)
+            : base(appSettings, realm, provider)
+        {
+            this.AuthorizeUrl = this.AuthorizeUrl ?? Realm;
+            this.AccessTokenUrl = this.AccessTokenUrl ?? "https://connect.mail.ru/oauth/token";
+            this.UserProfileUrl = this.UserProfileUrl ?? "https://www.appsmail.ru/platform/api";
+        }
+
+        protected override Dictionary<string, string> CreateAuthInfo(string accessToken)
+        {
+            var url = this.UserProfileUrl.AddQueryParam("access_token", accessToken);
+            string json = url.GetJsonFromUrl();
+            var obj = JsonObject.Parse(json);
+
+            var authInfo = new Dictionary<string, string>
+                {
+                    { "user_id", obj["uid"] },
+                    { "username", obj["email"] },
+                    { "email", obj["email"] },
+                    { "name", obj["nick"] },
+                    { "first_name", obj["first_name"] },
+                    { "last_name", obj["last_name"] },
+                    { "gender", obj["sex"] },
+                    { "birthday", obj["birthday"] },
+                    { "link", obj["link"] },
+                    { "picture", obj["pic"] },
+                    { AuthMetadataProvider.ProfileUrlKey, obj["pic_180"] }
+                };
+
+            return authInfo;
+        }
+    }
+}
