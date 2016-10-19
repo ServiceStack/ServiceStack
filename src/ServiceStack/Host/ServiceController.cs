e8dca4084ef8801cee40926cbcda2808ba9dd7e3
xplicit <svg@ngs.ru>
2016-10-19 18:01:53 +0700

    Fix response filters tests

--- src/ServiceStack/Host/ServiceController.cs
+++ src/ServiceStack/Host/ServiceController.cs
@@ -155,8 +155,11 @@ namespace ServiceStack.Host
                           returnMarker.GetGenericArguments()[0]
                         : mi.ReturnType != typeof(object) && mi.ReturnType != typeof(void) ?
                           mi.ReturnType
+#if NETSTANDARD1_6
+                        : Type.GetType(requestType.FullName + ResponseDtoSuffix + "," + requestType.GetAssembly().GetName().Name);
+#else                                                  
                         : AssemblyUtils.FindType(requestType.FullName + ResponseDtoSuffix);
-
+#endif
                     RegisterRestPaths(requestType);
 
                     appHost.Metadata.Add(serviceType, requestType, responseType);
