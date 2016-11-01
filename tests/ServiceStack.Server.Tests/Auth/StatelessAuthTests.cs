cb5222b67a80004ffb176979952ca9aa7e2f479f
Demis Bellot <demis.bellot@gmail.com>
2016-11-01 00:35:46 -0400

    Extract and move StatelessAuthTests to Server.Tests

--- tests/ServiceStack.Server.Tests/Auth/StatelessAuthTests.cs
+++ tests/ServiceStack.Server.Tests/Auth/StatelessAuthTests.cs
@@ -10,17 +10,15 @@ using Amazon.DynamoDBv2;
 using Funq;
 using MongoDB.Driver;
 using NUnit.Framework;
-using ServiceStack;
 using ServiceStack.Auth;
 using ServiceStack.Authentication.MongoDb;
 using ServiceStack.Aws.DynamoDb;
 using ServiceStack.Configuration;
 using ServiceStack.Data;
-using ServiceStack.OrmLite;
 using ServiceStack.Redis;
 using ServiceStack.Text;
 
-namespace RazorRockstars.Console.Files
+namespace ServiceStack.Server.Tests.Auth
 {
     public class CustomUserSession : AuthUserSession
     {
@@ -132,7 +130,6 @@ namespace RazorRockstars.Console.Files
 
             return new AppHost
             {
-                EnableAuth = true,
                 Use = container => container.Register<IAuthRepository>(c => new DynamoDbAuthRepository(pocoDynamo))
             };
         }
@@ -144,7 +141,6 @@ namespace RazorRockstars.Console.Files
         {
             return new AppHost
             {
-                EnableAuth = true,
                 Use = container => container.Register<IAuthRepository>(c => new InMemoryAuthRepository())
             };
         }
@@ -162,7 +158,6 @@ namespace RazorRockstars.Console.Files
 
             return new AppHost
             {
-                EnableAuth = true,
                 Use = container => container.Register<IAuthRepository>(c => new RedisAuthRepository(redisManager))
             };
         }
@@ -178,7 +173,6 @@ namespace RazorRockstars.Console.Files
 
             return new AppHost
             {
-                EnableAuth = true,
                 Use = container => container.Register<IAuthRepository>(c => new MongoDbAuthRepository(mongoDatabase, true))
             };
         }
@@ -212,13 +206,13 @@ namespace RazorRockstars.Console.Files
         }
     }
 
+    [Explicit]
     public class OrmLiteMultitenancyStatelessAuthTests : StatelessAuthTests
     {
         protected override ServiceStackHost CreateAppHost()
         {
             return new AppHost
             {
-                EnableAuth = true,
                 Use = container => container.Register<IAuthRepository>(c =>
                     new OrmLiteAuthRepositoryMultitenancy(c.TryResolve<IDbConnectionFactory>(),
                         new[] {
@@ -339,7 +333,6 @@ namespace RazorRockstars.Console.Files
         {
             return new AppHost
             {
-                EnableAuth = true,
                 JwtRsaPrivateKey = RsaUtils.CreatePrivateKeyParams(),
                 Use = container => container.Register<IAuthRepository>(c =>
                     new OrmLiteAuthRepository(c.Resolve<IDbConnectionFactory>()))
@@ -359,7 +352,6 @@ namespace RazorRockstars.Console.Files
 
             return new AppHost
             {
-                EnableAuth = true,
                 JwtRsaPrivateKey = privateKey,
                 JwtEncryptPayload = true,
                 Use = container => container.Register<IAuthRepository>(c =>
@@ -668,7 +660,6 @@ namespace RazorRockstars.Console.Files
         {
             return new AppHost
             {
-                EnableAuth = true,
                 Use = container => container.Register<IAuthRepository>(c =>
                     new OrmLiteAuthRepository(c.Resolve<IDbConnectionFactory>()))
             };
@@ -764,10 +755,7 @@ namespace RazorRockstars.Console.Files
             };
         }
 
-        protected virtual IServiceClient GetClient()
-        {
-            return new JsonServiceClient(ListeningOn);
-        }
+        protected virtual IServiceClient GetClient() => new JsonServiceClient(ListeningOn);
 
         [Test]
         public void Does_create_multiple_ApiKeys()
@@ -1129,195 +1117,6 @@ namespace RazorRockstars.Console.Files
             Assert.That(response.Result, Is.EqualTo(request.Name));
 
             apiProvider.SessionCacheDuration = null;
-        }
-
-        [Test]
-        public void Can_not_access_Secured_Pages_without_Authentication()
-        {
-            Assert.That(ListeningOn.CombineWith("/secured").GetStringFromUrl(),
-                Is.StringContaining("<!--page:Login.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/SecuredPage").GetStringFromUrl(),
-                Is.StringContaining("<!--page:Login.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/TestSessionPage").GetStringFromUrl(),
-                Is.StringContaining("IsAuthenticated:False"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session/view").GetStringFromUrl(),
-                Is.StringContaining("IsAuthenticated:False"));
-        }
-
-        [Test]
-        public void Can_access_Secured_Pages_with_BasicAuth()
-        {
-            Assert.That(ListeningOn.CombineWith("/secured").GetStringFromUrl(
-                requestFilter: req => req.AddBasicAuth(Username, Password)),
-                Is.StringContaining("<!--view:Secured.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/SecuredPage").GetStringFromUrl(
-                requestFilter: req => req.AddBasicAuth(Username, Password)),
-                Is.StringContaining("<!--page:SecuredPage.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session").GetJsonFromUrl(
-                requestFilter: req => req.AddBasicAuth(Username, Password)),
-                Is.StringContaining("\"IsAuthenticated\":true"));
-
-            Assert.That(ListeningOn.CombineWith("/TestSessionPage").GetStringFromUrl(
-                requestFilter: req => req.AddBasicAuth(Username, Password)),
-                Is.StringContaining("IsAuthenticated:True"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session/view").GetStringFromUrl(
-                requestFilter: req => req.AddBasicAuth(Username, Password)),
-                Is.StringContaining("IsAuthenticated:True"));
-        }
-
-        [Test]
-        public void Can_access_Secured_Pages_with_JWT()
-        {
-            var client = GetClientWithUserPassword(alwaysSend: true);
-            var authResponse = client.Send(new Authenticate());
-
-            Assert.That(ListeningOn.CombineWith("/secured").GetStringFromUrl(
-                requestFilter: req => req.AddBearerToken(authResponse.BearerToken)),
-                Is.StringContaining("<!--view:Secured.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/SecuredPage").GetStringFromUrl(
-                requestFilter: req => req.AddBearerToken(authResponse.BearerToken)),
-                Is.StringContaining("<!--page:SecuredPage.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session").GetJsonFromUrl(
-                requestFilter: req => req.AddBearerToken(authResponse.BearerToken)),
-                Is.StringContaining("\"IsAuthenticated\":true"));
-
-            Assert.That(ListeningOn.CombineWith("/TestSessionPage").GetStringFromUrl(
-                requestFilter: req => req.AddBearerToken(authResponse.BearerToken)),
-                Is.StringContaining("IsAuthenticated:True"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session/view").GetStringFromUrl(
-                requestFilter: req => req.AddBearerToken(authResponse.BearerToken)),
-                Is.StringContaining("IsAuthenticated:True"));
-        }
-
-        [Test]
-        public void Can_access_Secured_Pages_with_ApiKeyAuth()
-        {
-            Assert.That(ListeningOn.CombineWith("/secured").GetStringFromUrl(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("<!--view:Secured.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/SecuredPage").GetStringFromUrl(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("<!--page:SecuredPage.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session").GetJsonFromUrl(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("\"IsAuthenticated\":true"));
-
-            Assert.That(ListeningOn.CombineWith("/TestSessionPage").GetStringFromUrl(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("IsAuthenticated:True"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session/view").GetStringFromUrl(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("IsAuthenticated:True"));
-        }
-
-        [Test]
-        public async Task Can_access_Secured_Pages_with_ApiKeyAuth_async()
-        {
-            Assert.That(await ListeningOn.CombineWith("/secured").GetStringFromUrlAsync(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("<!--view:Secured.cshtml-->"));
-
-            Assert.That(await ListeningOn.CombineWith("/SecuredPage").GetStringFromUrlAsync(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("<!--page:SecuredPage.cshtml-->"));
-
-            Assert.That(await ListeningOn.CombineWith("/test/session").GetJsonFromUrlAsync(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("\"IsAuthenticated\":true"));
-
-            Assert.That(await ListeningOn.CombineWith("/TestSessionPage").GetStringFromUrlAsync(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("IsAuthenticated:True"));
-
-            Assert.That(await ListeningOn.CombineWith("/test/session/view").GetStringFromUrlAsync(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("IsAuthenticated:True"));
-        }
-
-        [Test]
-        public void Can_access_Secured_Pages_with_ApiKeyAuth_BearerToken()
-        {
-            Assert.That(ListeningOn.CombineWith("/secured").GetStringFromUrl(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("<!--view:Secured.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/SecuredPage").GetStringFromUrl(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("<!--page:SecuredPage.cshtml-->"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session").GetJsonFromUrl(
-                requestFilter: req => req.AddApiKeyAuth(ApiKey)),
-                Is.StringContaining("\"IsAuthenticated\":true"));
-
-            Assert.That(ListeningOn.CombineWith("/TestSessionPage").GetStringFromUrl(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("IsAuthenticated:True"));
-
-            Assert.That(ListeningOn.CombineWith("/test/session/view").GetStringFromUrl(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("IsAuthenticated:True"));
-        }
-
-        [Test]
-        public async Task Can_access_Secured_Pages_with_ApiKeyAuth_BearerToken_Async()
-        {
-            Assert.That(await ListeningOn.CombineWith("/secured").GetStringFromUrlAsync(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("<!--view:Secured.cshtml-->"));
-
-            Assert.That(await ListeningOn.CombineWith("/SecuredPage").GetStringFromUrlAsync(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("<!--page:SecuredPage.cshtml-->"));
-
-            Assert.That(await ListeningOn.CombineWith("/test/session").GetJsonFromUrlAsync(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("\"IsAuthenticated\":true"));
-
-            Assert.That(await ListeningOn.CombineWith("/TestSessionPage").GetStringFromUrlAsync(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("IsAuthenticated:True"));
-
-            Assert.That(await ListeningOn.CombineWith("/test/session/view").GetStringFromUrlAsync(
-                requestFilter: req => req.AddBearerToken(ApiKey)),
-                Is.StringContaining("IsAuthenticated:True"));
-        }
-
-        [Test]
-        public void Can_access_Secured_Pages_with_CredentialsAuth()
-        {
-            var client = GetClient();
-            client.Post(new Authenticate
-            {
-                provider = "credentials",
-                UserName = Username,
-                Password = Password,
-            });
-
-            Assert.That(client.Get<string>("/secured?format=html"),
-                Is.StringContaining("<!--view:Secured.cshtml-->"));
-
-            Assert.That(client.Get<string>("/SecuredPage?format=html"),
-                Is.StringContaining("<!--page:SecuredPage.cshtml-->"));
-
-            Assert.That(client.Get(new TestSession()).IsAuthenticated);
-
-            Assert.That(client.Get<string>("/test/session"),
-                Is.StringContaining("\"IsAuthenticated\":true"));
-
-            Assert.That(client.Get<string>("/TestSessionPage"),
-                Is.StringContaining("IsAuthenticated:True"));
         }
 
         public static void AssertNoAccessToSecuredByRoleAndPermission(IServiceClient client)
