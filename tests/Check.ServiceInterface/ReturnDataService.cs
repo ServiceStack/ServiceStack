21c785f3552672fd9b92d525a3dc127e6feb69a5
Demis Bellot <demis.bellot@gmail.com>
2016-11-03 04:32:24 -0400

    Add ReturnData Service

---
+++ tests/Check.ServiceInterface/ReturnDataService.cs
@@ -0,0 +1,13 @@
+﻿using Check.ServiceModel;
+using ServiceStack;
+
+namespace Check.ServiceInterface
+{
+    public class ReturnDataService : Service
+    {
+        public object Any(ReturnString request) => request.Data;
+        public object Any(ReturnBytes request) => request.Data;
+        public object Any(ReturnStream request) => request.Data;
+        public object Any(ReturnHttpWebResponse request) => request.Data;
+    }
+}
---
+++ tests/Check.ServiceModel/ReturnData.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Net;
+using ServiceStack;
+
+namespace Check.ServiceModel
+{
+    [Route("/return/string")]
+    public class ReturnString : IReturn<string>
+    {
+        public string Data { get; set; }
+    }
+
+    [Route("/return/bytes")]
+    public class ReturnBytes : IReturn<byte[]>
+    {
+        public byte[] Data { get; set; }
+    }
+
+    [Route("/return/stream")]
+    public class ReturnStream : IReturn<Stream>
+    {
+        public byte[] Data { get; set; }
+    }
+
+    [Route("/return/httpwebresponse")]
+    public class ReturnHttpWebResponse : IReturn<HttpWebResponse>
+    {
+        public byte[] Data { get; set; }
+    }
+}
