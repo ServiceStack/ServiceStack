aaec0b268efa2ed1a891c3de65d80df0f9b82bfb
Demis Bellot <demis.bellot@gmail.com>
2016-11-19 22:16:52 -0500

    Move PgSql* + SqlServerMemoryOptimized data annotation attrs to SS.Interfaces

--- src/ServiceStack.Interfaces/DataAnnotations/CustomFieldAttribute.cs
+++ src/ServiceStack.Interfaces/DataAnnotations/CustomFieldAttribute.cs
@@ -12,4 +12,34 @@ namespace ServiceStack.DataAnnotations
             Sql = sql;
         }
     }
+
+    public class PgSqlJsonAttribute : CustomFieldAttribute
+    {
+        public PgSqlJsonAttribute() : base("json") { }
+    }
+
+    public class PgSqlJsonBAttribute : CustomFieldAttribute
+    {
+        public PgSqlJsonBAttribute() : base("jsonb") { }
+    }
+
+    public class PgSqlHStoreAttribute : CustomFieldAttribute
+    {
+        public PgSqlHStoreAttribute() : base("hstore") { }
+    }
+
+    public class PgSqlTextArrayAttribute : CustomFieldAttribute
+    {
+        public PgSqlTextArrayAttribute() : base("text[]") { }
+    }
+
+    public class PgSqlIntArrayAttribute : CustomFieldAttribute
+    {
+        public PgSqlIntArrayAttribute() : base("integer[]") { }
+    }
+
+    public class PgSqlBigIntArrayAttribute : CustomFieldAttribute
+    {
+        public PgSqlBigIntArrayAttribute() : base("bigint[]") { }
+    }
 }
---
+++ src/ServiceStack.Interfaces/DataAnnotations/SqlServerMemoryOptimizedAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ServiceStack.DataAnnotations
+{
+    // SQL 2014: https://msdn.microsoft.com/en-us/library/dn553122(v=sql.120).aspx
+    // SQL 2016: https://msdn.microsoft.com/en-us/library/dn553122(v=sql.130).aspx
+    public class SqlServerMemoryOptimizedAttribute : Attribute
+    {
+        public SqlServerMemoryOptimizedAttribute() { }
+
+        public SqlServerMemoryOptimizedAttribute(SqlServerDurability durability) { Durability = durability; }
+
+        public SqlServerDurability? Durability { get; set; }
+    }
+
+    public enum SqlServerDurability
+    {
+        SchemaOnly, // (non-durable table) recreated upon server restart, data is lost, no transaction logging and checkpoints
+        SchemaAndData  // (durable table) data persists upon server restart
+    }
+}
