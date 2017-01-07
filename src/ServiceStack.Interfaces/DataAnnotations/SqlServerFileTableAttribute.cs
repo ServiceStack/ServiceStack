97ddea58e1f6d8e6982728c14d73e196b4522f40
Demis Bellot <demis.bellot@gmail.com>
2017-01-07 10:34:04 -0500

    Move SqlServerFileTableAttribute to SS.Interfaces

---
+++ src/ServiceStack.Interfaces/DataAnnotations/SqlServerFileTableAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ServiceStack.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class SqlServerFileTableAttribute : AttributeBase
+    {
+        public SqlServerFileTableAttribute() { }
+
+        public SqlServerFileTableAttribute(string directory, string collateFileName = null)
+        {
+            FileTableDirectory = directory;
+            FileTableCollateFileName = collateFileName;
+        }
+
+        public string FileTableDirectory { get; internal set; }
+
+        public string FileTableCollateFileName { get; internal set; }
+    }
+}
