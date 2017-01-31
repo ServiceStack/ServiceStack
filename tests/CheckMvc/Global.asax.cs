85b7719b807adbadfbbdaa7a482bb2d51bf4779b
Demis Bellot <demis.bellot@gmail.com>
2017-01-31 13:30:26 -0500

    Add example of using static dep in MVC App

--- tests/CheckMvc/Global.asax.cs
+++ tests/CheckMvc/Global.asax.cs
@@ -1,9 +1,14 @@
-﻿using System.Web.Mvc;
+﻿using System.Configuration;
+using System.Data;
+using System.Web.Mvc;
 using System.Web.Routing;
 using Check.ServiceInterface;
 using Funq;
 using ServiceStack;
+using ServiceStack.Configuration;
+using ServiceStack.Data;
 using ServiceStack.Mvc;
+using ServiceStack.OrmLite;
 using ServiceStack.Redis;
 using ServiceStack.Text;
 
@@ -32,8 +37,14 @@ namespace CheckMvc
     // visit http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static IDbConnectionFactory DbFactory;
+
         protected void Application_Start()
         {
+            DbFactory = new OrmLiteConnectionFactory(
+                ConfigurationManager.AppSettings["connectionString"],
+                SqlServerDialect.Provider);
+
             JsConfig.DateHandler = DateHandler.ISO8601;
             JsConfig.EmitCamelCaseNames = true;
 
@@ -44,6 +55,18 @@ namespace CheckMvc
             RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             new AppHost().Init();
+        }
+    }
+
+    public abstract class ControllerBase : Controller
+    {
+        private IDbConnection db;
+        public IDbConnection Db => db ?? (db = MvcApplication.DbFactory.OpenDbConnection());
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            db?.Close();
         }
     }
 }
