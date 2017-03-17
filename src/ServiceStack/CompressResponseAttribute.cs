d71b6caeacad298c21bc972be6f2736878a285cd
Demis Bellot <demis.bellot@gmail.com>
2017-03-17 15:36:01 -0400

    Enable CompressResponse to compress HttpResult's containing file/Stream/byte[]/string

--- src/ServiceStack/CompressResponseAttribute.cs
+++ src/ServiceStack/CompressResponseAttribute.cs
@@ -8,23 +8,30 @@ namespace ServiceStack
         public override void Execute(IRequest req, IResponse res, object response)
         {
             var httpResult = response as IHttpResult;
-            var dto = httpResult != null ? httpResult.Response : response;
-            if (dto == null || dto is IPartialWriter || dto is IStreamWriter)
+            var src = httpResult != null ? httpResult.Response : response;
+            if (src == null)
+            {
+                var concreteResult = response as HttpResult;
+                src = concreteResult?.ResponseStream 
+                    ?? (object) concreteResult?.ResponseText;
+            }
+
+            if (src == null || src is IPartialWriter || src is IStreamWriter)
                 return;
 
             var encoding = req.GetCompressionType();
             if (encoding == null) //Client doesn't support compression
                 return;
 
-            var responseBytes = dto as byte[];
+            var responseBytes = src as byte[];
             if (responseBytes == null)
             {
-                var rawStr = dto as string;
+                var rawStr = src as string;
                 if (rawStr != null)
                     responseBytes = rawStr.ToUtf8Bytes();
                 else
                 {
-                    var stream = dto as Stream;
+                    var stream = src as Stream;
                     if (stream != null)
                         responseBytes = stream.ReadFully();
                 }
@@ -33,14 +40,14 @@ namespace ServiceStack
             if (responseBytes != null || req.ResponseContentType.IsBinary())
             {
                 if (responseBytes == null)
-                    responseBytes = HostContext.ContentTypes.SerializeToBytes(req, dto);
+                    responseBytes = HostContext.ContentTypes.SerializeToBytes(req, src);
 
                 res.AddHeader(HttpHeaders.ContentEncoding, encoding);
                 responseBytes = responseBytes.CompressBytes(encoding);
             }
             else
             {
-                var serializedDto = req.SerializeToString(dto);
+                var serializedDto = req.SerializeToString(src);
                 if (req.ResponseContentType.MatchesContentType(MimeTypes.Json))
                 {
                     var jsonp = req.GetJsonpCallback();
