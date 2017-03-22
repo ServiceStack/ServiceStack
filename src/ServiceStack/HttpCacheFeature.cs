e045e595d4fd269215d5d01d21adc97b107b4ca4
Demis Bellot <demis.bellot@gmail.com>
2017-03-22 14:46:16 -0400

    fix NotModifiedSince

--- src/ServiceStack/HttpCacheFeature.cs
+++ src/ServiceStack/HttpCacheFeature.cs
@@ -244,7 +244,7 @@ namespace ServiceStack
                 {
                     DateTime modifiedSinceDate;
                     if (DateTime.TryParse(ifModifiedSince, new DateTimeFormatInfo(), DateTimeStyles.RoundtripKind, out modifiedSinceDate))
-                        return modifiedSinceDate <= lastModified.Value.ToUniversalTime();
+                        return modifiedSinceDate >= lastModified.Value.ToUniversalTime();
                 }
             }
 
