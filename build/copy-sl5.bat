7f662b6636ddf04001acc1b21f71e0b56daf9d4c
Demis Bellot <demis.bellot@gmail.com>
2017-05-29 02:21:45 -0400

    Update sl5 .dlls

--- build/copy-sl5.bat
+++ build/copy-sl5.bat
@@ -4,4 +4,4 @@ REM SET BUILD=Debug
 SET BUILD=Release
 
 COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\Sl5\ServiceStack.Text.* ..\lib\sl5
-COPY ..\src\ServiceStack.Client\bin\SL5\ServiceStack.Client.* ..\lib\sl5
+COPY ..\src\ServiceStack.Client\bin\Release\sl5\ServiceStack.Client.* ..\lib\sl5
