2103b2a059bf969dddabf42b7486c5804b9adfd7
Demis Bellot <demis.bellot@gmail.com>
2017-05-29 01:02:32 -0400

    Update .Signed .dlls + .csprojs

--- build/copy-sn.bat
+++ build/copy-sn.bat
@@ -1,22 +1,22 @@
 SET BUILD=Signed
 
-COPY ..\src\ServiceStack.Common\bin\%BUILD%\ServiceStack.Common.dll ..\lib\signed
-COPY ..\src\ServiceStack.Common\bin\%BUILD%\ServiceStack.Common.dll ..\..\ServiceStack.Redis\lib\signed
-COPY ..\src\ServiceStack.Common\bin\%BUILD%\ServiceStack.Common.dll ..\..\ServiceStack.OrmLite\lib\signed
-COPY ..\src\ServiceStack.Common\bin\%BUILD%\ServiceStack.Common.dll ..\..\ServiceStack.Aws\lib\signed
+COPY ..\src\ServiceStack.Common\bin\%BUILD%\net45\ServiceStack.Common.dll ..\lib\signed
+COPY ..\src\ServiceStack.Common\bin\%BUILD%\net45\ServiceStack.Common.dll ..\..\ServiceStack.Redis\lib\signed
+COPY ..\src\ServiceStack.Common\bin\%BUILD%\net45\ServiceStack.Common.dll ..\..\ServiceStack.OrmLite\lib\signed
+COPY ..\src\ServiceStack.Common\bin\%BUILD%\net45\ServiceStack.Common.dll ..\..\ServiceStack.Aws\lib\signed
 
-COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\ServiceStack.Text.dll ..\lib\signed
-COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\ServiceStack.Text.dll ..\..\ServiceStack.Redis\lib\signed
-COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\ServiceStack.Text.dll ..\..\ServiceStack.OrmLite\lib\signed
-COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\ServiceStack.Text.dll ..\..\ServiceStack.Aws\lib\signed
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.dll ..\lib\signed
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.dll ..\..\ServiceStack.Redis\lib\signed
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.dll ..\..\ServiceStack.OrmLite\lib\signed
+COPY ..\..\ServiceStack.Text\src\ServiceStack.Text\bin\%BUILD%\net45\ServiceStack.Text.dll ..\..\ServiceStack.Aws\lib\signed
 
-COPY ..\src\ServiceStack.Client\bin\%BUILD%\ServiceStack.Client.dll ..\lib\signed
+COPY ..\src\ServiceStack.Client\bin\%BUILD%\net45\ServiceStack.Client.dll ..\lib\signed
 
-COPY ..\src\ServiceStack\bin\%BUILD%\ServiceStack.dll ..\lib\signed
+COPY ..\src\ServiceStack\bin\%BUILD%\net45\ServiceStack.dll ..\lib\signed
 
-COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite\bin\%BUILD%\ServiceStack.OrmLite.dll ..\lib\signed
-COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite\bin\%BUILD%\ServiceStack.OrmLite.dll ..\..\ServiceStack.OrmLite\lib\signed
+COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite\bin\%BUILD%\net45\ServiceStack.OrmLite.dll ..\lib\signed
+COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite\bin\%BUILD%\net45\ServiceStack.OrmLite.dll ..\..\ServiceStack.OrmLite\lib\signed
 
-COPY ..\..\ServiceStack.Redis\src\ServiceStack.Redis\bin\%BUILD%\ServiceStack.Redis.dll ..\lib\signed
-COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\ServiceStack.OrmLite.SqlServer.dll ..\lib\signed
+COPY ..\..\ServiceStack.Redis\src\ServiceStack.Redis\bin\%BUILD%\net45\ServiceStack.Redis.dll ..\lib\signed
+COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\net45\ServiceStack.OrmLite.SqlServer.dll ..\lib\signed
 
