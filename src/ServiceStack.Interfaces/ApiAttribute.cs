046204c430bfbd565908fd205a72de5052304d39
Sergey Zhukov <svg@ngs.ru>
2017-06-15 15:10:20 +0700

    Add GenerateBodyDtoParam for ApiAttribute

--- src/ServiceStack.Interfaces/ApiAttribute.cs
+++ src/ServiceStack.Interfaces/ApiAttribute.cs
@@ -13,6 +13,19 @@ namespace ServiceStack
         /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Create or not body param for request type when verb is POST or PUT. 
+        /// If `true` body param is always generated.
+        /// If `false` body param is never generated
+        /// If `null` (default) body param is generated only if `DisableAutoDtoInBodyParam = false`
+        /// </summary>
+        public bool? GenerateBodyDtoParam { get; set; }
+
+        /// <summary>
+        /// Tells if body param is required
+        /// </summary>
+        public bool? IsRequired { get; set; }
+
         public ApiAttribute(string description)
         {
             Description = description;
