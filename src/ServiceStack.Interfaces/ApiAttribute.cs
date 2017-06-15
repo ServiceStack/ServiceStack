693b63da2503817a620d7e738c8d486f12ca5353
Sergey Zhukov <svg@ngs.ru>
2017-06-15 17:48:03 +0700

    Change types of properties in ApiAttribute

--- src/ServiceStack.Interfaces/ApiAttribute.cs
+++ src/ServiceStack.Interfaces/ApiAttribute.cs
@@ -5,6 +5,22 @@ using System;
 
 namespace ServiceStack
 {
+    public static class GenerateBodyParam
+    {
+        /// <summary>
+        /// Generates body DTO parameter only if `DisableAutoDtoInBodyParam = false`
+        /// </summary>
+        public const int IfNotDisabled = 0;
+        /// <summary>
+        /// Always generate body DTO for request
+        /// </summary>
+        public const int Always = 1;
+        /// <summary>
+        /// Never generate body DTO for request
+        /// </summary>
+        public const int Never = 2;
+    }
+
     [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
     public class ApiAttribute : AttributeBase
     {
@@ -14,23 +30,28 @@ namespace ServiceStack
         public string Description { get; set; }
 
         /// <summary>
-        /// Create or not body param for request type when verb is POST or PUT. 
-        /// If `true` body param is always generated.
-        /// If `false` body param is never generated
-        /// If `null` (default) body param is generated only if `DisableAutoDtoInBodyParam = false`
+        /// Create or not body param for request type when verb is POST or PUT.
+        /// Value can be one of the constants of `GenerateBodyParam` class:
+        /// `GenerateBodyParam.IfNotDisabled` (default value), `GenerateBodyParam.Always`, `GenerateBodyParam.Never`
         /// </summary>
-        public bool? GenerateBodyDtoParam { get; set; }
+        public int BodyParam { get; set; }
 
         /// <summary>
         /// Tells if body param is required
         /// </summary>
-        public bool? IsRequired { get; set; }
-
-        public ApiAttribute(string description)
-        {
-            Description = description;
-        }
+        public bool IsRequired { get; set; }
 
         public ApiAttribute() { }
+
+        public ApiAttribute(string description) : this(description, GenerateBodyParam.IfNotDisabled) { }
+
+        public ApiAttribute(string description, int generateBodyParam) : this(description, generateBodyParam, false) {}
+
+        public ApiAttribute(string description, int generateBodyParam, bool isRequired)
+        {
+            Description = description;
+            BodyParam = generateBodyParam;
+            IsRequired = isRequired;
+        }
     }
 }
