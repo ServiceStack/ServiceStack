a405f20f973ec59465fdf45e81cbc2b2db99b840
Demis Bellot <demis.bellot@gmail.com>
2017-07-04 05:35:33 -0400

    Add IStreamWriterAsync

--- src/ServiceStack.Interfaces/Web/IStreamWriter.cs
+++ src/ServiceStack.Interfaces/Web/IStreamWriter.cs
@@ -1,9 +1,16 @@
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace ServiceStack.Web
 {
     public interface IStreamWriter
     {
         void WriteTo(Stream responseStream);
+    }
+
+    public interface IStreamWriterAsync
+    {
+        Task WriteToAsync(Stream responseStream, CancellationToken token = default(CancellationToken));
     }
 }
