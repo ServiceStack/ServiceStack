a3ea35f664ece30a4e7ee690eb7c6518d76ba1c5
Demis Bellot <demis.bellot@gmail.com>
2017-07-16 03:12:12 -0400

    Add initial support for linq examples

---
+++ tests/ServiceStack.WebHost.Endpoints.Tests/TemplateTests/TemplateQueryFilterTests.cs
@@ -0,0 +1,85 @@
+﻿using NUnit.Framework;
+using ServiceStack.Templates;
+
+namespace ServiceStack.WebHost.Endpoints.Tests.TemplateTests
+{
+    public class TemplateQueryFilterTests
+    {
+        private static TemplatePagesContext CreateContext()
+        {
+            return new TemplatePagesContext
+            {
+                Args =
+                {
+                    ["numbers"] = new[] { 5, 4, 1, 3, 9, 8, 6, 7, 2, 0 },
+                    ["products"] = TemplateQueryData.Products,
+                    ["customers"] = TemplateQueryData.Customers,
+                }
+            }.Init();
+        }
+
+        [Test]
+        public void linq1()
+        {
+            var context = CreateContext();
+            
+            Assert.That(context.EvaluateTemplate(@"
+Numbers < 5:
+{{ numbers | where('it < 5') | select('{{ it }}\n') }}").NormalizeNewLines(), 
+                
+                Is.EqualTo(@"
+Numbers < 5:
+4
+1
+3
+2
+0
+".NormalizeNewLines()));
+        }
+
+        [Test]
+        public void linq2()
+        {
+            var context = CreateContext();
+            
+            Assert.That(context.EvaluateTemplate(@"
+Sold out products:
+{{ products 
+   | where('it.UnitsInStock = 0') 
+   | select('{{ it.productName | raw }} is sold out!\n')
+}}
+").NormalizeNewLines(),
+                
+                Is.EqualTo(@"
+Sold out products:
+Chef Anton's Gumbo Mix is sold out!
+Alice Mutton is sold out!
+Thüringer Rostbratwurst is sold out!
+Gorgonzola Telino is sold out!
+Perth Pasties is sold out!
+".NormalizeNewLines()));
+        }
+
+        [Test]
+        public void linq3()
+        {
+            var context = CreateContext();
+            
+            Assert.That(context.EvaluateTemplate(@"
+In-stock products that cost more than 3.00:
+{{ products 
+   | where('it.UnitsInStock > 0 and it.UnitPrice > 3') 
+   | select('{{ it.productName | raw }} is in stock and costs more than 3.00.\n') 
+}}
+").NormalizeNewLines(),
+                
+                Does.StartWith(@"
+In-stock products that cost more than 3.00:
+Chai is in stock and costs more than 3.00.
+Chang is in stock and costs more than 3.00.
+Aniseed Syrup is in stock and costs more than 3.00.
+".NormalizeNewLines()));
+        }
+
+    }
+}
