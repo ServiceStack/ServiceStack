6c02036cb3b04a5098ce1bd70d2dbb3de65bd173
Demis Bellot <demis.bellot@gmail.com>
2017-07-30 16:44:20 -0400

    clean up MyFilters example

--- tests/ServiceStack.WebHost.Endpoints.Tests/TemplateTests/TemplatePageRawTests.cs
+++ tests/ServiceStack.WebHost.Endpoints.Tests/TemplateTests/TemplatePageRawTests.cs
@@ -242,11 +242,12 @@ Brackets in Layout < & >
         class MyFilter : TemplateFilter
         {
             public string echo(string text) => $"{text} {text}";
+            public double squared(double value) => value * value;
             public string greetArg(string key) => $"Hello {Context.Args[key]}";
         }
 
         [Test]
-        public async Task Does_use_custom_filter()
+        public void Does_use_custom_filter()
         {
             var context = new TemplateContext
             {
@@ -256,19 +257,23 @@ Brackets in Layout < & >
                 },                
             }.Init();
             
-            context.VirtualFiles.WriteFile("page.html", "<h1>{{ 'hello' | echo }}</h1>");
-            var result = await new PageResult(context.GetPage("page"))
+            var output = new PageResult(context.OneTimePage("<h1>{{ 'hello' | echo }}</h1>"))
             {
                 TemplateFilters = { new MyFilter() }
-            }.RenderToStringAsync();
-            Assert.That(result, Is.EqualTo("<h1>hello hello</h1>"));
+            }.Result;
+            Assert.That(output, Is.EqualTo("<h1>hello hello</h1>"));
 
-            context.VirtualFiles.WriteFile("page-greet.html", "<h1>{{ 'contextArg' | greetArg }}</h1>");
-            result = await new PageResult(context.GetPage("page-greet"))
+            output = new PageResult(context.OneTimePage("<h1>{{ 10 | squared }}</h1>"))
             {
-                TemplateFilters = { new MyFilter() }
-            }.RenderToStringAsync();
-            Assert.That(result, Is.EqualTo("<h1>Hello foo</h1>"));
+                TemplateFilters = {new MyFilter()}
+            }.Result;
+            Assert.That(output, Is.EqualTo("<h1>100</h1>"));
+
+            output = new PageResult(context.OneTimePage("<h1>{{ 'contextArg' | greetArg }}</h1>"))
+            {
+                TemplateFilters = {new MyFilter()}
+            }.Result;
+            Assert.That(output, Is.EqualTo("<h1>Hello foo</h1>"));
         }
 
         [Test]
