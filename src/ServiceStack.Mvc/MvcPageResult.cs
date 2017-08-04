48e9856dfa4e5498ccd91b52e4d29deeabae01b7
Demis Bellot <demis.bellot@gmail.com>
2017-08-04 00:14:44 -0400

    Add MvcPageResult

---
+++ src/ServiceStack.Mvc/MvcPageResult.cs
@@ -0,0 +1,52 @@
+﻿using System.Threading.Tasks;
+using ServiceStack.Templates;
+
+namespace ServiceStack.Mvc
+{
+#if !NETSTANDARD1_6
+    public class MvcPageResult : System.Web.Mvc.ActionResult
+    {
+        readonly PageResult pageResult;
+
+        public MvcPageResult(PageResult pageResult) => this.pageResult = pageResult;
+
+        public override async void ExecuteResult(System.Web.Mvc.ControllerContext context)
+        {
+            foreach (var entry in pageResult.Options)
+            {
+                if (entry.Key == HttpHeaders.ContentType)
+                    context.HttpContext.Response.ContentType = entry.Value;
+                else
+                    context.HttpContext.Response.Headers[entry.Key] = entry.Value;
+            }
+            
+            await pageResult.WriteToAsync(context.HttpContext.Response.OutputStream);
+        }
+    }
+#else
+    public class MvcPageResult : Microsoft.AspNetCore.Mvc.ActionResult
+    {
+        readonly PageResult pageResult;
+
+        public MvcPageResult(PageResult pageResult) => this.pageResult = pageResult;
+
+        public override async Task ExecuteResultAsync(Microsoft.AspNetCore.Mvc.ActionContext context)
+        {
+            foreach (var entry in pageResult.Options)
+            {
+                if (entry.Key == HttpHeaders.ContentType)
+                    context.HttpContext.Response.ContentType = entry.Value;
+                else
+                    context.HttpContext.Response.Headers[entry.Key] = entry.Value;
+            }
+
+            await pageResult.WriteToAsync(context.HttpContext.Response.Body);
+        }
+    }
+#endif    
+    
+    public static class MvcPageResultExtensions
+    {
+        public static MvcPageResult ToMvcResult(this PageResult pageResult) => new MvcPageResult(pageResult);        
+    }
+}
