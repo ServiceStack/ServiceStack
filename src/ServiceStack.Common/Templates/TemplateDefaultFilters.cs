8ec05841475839cb8014218fce8f382ab00b48f9
Demis Bellot <demis.bellot@gmail.com>
2017-08-13 06:06:55 -0400

    fix typo

--- src/ServiceStack.Common/Templates/TemplateDefaultFilters.cs
+++ src/ServiceStack.Common/Templates/TemplateDefaultFilters.cs
@@ -209,7 +209,7 @@ namespace ServiceStack.Templates
 
         public string urlEncode(string value, bool upperCase) => value.UrlEncode(upperCase);
         public string urlEncode(string value) => value.UrlEncode();
-        public string urlDencode(string value) => value.UrlDecode();
+        public string urlDecode(string value) => value.UrlDecode();
 
         public Dictionary<string, string> parseKeyValueText(string target) => target?.ParseKeyValueText();
         public Dictionary<string, string> parseKeyValueText(string target, string delimiter) => target?.ParseKeyValueText(delimiter);
