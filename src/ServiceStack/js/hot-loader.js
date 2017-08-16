b42b7b591aca3ca631342072d1acc9f38f8c4f3a
Demis Bellot <demis.bellot@gmail.com>
2017-08-16 09:20:19 -0400

    add support for preserving scroll position in hot reload

--- src/ServiceStack/js/hot-loader.js
+++ src/ServiceStack/js/hot-loader.js
@@ -1,21 +1,40 @@
-var _lastEtag = "";
-(function shouldReload(){
-    fetch("/templates/hotreload/page.json?path=" + encodeURIComponent(location.pathname) + "&eTag=" + _lastEtag)
-    .then(function(res){ 
-        if (res.status !== 200) {
-            console.log("/reload/page failed: " + res.status)
-            if (res.status != 404 && res.status != 405) setTimeout(shouldReload, 1000)
-        } else {
-            res.json().then(function(r){
-                if (!_lastEtag) {
-                    _lastEtag = r.eTag
-                } else if (r.reload) {
-                    console.log("page modified, reloading...")
-                    location.href = location.href
-                }
-                setTimeout(shouldReload, 1000)
-            })
+var __lastEtag = "";
+(function (qs) {
+    qs = qs.split('+').join(' ')
+    var params = {}, tokens, re = /[?&]?([^=]+)=([^&]*)/g
+    while (tokens = re.exec(qs)) {
+        if (tokens[1] === "scroll") {
+            setTimeout(function(){ window.scrollTo(0, tokens[2]); }, 1)
+            return
         }
-    })
-    .catch(function(err){ console.log('fetch() error: ', err) })
+    }
+})(document.location.search);
+(function shouldReload(){
+    function _replace(uri, key, value, strip) {
+        var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i")
+        return uri.match(re) ?
+            uri.replace(re, strip ? '' : ('$1' + key + "=" + value + '$2')) :
+            strip ? url : uri + (uri.indexOf('?') !== -1 ? "&" : "?") + key + "=" + value
+    }
+    fetch("/templates/hotreload/page.json?path=" + encodeURIComponent(location.pathname) + "&eTag=" + __lastEtag)
+        .then(function(res){
+            if (res.status !== 200) {
+                console.log("hotreload failed: " + res.status)
+                if (res.status != 404 && res.status != 405) setTimeout(shouldReload, 1000)
+            } else {
+                res.json().then(function(r){
+                    if (!__lastEtag) {
+                        __lastEtag = r.eTag
+                    } else if (r.reload) {
+                        var scroll = (document.documentElement.scrollTop || document.body.scrollTop);
+                        if (location.href.indexOf('#') >= 0)
+                            location.reload();
+                        else
+                            location.href = _replace(location.href,'scroll', scroll, scroll === 0)
+                    }
+                    setTimeout(shouldReload, 1000)
+                })
+            }
+        })
+        .catch(function(err){ console.log('hotreload error: ', err) })
 })();
