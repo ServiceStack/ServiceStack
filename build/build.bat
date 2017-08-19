18501f238cc9bdfcff18907895ba79f50d4bb93f
Demis Bellot <demis.bellot@gmail.com>
2017-08-19 18:05:00 -0400

    Update dlls

--- build/build.bat
+++ build/build.bat
@@ -3,7 +3,9 @@ SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBui
 REM %MSBUILD% build.proj /target:NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
 REM %MSBUILD% build-sn.proj /target:NuGetPack /property:Configuration=Signed;RELEASE=true;PatchVersion=0
 REM %MSBUILD% build-pcl.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;RELEASE=true;PatchVersion=0;PatchCoreVersion=0
-%MSBUILD% build-deps.proj /target:Build /property:Configuration=Release;RELEASE=true;PackageVersion=4.5.0
+REM %MSBUILD% build-deps.proj /target:Build /property:Configuration=Release;RELEASE=true;PackageVersion=4.5.0
 
 REM Debug Task
 REM C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild.exe /t:rebuild /debug C:\src\ServiceStack\tests\RazorRockstars.BuildTask\RazorRockstars.BuildTask.csproj
+
+msbuild /p:Configuration=Release ..\src\ServiceStack.sln
