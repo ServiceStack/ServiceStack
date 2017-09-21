5d00b939ccc983e153d879af77a8d10aac60f830
Demis Bellot <demis.bellot@gmail.com>
2017-09-21 14:06:35 -0400

    update copy.bat

--- lib/copy.bat
+++ lib/copy.bat
@@ -38,7 +38,7 @@ COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite\bin\%BUILD%\netstandard
 COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite\bin\Signed\net45\ServiceStack.OrmLite.* signed
 
 COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\net45\ServiceStack.OrmLite.Sqlite.* net45
-COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\netstandard1.3\ServiceStack.OrmLite.Sqlite.* netstandard1.3
+COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite.Sqlite\bin\%BUILD%\netstandard2.0\ServiceStack.OrmLite.Sqlite.* netstandard2.0
 
 COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\net45\ServiceStack.OrmLite.SqlServer.* net45
 COPY ..\..\ServiceStack.OrmLite\src\ServiceStack.OrmLite.SqlServer\bin\%BUILD%\netstandard1.3\ServiceStack.OrmLite.SqlServer.* netstandard1.3
