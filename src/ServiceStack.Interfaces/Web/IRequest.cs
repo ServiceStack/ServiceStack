fbf13591e71112a84916d34dfbada15ece12e21d
sundarchi1 <31782486+sundarchi1@users.noreply.github.com>
2017-12-11 21:08:55 -0500

    Quick docs fix

--- src/ServiceStack.Interfaces/Web/IRequest.cs
+++ src/ServiceStack.Interfaces/Web/IRequest.cs
@@ -85,17 +85,17 @@ namespace ServiceStack.Web
         Dictionary<string, object> Items { get; }
 
         /// <summary>
-        /// The HTTP Headers in an INameValueCollection
+        /// The HTTP Headers in an NameValueCollection
         /// </summary>
         NameValueCollection Headers { get; }
 
         /// <summary>
-        /// The ?query=string in an INameValueCollection
+        /// The ?query=string in an NameValueCollection
         /// </summary>
         NameValueCollection QueryString { get; }
 
         /// <summary>
-        /// The HTTP POST'ed Form Data in an INameValueCollection
+        /// The HTTP POST'ed Form Data in an NameValueCollection
         /// </summary>
         NameValueCollection FormData { get; }
         /// <summary>
