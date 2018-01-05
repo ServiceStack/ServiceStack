37577ae380acf077537a9bf66980f393da1334e7
Demis Bellot <demis.bellot@gmail.com>
2018-01-05 10:55:34 -0500

    remove unused namespace

--- src/ServiceStack.Razor/Compilation/CompilerServices.cs
+++ src/ServiceStack.Razor/Compilation/CompilerServices.cs
@@ -4,7 +4,6 @@ using System.Dynamic;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
-using ServiceStack.OrmLite;
 
 namespace ServiceStack.Razor.Compilation
 {
