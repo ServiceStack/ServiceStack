75b0d1812dd44bf482fe769c247d56fa445ec32b
Luis Madaleno <madaleno@magnisoft.com>
2018-02-22 09:37:54 +0000

    Removed unused Return attributes
    Removed unused Return attributes

--- src/ServiceStack.Interfaces/DataAnnotations/ReturnAttribute.cs
+++ src/ServiceStack.Interfaces/DataAnnotations/ReturnAttribute.cs
@@ -7,20 +7,8 @@ namespace ServiceStack.DataAnnotations
     /// <summary>
     /// ReturnAttribute
     /// Use to indicate that a property should be included in the 
-    /// returning/output clause of INSERT and UPDATE sql sentences
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
-    public class ReturnAttribute : AttributeBase {}
-
-    /// <summary>
-    /// Return this property in INSERT statements
+    /// returning/output clause of INSERT sql sentences
     /// </summary>
     [AttributeUsage(AttributeTargets.Property)]
     public class ReturnOnInsertAttribute : AttributeBase { }
-
-    /// <summary>
-    /// Return this property in UPDATE statements
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Property)]
-    public class ReturnOnUpdateAttribute : AttributeBase { }
 }
