9c93ef2ec36de2a6eec196b620c58aa0da9bcba1
Demis Bellot <demis.bellot@gmail.com>
2018-04-27 08:09:43 -0400

    Add new CheckWebCore project

---
+++ tests/CheckWebCore/Startup.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Funq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceStack;
+using ServiceStack.Templates;
+using ServiceStack.Auth;
+using ServiceStack.Caching;
+using ServiceStack.DataAnnotations;
+
+namespace CheckWebCore
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services) { }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseServiceStack(new AppHost
+            {
+                AppSettings = new NetCoreAppSettings(Configuration)
+            });
+        }
+    }
+    
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("TestLogin", typeof(MyServices).Assembly) { }
+
+
+        // http://localhost:5000/auth/credentials?username=testman@test.com&&password=!Abc1234
+        // Configure your AppHost with the necessary configuration and dependencies your App needs
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new TemplatePagesFeature()); // enable server-side rendering, see: http://templates.servicestack.net
+
+            SetConfig(new HostConfig
+            {
+                AddRedirectParamsToQueryString = true,
+                DebugMode = AppSettings.Get(nameof(HostConfig.DebugMode), false)
+            });
+
+            Plugins.Add(new AuthFeature(() => new AuthUserSession(),
+                new IAuthProvider[] {
+                    new BasicAuthProvider(), //Sign-in with HTTP Basic Auth
+                    new CredentialsAuthProvider(), //HTML Form post of UserName/Password credentials
+                }));
+
+            Plugins.Add(new RegistrationFeature());
+
+            container.Register<ICacheClient>(new MemoryCacheClient());
+            var userRep = new InMemoryAuthRepository();
+            container.Register<IAuthRepository>(userRep);
+
+            var authRepo = userRep;
+
+            var newAdmin = new UserAuth {Email = "testman@test.com"};
+            var user = authRepo.CreateUserAuth(newAdmin, "!Abc1234");
+            authRepo.AssignRoles(user, new List<string> {"Admin"});
+        }
+    }
+    
+    [Exclude(Feature.Metadata)]
+    [FallbackRoute("/{PathInfo*}", Matches="AcceptsHtml")]
+    public class FallbackForClientRoutes
+    {
+        public string PathInfo { get; set; }
+    }
+    
+    [Route("/hello")]
+    [Route("/hello/{Name}")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+
+    [Authenticate]
+    public class MyServices : Service
+    {
+        //Return index.html for unmatched requests so routing is handled on client
+        public object Any(FallbackForClientRoutes request) => 
+            Request.GetPageResult("/");
+//            new PageResult(Request.GetPage("/")) { Args = { [nameof(Request)] = Request } };
+
+        public object Any(Hello request)
+        {
+            return new HelloResponse { Result = $"Hello, {request.Name}!" };
+        }
+    }
+}
