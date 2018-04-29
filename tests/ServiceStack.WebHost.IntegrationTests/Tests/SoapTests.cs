d1c9fae07f589920aeb54b72e37f527572e86437
Demis Bellot <demis.bellot@gmail.com>
2018-04-28 23:24:21 -0400

    Add SoapTest

--- tests/ServiceStack.WebHost.IntegrationTests/Tests/SoapTests.cs
+++ tests/ServiceStack.WebHost.IntegrationTests/Tests/SoapTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Serialization;
 using NUnit.Framework;
 using ServiceStack.Text;
 using ServiceStack.WebHost.IntegrationTests.Services;
@@ -52,5 +54,83 @@ xmlns:a=""http://www.w3.org/2005/08/addressing"">
 
             Assert.That(soapXml, Does.StartWith(@"<?xml version=""1.0"" encoding=""utf-8""?><s:Envelope"));
         }
+
+        [Test]
+        public void Can_send_SoapRequest()
+        {
+            var client = new Soap12ServiceClient(Config.ServiceStackBaseUri);
+//            var client = new Soap12ServiceClient("http://test.servicestack.net");
+            
+            var request = new GetStuff {
+                SummaryDate = DateTime.Parse("2018-04-26"),
+                SummaryEndDate = DateTime.Parse("2018-04-26"),
+                Symbol = "TOU",
+                Email = "Guy.Smiley@myCompany.net",
+                IsEnabled = true,
+            };
+
+            var response = client.Send(request);
+            
+            Assert.That(response.SummaryDate, Is.EqualTo(request.SummaryDate));
+            Assert.That(response.SummaryEndDate, Is.EqualTo(request.SummaryEndDate));
+            Assert.That(response.Symbol, Is.EqualTo(request.Symbol));
+            Assert.That(response.Email, Is.EqualTo(request.Email));
+            Assert.That(response.IsEnabled, Is.EqualTo(request.IsEnabled));
+        }
+    }
+    
+    [DataContract(Namespace = "http://schemas.servicestack.net/types")]
+    [Route("/Stuff")]
+    public class GetStuff : IReturn<GetStuffResponse>
+    {
+        [DataMember]
+        [ApiMember(Name = "Summary Date",
+            DataType = "DateTime",
+            IsRequired = false)]
+        public DateTime? SummaryDate { get; set; }
+
+        [DataMember]
+        [ApiMember(Name = "Summary End Date",
+            DataType = "DateTime",
+            IsRequired = false)]
+        public DateTime? SummaryEndDate { get; set; }
+
+        [DataMember]
+        [ApiMember(Name = "Symbol",
+            DataType = "string",
+            IsRequired = false)]
+        public string Symbol { get; set; }
+
+        [DataMember]
+        [ApiMember(Name = "Email",
+            DataType = "string",
+            IsRequired = false)]
+        public string Email { get; set; }
+
+        [DataMember]
+        [ApiMember(Name = "Is Enabled",
+            DataType = "bool",
+            IsRequired = false)]
+        public bool? IsEnabled { get; set; }
+    }
+
+    [DataContract(Namespace = "http://schemas.servicestack.net/types")]
+    public class GetStuffResponse
+    {
+        [DataMember]
+        public DateTime? SummaryDate { get; set; }
+        [DataMember]
+        public DateTime? SummaryEndDate { get; set; }
+        [DataMember]
+        public string Symbol { get; set; }
+        [DataMember]
+        public string Email { get; set; }
+        [DataMember]
+        public bool? IsEnabled { get; set; }
+    }
+
+    public class SoapTestServices : Service
+    {
+        public object Any(GetStuff request) => request.ConvertTo<GetStuffResponse>();
     }
 }
