4aa07bb88815a64e1a055da24dc04820804b2967
Demis Bellot <demis.bellot@gmail.com>
2018-07-01 17:29:05 -0400

    Fix build error

--- src/ServiceStack/Host/NetCore/NetCoreResponse.cs
+++ src/ServiceStack/Host/NetCore/NetCoreResponse.cs
@@ -9,6 +9,7 @@ using System.Net;
 
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Primitives;
 using ServiceStack.Logging;
 using System.Threading;
 using System.Threading.Tasks;
