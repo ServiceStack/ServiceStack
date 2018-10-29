cd9c9dd3b843e17ab452cc632dab1981dbe5122b
Demis Bellot <demis.bellot@gmail.com>
2018-10-29 09:13:01 -0400

    tweak ValidationFilters

--- src/ServiceStack/Validation/ValidationFilters.cs
+++ src/ServiceStack/Validation/ValidationFilters.cs
@@ -27,67 +27,46 @@ namespace ServiceStack.Validation
             if (validator == null)
                 return;
 
-            try
+            using (validator as IDisposable)
             {
-                var validationResult = await Validate(validator, req, requestDto);
-
-                if (treatInfoAndWarningsAsErrors && validationResult.IsValid)
+                try
                 {
-                    return;
-                }
-
-                if (!treatInfoAndWarningsAsErrors &&
-                    (validationResult.IsValid || validationResult.Errors.All(v => v.Severity != Severity.Error)))
-                {
-                    return;
-                }
-
-                var errorResponse =
-                    await HostContext.RaiseServiceException(req, requestDto, validationResult.ToException())
-                    ?? DtoUtils.CreateErrorResponse(requestDto, validationResult.ToErrorResult());
-
-                var autoBatchIndex = req.GetItem("AutoBatchIndex")?.ToString();
-                if (autoBatchIndex != null)
-                {
-                    var responseStatus = errorResponse.GetResponseStatus();
-                    if (responseStatus != null)
+                    var validationResult = await Validate(validator, req, requestDto);
+    
+                    if (treatInfoAndWarningsAsErrors && validationResult.IsValid)
+                        return;
+    
+                    if (!treatInfoAndWarningsAsErrors &&
+                        (validationResult.IsValid || validationResult.Errors.All(v => v.Severity != Severity.Error)))
+                        return;
+    
+                    var errorResponse =
+                        await HostContext.RaiseServiceException(req, requestDto, validationResult.ToException())
+                        ?? DtoUtils.CreateErrorResponse(requestDto, validationResult.ToErrorResult());
+    
+                    var validationFeature = HostContext.GetPlugin<ValidationFeature>();
+                    if (validationFeature?.ErrorResponseFilter != null)
                     {
-                        if (responseStatus.Meta == null)
-                            responseStatus.Meta = new Dictionary<string, string>();
-
-                        responseStatus.Meta["AutoBatchIndex"] = autoBatchIndex;
+                        errorResponse = validationFeature.ErrorResponseFilter(validationResult, errorResponse);
                     }
+    
+                    await res.WriteToResponse(req, errorResponse);
                 }
-
-                var validationFeature = HostContext.GetPlugin<ValidationFeature>();
-                if (validationFeature?.ErrorResponseFilter != null)
+                catch (Exception ex)
                 {
-                    errorResponse = validationFeature.ErrorResponseFilter(validationResult, errorResponse);
-                }
-
-                await res.WriteToResponse(req, errorResponse);
-            }
-            catch (Exception ex)
-            {
-                var validationEx = ex.UnwrapIfSingleException();
-
-                var errorResponse = await HostContext.RaiseServiceException(req, requestDto, validationEx)
-                                    ?? DtoUtils.CreateErrorResponse(requestDto, validationEx);
-
-                await res.WriteToResponse(req, errorResponse);
-            }
-            finally
-            {
-                using (validator as IDisposable)
-                {
+                    var validationEx = ex.UnwrapIfSingleException();
+    
+                    var errorResponse = await HostContext.RaiseServiceException(req, requestDto, validationEx)
+                                        ?? DtoUtils.CreateErrorResponse(requestDto, validationEx);
+    
+                    await res.WriteToResponse(req, errorResponse);
                 }
             }
         }
 
         public static async Task ResponseFilterAsync(IRequest req, IResponse res, object requestDto)
         {
-            var response = requestDto as IHasResponseStatus;
-            if (response == null)
+            if (!(requestDto is IHasResponseStatus response))
                 return;
 
             var validator = ValidatorCache.GetValidator(req, req.Dto.GetType());
@@ -96,11 +75,8 @@ namespace ServiceStack.Validation
 
             var validationResult = await Validate(validator, req, req.Dto);
 
-            if (validationResult.IsValid) 
-                return;
-            
-            var responseStatus = response.ResponseStatus ??
-                                 DtoUtils.CreateResponseStatus(validationResult.Errors[0].ErrorCode);
+            var responseStatus = response.ResponseStatus 
+                 ?? DtoUtils.CreateResponseStatus(validationResult.Errors[0].ErrorCode);
             foreach (var error in validationResult.Errors)
             {
                 var responseError = new ResponseError
@@ -122,26 +98,21 @@ namespace ServiceStack.Validation
 
             ValidationResult validationResult;
 
-            if (validator.HasAsyncValidators(ruleSet))
+            using (validator as IDisposable)
             {
-                validationResult = await validator.ValidateAsync(
-                    new ValidationContext(requestDto, null, new MultiRuleSetValidatorSelector(ruleSet))
-                    {
-                        Request = req
-                    });
-            }
-            else
-            {
-                validationResult = validator.Validate(
-                    new ValidationContext(requestDto, null, new MultiRuleSetValidatorSelector(ruleSet))
-                    {
-                        Request = req
-                    });
-            }
+                if (validator.HasAsyncValidators(ruleSet))
+                {
+                    return await validator.ValidateAsync(
+                        new ValidationContext(requestDto, null, new MultiRuleSetValidatorSelector(ruleSet))
+                        {
+                            Request = req
+                        });
+                }
 
-            using (validator as IDisposable) { }
-            
-            return validationResult;
+                return validator.Validate(new ValidationContext(requestDto, null, new MultiRuleSetValidatorSelector(ruleSet)) {
+                        Request = req
+                    });
+            }
         }
     }
 }
