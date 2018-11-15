34894f668a40a632ad471c104b928dfbf6284d08
Demis Bellot <demis.bellot@gmail.com>
2018-11-15 15:37:02 -0500

    Add new CheckMvcIdentityCore App to test .NET Core Identity Auth Integration

---
+++ tests/CheckMvcIdentityCore/Startup.cs
@@ -0,0 +1,194 @@
+﻿using IdentityDemo.Data;
+using IdentityDemo.Models;
+using IdentityDemo.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Funq;
+using ServiceStack;
+using ServiceStack.Auth;
+
+namespace IdentityDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                // options.Password.RequireDigit = true;
+                // options.Password.RequiredLength = 8;
+                // options.Password.RequireNonAlphanumeric = false;
+                // options.Password.RequireUppercase = true;
+                // options.Password.RequireLowercase = false;
+                // options.Password.RequiredUniqueChars = 6;
+
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequiredUniqueChars = 2;
+
+
+                // Lockout settings
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30);
+                options.Lockout.MaxFailedAccessAttempts = 10;
+                options.Lockout.AllowedForNewUsers = true;
+
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                // Cookie settings
+                options.Cookie.HttpOnly = true;
+                options.Cookie.Expiration = TimeSpan.FromDays(150);
+                // If the LoginPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/Login.
+                options.LoginPath = "/Account/Login";
+                // If the AccessDeniedPath isn't set, ASP.NET Core defaults 
+                // the path to /Account/AccessDenied.
+                options.AccessDeniedPath = "/Account/AccessDenied";
+                options.SlidingExpiration = true;
+            });
+
+            // Add application services.
+            services.AddTransient<IEmailSender, EmailSender>();
+
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseServiceStack(new AppHost {
+                AppSettings = new NetCoreAppSettings(Configuration)
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            CreateRoles(app.ApplicationServices).Wait();
+        }
+        
+        private async Task CreateRoles(IServiceProvider serviceProvider)
+        {
+            //initializing custom roles 
+            var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            string[] roleNames = { "Admin", "Manager", "Member" };
+            IdentityResult roleResult;
+
+            foreach (var roleName in roleNames)
+            {
+                var roleExist = await RoleManager.RoleExistsAsync(roleName);
+                if (!roleExist)
+                {
+                    //create the roles and seed them to the database: Question 1
+                    roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+                }
+            }
+            
+            var user = await UserManager.FindByEmailAsync("test@gmail.com");
+            if (user != null)
+            {
+                foreach (var role in roleNames)
+                {
+                    //here we tie the new user to the role
+                    await UserManager.AddToRoleAsync(user, role);
+                }
+            }
+        }
+    }
+    
+    public class AppHost : AppHostBase
+    {
+        public AppHost() : base("IdentityDemo", typeof(HelloService).Assembly) { }
+
+        public override void Configure(Container container)
+        {
+            Plugins.Add(new AuthFeature(() => new AuthUserSession(), 
+                new IAuthProvider[] {
+                    new NetCoreIdentityAuthProvider(AppSettings) 
+                    {
+                        PopulateSessionFilter = (session, principal, req) => 
+                        {
+                            //Example of populating ServiceStack Session Roles for EF Identity DB
+                            var userManager = req.TryResolve<UserManager<ApplicationUser>>();
+                            var user = userManager.FindByIdAsync(session.Id).Result;
+                            var roles = userManager.GetRolesAsync(user).Result;
+                            session.Roles = roles.ToList();
+                        }
+                    }, 
+                }));
+        }
+    }
+
+    [Route("/hello")]
+    public class Hello : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    [Route("/helloauth")]
+    public class HelloAuth : IReturn<HelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    public class HelloService : Service
+    {
+        public object Any(Hello request) => new HelloResponse { Result = $"Hello, {request.Name}!" };
+
+        [Authenticate]
+        public object Any(HelloAuth request) => new HelloResponse { Result = $"Hello Auth, {request.Name}!" };
+    }
+}
