ff66b739abe9b03114fa142e9184cb03b7528507
Demis Bellot <demis.bellot@gmail.com>
2018-12-15 22:49:42 +0800

    Refector to use Directory.Build.props + SourceLink + Embedded .pdbs + CI pkgs

--- src/ServiceStack.Common/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Common/Properties/AssemblyInfo.cs
@@ -1,32 +1,6 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-using System.Runtime.Serialization;
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("3871f659-64fb-4dfb-a49f-17dc2f8a47e2")]
+[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ServiceStack")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ServiceStack, Inc.")]
-[assembly: AssemblyProduct("ServiceStack")]
-[assembly: AssemblyCopyright("Copyright (c) ServiceStack 2018")]
-[assembly: AssemblyTrademark("ServiceStack")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3871f659-64fb-4dfb-a49f-17dc2f8a47e2")]
-
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
-
-// CCB Custom
-[assembly: ContractNamespace("http://schemas.servicestack.net/types",
+[assembly: System.Runtime.Serialization.ContractNamespace("http://schemas.servicestack.net/types",
     ClrNamespace = "ServiceStack")]
-
--- src/ServiceStack/Properties/AssemblyInfo.cs
+++ src/ServiceStack/Properties/AssemblyInfo.cs
@@ -1,25 +1,3 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("ServiceStack")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("ServiceStack, Inc.")]
-[assembly: AssemblyProduct("ServiceStack")]
-[assembly: AssemblyCopyright("Copyright (c) ServiceStack 2018")]
-[assembly: AssemblyTrademark("ServiceStack")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("06704d66-af8e-411f-8260-8d05de5ce6ad")]
-
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.0")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: System.Runtime.InteropServices.Guid("06704d66-af8e-411f-8260-8d05de5ce6ad")]
+[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
