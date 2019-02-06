b1d5d3e1c26cf30c37da9964bf4f1681249d4de1
Demis Bellot <demis.bellot@gmail.com>
2019-02-06 19:43:12 +0930

    Add FiltersInfo test

--- tests/ServiceStack.WebHost.Endpoints.Tests/TemplateTests/TemplateUseCaseTests.cs
+++ tests/ServiceStack.WebHost.Endpoints.Tests/TemplateTests/TemplateUseCaseTests.cs
@@ -1,4 +1,8 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using NUnit.Framework;
 using ServiceStack.Templates;
 using ServiceStack.Text;
 
@@ -64,5 +68,103 @@ Total            <b>$1,700.00</b>
 Monthly Savings: <b>$2,500.00</b>".NormalizeNewLines()));
         }
 
+        class FilterInfoFilters : TemplateFilter
+        {
+            Type GetFilterType(string name)
+            {
+                switch(name)
+                {
+                    case nameof(TemplateDefaultFilters):
+                        return typeof(TemplateDefaultFilters);
+                    case nameof(TemplateHtmlFilters):
+                        return typeof(TemplateHtmlFilters);
+                    case nameof(TemplateProtectedFilters):
+                        return typeof(TemplateProtectedFilters);
+                    case nameof(TemplateInfoFilters):
+                        return typeof(TemplateInfoFilters);
+                    case nameof(TemplateServiceStackFilters):
+                        return typeof(TemplateServiceStackFilters);
+                    case nameof(TemplateAutoQueryFilters):
+                        return typeof(TemplateAutoQueryFilters);
+                }
+
+                throw new NotSupportedException("Unknown Filter: " + name);
+            }
+
+            public FilterInfo[] filtersAvailable(string name)
+            {
+                var filterType = GetFilterType(name);
+                var filters = filterType.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+                var to = filters
+                    .OrderBy(x => x.Name)
+                    .ThenBy(x => x.GetParameters().Count())
+                    .Where(x => x.DeclaringType != typeof(TemplateFilter) && x.DeclaringType != typeof(object))
+                    .Where(m => !m.IsSpecialName)                
+                    .Select(x => FilterInfo.Create(x));
+
+                return to.ToArray();
+            }            
+        }
+        
+        public class FilterInfo
+        {
+            public string Name { get; set; }
+            public string FirstParam { get; set; }
+            public string ReturnType { get; set; }
+            public int ParamCount { get; set; }
+            public string[] RemainingParams { get; set; }
+
+            public static FilterInfo Create(MethodInfo mi)
+            {
+                var paramNames = mi.GetParameters()
+                    .Where(x => x.ParameterType != typeof(TemplateScopeContext))
+                    .Select(x => x.Name)
+                    .ToArray();
+
+                var to = new FilterInfo {
+                    Name = mi.Name,
+                    FirstParam = paramNames.FirstOrDefault(),
+                    ParamCount = paramNames.Length,
+                    RemainingParams = paramNames.Length > 1 ? paramNames.Skip(1).ToArray() : new string[]{},
+                    ReturnType = mi.ReturnType?.Name,
+                };
+
+                return to;
+            }
+
+            public string Return => ReturnType != null && ReturnType != nameof(StopExecution) ? " -> " + ReturnType : "";
+
+            public string Body => ParamCount == 0
+                ? $"{Name}"
+                : ParamCount == 1
+                    ? $"| {Name}"
+                    : $"| {Name}(" + string.Join(", ", RemainingParams) + $")";
+
+            public string Display => ParamCount == 0
+                ? $"{Name}{Return}"
+                : ParamCount == 1
+                    ? $"{FirstParam} | {Name}{Return}"
+                    : $"{FirstParam} | {Name}(" + string.Join(", ", RemainingParams) + $"){Return}";
+        }
+        
+        [Test]
+        public void Can_query_filters()
+        {
+            var context = new TemplateContext
+            {
+                TemplateFilters = { new FilterInfoFilters() }
+            }.Init();
+
+            var results = context.EvaluateTemplate(@"{{ 'TemplateDefaultFilters' | assignTo: filter }}
+{{ filter | filtersAvailable | where => contains(lower(it.Name), lower(nameContains ?? ''))  
+          | assignTo: filters }}
+{{#each filters}}
+{{Body}}
+{{/each}}", new Dictionary<string, object> { ["nameContains"] = "atan" });
+            
+            Assert.That(results.NormalizeNewLines(), Is.EqualTo(@"
+| atan
+| atan2(x)".NormalizeNewLines()));
+        }
     }
 }
