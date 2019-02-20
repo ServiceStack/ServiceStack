86616fad4f05ac13f6b47041b5a3bef8c6781e89
Demis Bellot <demis.bellot@gmail.com>
2019-02-19 19:04:40 -0500

    after too long T&E fixing the build seems to require changing to <TargetFrameworks>

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,5 @@
 SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
 
 REM %MSBUILD% build-common.proj /property:Configuration=Release;MinorVersion=4;PatchVersion=1
-%MSBUILD% build.proj /property:Configuration=Release;MinorVersion=4;PatchVersion=1
+REM %MSBUILD% build.proj /property:Configuration=Release;MinorVersion=4;PatchVersion=1
+%MSBUILD% build-core.proj /property:Configuration=Release;MinorVersion=4;PatchVersion=1
