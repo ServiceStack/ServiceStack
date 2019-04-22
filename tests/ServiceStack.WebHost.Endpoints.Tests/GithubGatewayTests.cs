12fc836f83d322b669cb265f9a29a583f4783a86
Demis Bellot <demis.bellot@gmail.com>
2019-04-22 01:30:16 -0400

    Add GithubScripts and GithubPlugin

--- tests/ServiceStack.WebHost.Endpoints.Tests/GithubGatewayTests.cs
+++ tests/ServiceStack.WebHost.Endpoints.Tests/GithubGatewayTests.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Net;
-using System.Threading.Tasks;
+using System.Collections.Generic;
 using NUnit.Framework;
 using ServiceStack.Text;
 
@@ -12,6 +11,42 @@ namespace ServiceStack.WebHost.Endpoints.Tests
         public static readonly string GistId = "67bc8f75273a29a1ba0609675b8ed1ae";
         public static readonly string AccessToken = Environment.GetEnvironmentVariable("GITHUB_GIST_TOKEN");
 
+        [Test]
+        public void Can_create_gist()
+        {
+            var gateway = new GithubGateway(AccessToken);
+
+            var gist = gateway.CreateGithubGist(
+                description: "Hello World Examples",
+                isPublic: true,
+                files: new Dictionary<string, string> {
+                    ["hello_world_ruby.txt"] = "Run `ruby hello_world.rb` to print Hello World",
+                    ["hello_world_python.txt"] = "Run `python hello_world.py` to print Hello World",
+                });
+            
+            gist.PrintDump();
+
+            Assert.That(gist.Owner.Login, Is.EqualTo("gistlyn"));
+            Assert.That(gist.Owner.Url, Is.EqualTo("https://api.github.com/users/gistlyn"));
+            Assert.That(gist.Owner.Html_Url, Is.EqualTo("https://github.com/gistlyn"));
+            
+            var file = gist.Files["hello_world_ruby.txt"];
+            Assert.That(file.Filename, Is.EqualTo("hello_world_ruby.txt"));
+            Assert.That(file.Type, Is.EqualTo("text/plain"));
+            Assert.That(file.Language, Is.EqualTo("Text"));
+            Assert.That(file.Raw_Url, Does.EndWith("/hello_world_ruby.txt"));
+            Assert.That(file.Size, Is.GreaterThan(0));
+            Assert.That(file.Content, Does.Contain("Run `ruby hello_world.rb` to print Hello World"));
+            
+            file = gist.Files["hello_world_python.txt"];
+            Assert.That(file.Filename, Is.EqualTo("hello_world_python.txt"));
+            Assert.That(file.Type, Is.EqualTo("text/plain"));
+            Assert.That(file.Language, Is.EqualTo("Text"));
+            Assert.That(file.Raw_Url, Does.EndWith("/hello_world_python.txt"));
+            Assert.That(file.Size, Is.GreaterThan(0));
+            Assert.That(file.Content, Does.Contain("Run `python hello_world.py` to print Hello World"));
+        }
+        
         [Test]
         public void Can_download_public_gist()
         {
