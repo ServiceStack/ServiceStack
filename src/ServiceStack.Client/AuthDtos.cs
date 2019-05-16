2759782284f394841e0822721b93461001182deb
Demis Bellot <demis.bellot@gmail.com>
2019-05-16 07:41:04 -0400

    Add ProfileUrl to AuthenticateResponse

--- src/ServiceStack.Client/AuthDtos.cs
+++ src/ServiceStack.Client/AuthDtos.cs
@@ -52,11 +52,12 @@ namespace ServiceStack
         [DataMember(Order = 5)] public string ReferrerUrl { get; set; }
         [DataMember(Order = 6)] public string BearerToken { get; set; }
         [DataMember(Order = 7)] public string RefreshToken { get; set; }
-        [DataMember(Order = 8)] public List<string> Roles { get; set; } 
-        [DataMember(Order = 9)] public List<string> Permissions { get; set; } 
+        [DataMember(Order = 8)] public string ProfileUrl { get; set; }
+        [DataMember(Order = 9)] public List<string> Roles { get; set; } 
+        [DataMember(Order = 10)] public List<string> Permissions { get; set; } 
 
-        [DataMember(Order = 10)] public ResponseStatus ResponseStatus { get; set; }
-        [DataMember(Order = 11)] public Dictionary<string, string> Meta { get; set; }
+        [DataMember(Order = 11)] public ResponseStatus ResponseStatus { get; set; }
+        [DataMember(Order = 12)] public Dictionary<string, string> Meta { get; set; }
     }
 
     [DataContract]
