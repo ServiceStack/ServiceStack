ba1942151b6fb17611e14aabe791b488fbc0c2eb
Demis Bellot <demis.bellot@gmail.com>
2019-05-21 13:24:12 -0400

    Add Auth/Nav/ModularStartup to Check Razor Core

---
+++ tests/CheckRazorCore/src/components/sub/SubB.js
@@ -0,0 +1,8 @@
+"use strict";
+var SubB = /** @class */ (function () {
+    function SubB() {
+    }
+    return SubB;
+}());
+new SubB().template = "/src/components/sub/SubB.ts " + Date();
+//# sourceMappingURL=SubB.js.map
