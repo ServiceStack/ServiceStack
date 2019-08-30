f60ae368471d1e142d4493c597bbd409bf13b20b
Demis Bellot <demis.bellot@gmail.com>
2019-08-30 11:49:04 -0400

    Don't show StackTrace for unhandled HttpListener Exceptions in !DebugMode

---
+++ tests/ServiceStack.WebHost.Endpoints.Tests/InvalidRequests.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Net;
+using System.Reflection;
+using Funq;
+using NUnit.Framework;
+using ServiceStack.Text;
+
+namespace ServiceStack.WebHost.Endpoints.Tests
+{
+    public class InvalidRequests
+    {
+        class AppHost : AppSelfHostBase
+        {
+            public AppHost() 
+                : base(nameof(InvalidRequests), typeof(MyServices).Assembly) { }
+            
+            public override void Configure(Container container)
+            {
+                SetConfig(new HostConfig {
+                    DebugMode = false
+                });
+            }
+        }
+
+        private readonly ServiceStackHost appHost;
+        public InvalidRequests()
+        {
+            appHost = new AppHost()
+                .Init()
+                .Start(Config.ListeningOn);
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() => appHost.Dispose();
+
+        [Test]
+        public void Invalid_Request_does_not_return_StackTrace_when_not_DebugMode()
+        {
+            try
+            {
+                var response = Config.ListeningOn.CombineWith("*|?")
+                    .GetJsonFromUrl();
+
+                Assert.Fail("Should throw");
+            }
+            catch (WebException e)
+            {
+                var errorBody = e.GetResponseBody();
+                Assert.That(errorBody.ToLower(), Does.Not.Contain("stacktrace"));
+            }
+        }
+    }
+}
