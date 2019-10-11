62bdc351222895389106383e36a8f1758de83383
Demis Bellot <demis.bellot@gmail.com>
2019-10-11 00:02:28 -0400

    Add UniqueIdAttribute

---
+++ src/ServiceStack.Interfaces/DataAnnotations/UniqueIdAttribute.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ServiceStack.DataAnnotations
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Method)]
+    public class UniqueIdAttribute : AttributeBase
+    {
+        public int Id { get; }
+        public UniqueIdAttribute(int id) => Id = id;
+    }
+}
