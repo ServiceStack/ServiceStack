b59c5b426e8612f7ab69f00620953f2036c8b479
Demis Bellot <demis.bellot@gmail.com>
2019-11-09 15:19:24 -0500

    Create IRestClientSync.cs

---
+++ src/ServiceStack.Interfaces/IRestClientSync.cs
@@ -0,0 +1,27 @@
+namespace ServiceStack
+{
+    public interface IRestClientSync
+    {
+        void SetCredentials(string userName, string password);
+
+        TResponse Get<TResponse>(IReturn<TResponse> requestDto);
+        TResponse Get<TResponse>(object requestDto);
+        void Get(IReturnVoid requestDto);
+
+        TResponse Delete<TResponse>(IReturn<TResponse> requestDto);
+        TResponse Delete<TResponse>(object requestDto);
+        void Delete(IReturnVoid requestDto);
+
+        TResponse Post<TResponse>(IReturn<TResponse> requestDto);
+        TResponse Post<TResponse>(object requestDto);
+        void Post(IReturnVoid requestDto);
+
+        TResponse Put<TResponse>(IReturn<TResponse> requestDto);
+        TResponse Put<TResponse>(object requestDto);
+        void Put(IReturnVoid requestDto);
+
+        TResponse CustomMethod<TResponse>(string httpVerb, IReturn<TResponse> requestDto);
+        TResponse CustomMethod<TResponse>(string httpVerb, object requestDto);
+        void CustomMethod(string httpVerb, IReturnVoid requestDto);
+    }
+}
