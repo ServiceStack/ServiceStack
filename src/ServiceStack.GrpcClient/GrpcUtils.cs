587c4931c4caa418dc4951dafa5ffb5611db9cd8
Demis Bellot <demis.bellot@gmail.com>
2019-12-17 18:29:42 -0500

    Add UX-friendly helpers for common tasks

--- src/ServiceStack.GrpcClient/GrpcUtils.cs
+++ src/ServiceStack.GrpcClient/GrpcUtils.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Grpc.Core;
 
@@ -25,6 +30,24 @@ namespace ServiceStack
             {
                 return await auc.ResponseAsync;
             }
+        }
+
+        public static HttpClientHandler AddPemCertificateFromFile(this HttpClientHandler handler, string fileName, 
+            Func<HttpRequestMessage, X509Certificate2, X509Chain, SslPolicyErrors, bool> serverCertificateCustomValidationCallback = null)
+        {
+            handler.ClientCertificates.Add(new X509Certificate2(fileName));
+            if (serverCertificateCustomValidationCallback != null)
+                handler.ServerCertificateCustomValidationCallback = serverCertificateCustomValidationCallback;
+            return handler;
+        }
+
+        public static HttpClientHandler AllowSelfSignedCertificatesFrom(this HttpClientHandler handler, string dnsName)
+        {
+            handler.ServerCertificateCustomValidationCallback = (req, cert, certChain, sslPolicyErrors) =>
+                cert.SubjectName.RawData.SequenceEqual(cert.IssuerName.RawData) && // self-signed
+                cert.GetNameInfo(X509NameType.DnsName, forIssuer: false) == dnsName &&
+                (sslPolicyErrors & ~SslPolicyErrors.RemoteCertificateChainErrors) == SslPolicyErrors.None; // only this
+            return handler;
         }
     }
 }
