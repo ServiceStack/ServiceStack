f96edd5f3f6eca3c5a5cea114fa9ebf7339a6f66
Demis Bellot <demis.bellot@gmail.com>
2020-01-17 11:33:01 -0500

    Update GrpcIntegrationTests.cs

--- tests/ServiceStack.Extensions.Tests/GrpcIntegrationTests.cs
+++ tests/ServiceStack.Extensions.Tests/GrpcIntegrationTests.cs
@@ -3,6 +3,7 @@ using System.Runtime.Serialization;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using ProtoBuf.Grpc.Client;
 using ServiceStack.Text;
 
 namespace ServiceStack.Extensions.Tests
@@ -15,34 +16,40 @@ namespace ServiceStack.Extensions.Tests
         public partial class Hello
             : IReturn<HelloResponse>
         {
-            [DataMember(Order=1)]
+            [DataMember(Order = 1)]
             public virtual string Name { get; set; }
         }
 
         [DataContract]
         public partial class HelloResponse
         {
-            [DataMember(Order=1)]
+            [DataMember(Order = 1)]
             public virtual string Result { get; set; }
 
-            [DataMember(Order=2)]
+            [DataMember(Order = 2)]
             public virtual ResponseStatus ResponseStatus { get; set; }
         }
 
         // [Test] // Integration Test
         public async Task Can_call_external_secure_service_using_remote_certificate()
         {
-            try 
-            { 
+            try
+            {
                 // File.WriteAllBytes("grpc.crt", "https://todoworld.servicestack.net/grpc.crt".GetBytesFromUrl());
                 // var cert = new X509Certificate2("grpc.crt");
-            
-                var client = new GrpcServiceClient("https://todoworld.servicestack.net:50051", 
-                    new X509Certificate2("https://todoworld.servicestack.net/grpc.crt".GetBytesFromUrl()), 
-                    GrpcUtils.AllowSelfSignedCertificatesFrom("todoworld.servicestack.net"));
+                var cert = new X509Certificate2("https://todoworld.servicestack.net/grpc.crt".GetBytesFromUrl());
 
-                var response = await client.GetAsync(new Hello { Name = "gRPC C#" });
-            
+                var client = new GrpcServiceClient("https://todoworld.servicestack.net:50051",
+                    cert, GrpcUtils.AllowSelfSignedCertificatesFrom("todoworld.servicestack.net"));
+
+                var response = await client.GetAsync(new Hello {Name = "gRPC SSL C# 50051"});
+                response.Result.Print();
+
+                client = new GrpcServiceClient("https://todoworld.servicestack.net:5051",
+                    cert, GrpcUtils.AllowSelfSignedCertificatesFrom("todoworld.servicestack.net"));
+
+                response = await client.GetAsync(new Hello {Name = "gRPC SSL C# 5051"});
+
                 response.Result.Print();
             }
             catch (Exception e)
@@ -50,6 +57,19 @@ namespace ServiceStack.Extensions.Tests
                 Console.WriteLine(e);
                 throw;
             }
+        }
+
+        // [Test] // Integration Test
+        public async Task Can_call_external_plaintext_service()
+        {
+            GrpcClientFactory.AllowUnencryptedHttp2 = true;
+            var client = new GrpcServiceClient("http://todoworld.servicestack.net:50054");
+            var response = await client.GetAsync(new Hello {Name = "gRPC Text C# 50054"});
+            response.Result.Print();
+
+            client = new GrpcServiceClient("http://todoworld.servicestack.net:5054");
+            response = await client.GetAsync(new Hello {Name = "gRPC Text C# 5054"});
+            response.Result.Print();
         }
     }
 }
