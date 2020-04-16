0e411d10c0735856d2a0a95591586862e66b35f4
Demis Bellot <demis.bellot@gmail.com>
2020-04-17 02:32:11 +0800

    Create XmlSerializerFormat.cs

---
+++ src/ServiceStack/Formats/XmlSerializerFormat.cs
@@ -0,0 +1,31 @@
+using System;
+using System.IO;
+using ServiceStack.Web;
+using System.Xml.Serialization;
+
+namespace ServiceStack.Formats
+{
+    /// <summary>
+    /// Use .NET XmlSerializer instead of .NET XML DataContract Serializer
+    /// </summary>
+    public class XmlSerializerFormat : IPlugin
+    {
+        public static void Serialize(IRequest req, object response, Stream stream)
+        {
+            var serializer = new XmlSerializer(response.GetType());
+            serializer.Serialize(stream, response);
+        }
+
+        public static object Deserialize(Type type, Stream stream)
+        {
+            var serializer = new XmlSerializer(type.GetType());
+            var obj = (Type) serializer.Deserialize(stream);
+            return obj;
+        }
+
+        public void Register(IAppHost appHost)
+        {
+            appHost.ContentTypes.Register(MimeTypes.Xml, Serialize, Deserialize);
+        }
+    }
+}
