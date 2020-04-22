59531d83d519e93d342c549dc14801f0a071fa2c
Demis Bellot <demis.bellot@gmail.com>
2020-04-22 15:48:13 +0800

    Add CustomInsert/Update attrs

--- src/ServiceStack.Interfaces/DataAnnotations/CustomSelectAttribute.cs
+++ src/ServiceStack.Interfaces/DataAnnotations/CustomSelectAttribute.cs
@@ -6,10 +6,20 @@ namespace ServiceStack.DataAnnotations
     public class CustomSelectAttribute : AttributeBase
     {
         public string Sql { get; set; }
+        public CustomSelectAttribute(string sql) => Sql = sql;
+    }
 
-        public CustomSelectAttribute(string sql)
-        {
-            Sql = sql;
-        }
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CustomInsertAttribute : AttributeBase
+    {
+        public string Sql { get; set; }
+        public CustomInsertAttribute(string sql) => Sql = sql;
+    }
+
+    [AttributeUsage(AttributeTargets.Property)]
+    public class CustomUpdateAttribute : AttributeBase
+    {
+        public string Sql { get; set; }
+        public CustomUpdateAttribute(string sql) => Sql = sql;
     }
 }
