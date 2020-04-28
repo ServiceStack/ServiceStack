792498728d005de7a1ab11128005c54d8be0ecfc
Demis Bellot <demis.bellot@gmail.com>
2020-04-29 05:04:41 +0800

    update servicestack-desktop

--- src/ServiceStack.Desktop/lib/js/@servicestack/desktop/servicestack-desktop.d.ts
+++ src/ServiceStack.Desktop/lib/js/@servicestack/desktop/servicestack-desktop.d.ts
@@ -16,3 +16,115 @@ export declare function evaluateLispAsync(scriptSrc: string): Promise<any>;
 export declare function renderScriptAsync(scriptSrc: string): Promise<string>;
 export declare function renderCodeAsync(scriptSrc: string): Promise<string>;
 export declare function renderLispAsync(scriptSrc: string): Promise<string>;
+export interface IDesktopInfo {
+    tool: string;
+    toolVersion: string;
+    chromeVersion: string;
+}
+export declare function desktopInfo(): Promise<IDesktopInfo>;
+/**
+ * Send Window to Foreground
+ * @param windowName - The name of the window to send to foreground, supported: browser
+ */
+export declare function sendToForeground(windowName: string): Promise<IDesktopInfo>;
+export declare function expandEnvVars(name: string): Promise<any>;
+/**
+ * Get Clipboard Contents as a UTF-8 string
+ */
+export declare function clipboard(): Promise<any>;
+/**
+ * Set the Clipboard Contents with a UTF-8 string
+ */
+export declare function setClipboard(contents: string): Promise<any>;
+export declare enum OpenFolderFlags {
+    AllowMultiSelect = 512,
+    CreatePrompt = 8192,
+    DontAddToRecent = 33554432,
+    EnableHook = 32,
+    EnableIncludeNotify = 4194304,
+    EnableSizing = 8388608,
+    EnableTemplate = 64,
+    EnableTemplateHandle = 128,
+    Explorer = 524288,
+    ExtensionIsDifferent = 1024,
+    FileMustExist = 4096,
+    ForceShowHidden = 268435456,
+    HideReadOnly = 4,
+    LongNames = 2097152,
+    NoChangeDir = 8,
+    NoDereferenceLinks = 1048576,
+    NoLongNames = 262144,
+    NoNetworkButton = 131072,
+    NoReadOnlyReturn = 32768,
+    NoTestFileCreate = 65536,
+    NoValidate = 256,
+    OverwritePrompt = 2,
+    PathMustExist = 2048,
+    ReadOnly = 1,
+    ShareAware = 16384,
+    ShowHelp = 16
+}
+/**
+ * Refer to the Win32 GetOpenFileName options at:
+ * https://docs.microsoft.com/en-us/windows/win32/api/commdlg/ns-commdlg-openfilenamea
+ */
+export interface OpenFolderOptions {
+    flags?: OpenFolderFlags;
+    title?: string;
+    filter?: string;
+    initialDir?: string;
+    defaultExt?: string;
+}
+export interface DialogResult {
+    folderPath: string;
+    fileTitle: string;
+    ok: boolean;
+}
+export declare function openFolder(options: OpenFolderOptions): Promise<DialogResult>;
+export declare enum MessageBoxType {
+    AbortRetryIgnore = 2,
+    CancelTryContinue = 6,
+    Help = 16384,
+    Ok = 0,
+    OkCancel = 1,
+    RetryCancel = 5,
+    YesNo = 4,
+    YesNoCancel = 3,
+    IconExclamation = 48,
+    IconWarning = 48,
+    IconInformation = 64,
+    IconQuestion = 32,
+    IconStop = 16,
+    DefaultButton1 = 0,
+    DefaultButton2 = 256,
+    DefaultButton3 = 512,
+    DefaultButton4 = 768,
+    AppModal = 0,
+    SystemModal = 4096,
+    TaskModal = 8192,
+    DefaultDesktopOnly = 131072,
+    RightJustified = 524288,
+    RightToLeftReading = 1048576,
+    SetForeground = 65536,
+    TopMost = 262144,
+    ServiceNotification = 2097152
+}
+export declare enum MessageBoxReturn {
+    Abort = 3,
+    Cancel = 2,
+    Continue = 11,
+    Ignore = 5,
+    No = 7,
+    Ok = 1,
+    Retry = 4,
+    TryAgain = 10,
+    Yes = 6
+}
+/**
+ * Refer to Win32 API
+ * https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
+ * @param title
+ * @param caption
+ * @param type
+ */
+export declare function messageBox(title: string, caption?: string, type?: MessageBoxType): Promise<MessageBoxReturn>;
--- src/ServiceStack.Desktop/src/servicestack-desktop.ts
+++ src/ServiceStack.Desktop/src/servicestack-desktop.ts
@@ -83,3 +83,142 @@ export async function renderCodeAsync(scriptSrc:string) {
 export async function renderLispAsync(scriptSrc:string) {
     return await invokeHostTextMethod('script', { 'RenderLispAsync':scriptSrc });
 }
+
+function quote(text:string) {
+    return '"' + text.replace('"','\\"') + '"';
+}
+
+export interface IDesktopInfo {
+    tool: string;
+    toolVersion: string;
+    chromeVersion: string;
+}
+
+export async function desktopInfo() { return (await evaluateCode('desktopInfo')) as IDesktopInfo; }
+
+/**
+ * Send Window to Foreground
+ * @param windowName - The name of the window to send to foreground, supported: browser 
+ */
+export async function sendToForeground(windowName:string) { 
+    return (await evaluateCode('desktopInfo')) as IDesktopInfo; 
+}
+
+export async function expandEnvVars(name:string) { 
+    return await evaluateCode(`expandEnvVars(${quote(name)})`); 
+}
+
+/**
+ * Get Clipboard Contents as a UTF-8 string
+ */
+export async function clipboard() { return await evaluateCode('clipboard'); }
+
+/**
+ * Set the Clipboard Contents with a UTF-8 string
+ */
+export async function setClipboard(contents:string) { 
+    return await evaluateCode(`setClipboard(${quote(contents)})`); 
+}
+
+export enum OpenFolderFlags {
+    AllowMultiSelect = 0x00000200,
+    CreatePrompt = 0x00002000,
+    DontAddToRecent = 0x02000000,
+    EnableHook = 0x00000020,
+    EnableIncludeNotify = 0x00400000,
+    EnableSizing = 0x00800000,
+    EnableTemplate = 0x00000040,
+    EnableTemplateHandle = 0x00000080,
+    Explorer = 0x00080000,
+    ExtensionIsDifferent = 0x00000400,
+    FileMustExist = 0x00001000,
+    ForceShowHidden = 0x10000000,
+    HideReadOnly = 0x00000004,
+    LongNames = 0x00200000,
+    NoChangeDir = 0x00000008,
+    NoDereferenceLinks = 0x00100000,
+    NoLongNames = 0x00040000,
+    NoNetworkButton = 0x00020000,
+    NoReadOnlyReturn = 0x00008000,
+    NoTestFileCreate = 0x00010000,
+    NoValidate = 0x00000100,
+    OverwritePrompt = 0x00000002,
+    PathMustExist = 0x00000800,
+    ReadOnly = 0x00000001,
+    ShareAware = 0x00004000,
+    ShowHelp = 0x00000010,
+}
+
+/**
+ * Refer to the Win32 GetOpenFileName options at:
+ * https://docs.microsoft.com/en-us/windows/win32/api/commdlg/ns-commdlg-openfilenamea
+ */
+export interface OpenFolderOptions {
+    flags?: OpenFolderFlags;
+    title?: string;
+    filter?: string;
+    initialDir?: string;
+    defaultExt?: string;
+}
+
+export interface DialogResult {
+    folderPath:string;
+    fileTitle:string;
+    ok:boolean;
+}
+
+export async function openFolder(options:OpenFolderOptions) {
+    return (await evaluateCode(`openFolder(${JSON.stringify(options)})`)) as DialogResult;
+}
+
+export enum MessageBoxType {
+    AbortRetryIgnore = 0x00000002,
+    CancelTryContinue = 0x00000006,
+    Help = 0x00004000,
+    Ok = 0x00000000,
+    OkCancel = 0x00000001,
+    RetryCancel = 0x00000005,
+    YesNo = 0x00000004,
+    YesNoCancel = 0x00000003,
+    IconExclamation = 0x00000030,
+    IconWarning = 0x00000030,
+    IconInformation = 0x00000040,
+    IconQuestion = 0x00000020,
+    IconStop = 0x00000010,
+    DefaultButton1 = 0x00000000,
+    DefaultButton2 = 0x00000100,
+    DefaultButton3 = 0x00000200,
+    DefaultButton4 = 0x00000300,
+    AppModal = 0x00000000,
+    SystemModal = 0x00001000,
+    TaskModal = 0x00002000,
+    DefaultDesktopOnly = 0x00020000,
+    RightJustified = 0x00080000,
+    RightToLeftReading = 0x00100000,
+    SetForeground = 0x00010000,
+    TopMost = 0x00040000,
+    ServiceNotification = 0x00200000,
+}
+
+export enum MessageBoxReturn {
+    Abort = 3,
+    Cancel = 2,
+    Continue = 11,
+    Ignore = 5,
+    No = 7,
+    Ok = 1,
+    Retry = 4,
+    TryAgain = 10,
+    Yes = 6,
+}
+
+/**
+ * Refer to Win32 API 
+ * https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
+ * @param title
+ * @param caption
+ * @param type
+ */
+export async function messageBox(title:string, caption:string="", type:MessageBoxType=MessageBoxType.Ok) {
+    return (await evaluateCode(`messageBox(${quote(title)},${quote(caption)},${type})`)) as MessageBoxReturn;
+}
