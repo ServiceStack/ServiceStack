7b4230e8d85abcba9054faf7f22690c7d86271ef
Demis Bellot <demis.bellot@gmail.com>
2020-05-01 04:32:17 +0800

    Move writable config to DesktopState

--- src/ServiceStack.Desktop/DesktopConfig.cs
+++ src/ServiceStack.Desktop/DesktopConfig.cs
@@ -7,14 +7,11 @@ namespace ServiceStack.Desktop
     public class DesktopConfig
     {
         public static DesktopConfig Instance { get; } = new DesktopConfig();
-        
-        public string Tool { get; set; }
-        public string ToolVersion { get; set; }
-        public string ChromeVersion { get; set; }
-        
         public List<ProxyConfig> ProxyConfigs { get; set; } = new List<ProxyConfig>();
         public List<string> ImportParams { get; set; } = new List<string>();
         public string MinToolVersion { get; set; }
+        public Action OnExit { get; set; }
+        public Action<Exception> OnError { get; set; }
     }
 
     public class ProxyConfig
@@ -26,5 +23,18 @@ namespace ServiceStack.Desktop
         public List<string> IgnoreHeaders { get; set; } = new List<string>();
         public Dictionary<string,string> AddHeaders { get; set; } = new Dictionary<string, string>();
         public Action<NameValueCollection> OnResponseHeaders { get; set; }
+    }
+
+    public static class DesktopState
+    {
+        public static bool AppDebug { get; set; }
+        public static IntPtr BrowserHandle { get; set; } 
+        public static IntPtr ConsoleHandle { get; set; } 
+        public static string Tool { get; set; }
+        public static string ToolVersion { get; set; }
+        public static string ChromeVersion { get; set; }
+        public static bool FromScheme { get; set; }
+        public static string[] OriginalCommandArgs { get; set; }
+        public static string[] CommandArgs { get; set; }
     }
 }
