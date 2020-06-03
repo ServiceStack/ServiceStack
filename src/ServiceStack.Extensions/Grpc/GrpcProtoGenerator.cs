8b4d6844fa332c7dc84240e094d99bbe3a1f2c56
Demis Bellot <demis.bellot@gmail.com>
2020-06-03 18:04:43 +0800

    Use public GetSchemaTypeName API

--- src/ServiceStack.Extensions/Grpc/GrpcProtoGenerator.cs
+++ src/ServiceStack.Extensions/Grpc/GrpcProtoGenerator.cs
@@ -42,25 +42,7 @@ namespace ServiceStack.Grpc
             grpc = HostContext.AssertPlugin<GrpcFeature>();
         }
 
-        static MethodInfo getSchemaMethod;
-        static readonly object[] getSchemaMethodArgs = { null };
-        static readonly ConcurrentDictionary<Type, string> SchemaTypeNamesCache = new ConcurrentDictionary<Type, string>();
-
-        public string GetSchemaTypeName(Type type)
-        {
-            if (getSchemaMethod == null)
-            {
-                getSchemaMethod = typeof(MetaType).GetInstanceMethods()
-                    .First(x => x.Name == "GetSchemaTypeName" && x.GetParameters().Length == 1);
-            }
-
-            return SchemaTypeNamesCache.GetOrAdd(type, key => {
-                if (getSchemaMethod != null)
-                    return (string) getSchemaMethod.Invoke(GrpcConfig.TypeModel[type], getSchemaMethodArgs);
-
-                return type.Name;
-            });
-        }
+        public string GetSchemaTypeName(Type type) => GrpcConfig.TypeModel[type].GetSchemaTypeName();
 
         public string GetCode(MetadataTypes metadata, IRequest request)
         {
