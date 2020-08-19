00710778dbc6b1032c034e646c7116d972259722
Damith Gunner <djnitehawk83@gmail.com>
2020-08-19 11:15:24 +0530

    fix for PATCH requests with multipart/form-data not populating the request dto

--- src/ServiceStack/HttpRequestExtensions.cs
+++ src/ServiceStack/HttpRequestExtensions.cs
@@ -552,7 +552,7 @@ namespace ServiceStack
                 map[name] = request.QueryString[name];
             }
 
-            if ((request.Verb == HttpMethods.Post || request.Verb == HttpMethods.Put)
+            if ((request.Verb == HttpMethods.Post || request.Verb == HttpMethods.Put || request.Verb == HttpMethods.Patch)
                 && request.FormData != null)
             {
                 foreach (var name in request.FormData.AllKeys)
