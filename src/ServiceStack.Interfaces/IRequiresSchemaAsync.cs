af36a23a8f20d274de8b9e194fd1e70556983811
Demis Bellot <demis.bellot@gmail.com>
2020-08-24 21:49:06 +0800

    Create IRequiresSchemaAsync.cs

---
+++ src/ServiceStack.Interfaces/IRequiresSchemaAsync.cs
@@ -0,0 +1,13 @@
+using System.Threading.Tasks;
+
+namespace ServiceStack
+{
+    public interface IRequiresSchemaAsync
+    {
+        /// <summary>
+        /// Unified API to create any missing Tables, Data Structure Schema 
+        /// or perform any other tasks dependencies require to run at Startup.
+        /// </summary>
+        Task InitSchemaAsync();
+    }
+}
