1531130166194268ca11048069c59d68d0b4fcf1
Demis Bellot <demis.bellot@gmail.com>
2020-08-25 00:08:14 +0800

    Update ISequenceSourceAsync.cs

--- src/ServiceStack.Interfaces/ISequenceSourceAsync.cs
+++ src/ServiceStack.Interfaces/ISequenceSourceAsync.cs
@@ -7,6 +7,6 @@ namespace ServiceStack
     {
         Task<long> IncrementAsync(string key, long amount = 1, CancellationToken token = default);
 
-        Task Reset(string key, long startingAt = 0, CancellationToken token = default);
+        Task ResetAsync(string key, long startingAt = 0, CancellationToken token = default);
     }
 }
