4398438e058851847033f2da923fe0221a75d3b3
Demis Bellot <demis.bellot@gmail.com>
2020-11-18 12:33:05 +0800

    Add IRestGatewayAsync

---
+++ src/ServiceStack.Interfaces/IRestGatewayAsync.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) ServiceStack, Inc. All Rights Reserved.
+// License: https://raw.github.com/ServiceStack/ServiceStack/master/license.txt
+
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServiceStack
+{
+    public interface IRestGatewayAsync
+    {
+        Task<T> SendAsync<T>(IReturn<T> request, CancellationToken token);
+        Task<T> GetAsync<T>(IReturn<T> request, CancellationToken token);
+        Task<T> PostAsync<T>(IReturn<T> request, CancellationToken token);
+        Task<T> PutAsync<T>(IReturn<T> request, CancellationToken token);
+        Task<T> DeleteAsync<T>(IReturn<T> request, CancellationToken token);
+    }
+}
