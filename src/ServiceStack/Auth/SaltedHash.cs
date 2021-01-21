e866d58b46464bb312ad22f7d165282b0a6d827e
Demis Bellot <demis.bellot@gmail.com>
2021-01-21 16:21:21 +0800

    Clean up HashExtensions

--- src/ServiceStack/Auth/SaltedHash.cs
+++ src/ServiceStack/Auth/SaltedHash.cs
@@ -89,34 +89,35 @@ namespace ServiceStack.Auth
 
     public static class HashExtensions
     {
-        public static string ToSha256Hash(this string value)
+        public static string HexHash(HashAlgorithm hash, string s) => HexHash(hash, s.ToUtf8Bytes());
+        public static string HexHash(HashAlgorithm hash, byte[] bytes)
         {
-            var sb = StringBuilderCache.Allocate();
-            using (var hash = SHA256.Create())
-            {
-                var result = hash.ComputeHash(value.ToUtf8Bytes());
-                foreach (var b in result)
-                {
-                    sb.Append(b.ToString("x2"));
-                }
-            }
-            return StringBuilderCache.ReturnAndFree(sb);
+            using var _ = hash;
+            return bytes == null || bytes.Length == 0 ? null : _.ComputeHash(bytes).ToHex();
         }
+
+        public static string ToSha1Hash(this string value) => HexHash(new SHA1Managed(), value);
+
+        public static byte[] ToSha1HashBytes(this byte[] bytes)
+        {
+            using var hash = new SHA1Managed();
+            return hash.ComputeHash(bytes);
+        }
+
+        public static string ToSha256Hash(this string value) => HexHash(SHA256.Create(), value);
 
         public static byte[] ToSha256HashBytes(this byte[] bytes)
         {
-            using (var hash = SHA256.Create())
-            {
-                return hash.ComputeHash(bytes);
-            }
+            using var hash = SHA256.Create();
+            return hash.ComputeHash(bytes);
         }
+
+        public static string ToSha512Hash(this string value) => HexHash(SHA512.Create(), value);
 
         public static byte[] ToSha512HashBytes(this byte[] bytes)
         {
-            using (var hash = SHA512.Create())
-            {
-                return hash.ComputeHash(bytes);
-            }
+            using var hash = SHA512.Create();
+            return hash.ComputeHash(bytes);
         }
     }
 
