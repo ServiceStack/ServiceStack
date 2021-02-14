b5f507e21bc48d8d252834095280173a8363dc14
Demis Bellot <demis.bellot@gmail.com>
2021-02-15 03:42:32 +0800

    Update ProcessUtils.cs

--- src/ServiceStack.Common/ProcessUtils.cs
+++ src/ServiceStack.Common/ProcessUtils.cs
@@ -24,7 +24,7 @@ namespace ServiceStack
                     : "/bin/bash",
                 WorkingDirectory = startInfo.WorkingDirectory,
                 Arguments = Env.IsWindows
-                    ? $"/c \"{startInfo.FileName} {startInfo.Arguments}\""
+                    ? $"/c \"\"{startInfo.FileName}\" {startInfo.Arguments}\""
                     : $"-c \"{startInfo.FileName} {startInfo.Arguments}\"",
             };
             return to;
