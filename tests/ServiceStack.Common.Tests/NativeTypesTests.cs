6707b190bdc7b3a109fdd08a62dac7705187583b
Demis Bellot <demis.bellot@gmail.com>
2021-02-21 13:31:16 +0800

    Update NativeTypesTests.cs

--- tests/ServiceStack.Common.Tests/NativeTypesTests.cs
+++ tests/ServiceStack.Common.Tests/NativeTypesTests.cs
@@ -377,6 +377,37 @@ namespace ServiceStack.Common.Tests
             Assert.That(src, Does.Contain(
             "[ValidateRequest(\"HasRole('Accounts')\", ErrorCode=\"ExCode\", Message=\"'Id' Is Required\")]"));
         }
+
+        [Test]
+        public void Can_generate_Swift_PocoLookupMap()
+        {
+            var typeName = "Dictionary`2";
+            var genericArgs = new[] { "String", "List<Dictionary<String,Poco>>" };
+
+            var gen = new ServiceStack.NativeTypes.Swift.SwiftGenerator(new MetadataTypesConfig());
+            var type = gen.Type(typeName, genericArgs);
+            
+            Assert.That(type, Is.EqualTo("[String:[[String:Poco]]]"));
+        }
+
+        [Test]
+        public void Does_generate_Swift_IntArray()
+        {
+            var genericArgs = new string[] { };
+
+            var gen = new ServiceStack.NativeTypes.Swift.SwiftGenerator(new MetadataTypesConfig());
+            Assert.That(gen.Type("Int32[]", genericArgs), Is.EqualTo("[Int]"));
+            Assert.That(gen.Type("Int64[]", genericArgs), Is.EqualTo("[Int]"));
+        }
+
+        [Test]
+        public void Does_generate_Swift_IntList()
+        {
+            var gen = new ServiceStack.NativeTypes.Swift.SwiftGenerator(new MetadataTypesConfig());
+            Assert.That(gen.Type("List`1", new[] { "Int32" }), Is.EqualTo("[Int]"));
+            Assert.That(gen.Type("List`1", new[] { "Int64" }), Is.EqualTo("[Int]"));
+        }
+        
     }
 
     public class NativeTypesTestService : Service
