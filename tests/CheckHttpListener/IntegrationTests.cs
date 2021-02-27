0b833eea6b8d0a750c51a0a87ab81a172ea47150
Demis Bellot <demis.bellot@gmail.com>
2021-02-27 08:55:37 +0800

    Update IntegrationTests.cs

--- tests/CheckHttpListener/IntegrationTests.cs
+++ tests/CheckHttpListener/IntegrationTests.cs
@@ -20,5 +20,35 @@ namespace CheckHttpListener
             });
             response.PrintDump();
         }
+        
+        [Route("/testauth")]
+        public partial class TestAuth
+            : IReturn<TestAuthResponse>
+        {
+        }
+
+        public partial class TestAuthResponse
+        {
+            public virtual string UserId { get; set; }
+            public virtual string SessionId { get; set; }
+            public virtual string UserName { get; set; }
+            public virtual string DisplayName { get; set; }
+            public virtual ResponseStatus ResponseStatus { get; set; }
+        }
+        //static string TestUrl = "http://test.servicestack.net";
+        static string TestUrl = "https://localhost:5001";
+
+        [Test]
+        public void Can_authenticate_via_HTTP_BasicAuth()
+        {
+            var client = new JsonServiceClient(TestUrl) {
+                UserName = "test",
+                Password = "test",
+                AlwaysSendBasicAuthHeader = true
+            };
+
+            var response = client.Get(new TestAuth());
+            response.PrintDump();
+        }
     }
 }
