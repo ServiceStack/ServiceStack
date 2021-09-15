d43ec568d6854ae849dca631e6df59fd97e19887
Demis Bellot <demis.bellot@gmail.com>
2021-09-15 19:33:50 +0800

    Add String/Int/BoolResponse

--- src/ServiceStack.Interfaces/EmptyResponse.cs
+++ src/ServiceStack.Interfaces/EmptyResponse.cs
@@ -24,19 +24,6 @@ namespace ServiceStack
     }
     
     [DataContract]
-    public class StringResponse : IHasResponseStatus, IMeta
-    {
-        [DataMember(Order = 1)]
-        public string Results { get; set; }
-
-        [DataMember(Order = 2)]
-        public Dictionary<string, string> Meta { get; set; }
-
-        [DataMember(Order = 3)]
-        public ResponseStatus ResponseStatus { get; set; }
-    }
-    
-    [DataContract]
     public class StringsResponse : IHasResponseStatus, IMeta
     {
         [DataMember(Order = 1)]
@@ -48,5 +35,44 @@ namespace ServiceStack
         [DataMember(Order = 3)]
         public ResponseStatus ResponseStatus { get; set; }
     }
+     
+    [DataContract]
+    public class StringResponse : IHasResponseStatus, IMeta
+    {
+        [DataMember(Order = 1)]
+        public string Result { get; set; }
+
+        [DataMember(Order = 2)]
+        public Dictionary<string, string> Meta { get; set; }
+
+        [DataMember(Order = 3)]
+        public ResponseStatus ResponseStatus { get; set; }
+    }
     
+    [DataContract]
+    public class IntResponse : IHasResponseStatus, IMeta
+    {
+        [DataMember(Order = 1)]
+        public int Result { get; set; }
+
+        [DataMember(Order = 2)]
+        public Dictionary<string, string> Meta { get; set; }
+
+        [DataMember(Order = 3)]
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+     
+    [DataContract]
+    public class BoolResponse : IHasResponseStatus, IMeta
+    {
+        [DataMember(Order = 1)]
+        public bool Result { get; set; }
+
+        [DataMember(Order = 2)]
+        public Dictionary<string, string> Meta { get; set; }
+
+        [DataMember(Order = 3)]
+        public ResponseStatus ResponseStatus { get; set; }
+    }
+   
 }
