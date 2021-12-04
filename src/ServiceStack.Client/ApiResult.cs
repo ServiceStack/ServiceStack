033cc223c764d89db932a1d627d70b337c4a3b06
Demis Bellot <demis.bellot@gmail.com>
2021-12-04 14:28:01 +0800

    Add more useful ResponseStatus extension methods + CssUtils

--- src/ServiceStack.Client/ApiResult.cs
+++ src/ServiceStack.Client/ApiResult.cs
@@ -13,7 +13,7 @@ public class ApiResult<TResponse>
     public ResponseStatus? ErrorStatus { get; private set; }
 
     public bool Completed => Response != null || ErrorStatus != null;
-    public bool IsError => ErrorStatus != null;
+    public bool IsError => ErrorStatus.IsError();
     public bool IsSuccess => !IsError && Response != null;
 
     public string? ErrorMessage => ErrorStatus?.Message;
@@ -22,11 +22,11 @@ public class ApiResult<TResponse>
         ? ErrorStatus.Message
         : null;
 
-    public string? FieldErrorMessage(string fieldName) => ErrorStatus?.FieldErrorMessage(fieldName);
+    public string? FieldErrorMessage(string fieldName) => ErrorStatus.FieldErrorMessage(fieldName);
 
-    public ResponseError? FieldError(string fieldName) => ErrorStatus?.FieldError(fieldName);
+    public ResponseError? FieldError(string fieldName) => ErrorStatus.FieldError(fieldName);
 
-    public bool HasFieldError(string fieldName) => ErrorStatus?.FieldError(fieldName) != null;
+    public bool HasFieldError(string fieldName) => ErrorStatus.HasFieldError(fieldName);
 
     public ApiResult(TResponse response)
     {
@@ -63,11 +63,51 @@ public static class ApiResultUtils
 {
     public const string FieldErrorCode = "ValidationException";
 
-    public static ResponseError? FieldError(this ResponseStatus status, string fieldName) => status?.Errors?
+    public static bool IsSuccess(this ResponseStatus? status) => !status.IsError();
+    public static bool IsError(this ResponseStatus? status) => status?.ErrorCode != null || status?.Message != null;
+
+    public static ResponseError? FieldError(this ResponseStatus? status, string fieldName) => status?.Errors?
         .FirstOrDefault(x => string.Equals(x.FieldName, fieldName, StringComparison.OrdinalIgnoreCase));
 
-    public static string? FieldErrorMessage(this ResponseStatus status, string fieldName) => status?.Errors?
+    public static string? FieldErrorMessage(this ResponseStatus? status, string fieldName) => status?.Errors?
         .FirstOrDefault(x => string.Equals(x.FieldName, fieldName, StringComparison.OrdinalIgnoreCase))?.Message;
+
+    public static bool HasFieldError(this ResponseStatus? status, string fieldName) => status?.FieldError(fieldName) != null;
+
+    public static bool ShowSummary(this ResponseStatus? status, params string[] exceptFields)
+    {
+        if (!status.IsError())
+            return false;
+
+        // Don't show summary message if an error for a visible field exists
+        foreach (var fieldName in exceptFields)
+        {
+            if (status.HasFieldError(fieldName))
+                return false;
+        }
+        return true;
+    }
+
+    public static string? SummaryMessage(this ResponseStatus? status, params string[] exceptFields)
+    {
+        if (status.ShowSummary(exceptFields))
+        {
+            // Return first field error that's not visible
+            if (status?.Errors != null)
+            {
+                var fieldSet = new HashSet<string>(exceptFields, StringComparer.OrdinalIgnoreCase);
+                foreach (var errorField in status.Errors)
+                {
+                    if (fieldSet.Contains(errorField.FieldName))
+                        continue;
+                    return errorField.Message;
+                }
+            }
+            // otherwise return summary message
+            return status?.Message;
+        }
+        return null;
+    }
 
     public static ResponseStatus CreateError(string message, string? errorCode = nameof(Exception)) =>
         new() {
---
+++ src/ServiceStack.Client/CssUtils.cs
@@ -0,0 +1,32 @@
+using System.Text;
+
+namespace ServiceStack;
+
+public static class CssUtils
+{
+    public static class Bootstrap
+    {
+        public static string InvalidClass<T>(ApiResult<T> apiResult, string fieldName) => apiResult.ErrorStatus.HasFieldError(fieldName)
+            ? "is-invalid"
+            : "";
+
+        public static string InvalidClass(ResponseStatus? status, string fieldName) => status?.FieldError(fieldName) != null
+            ? "is-invalid"
+            : "";
+    }
+    
+    public static string ClassNames(params string[] classes)
+    {
+        var sb = new StringBuilder();
+        foreach (var cls in classes)
+        {
+            if (cls.IsNullOrEmpty())
+                continue;
+
+            if (sb.Length > 0)
+                sb.Append(' ');
+            sb.Append(cls);
+        }
+        return sb.ToString();
+    }
+}
