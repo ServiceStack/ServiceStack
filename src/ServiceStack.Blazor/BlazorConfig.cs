64e1ab711215d929c1b96dce8d5a0a53d17895da
Demis Bellot <demis.bellot@gmail.com>
2021-12-05 21:16:28 +0800

    Merge BS/TW components & use BlazorConfig.Theme to control them

---
+++ src/ServiceStack.Blazor/BlazorConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceStack.Blazor
+{
+    public enum Theme
+    {
+        Bootstrap5,
+        Tailwind,
+    }
+
+    public static class BlazorConfig
+    {
+        public static Theme Theme { get; set; } = Theme.Bootstrap5;
+    }
+}
