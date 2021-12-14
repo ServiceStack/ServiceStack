a831110d10758dd2e1edd56d199e4094fdca4bed
Demis Bellot <demis.bellot@gmail.com>
2021-12-14 14:29:10 +0800

    Refactor Blazor test proj to match template

--- tests/ServiceStack.Blazor.Tests/Server/Pages/Error.cshtml.cs
+++ tests/ServiceStack.Blazor.Tests/Server/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Diagnostics;
 
-namespace ServiceStack.Blazor.Tests.Server.Pages
+namespace MyApp.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
