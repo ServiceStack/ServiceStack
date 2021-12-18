dc1bebb67e23d8d90b48adc9a20b9bb841b7cbd5
Demis Bellot <demis.bellot@gmail.com>
2021-12-18 14:04:16 +0800

    Revert "Try detect if markdown page is pre-rendered when published."
    This reverts commit dae99eb2491a856dc458256a4efc346754f3b03f.

--- tests/ServiceStack.Blazor.Tests/Server/Tasks.cs
+++ tests/ServiceStack.Blazor.Tests/Server/Tasks.cs
@@ -147,12 +147,7 @@ public static class TaskRunner
     </div>
 </div>
 ";
-                var taggedAsPrerendered = @"
-<script>
-window.isPagePrerendered = function () { return true; };
-</script>";
 
-                mdBody += taggedAsPrerendered;
                 var prerenderedPage = IndexTemplate.Render(cmd, mdBody);
                 string htmlPath = Path.GetFullPath(Path.Combine(dstDir, $"{name}.html"));
                 File.WriteAllText(htmlPath, prerenderedPage);
