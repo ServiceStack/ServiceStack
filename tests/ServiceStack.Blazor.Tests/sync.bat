6fdc9baaee0b9219f24d933a5d99b02ae6d2b79d
Demis Bellot <demis.bellot@gmail.com>
2022-01-18 17:16:14 +0800

    Update API docs

--- tests/ServiceStack.Blazor.Tests/sync.bat
+++ tests/ServiceStack.Blazor.Tests/sync.bat
@@ -5,6 +5,8 @@ RD /q /s %TO%\MyApp.Client
 MD %TO%\MyApp.Client
 XCOPY /Y /E /H /C /I Client %TO%\
 MOVE MyApp.Client.csproj %TO%\
+RD /q /s ..\..\..\NetCoreTemplates\blazor-wasm\MyApp\wwwroot\modules\
+XCOPY /Y /E /H /C /I Server\modules ..\..\..\NetCoreTemplates\blazor-wasm\MyApp\wwwroot\modules\
 
 COPY Server\*.cs ..\..\..\NetCoreTemplates\blazor-wasm\MyApp\
 COPY ServiceModel\*.cs ..\..\..\NetCoreTemplates\blazor-wasm\MyApp.ServiceModel\
