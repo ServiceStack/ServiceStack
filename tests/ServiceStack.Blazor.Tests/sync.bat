17c78c15a28d677186347f395958a8ff18a029f1
Demis Bellot <demis.bellot@gmail.com>
2022-01-19 21:01:07 +0800

    Incude Brand.html as a /ui customization example

--- tests/ServiceStack.Blazor.Tests/sync.bat
+++ tests/ServiceStack.Blazor.Tests/sync.bat
@@ -5,13 +5,15 @@ RD /q /s %TO%\MyApp.Client
 MD %TO%\MyApp.Client
 XCOPY /Y /E /H /C /I Client %TO%\
 MOVE MyApp.Client.csproj %TO%\
-RD /q /s ..\..\..\NetCoreTemplates\nextjs\ui\public\modules\
-RD /q /s ..\..\..\NetCoreTemplates\vue-ssg\ui\public\modules\
-RD /q /s ..\..\..\NetCoreTemplates\vue-vite\ui\public\modules\
+
+REM /shared/Brand.html unique to blazor-wasm
+REM RD /q /s ..\..\..\NetCoreTemplates\nextjs\ui\public\modules\
+REM RD /q /s ..\..\..\NetCoreTemplates\vue-ssg\ui\public\modules\
+REM RD /q /s ..\..\..\NetCoreTemplates\vue-vite\ui\public\modules\
 RD /q /s ..\..\..\NetCoreTemplates\blazor-wasm\MyApp\wwwroot\modules\
-XCOPY /Y /E /H /C /I Server\modules ..\..\..\NetCoreTemplates\nextjs\ui\public\modules\
-XCOPY /Y /E /H /C /I Server\modules ..\..\..\NetCoreTemplates\vue-ssg\ui\publicwwwroot\modules\
-XCOPY /Y /E /H /C /I Server\modules ..\..\..\NetCoreTemplates\vue-vite\ui\public\modules\
+REM XCOPY /Y /E /H /C /I Server\modules ..\..\..\NetCoreTemplates\nextjs\ui\public\modules\
+REM XCOPY /Y /E /H /C /I Server\modules ..\..\..\NetCoreTemplates\vue-ssg\ui\publicwwwroot\modules\
+REM XCOPY /Y /E /H /C /I Server\modules ..\..\..\NetCoreTemplates\vue-vite\ui\public\modules\
 XCOPY /Y /E /H /C /I Server\modules ..\..\..\NetCoreTemplates\blazor-wasm\MyApp\wwwroot\modules\
 
 COPY Server\*.cs ..\..\..\NetCoreTemplates\blazor-wasm\MyApp\
