a3d200bb399a42e20ece6900621c36ca0012e892
Demis Bellot <demis.bellot@gmail.com>
2022-02-02 19:38:46 +0800

    Move ServiceStack.Logging to own top-level folder

--- ServiceStack/src/ServiceStack.Logging.EntLib5/Properties/AssemblyInfo.cs
+++
@@ -1,3 +0,0 @@
-[assembly: System.Runtime.InteropServices.ComVisible(false)]
-[assembly: System.Runtime.InteropServices.Guid("a4ed4cac-c55c-4f8c-83ca-b4a2ede918d3")]
-[assembly: System.Reflection.AssemblyVersion("5.0.0.0")]
