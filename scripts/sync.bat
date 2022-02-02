e9a8af37301f420b99c052d941a73ef100719805
Demis Bellot <demis.bellot@gmail.com>
2022-02-02 21:46:24 +0800

    Add scripts to sync Directory.Build.props to /src + /tests

---
+++ scripts/sync.bat
@@ -0,0 +1,30 @@
+COPY src\*.* ..\ServiceStack\src\
+COPY tests\*.* ..\ServiceStack\tests\
+
+COPY src\*.* ..\ServiceStack.Aws\src\
+COPY tests\*.* ..\ServiceStack.Aws\tests\
+
+COPY src\*.* ..\ServiceStack.Azure\src\
+COPY tests\*.* ..\ServiceStack.Azure\tests\
+
+COPY src\*.* ..\ServiceStack.Blazor\src\
+COPY tests\*.* ..\ServiceStack.Blazor\tests\
+
+COPY src\*.* ..\ServiceStack.CefGlue\src\
+COPY tests\*.* ..\ServiceStack.CefGlue\tests\
+
+COPY src\*.* ..\ServiceStack.Logging\src\
+COPY tests\*.* ..\ServiceStack.Logging\tests\
+
+COPY src\*.* ..\ServiceStack.OrmLite\src\
+COPY tests\*.* ..\ServiceStack.OrmLite\tests\
+
+COPY src\*.* ..\ServiceStack.Redis\src\
+COPY tests\*.* ..\ServiceStack.Redis\tests\
+
+COPY src\*.* ..\ServiceStack.Stripe\src\
+COPY tests\*.* ..\ServiceStack.Stripe\tests\
+
+COPY src\*.* ..\ServiceStack.Text\src\
+COPY tests\*.* ..\ServiceStack.Text\tests\
+
