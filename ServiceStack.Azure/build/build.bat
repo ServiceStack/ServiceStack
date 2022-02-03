70f929101841b53a3359f9cf066fb459843a24cb
Demis Bellot <demis.bellot@gmail.com>
2022-02-03 19:06:00 +0800

    sync new build.bat

--- ServiceStack.Azure/build/build.bat
+++ ServiceStack.Azure/build/build.bat
@@ -1,4 +1,4 @@
-for /f "usebackq tokens=*" %%i in (`..\..\scripts\vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
+for /f "usebackq tokens=*" %%i in (`..\..\build\vswhere.exe -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
   SET MSBUILD="%%i"
 )
 %MSBUILD% build.proj /property:Configuration=Release;MinorVersion=0;PatchVersion=3
