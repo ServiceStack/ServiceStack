16339bd6904e62d5315a6ee5ddd33d7d1f3ed936
Demis Bellot <demis.bellot@gmail.com>
2022-02-04 16:11:06 +0800

    Create build-all.bat

---
+++ build/build-all.bat
@@ -0,0 +1,18 @@
+cd ..\ServiceStack\build
+build.bat
+cd ..\..\ServiceStack.Aws\build
+build.bat
+cd ..\..\ServiceStack.Azure\build
+build.bat
+cd ..\..\ServiceStack.Blazor\build
+build.bat
+cd ..\..\ServiceStack.Logging\build
+build.bat
+cd ..\..\ServiceStack.OrmLite\build
+build.bat
+cd ..\..\ServiceStack.Redis\build
+build.bat
+cd ..\..\ServiceStack.Stripe\build
+build.bat
+cd ..\..\ServiceStack.Text\build
+build.bat
