5d15dee70af08d9934a29f33827c5d4e9d92b9cd
Demis Bellot <demis.bellot@gmail.com>
2022-02-06 09:03:10 +0800

    Update DateTimeExtensions.cs

--- ServiceStack.Text/src/ServiceStack.Text/DateTimeExtensions.cs
+++ ServiceStack.Text/src/ServiceStack.Text/DateTimeExtensions.cs
@@ -46,6 +46,9 @@ namespace ServiceStack.Text
             return (dateTime.ToStableUniversalTime().Ticks - UnixEpoch) / TimeSpan.TicksPerMillisecond;
         }
 
+        public static long ToUnixTimeMs(this DateTimeOffset dateTimeOffset) => 
+            (long)ToDateTimeSinceUnixEpoch(dateTimeOffset.UtcDateTime).TotalMilliseconds;
+
         public static long ToUnixTimeMs(this DateTime dateTime)
         {
             var universal = ToDateTimeSinceUnixEpoch(dateTime);
