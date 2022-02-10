12a07190dcf0008f9068dc8907633710a99fcb5a
Darren Reid <darren@reidmail.org>
2022-02-10 11:55:31 +1100

    Add staging script.

---
+++ build/stage-output.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+rm -rf ./staging
+mkdir -p staging
+cp ../ServiceStack/NuGet/*.* ./staging/
+cp ../ServiceStack.Aws/NuGet/*.* ./staging/
+cp ../ServiceStack.Azure/NuGet/*.* ./staging/
+cp ../ServiceStack.Blazor/NuGet/*.* ./staging/
+cp ../ServiceStack.CefGlue/NuGet/*.* ./staging/
+cp ../ServiceStack.Logging/NuGet/*.* ./staging/
+cp ../ServiceStack.OrmLite/NuGet/*.* ./staging/
+cp ../ServiceStack.Redis/NuGet/*.* ./staging/
+cp ../ServiceStack.Stripe/NuGet/*.* ./staging/
+cp ../ServiceStack.Text/NuGet/*.* ./staging/
+cp ../ServiceStack.Core/NuGet/*.* ./staging/
+
+
