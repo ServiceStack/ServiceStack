8e3e59bf97704f9162608794a11478c4ba9e1850
Demis Bellot <demis.bellot@gmail.com>
2022-03-03 12:23:26 +0800

    Add UserAuthBase + UserAuthDetailsBase

--- ServiceStack/src/ServiceStack.Interfaces/Auth/IUserAuth.cs
+++ ServiceStack/src/ServiceStack.Interfaces/Auth/IUserAuth.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using ServiceStack.DataAnnotations;
 
 namespace ServiceStack.Auth
 {
@@ -24,6 +25,56 @@ namespace ServiceStack.Auth
         DateTime ModifiedDate { get; set; }
     }
 
+    public class UserAuthBase : IUserAuth
+    {
+        [AutoIncrement]
+        public virtual int Id { get; set; }
+
+        [Index]
+        public virtual string UserName { get; set; }
+        [Index]
+        public virtual string Email { get; set; }
+
+        public virtual string DisplayName { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Company { get; set; }
+        public virtual string PhoneNumber { get; set; }
+        public virtual DateTime? BirthDate { get; set; }
+        public virtual string BirthDateRaw { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string Address2 { get; set; }
+        public virtual string City { get; set; }
+        public virtual string State { get; set; }
+        public virtual string Country { get; set; }
+        public virtual string Culture { get; set; }
+        public virtual string FullName { get; set; }
+        public virtual string Gender { get; set; }
+        public virtual string Language { get; set; }
+        public virtual string MailAddress { get; set; }
+        public virtual string Nickname { get; set; }
+        public virtual string PostalCode { get; set; }
+        public virtual string TimeZone { get; set; }
+        
+        public virtual string PrimaryEmail { get; set; }
+        public virtual string Salt { get; set; }
+        public virtual string PasswordHash { get; set; }
+        public virtual string DigestHa1Hash { get; set; }
+        public virtual List<string> Roles { get; set; } = new();
+        public virtual List<string> Permissions { get; set; } = new();
+        //Custom reference data
+        public virtual int? RefId { get; set; }
+        public virtual string RefIdStr { get; set; }
+
+        public virtual int InvalidLoginAttempts { get; set; }
+        public virtual DateTime? LastLoginAttempt { get; set; }
+        public virtual DateTime? LockedDate { get; set; }
+
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+        public virtual Dictionary<string, string> Meta { get; set; }
+    }
+    
     public interface IUserAuthDetails : IAuthTokens, IMeta
     {
         int Id { get; set; }
@@ -72,4 +123,53 @@ namespace ServiceStack.Auth
         string RequestTokenSecret { get; set; }
         Dictionary<string, string> Items { get; set; }
     }
+    
+    public class UserAuthDetailsBase : IUserAuthDetails
+    {
+        [AutoIncrement]
+        public virtual int Id { get; set; }
+
+        public virtual int UserAuthId { get; set; }
+        public virtual string Provider { get; set; }
+        public virtual string UserId { get; set; }
+        public virtual string UserName { get; set; }
+        public virtual string FullName { get; set; }
+        public virtual string DisplayName { get; set; }
+        public virtual string FirstName { get; set; }
+        public virtual string LastName { get; set; }
+        public virtual string Company { get; set; }
+        public virtual string Email { get; set; }
+        public virtual string PhoneNumber { get; set; }
+
+        public virtual DateTime? BirthDate { get; set; }
+        public virtual string BirthDateRaw { get; set; }
+        public virtual string Address { get; set; }
+        public virtual string Address2 { get; set; }
+        public virtual string City { get; set; }
+        public virtual string State { get; set; }
+        public virtual string Country { get; set; }
+        public virtual string Culture { get; set; }
+        public virtual string Gender { get; set; }
+        public virtual string Language { get; set; }
+        public virtual string MailAddress { get; set; }
+        public virtual string Nickname { get; set; }
+        public virtual string PostalCode { get; set; }
+        public virtual string TimeZone { get; set; }
+
+        public virtual string RefreshToken { get; set; }
+        public virtual DateTime? RefreshTokenExpiry { get; set; }
+        public virtual string RequestToken { get; set; }
+        public virtual string RequestTokenSecret { get; set; }
+        public virtual Dictionary<string, string> Items { get; set; } = new();
+        public virtual string AccessToken { get; set; }
+        public virtual string AccessTokenSecret { get; set; }
+        public virtual DateTime CreatedDate { get; set; }
+        public virtual DateTime ModifiedDate { get; set; }
+
+        //Custom Reference Data
+        public virtual int? RefId { get; set; }
+        public virtual string RefIdStr { get; set; }
+        public virtual Dictionary<string, string> Meta { get; set; }
+    }
+    
 }
