2fbd17d3ead64a146a6d0bd85ffb6b7be6178ea2
Demis Bellot <demis.bellot@gmail.com>
2022-03-08 14:41:19 +0800

    Add WithAudit ext method

--- ServiceStack/src/ServiceStack/AutoQueryDataFeature.cs
+++ ServiceStack/src/ServiceStack/AutoQueryDataFeature.cs
@@ -1586,5 +1586,21 @@ namespace ServiceStack
             var pi = fieldExpr.ToPropertyInfo();
             q.AddCondition(QueryTerm.Or, pi, condition, value);
         }
+        
+        public static T WithAudit<T>(this T row, IRequest req, DateTime? date = null) where T : AuditBase =>
+            row.WithAudit(req.GetSession().UserAuthId, date);   
+
+        public static T WithAudit<T>(this T row, string by, DateTime? date = null) where T : AuditBase
+        {
+            var useDate = date ?? DateTime.UtcNow;
+            if (string.IsNullOrEmpty(row.CreatedBy))
+            {
+                row.CreatedBy = by;
+                row.CreatedDate = useDate;
+            }
+            row.ModifiedBy = by;
+            row.ModifiedDate = useDate;
+            return row;
+        }
     }
 }
