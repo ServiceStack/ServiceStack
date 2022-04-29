528ffc46f7d10be5eca0b4aa798f4af0bf81966b
Demis Bellot <demis.bellot@gmail.com>
2022-04-29 13:11:27 +0800

    Update ContainerTests.cs

--- ServiceStack/tests/NetCoreTests/ContainerTests.cs
+++ ServiceStack/tests/NetCoreTests/ContainerTests.cs
@@ -6,85 +6,85 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using NUnit.Framework;
 using ServiceStack;
+using ServiceStack.Auth;
+using ServiceStack.Testing;
 
-namespace NetCoreTests
+namespace NetCoreTests;
+
+public class ContainerTests
 {
-    public class ContainerTests
+    class AppHost : AppSelfHostBase
     {
-        class AppHost : AppSelfHostBase
+        public AppHost() : base(nameof(ContainerTests), typeof(ContainerTests).Assembly)
         {
-            public AppHost() : base(nameof(ContainerTests), typeof(ContainerTests).Assembly)
-            {
-                Configuration = new ConfigurationBuilder()
-                    .SetBasePath(Directory.GetCurrentDirectory())
-                    .AddJsonFile("appsettings.json")
-                    .Build();
-                AppSettings = new NetCoreAppSettings(Configuration);
-            }
+            Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            AppSettings = new NetCoreAppSettings(Configuration);
+        }
 
-            public override void Configure(Container container)
-            {
-                var String = AppSettings.GetString("String");
-                var Num = AppSettings.Get<int>("Num");
-            }
+        public override void Configure(Container container)
+        {
+            var String = AppSettings.GetString("String");
+            var Num = AppSettings.Get<int>("Num");
         }
+    }
         
-        [Route("/haskeys")]
-        class HasQueryStringKeysTestDto {}
+    [Route("/haskeys")]
+    class HasQueryStringKeysTestDto {}
         
-        [Route("/numberofkeys")]
-        class NumberOfKeysTestDto {}
+    [Route("/numberofkeys")]
+    class NumberOfKeysTestDto {}
 
-        class HasKeysService : Service
+    class HasKeysService : Service
+    {
+        public string Get(HasQueryStringKeysTestDto request)
         {
-            public string Get(HasQueryStringKeysTestDto request)
-            {
-                return this.Request.QueryString.HasKeys().ToString();
-            }
+            return this.Request.QueryString.HasKeys().ToString();
+        }
             
-            public string Get(NumberOfKeysTestDto request)
-            {
-                return this.Request.QueryString.Count.ToString();
-            }
-        }
-        
-        
-        [Test]
-        public void Can_resolve_dependency_in_multiple_AppHosts()
+        public string Get(NumberOfKeysTestDto request)
         {
-            using (var appHost = new AppHost().Init().Start("http://localhost:2000/"))
-            {
-                var logFactory = appHost.TryResolve<Microsoft.Extensions.Logging.ILoggerFactory>();
-                var log = logFactory.CreateLogger("categoryName");
-            }
+            return this.Request.QueryString.Count.ToString();
+        }
+    }
+        
+    [Test]
+    public void Can_resolve_dependency_in_multiple_AppHosts()
+    {
+        using (var appHost = new AppHost().Init().Start("http://localhost:2000/"))
+        {
+            var logFactory = appHost.TryResolve<Microsoft.Extensions.Logging.ILoggerFactory>();
+            var log = logFactory.CreateLogger("categoryName");
+        }
             
-            using (var appHost = new AppHost().Init().Start("http://localhost:2000/"))
-            {
-                var logFactory = appHost.TryResolve<Microsoft.Extensions.Logging.ILoggerFactory>();
-                var log = logFactory.CreateLogger("categoryName");
-            }
-        }
-        
-        [Test]
-        public async Task Can_use_haskeys()
+        using (var appHost = new AppHost().Init().Start("http://localhost:2000/"))
         {
-            using (var appHost = new AppHost().Init().Start("http://localhost:2000/"))
-            {
-                var client = new HttpClient();
-                var result = await client.GetStringAsync("http://localhost:2000/haskeys?test=foo");
-                Assert.That(result.ToLower(), Is.EqualTo("true"));
-            }
+            var logFactory = appHost.TryResolve<Microsoft.Extensions.Logging.ILoggerFactory>();
+            var log = logFactory.CreateLogger("categoryName");
         }
+    }
         
-        [Test]
-        public async Task Can_use_AllKeys()
+    [Test]
+    public async Task Can_use_haskeys()
+    {
+        using (var appHost = new AppHost().Init().Start("http://localhost:2000/"))
         {
-            using (var appHost = new AppHost().Init().Start("http://localhost:2000/"))
-            {
-                var client = new HttpClient();
-                var result = await client.GetStringAsync("http://localhost:2000/numberofkeys?test=foo");
-                Assert.That(result, Is.EqualTo("1"));
-            }
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("http://localhost:2000/haskeys?test=foo");
+            Assert.That(result.ToLower(), Is.EqualTo("true"));
+        }
+    }
+        
+    [Test]
+    public async Task Can_use_AllKeys()
+    {
+        using (var appHost = new AppHost().Init().Start("http://localhost:2000/"))
+        {
+            var client = new HttpClient();
+            var result = await client.GetStringAsync("http://localhost:2000/numberofkeys?test=foo");
+            Assert.That(result, Is.EqualTo("1"));
         }
     }
 }
