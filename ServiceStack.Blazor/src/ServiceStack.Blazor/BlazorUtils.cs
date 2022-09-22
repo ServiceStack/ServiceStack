5f956b38ff97a3520ef38ceac83f60aae72e3dcb
Demis Bellot <demis.bellot@gmail.com>
2022-09-22 11:15:11 +0800

    refactor FormatValue to avoid string builder

--- ServiceStack.Blazor/src/ServiceStack.Blazor/BlazorUtils.cs
+++ ServiceStack.Blazor/src/ServiceStack.Blazor/BlazorUtils.cs
@@ -16,7 +16,8 @@ public static class BlazorUtils
             Console.WriteLine(message ?? "");
     }
 
-    public static string FormatValue(object? value)
+    public static string FormatValue(object? value) => FormatValue(value, BlazorConfig.Instance.MaxFieldLength);
+    public static string FormatValue(object? value, int maxFieldLength)
     {
         if (value == null)
             return string.Empty;
@@ -24,9 +25,9 @@ public static class BlazorUtils
         if (TextUtils.IsComplexType(value?.GetType()))
         {
             var str = TypeSerializer.Dump(value).TrimStart();
-            if (str.Length < BlazorConfig.Instance.MaxFieldLength)
+            if (str.Length < maxFieldLength)
                 return str;
-            var to = TextUtils.Truncate(str, BlazorConfig.Instance.MaxFieldLength);
+            var to = TextUtils.Truncate(str, maxFieldLength);
             if (to.StartsWith("{"))
                 return to + "... }";
             else if (to.StartsWith("["))
@@ -35,7 +36,51 @@ public static class BlazorUtils
         }
         {
             var str = TextUtils.GetScalarText(value);
-            return TextUtils.Truncate(str, BlazorConfig.Instance.MaxFieldLength);
+            return TextUtils.Truncate(str, maxFieldLength);
         }
     }
+
+    public static string FormatValueAsHtml(object? Value)
+    {
+        string wrap(string raw, string html) => $"<span title=\"{raw.HtmlEncode()}\">" + html + "</span>";
+
+        var sb = StringBuilderCache.Allocate();
+        if (Value is System.Collections.IEnumerable e)
+        {
+            var first = TextUtils.FirstOrDefault(e);
+            if (first == null)
+                return "[]";
+
+            if (TextUtils.IsComplexType(first.GetType()))
+                return wrap(TypeSerializer.Dump(Value).HtmlEncode(), FormatValue(Value));
+
+            foreach (var item in e)
+            {
+                if (sb.Length > 0)
+                    sb.Append(',');
+                sb.Append(TextUtils.GetScalarText(item));
+            }
+
+        }
+        var dict = Value.ToObjectDictionary();
+        var keys = dict.Keys.ToList();
+        var len = Math.Min(BlazorConfig.Instance.MaxNestedFields, keys.Count);
+        for (var i = 0; i < len; i++)
+        {
+            var key = keys[i];
+            var val = dict[key];
+            var value = FormatValue(val, BlazorConfig.Instance.MaxFieldLength);
+            var str = TextUtils.Truncate(value, BlazorConfig.Instance.MaxNestedFieldLength).HtmlEncode();
+            if (sb.Length > 0)
+                sb.Append(", ");
+
+            sb.AppendLine($"<b class=\"font-medium\">{key}</b>: {str}");
+        }
+        if (keys.Count > len)
+            sb.AppendLine("...");
+
+        var html = StringBuilderCache.ReturnAndFree(sb);
+        return wrap(TypeSerializer.Dump(Value).HtmlEncode(), "{ " + html + " }");
+    }
+
 }
