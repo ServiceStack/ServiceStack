2927bb9d19a25c8485f4d1740c141e0eb969e5ee
Demis Bellot <demis.bellot@gmail.com>
2022-09-27 21:41:10 +0800

    Fix unique Resource VFS files in files/* ModuleHandler

--- ServiceStack/src/ServiceStack/HtmlModules/FilesHandler.cs
+++ ServiceStack/src/ServiceStack/HtmlModules/FilesHandler.cs
@@ -17,6 +17,25 @@ public class FilesHandler : IHtmlModulesHandler
     public string Name { get; }
     public FilesHandler(string name) => Name = name;
     public Func<HtmlModuleContext,IVirtualPathProvider>? VirtualFilesResolver { get; set; }
+
+    public Func<string, string> NormalizeVirtualPath { get; set; } = DefaultNormalizeVirtualPath;
+
+    // Resource VFS replaces '-' with '_' 
+    public static Dictionary<string, string> NormalizedPaths { get; set; } = new()
+    {
+        ["modules/admin_ui"] = "modules/admin-ui"
+    };
+
+    public static string DefaultNormalizeVirtualPath(string path)
+    {
+        foreach (var entry in NormalizedPaths)
+        {
+            if (path.StartsWith(entry.Key))
+                return entry.Value + path.Substring(entry.Key.Length);
+        }
+        return path;
+    }
+
     public ReadOnlyMemory<byte> Execute(HtmlModuleContext ctx, string paths)
     {
         return ctx.Cache($"{Name}:{ctx.Module.DirPath}:{paths}", _ => {
@@ -26,14 +45,21 @@ public class FilesHandler : IHtmlModulesHandler
                 ? paths
                 : ctx.Module.DirPath.CombineWith(paths);
 
-            var allFiles = useVfs.GetAllMatchingFiles(usePath);
-            // Remove duplicates (from overriding), last file wins
-            var allFilesMap = new Dictionary<string, IVirtualFile>();
+            var allFiles = useVfs.GetAllMatchingFiles(usePath).ToList();
+
+            // Remove duplicates (from overriding), first file wins
+            var uniqueFiles = new List<IVirtualFile>();
+            var existingFiles = new HashSet<string>(); 
             foreach (var file in allFiles)
             {
-                allFilesMap[file.VirtualPath] = file;
+                var path = NormalizeVirtualPath(file.VirtualPath);
+                if (existingFiles.Contains(path))
+                    continue;
+                existingFiles.Add(path);
+                uniqueFiles.Add(file);
             }
-            var sortedFiles = allFilesMap.Values
+            
+            var sortedFiles = uniqueFiles
                 .OrderBy(file => file.VirtualPath).ToList();
             foreach (var file in sortedFiles)
             {
