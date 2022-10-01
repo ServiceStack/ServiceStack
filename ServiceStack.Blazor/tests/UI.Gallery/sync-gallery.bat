ea94a2defef27729b42df61a18b883efc4ccb186
Demis Bellot <demis.bellot@gmail.com>
2022-10-01 14:24:09 +0800

    Update sync-gallery.bat

--- ServiceStack.Blazor/tests/UI.Gallery/sync-gallery.bat
+++ ServiceStack.Blazor/tests/UI.Gallery/sync-gallery.bat
@@ -3,37 +3,57 @@ REM Copies from Gallery.Server -> Gallery.Wasm -> blazor-gallery.servicestack.ne
 REM Update Gallery.Wasm
 SET TO=Gallery.Wasm
 
-RD /q /s %TO%\Client\Pages
+RD /q /s %TO%\Gallery.Wasm\Pages
 
-RD /q /s %TO%\Server\Migrations
-XCOPY /Y /E /H /C /I Gallery.Server\Migrations %TO%\Server\Migrations
+RD /q /s %TO%\Gallery.Wasm.Server\Migrations
+XCOPY /Y /E /H /C /I Gallery.Server\Migrations %TO%\Gallery.Wasm.Server\Migrations
 
 RD /q /s %TO%\Server\ServiceInterface
-XCOPY /Y /E /H /C /I Gallery.Server\ServiceInterface %TO%\Server\ServiceInterface
+XCOPY /Y /E /H /C /I Gallery.Server\ServiceInterface %TO%\Gallery.Wasm.Server\ServiceInterface
 
-RD /q /s %TO%\Client\ServiceModel
-XCOPY /Y /E /H /C /I Gallery.Server\ServiceModel %TO%\Client\ServiceModel
+RD /q /s %TO%\Gallery.Wasm\ServiceModel
+XCOPY /Y /E /H /C /I Gallery.Server\ServiceModel %TO%\Gallery.Wasm\ServiceModel
 
-XCOPY /Y /E /H /C /I Gallery.Server\Pages %TO%\Client\Pages
-DEL %TO%\Client\Pages\*.cshtml
+XCOPY /Y /E /H /C /I Gallery.Server\Pages %TO%\Gallery.Wasm\Pages
+DEL %TO%\Gallery.Wasm\Pages\*.cshtml
 
-RD /q /s %TO%\Client\wwwroot\css
-XCOPY /Y /E /H /C /I Gallery.Server\wwwroot\css %TO%\Client\wwwroot\css
+RD /q /s %TO%\Gallery.Wasm\wwwroot\css
+XCOPY /Y /E /H /C /I Gallery.Server\wwwroot\css %TO%\Gallery.Wasm\wwwroot\css
 
-RD /q /s %TO%\Client\wwwroot\img
-XCOPY /Y /E /H /C /I Gallery.Server\wwwroot\img %TO%\Client\wwwroot\img
+RD /q /s %TO%\Gallery.Wasm\wwwroot\img
+XCOPY /Y /E /H /C /I Gallery.Server\wwwroot\img %TO%\Gallery.Wasm\wwwroot\img
 
-RD /q /s %TO%\Client\wwwroot\tailwind
-XCOPY /Y /E /H /C /I Gallery.Server\wwwroot\tailwind %TO%\Client\wwwroot\tailwind
+RD /q /s %TO%\Gallery.Wasm\wwwroot\tailwind
+XCOPY /Y /E /H /C /I Gallery.Server\wwwroot\tailwind %TO%\Gallery.Wasm\wwwroot\tailwind
 
 
 REM Update blazor-gallery.servicestack.net
-REM SET TO=C:\src\netcore\BlazorGallery
+SET TO=C:\src\netcore\BlazorGallery
 
-REM MOVE %TO%\Gallery.Server\Gallery.Server.csproj .
+MOVE %TO%\Gallery.Server\Gallery.Server.csproj .
 
-REM RD /q /s %TO%\Gallery.Server
+RD /q /s %TO%\Gallery.Server
 
-REM XCOPY /Y /E /H /C /I Gallery.Server %TO%\Gallery.Server
+XCOPY /Y /E /H /C /I Gallery.Server %TO%\Gallery.Server
 
-REM MOVE Gallery.Server.csproj %TO%\Gallery.Server\
+MOVE Gallery.Server.csproj %TO%\Gallery.Server\
+
+
+REM Update blazor-gallery.jamstacks.net
+SET TO=C:\src\netcore\BlazorGalleryWasm
+
+MOVE %TO%\Gallery.Wasm\Gallery.Wasm.csproj .
+
+RD /q /s %TO%\Gallery.Wasm
+
+XCOPY /Y /E /H /C /I Gallery.Wasm\Gallery.Wasm %TO%\Gallery.Wasm
+
+MOVE Gallery.Wasm.csproj %TO%\Gallery.Wasm\
+
+MOVE %TO%\Gallery.Wasm.Server\Gallery.Wasm.Server.csproj .
+
+RD /q /s %TO%\Gallery.Wasm.Server
+
+XCOPY /Y /E /H /C /I Gallery.Wasm\Gallery.Wasm.Server %TO%\Gallery.Wasm.Server
+
+MOVE Gallery.Wasm.Server.csproj %TO%\Gallery.Wasm.Server\
