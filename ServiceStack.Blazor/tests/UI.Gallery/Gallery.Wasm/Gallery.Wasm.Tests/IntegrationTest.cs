2097613538b7f5cec176ad98cc993f8b0b0425ac
Demis Bellot <demis.bellot@gmail.com>
2022-10-02 10:07:55 +0800

    Add tests to Gallery.Wasm

---
+++ ServiceStack.Blazor/tests/UI.Gallery/Gallery.Wasm/Gallery.Wasm.Tests/IntegrationTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using Funq;
+using ServiceStack;
+using MyApp.ServiceInterface;
+using MyApp.ServiceModel;
+
+namespace MyApp.Tests;
+
+public class IntegrationTest
+{
+    const string BaseUri = "http://localhost:2000/";
+    private readonly ServiceStackHost appHost;
+
+    class AppHost : AppSelfHostBase
+    {
+        public AppHost() : base(nameof(IntegrationTest), typeof(MyServices).Assembly) { }
+
+        public override void Configure(Container container)
+        {
+        }
+    }
+
+    public IntegrationTest()
+    {
+        appHost = new AppHost()
+            .Init()
+            .Start(BaseUri);
+    }
+
+    [OneTimeTearDown]
+    public void OneTimeTearDown() => appHost.Dispose();
+
+    public IServiceClient CreateClient() => new JsonServiceClient(BaseUri);
+
+    [Test]
+    public void Can_call_Hello_Service()
+    {
+        var client = CreateClient();
+
+        var response = client.Get(new Hello { Name = "World" });
+
+        Assert.That(response.Result, Is.EqualTo("Hello, World!"));
+    }
+}
