a96ecca0a421a188346c400ffbb05e13793e5172
Demis Bellot <demis.bellot@gmail.com>
2022-10-04 14:36:43 +0800

    Update sync-tailwind

--- ServiceStack.Blazor/tests/sync-tailwind.bat
+++ ServiceStack.Blazor/tests/sync-tailwind.bat
@@ -2,6 +2,13 @@ REM Update servicestack-blazor.js
 COPY ServiceStack.Blazor.Tailwind.Tests\Client\wwwroot\js\servicestack-blazor.js ..\..\..\ServiceStack\src\ServiceStack\js\
 
 REM Copy local Blazor Server -> local WASM
+RD /q /s ServiceStack.Blazor.Tailwind.Tests\Client\wwwroot\img
+XCOPY /Y /E /H /C /I ServiceStack.Blazor.Server.Tests\Server\wwwroot\img ServiceStack.Blazor.Tailwind.Tests\Client\wwwroot\img
+RD /q /s ServiceStack.Blazor.Tailwind.Tests\Client\wwwroot\css
+XCOPY /Y /E /H /C /I ServiceStack.Blazor.Server.Tests\Server\wwwroot\css ServiceStack.Blazor.Tailwind.Tests\Client\wwwroot\css
+RD /q /s ServiceStack.Blazor.Tailwind.Tests\Client\wwwroot\tailwind
+XCOPY /Y /E /H /C /I ServiceStack.Blazor.Server.Tests\Server\wwwroot\tailwind ServiceStack.Blazor.Tailwind.Tests\Client\wwwroot\tailwind
+
 RD /q /s ServiceStack.Blazor.Tailwind.Tests\Client\Pages
 XCOPY /Y /E /H /C /I ServiceStack.Blazor.Server.Tests\Server\Pages ServiceStack.Blazor.Tailwind.Tests\Client\Pages
 DEL ServiceStack.Blazor.Tailwind.Tests\Client\Pages\*.cshtml
@@ -23,6 +30,28 @@ XCOPY /Y /E /H /C /I ServiceStack.Blazor.Server.Tests\Server\Migrations ServiceS
 
 DEL ServiceStack.Blazor.Tailwind.Tests\ServiceModel\*.cs
 COPY ServiceStack.Blazor.Server.Tests\ServiceModel\*.cs ServiceStack.Blazor.Tailwind.Tests\ServiceModel\
+
+REM Sync local Server -> blazor-server
+SET TO=%NetCoreTemplates%\blazor-server
+
+COPY %TO%\MyApp\MyApp.csproj .
+RD /q /s %TO%\MyApp
+XCOPY /Y /E /H /C /I ServiceStack.Blazor.Server.Tests\Server %TO%\MyApp
+RD /q /s %TO%\MyApp\ServiceInterface\
+MOVE MyApp.csproj %TO%\MyApp\
+
+COPY %TO%\MyApp.ServiceModel\MyApp.ServiceModel.csproj .
+RD /q /s %TO%\MyApp.ServiceModel
+XCOPY /Y /E /H /C /I ServiceStack.Blazor.Server.Tests\ServiceModel %TO%\MyApp.ServiceModel
+MOVE MyApp.ServiceModel.csproj %TO%\MyApp.ServiceModel\
+
+COPY %TO%\MyApp.Tests\MyApp.Tests.csproj .
+RD /q /s %TO%\MyApp.Tests
+XCOPY /Y /E /H /C /I ServiceStack.Blazor.Server.Tests\Tests %TO%\MyApp.Tests
+MOVE MyApp.Tests.csproj %TO%\MyApp.Tests\
+
+DEL %TO%\MyApp.ServiceInterface\*.cs
+COPY ServiceStack.Blazor.Server.Tests\Server\ServiceInterface\*.cs %TO%\MyApp.ServiceInterface\
 
 
 REM Sync local WASM -> blazor-tailwind
