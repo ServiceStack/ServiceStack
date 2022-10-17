3f1c7edd71c09f45c7b4d5893cccfe7b097c6b89
Demis Bellot <demis.bellot@gmail.com>
2022-10-18 03:38:20 +0800

    fix close button classs

--- ServiceStack.Blazor/src/ServiceStack.Blazor/CssDefaults.cs
+++ ServiceStack.Blazor/src/ServiceStack.Blazor/CssDefaults.cs
@@ -77,7 +77,7 @@ public static class CssDefaults
         public const string PanelClass = "pointer-events-auto w-screen xl:max-w-3xl md:max-w-xl max-w-lg";
         public const string TitlebarClass = "bg-gray-50 dark:bg-gray-900 px-4 py-6 sm:px-6";
         public const string HeadingClass = "text-lg font-medium text-gray-900 dark:text-gray-100";
-        public const string CloseButtonClass = "rounded-md bg-gray-50 dark:bg-gray-900 text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500";
+        public const string CloseButtonClass = "rounded-md bg-gray-50 dark:bg-gray-900 text-gray-400 dark:text-gray-500 hover:text-gray-500 dark:hover:text-gray-400 focus:outline-none focus:ring-2 focus:ring-indigo-500 dark:ring-offset-black";
 
         public static DataTransition SlideOverTransition = new DataTransition(
             entering: new(@class: "transform transition ease-in-out duration-500 sm:duration-700", from: "translate-x-full", to: "translate-x-0"),
