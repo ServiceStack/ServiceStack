cfc98d60a995f4183f00852ebae014ea46da2f2c
Demis Bellot <demis.bellot@gmail.com>
2022-10-19 23:26:50 +0800

    Add NavList component

---
+++ ServiceStack.Blazor/src/ServiceStack.Blazor/Components/Tailwind/NavList.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace ServiceStack.Blazor.Components.Tailwind;
+
+public partial class NavList : UiComponentBase
+{
+    [Parameter] public string? Title { get; set; }
+    [Parameter] public RenderFragment? ChildContent { get; set; }
+
+    readonly List<NavListItem> items = new();
+
+    internal void AddItem(NavListItem item)
+    {
+        items.Add(item);
+    }
+}
