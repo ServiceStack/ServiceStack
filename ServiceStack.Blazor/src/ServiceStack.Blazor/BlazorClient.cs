df827d7bbd59669f63f050294282f551fd9223e1
Demis Bellot <demis.bellot@gmail.com>
2022-10-26 17:02:52 +0800

    Update BlazorClient.cs

--- ServiceStack.Blazor/src/ServiceStack.Blazor/BlazorClient.cs
+++ ServiceStack.Blazor/src/ServiceStack.Blazor/BlazorClient.cs
@@ -1,39 +1,38 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 
-namespace ServiceStack.Blazor
+namespace ServiceStack.Blazor;
+
+public static class BlazorClient
 {
-    public static class BlazorClient
-    {
-        public static Func<HttpMessageHandler>? MessageHandlerFactory { get; set; } =
-            () => new EnableCorsMessageHandler();
+    public static Func<HttpMessageHandler>? MessageHandlerFactory { get; set; } =
+        () => new EnableCorsMessageHandler();
         
-        public static JsonApiClient Create(string baseUrl, Action<JsonApiClient>? configure = null) 
+    public static JsonApiClient Create(string baseUrl, Action<JsonApiClient>? configure = null) 
+    {
+        var client = new JsonApiClient(baseUrl) {
+            HttpMessageHandler = MessageHandlerFactory?.Invoke()
+        };
+        configure?.Invoke(client);
+        return client;
+    }
+
+    public static IHttpClientBuilder AddBlazorApiClient(this IServiceCollection services, string baseUrl) => 
+        services.AddBlazorApiClient(baseUrl, null);
+    public static IHttpClientBuilder AddBlazorApiClient(this IServiceCollection services, string baseUrl, Action<HttpClient>? configure)
+    {
+        if (BlazorConfig.Instance.UseLocalStorage)
         {
-            var client = new JsonApiClient(baseUrl) {
-                HttpMessageHandler = MessageHandlerFactory?.Invoke()
-            };
-            configure?.Invoke(client);
-            return client;
+            services.TryAddScoped<ILocalStorage,LocalStorage>();
+            services.TryAddScoped<LocalStorage>();
+            services.TryAddScoped<CachedLocalStorage>();
         }
 
-        public static IHttpClientBuilder AddBlazorApiClient(this IServiceCollection services, string baseUrl) => 
-            services.AddBlazorApiClient(baseUrl, null);
-        public static IHttpClientBuilder AddBlazorApiClient(this IServiceCollection services, string baseUrl, Action<HttpClient>? configure)
-        {
-            if (BlazorConfig.Instance.UseLocalStorage)
-            {
-                services.TryAddScoped<ILocalStorage,LocalStorage>();
-                services.TryAddScoped<LocalStorage>();
-                services.TryAddScoped<CachedLocalStorage>();
-            }
-
-            services.AddTransient<EnableCorsMessageHandler>();
-            return services.AddHttpClient<JsonApiClient>(client => {
-                    client.BaseAddress = new Uri(baseUrl);
-                    configure?.Invoke(client);
-                })
-                .AddHttpMessageHandler<EnableCorsMessageHandler>();
-        }
+        services.AddTransient<EnableCorsMessageHandler>();
+        return services.AddHttpClient<JsonApiClient>(client => {
+                client.BaseAddress = new Uri(baseUrl);
+                configure?.Invoke(client);
+            })
+            .AddHttpMessageHandler<EnableCorsMessageHandler>();
     }
 }
