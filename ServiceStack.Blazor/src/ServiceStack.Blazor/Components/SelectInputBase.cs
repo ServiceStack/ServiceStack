b5683d454a5edb83c1c2fcc105143fb6bcda21a1
Demis Bellot <demis.bellot@gmail.com>
2022-11-25 10:40:03 +0800

    Add support for Values + Entries params directly on SelectInput

--- ServiceStack.Blazor/src/ServiceStack.Blazor/Components/SelectInputBase.cs
+++ ServiceStack.Blazor/src/ServiceStack.Blazor/Components/SelectInputBase.cs
@@ -8,11 +8,18 @@ public class SelectInputBase<TValue> : TextInputBase<TValue>
 
     [Parameter] public InputInfo? Input { get; set; }
 
-    protected List<KeyValuePair<string, string>> KvpValues() => Input?.AllowableEntries?.Length > 0 
-        ? Input!.AllowableEntries.ToList()
-        : Input?.AllowableValues.Length > 0 
-            ? TextUtils.ToKeyValuePairs(Input?.AllowableValues)
-            : TextUtils.ToKeyValuePairs(Options);
+    [Parameter] public string[]? Values { get; set; }
+    [Parameter] public KeyValuePair<string, string>[]? Entries { get; set; }
+
+    protected List<KeyValuePair<string, string>> KvpValues() => Entries?.Length > 0
+        ? Entries.ToList()
+        : Values?.Length > 0
+            ? TextUtils.ToKeyValuePairs(Values)
+            : Input?.AllowableEntries?.Length > 0
+                ? Input!.AllowableEntries.ToList()
+                : Input?.AllowableValues.Length > 0
+                    ? TextUtils.ToKeyValuePairs(Input?.AllowableValues)
+                    : TextUtils.ToKeyValuePairs(Options);
 
     [Parameter]
     public RenderFragment? ChildContent { get; set; }
