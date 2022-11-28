946053d03d39e665ebe50b987baa8fb2f3a37f41
Demis Bellot <demis.bellot@gmail.com>
2022-11-28 18:42:55 +0800

    Add ApiBaseUrl to BlazorConfig

--- ServiceStack.Blazor/src/ServiceStack.Blazor/BlazorConfig.cs
+++ ServiceStack.Blazor/src/ServiceStack.Blazor/BlazorConfig.cs
@@ -81,6 +81,10 @@ public class BlazorConfig
     public ImageInfo DefaultTableIcon { get; set; } = new ImageInfo { Svg = "<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'><g fill='none' stroke='currentColor' stroke-width='1.5'><path d='M5 12v6s0 3 7 3s7-3 7-3v-6'/><path d='M5 6v6s0 3 7 3s7-3 7-3V6'/><path d='M12 3c7 0 7 3 7 3s0 3-7 3s-7-3-7-3s0-3 7-3Z'/></g></svg>" };
     
     /// <summary>
+    /// Capture the Server Api BaseUrl
+    /// </summary>
+    public string? ApiBaseUrl { get; set; }
+    /// <summary>
     /// Prefix added to relative Asset URLs
     /// </summary>
     public string? AssetsBasePath { get; init; }
