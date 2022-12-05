97112d4f66482ddb362bb3705e88232b49de4f40
Jan Trejbal <jan.trejbal@aviationexam.com>
2022-12-05 17:20:34 +0100

    Add IMessageByteSerializer

---
+++ ServiceStack/src/ServiceStack.Client/Messaging/ServiceStackTextMessageByteSerializer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ServiceStack.Messaging;
+
+public class ServiceStackTextMessageByteSerializer : IMessageByteSerializer
+{
+    public byte[] ToBytes(IMessage message) => message.ToBytes();
+
+    public byte[] ToBytes<T>(IMessage<T> message) => message.ToBytes();
+
+    public IMessage ToMessage(byte[] bytes, Type ofType) => bytes.ToMessage(ofType);
+
+    public Message<T> ToMessage<T>(byte[] bytes) => bytes.ToMessage<T>();
+}
