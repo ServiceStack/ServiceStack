17a70e8cf3d00edbd189646453a0ac119acd0f4f
Demis Bellot <demis.bellot@gmail.com>
2022-12-06 01:00:14 +0800

    move ServiceStack.Messaging to file scoped namespace

--- ServiceStack/src/ServiceStack.Interfaces/Messaging/IMessageQueueClient.cs
+++ ServiceStack/src/ServiceStack.Interfaces/Messaging/IMessageQueueClient.cs
@@ -1,49 +1,48 @@
 using System;
 
-namespace ServiceStack.Messaging
+namespace ServiceStack.Messaging;
+
+public interface IMessageQueueClient
+    : IMessageProducer
 {
-    public interface IMessageQueueClient
-        : IMessageProducer
-    {
-        /// <summary>
-        /// Publish the specified message into the durable queue @queueName
-        /// </summary>
-        void Publish(string queueName, IMessage message);
+    /// <summary>
+    /// Publish the specified message into the durable queue @queueName
+    /// </summary>
+    void Publish(string queueName, IMessage message);
 
-        /// <summary>
-        /// Publish the specified message into the transient queue @queueName
-        /// </summary>
-        void Notify(string queueName, IMessage message);
+    /// <summary>
+    /// Publish the specified message into the transient queue @queueName
+    /// </summary>
+    void Notify(string queueName, IMessage message);
 
-        /// <summary>
-        /// Synchronous blocking get.
-        /// </summary>
-        IMessage<T> Get<T>(string queueName, TimeSpan? timeOut=null);
+    /// <summary>
+    /// Synchronous blocking get.
+    /// </summary>
+    IMessage<T> Get<T>(string queueName, TimeSpan? timeOut=null);
 
-        /// <summary>
-        /// Non blocking get message
-        /// </summary>
-        IMessage<T> GetAsync<T>(string queueName);
+    /// <summary>
+    /// Non blocking get message
+    /// </summary>
+    IMessage<T> GetAsync<T>(string queueName);
 
-        /// <summary>
-        /// Acknowledge the message has been successfully received or processed
-        /// </summary>
-        void Ack(IMessage message);
+    /// <summary>
+    /// Acknowledge the message has been successfully received or processed
+    /// </summary>
+    void Ack(IMessage message);
 
-        /// <summary>
-        /// Negative acknowledgement the message was not processed correctly
-        /// </summary>
-        void Nak(IMessage message, bool requeue, Exception exception = null);
+    /// <summary>
+    /// Negative acknowledgement the message was not processed correctly
+    /// </summary>
+    void Nak(IMessage message, bool requeue, Exception exception = null);
 
-        /// <summary>
-        /// Create a typed message from a raw MQ Response artefact
-        /// </summary>
-        IMessage<T> CreateMessage<T>(object mqResponse);
+    /// <summary>
+    /// Create a typed message from a raw MQ Response artefact
+    /// </summary>
+    IMessage<T> CreateMessage<T>(object mqResponse);
 
-        /// <summary>
-        /// Create a temporary Queue for Request / Reply
-        /// </summary>
-        /// <returns></returns>
-        string GetTempQueueName();
-    }
+    /// <summary>
+    /// Create a temporary Queue for Request / Reply
+    /// </summary>
+    /// <returns></returns>
+    string GetTempQueueName();
 }
--- ServiceStack/src/ServiceStack.Interfaces/Messaging/QueueNames.cs
+++ ServiceStack/src/ServiceStack.Interfaces/Messaging/QueueNames.cs
@@ -1,100 +1,98 @@
 using System;
 using System.Text;
 
-namespace ServiceStack.Messaging
+namespace ServiceStack.Messaging;
+
+/// <summary>
+/// Util static generic class to create unique queue names for types
+/// </summary>
+/// <typeparam name="T"></typeparam>
+public static class QueueNames<T>
 {
-    /// <summary>
-    /// Util static generic class to create unique queue names for types
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public static class QueueNames<T>
+    static QueueNames() 
     {
-        static QueueNames() 
-        {
-            Priority = QueueNames.ResolveQueueNameFn(typeof(T).Name, ".priorityq");
-            In = QueueNames.ResolveQueueNameFn(typeof(T).Name, ".inq");
-            Out = QueueNames.ResolveQueueNameFn(typeof(T).Name, ".outq");
-            Dlq = QueueNames.ResolveQueueNameFn(typeof(T).Name, ".dlq");
-        }
-
-        public static string Priority { get; private set; }
-
-        public static string In { get; private set; }
-
-        public static string Out { get; private set; }
-
-        public static string Dlq { get; private set; }
-
-        public static string[] AllQueueNames
-        {
-            get
-            {
-                return new[] {
-                    In,
-                    Priority,
-                    Out,
-                    Dlq,
-                };
-            }
-        }
+        Priority = QueueNames.ResolveQueueNameFn(typeof(T).Name, ".priorityq");
+        In = QueueNames.ResolveQueueNameFn(typeof(T).Name, ".inq");
+        Out = QueueNames.ResolveQueueNameFn(typeof(T).Name, ".outq");
+        Dlq = QueueNames.ResolveQueueNameFn(typeof(T).Name, ".dlq");
     }
 
-    /// <summary>
-    /// Util class to create unique queue names for runtime types
-    /// </summary>
-    public class QueueNames
+    public static string Priority { get; private set; }
+
+    public static string In { get; private set; }
+
+    public static string Out { get; private set; }
+
+    public static string Dlq { get; private set; }
+
+    public static string[] AllQueueNames
     {
-        public static string Exchange = "mx.servicestack";
-        public static string ExchangeDlq = "mx.servicestack.dlq";
-        public static string ExchangeTopic = "mx.servicestack.topic";
-
-        public static string MqPrefix = "mq:";
-        public static string QueuePrefix = "";
-
-        public static string TempMqPrefix = MqPrefix + "tmp:";
-        public static string TopicIn = MqPrefix + "topic:in";
-        public static string TopicOut = MqPrefix + "topic:out";
-
-        public static Func<string, string, string> ResolveQueueNameFn = ResolveQueueName;
-
-        public static string ResolveQueueName(string typeName, string queueSuffix)
+        get
         {
-            return QueuePrefix + MqPrefix + typeName + queueSuffix;
-        }
-
-        public static bool IsTempQueue(string queueName)
-        {
-            return queueName != null 
-                && queueName.StartsWith(TempMqPrefix, StringComparison.OrdinalIgnoreCase);
-        }
-
-        public static void SetQueuePrefix(string prefix)
-        {
-            TopicIn = prefix + MqPrefix + "topic:in";
-            TopicOut = prefix + MqPrefix + "topic:out";
-            QueuePrefix = prefix;
-            TempMqPrefix = prefix + MqPrefix + "tmp:";
-        }
-
-        private readonly Type messageType;
-
-        public QueueNames(Type messageType)
-        {
-            this.messageType = messageType;
-        }
-
-        public string Priority => ResolveQueueNameFn(messageType.Name, ".priorityq");
-
-        public string In => ResolveQueueNameFn(messageType.Name, ".inq");
-
-        public string Out => ResolveQueueNameFn(messageType.Name, ".outq");
-
-        public string Dlq => ResolveQueueNameFn(messageType.Name, ".dlq");
-
-        public static string GetTempQueueName()
-        {
-            return TempMqPrefix + Guid.NewGuid().ToString("n");
+            return new[] {
+                In,
+                Priority,
+                Out,
+                Dlq,
+            };
         }
     }
+}
 
+/// <summary>
+/// Util class to create unique queue names for runtime types
+/// </summary>
+public class QueueNames
+{
+    public static string Exchange = "mx.servicestack";
+    public static string ExchangeDlq = "mx.servicestack.dlq";
+    public static string ExchangeTopic = "mx.servicestack.topic";
+
+    public static string MqPrefix = "mq:";
+    public static string QueuePrefix = "";
+
+    public static string TempMqPrefix = MqPrefix + "tmp:";
+    public static string TopicIn = MqPrefix + "topic:in";
+    public static string TopicOut = MqPrefix + "topic:out";
+
+    public static Func<string, string, string> ResolveQueueNameFn = ResolveQueueName;
+
+    public static string ResolveQueueName(string typeName, string queueSuffix)
+    {
+        return QueuePrefix + MqPrefix + typeName + queueSuffix;
+    }
+
+    public static bool IsTempQueue(string queueName)
+    {
+        return queueName != null 
+            && queueName.StartsWith(TempMqPrefix, StringComparison.OrdinalIgnoreCase);
+    }
+
+    public static void SetQueuePrefix(string prefix)
+    {
+        TopicIn = prefix + MqPrefix + "topic:in";
+        TopicOut = prefix + MqPrefix + "topic:out";
+        QueuePrefix = prefix;
+        TempMqPrefix = prefix + MqPrefix + "tmp:";
+    }
+
+    private readonly Type messageType;
+
+    public QueueNames(Type messageType)
+    {
+        this.messageType = messageType;
+    }
+
+    public string Priority => ResolveQueueNameFn(messageType.Name, ".priorityq");
+
+    public string In => ResolveQueueNameFn(messageType.Name, ".inq");
+
+    public string Out => ResolveQueueNameFn(messageType.Name, ".outq");
+
+    public string Dlq => ResolveQueueNameFn(messageType.Name, ".dlq");
+
+    public static string GetTempQueueName()
+    {
+        return TempMqPrefix + Guid.NewGuid().ToString("n");
+    }
 }
