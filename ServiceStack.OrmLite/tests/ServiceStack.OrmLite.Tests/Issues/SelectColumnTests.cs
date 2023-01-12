e2ce8054c3ae99ef0cda1792838cce4c2714b7a0
Oleg Nadymov <OlegNadymov@gmail.com>
2023-01-12 13:32:57 +0300

    Fix in Column<T> with nullable db column (#1306)
    Cheers!

---
+++ ServiceStack.OrmLite/tests/ServiceStack.OrmLite.Tests/Issues/SelectColumnTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using ServiceStack.DataAnnotations;
+using System.Linq;
+
+namespace ServiceStack.OrmLite.Tests.Issues
+{
+    [TestFixtureOrmLite]
+    public class SelectColumnTests : OrmLiteProvidersTestBase
+    {
+        public SelectColumnTests(DialectContext context) : base(context) { }
+
+        /// <summary>
+        /// The class in only to create the nullable column <see cref="Decimal"/>.
+        /// </summary>
+        public class TestWithNullable
+        {
+            public int Id { get; set; }
+            public decimal? Decimal { get; set; }
+        }
+
+        [Alias("TestWithNullable")]
+        public class Test
+        {
+            public int Id { get; set; }
+            public decimal Decimal { get; set; }
+        }
+
+        [Test]
+        public void Should_Return_Column_Value_From_Nullable_Column()
+        {
+            using (var db = OpenDbConnection())
+            {
+                db.DropAndCreateTable<TestWithNullable>();
+
+                var row1 = new TestWithNullable
+                {
+                    Id = 0,
+                    Decimal = 1.1M,
+                };
+                db.Insert(row1);
+
+                var row2 = new TestWithNullable
+                {
+                    Id = 1,
+                    Decimal = null
+                };
+                db.Insert(row2);
+
+                var q = db.From<Test>().OrderBy(x => x.Id);
+
+                Assert.That(db.SqlColumn<(int, decimal)>(q).Sum(x => x.Item2), Is.EqualTo(row1.Decimal));
+                Assert.That(db.SqlColumn<Test>(q).Sum(x => x.Decimal), Is.EqualTo(row1.Decimal));
+                
+                Assert.That(db.Column<decimal>(q.Select(x => x.Decimal)).Sum(), Is.EqualTo(row1.Decimal));
+                Assert.That(db.SqlColumn<decimal>(q.Select(x => x.Decimal)).Sum(), Is.EqualTo(row1.Decimal));
+                Assert.That(db.Scalar<decimal>(q.Select(x => x.Decimal)), Is.EqualTo(row1.Decimal));
+            }
+        }
+    }
+}
