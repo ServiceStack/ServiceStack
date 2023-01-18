a79622896e71457c4cc45a7880a88405a778c961
Demis Bellot <demis.bellot@gmail.com>
2023-01-19 00:39:51 +0800

    remove dependency on existing types in locode/js/dtos.js

--- ServiceStack/tests/NorthwindAuto/locode/js/dtos.js
+++ ServiceStack/tests/NorthwindAuto/locode/js/dtos.js
@@ -1,6 +1,31 @@
-import { QueryDb, QueryResponse } from "../../lib/types"
+exports.QueryResponse = exports.QueryBase = exports.QueryDb = exports.GetCrudEvents = void 0
 
-exports.GetCrudEvents = void 0
+var QueryResponse = /** @class */ (function () {
+    function QueryResponse(init) {
+        Object.assign(this, init);
+    }
+    return QueryResponse;
+}());
+exports.QueryResponse = QueryResponse;
+
+var QueryBase = /** @class */ (function () {
+    function QueryBase(init) {
+        Object.assign(this, init);
+    }
+    return QueryBase;
+}());
+exports.QueryBase = QueryBase;
+
+var QueryDb = /** @class */ (function (_super) {
+    __extends(QueryDb, _super);
+    function QueryDb(init) {
+        var _this = _super.call(this, init) || this;
+        Object.assign(_this, init);
+        return _this;
+    }
+    return QueryDb;
+}(QueryBase));
+exports.QueryBase = QueryBase
 
 var GetCrudEvents = /** @class */ (function (_super) {
     __extends(GetCrudEvents, _super)
@@ -14,5 +39,4 @@ var GetCrudEvents = /** @class */ (function (_super) {
     GetCrudEvents.prototype.getMethod = function () { return 'GET' }
     return GetCrudEvents
 }(QueryDb))
-
 exports.GetCrudEvents = GetCrudEvents 
