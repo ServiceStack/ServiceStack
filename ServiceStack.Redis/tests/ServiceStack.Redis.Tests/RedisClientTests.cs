a1fe7583ceb92f5c6cb405d22a65edd4582f4241
Demis Bellot <demis.bellot@gmail.com>
2023-02-07 22:51:14 +0800

    Update RedisClientTests.cs

--- ServiceStack.Redis/tests/ServiceStack.Redis.Tests/RedisClientTests.cs
+++ ServiceStack.Redis/tests/ServiceStack.Redis.Tests/RedisClientTests.cs
@@ -382,7 +382,7 @@ namespace ServiceStack.Redis.Tests
             }
         }
 
-        [Test, [Ignore("Flaky in CI")]]
+        [Test, Ignore("Flaky in CI")]
         public void Can_AcquireLock_TimeOut()
         {
             // guid here is to prevent competition between concurrent runtime tests
