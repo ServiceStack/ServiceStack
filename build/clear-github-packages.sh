f4505500021f8ab660913bd80cb306271b960c8d
Darren Reid <darren@reidmail.org>
2023-02-15 14:53:04 +1100

    Update clear-github-packages.sh

--- build/clear-github-packages.sh
+++ build/clear-github-packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ls -1 | grep .nupkg | sed -e 's/\.[^.]*$//' | while read line
 do 
-  gh api --method DELETE -H "Accept: application/vnd.github+json" "/orgs/ServiceStack/packages/nuget/$line"
+  gh api --method DELETE -H "Accept: application/vnd.github+json" /orgs/ServiceStack/packages/nuget/${line}
 done
