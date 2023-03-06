8370c143c2dd555224adb5a92708d4342a935f5e
Michal Gajek <michal.gajek@invent.us>
2023-03-06 09:41:33 +0100

    fix invalid 2d array type declaration in TypeScriptGenerator

---
+++ ServiceStack/tests/ServiceStack.Common.Tests/NativeTypesTypescriptGeneratorTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using ServiceStack.NativeTypes.TypeScript;
+
+namespace ServiceStack.Common.Tests
+{
+    [TestFixture]
+    public class NativeTypesTypescriptGeneratorTests
+    {
+        [Test]
+        public void TypeScript_Generator_Generates_2D_Array()
+        {
+            // arrange
+            var sut = new TypeScriptGenerator(null);
+
+            // act
+            var result = sut.Type("String[][]", null);
+
+            // assert
+            Assert.AreEqual("string[][]", result);
+        }
+
+        [Test]
+        public void TypeScript_Generator_Generates_Single_Dimension_Array()
+        {
+            // arrange
+            var sut = new TypeScriptGenerator(null);
+
+            // act
+            var result = sut.Type("String[]", null);
+
+            // assert
+            Assert.AreEqual("string[]", result);
+        }
+    }
+}
