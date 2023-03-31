e4b7af0f860b81b98ddd6d6683060582caaed731
Demis Bellot <demis.bellot@gmail.com>
2023-03-31 11:02:50 +0800

    Add support for nullable values in SqlSpread/SqlJoin

--- ServiceStack.OrmLite/src/ServiceStack.OrmLite/OrmLiteUtils.cs
+++ ServiceStack.OrmLite/src/ServiceStack.OrmLite/OrmLiteUtils.cs
@@ -578,7 +578,9 @@ namespace ServiceStack.OrmLite
             {
                 if (sb.Length > 0) 
                     sb.Append(",");
-                sb.Append(dialect.GetQuotedValue(value, value.GetType()));
+                sb.Append(value is null 
+                    ? "NULL" 
+                    : dialect.GetQuotedValue(value, value.GetType()));
             }
 
             return StringBuilderCache.ReturnAndFree(sb);
