7e0cfc53c4eb2966e0a9972db751b75889be392b
Darren Reid <darren@reidmail.org>
2023-05-12 11:41:28 +1000

    Better isolate test DTOs.

--- ServiceStack/tests/ServiceStack.WebHost.Endpoints.Tests/UseCases/ApiMemberTests.cs
+++ ServiceStack/tests/ServiceStack.WebHost.Endpoints.Tests/UseCases/ApiMemberTests.cs
@@ -10,6 +10,47 @@ public class ApiMemberTests
 {
     private readonly ServiceStackHost appHost;
     
+    class ApiMemberHello : IReturn<ApiMemberHelloResponse>
+    {
+        public string Name { get; set; }
+    }
+
+    class ApiMemberHelloResponse
+    {
+        public string Result { get; set; }
+    }
+
+    class MyServices : Service
+    {
+        public object Any(ApiMemberHello request)
+        {
+            return new ApiMemberHelloResponse() { Result = "" };
+        }
+    }
+
+    class AppHost : AppSelfHostBase
+    {
+        public AppHost()
+            : base(nameof(ApiMemberTests), typeof(MyServices))
+        {
+        }
+        public override void Configure(Container container)
+        {
+            typeof(ApiMemberHello)
+                .GetProperty("Name")
+                .AddAttributes(
+                    new ApiMemberAttribute
+                    {
+                        ParameterType = "path",
+                        Name = "MetaName",
+                        DataType = "foo",
+                        Description = "The name of the person to say hello to.",
+                        IsRequired = true,
+                    },
+                    new DataMemberAttribute());
+        }
+    }
+    
     public ApiMemberTests()
     {
         appHost = new AppHost()
@@ -32,51 +73,13 @@ public class ApiMemberTests
         Assert.That(metadata, Is.Not.Null);
         Assert.That(metadata.Api, Is.Not.Null);
         Assert.That(metadata.Api.Operations, Is.Not.Null);
+        Assert.That(metadata.Api.Operations.Count, Is.EqualTo(1));
+        Assert.That(metadata.Api.Operations[0].Request, Is.Not.Null);
+        Assert.That(metadata.Api.Operations[0].Request.Name, Is.EqualTo("ApiMemberHello"));
         var props = metadata.Api.Operations[0].Request.Properties;
         Assert.That(props, Is.Not.Null);
         Assert.That(props.Any(x => x is { Name: "Name" }));
         var nameDesc = props.Find(x => x is { Name: "Name" })?.Description;
         Assert.That(nameDesc, Is.EqualTo("The name of the person to say hello to."));
-    }
-}
-
-public class Hello : IReturn<HelloResponse>
-{
-    public string Name { get; set; }
-}
-
-public class HelloResponse
-{
-    public string Result { get; set; }
-}
-
-public class MyServices : Service
-{
-    public object Any(Hello request)
-    {
-        return new HelloResponse() { Result = "" };
-    }
-}
-
-public class AppHost : AppSelfHostBase
-{
-    public AppHost()
-        : base(nameof(ApiMemberTests), typeof(MyServices))
-    {
-    }
-    public override void Configure(Container container)
-    {
-        typeof(Hello)
-            .GetProperty("Name")
-            .AddAttributes(
-                new ApiMemberAttribute
-                {
-                    ParameterType = "path",
-                    Name = "MetaName",
-                    DataType = "foo",
-                    Description = "The name of the person to say hello to.",
-                    IsRequired = true,
-                },
-                new DataMemberAttribute());
     }
 }
