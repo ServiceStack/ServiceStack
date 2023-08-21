fc42168c3bb2fddd3fcd0c7f329fb4358286d45d
Demis Bellot <demis.bellot@gmail.com>
2023-08-21 19:15:42 +0800

    Create TypeScriptMethods.cs

---
+++ ServiceStack/src/ServiceStack.Common/Script/Methods/TypeScriptMethods.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace ServiceStack.Script;
+
+public class TypeScriptPlugin : IScriptPlugin
+{
+    public void Register(ScriptContext context)
+    {
+        context.ScriptMethods.Add(new TypeScriptMethods());
+    }
+}
+
+public class TypeScriptMethods : ScriptMethods
+{
+    public IRawString tsUnionStrings(IEnumerable<string> strings)
+    {
+        return new RawString(string.Join(" | ", strings.Map(x => $"'{x}'")));
+    }
+
+    public IRawString tsUnionTypes(IEnumerable<string> strings)
+    {
+        return new RawString(string.Join(" | ", strings));
+    }
+}
