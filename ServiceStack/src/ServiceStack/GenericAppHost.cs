daa83c1565fc0eacdae2cf3b1863a213e029b418
Demis Bellot <demis.bellot@gmail.com>
2023-08-27 21:43:05 +0800

    Add UseServiceStack ext method for MQ's GenericAppHost

--- ServiceStack/src/ServiceStack/GenericAppHost.cs
+++ ServiceStack/src/ServiceStack/GenericAppHost.cs
@@ -2,46 +2,64 @@ using System;
 using System.Reflection;
 using Funq;
 using ServiceStack.Host;
+using ServiceStack.NetCore;
 using ServiceStack.Web;
 
-namespace ServiceStack
+namespace ServiceStack;
+
+public class GenericAppHost : ServiceStackHost
 {
-    public class GenericAppHost : ServiceStackHost
-    {
-        public GenericAppHost(params Assembly[] serviceAssemblies)
-            : base(typeof (GenericAppHost).GetOperationName(),
-                serviceAssemblies.Length > 0 ? serviceAssemblies : new[]
-                {
+    public GenericAppHost(params Assembly[] serviceAssemblies)
+        : base(typeof (GenericAppHost).GetOperationName(),
+            serviceAssemblies.Length > 0 ? serviceAssemblies : new[]
+            {
 #if !NETCORE
                     Assembly.GetExecutingAssembly()
 #else
-                    typeof(GenericAppHost).Assembly
+                typeof(GenericAppHost).Assembly
 #endif
-                })
-        {
-            Plugins.Clear();
-        }
-
-        public override void Configure(Container container)
-        {
-            ConfigureAppHost?.Invoke(this);
-            ConfigureContainer?.Invoke(container);
-        }
-
-        public Action<Container> ConfigureContainer { get; set; }
-
-        public Action<GenericAppHost> ConfigureAppHost { get; set; }
-
-        public Action<HostConfig> ConfigFilter { get; set; }
-
-        public override IServiceGateway GetServiceGateway(IRequest req) => 
-            base.GetServiceGateway(req ?? new BasicRequest());
-
-        public override void OnConfigLoad()
-        {
-            base.OnConfigLoad();
-
-            ConfigFilter?.Invoke(Config);
-        }
+            })
+    {
+        Plugins.Clear();
     }
+
+    public override void Configure(Container container)
+    {
+        ConfigureAppHost?.Invoke(this);
+        ConfigureContainer?.Invoke(container);
+    }
+
+    public Action<Container> ConfigureContainer { get; set; }
+
+    public Action<GenericAppHost> ConfigureAppHost { get; set; }
+
+    public Action<HostConfig> ConfigFilter { get; set; }
+
+    public override IServiceGateway GetServiceGateway(IRequest req) => 
+        base.GetServiceGateway(req ?? new BasicRequest());
+
+    public override void OnConfigLoad()
+    {
+        base.OnConfigLoad();
+
+        ConfigFilter?.Invoke(Config);
+    }
+    
+#if NETCORE
+    public Microsoft.Extensions.Hosting.IHost Host { get; set; }
+#endif    
 }
+
+public static class GenericAppHostExtensions
+{
+#if NETCORE
+    public static Microsoft.Extensions.Hosting.IHost UseServiceStack(this Microsoft.Extensions.Hosting.IHost host, GenericAppHost appHost)
+    {
+        appHost.Host = host;
+        appHost.Container.Adapter = new NetCoreContainerAdapter(host.Services);
+        appHost.Init();
+        return host;
+    }
+#endif    
+}
+
