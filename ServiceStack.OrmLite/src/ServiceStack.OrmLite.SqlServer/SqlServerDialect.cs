5414281a9fc67d864d2233cef1180d068887ce9f
Demis Bellot <demis.bellot@gmail.com>
2023-09-07 20:28:27 +0800

    Add SqlServer2022OrmLiteDialectProvider

--- ServiceStack.OrmLite/src/ServiceStack.OrmLite.SqlServer/SqlServerDialect.cs
+++ ServiceStack.OrmLite/src/ServiceStack.OrmLite.SqlServer/SqlServerDialect.cs
@@ -1,47 +1,51 @@
 using ServiceStack.OrmLite.SqlServer;
 
-namespace ServiceStack.OrmLite
+namespace ServiceStack.OrmLite;
+
+public static class SqlServerDialect
 {
-    public static class SqlServerDialect
-    {
-        public static IOrmLiteDialectProvider Provider => SqlServer2012Dialect.Instance;
-        public static SqlServerOrmLiteDialectProvider Instance => SqlServer2012Dialect.Instance;
-    }
+    public static IOrmLiteDialectProvider Provider => SqlServer2012Dialect.Instance;
+    public static SqlServerOrmLiteDialectProvider Instance => SqlServer2012Dialect.Instance;
+}
 
-    public static class SqlServer2008Dialect
-    {
-        public static IOrmLiteDialectProvider Provider => SqlServer2008OrmLiteDialectProvider.Instance;
-        public static SqlServer2008OrmLiteDialectProvider Instance => SqlServer2008OrmLiteDialectProvider.Instance;
-    }
+public static class SqlServer2008Dialect
+{
+    public static IOrmLiteDialectProvider Provider => SqlServer2008OrmLiteDialectProvider.Instance;
+    public static SqlServer2008OrmLiteDialectProvider Instance => SqlServer2008OrmLiteDialectProvider.Instance;
+}
 
-    public static class SqlServer2012Dialect
-    {
-        public static IOrmLiteDialectProvider Provider => SqlServer2012OrmLiteDialectProvider.Instance;
-        public static SqlServer2012OrmLiteDialectProvider Instance => SqlServer2012OrmLiteDialectProvider.Instance;
-    }
+public static class SqlServer2012Dialect
+{
+    public static IOrmLiteDialectProvider Provider => SqlServer2012OrmLiteDialectProvider.Instance;
+    public static SqlServer2012OrmLiteDialectProvider Instance => SqlServer2012OrmLiteDialectProvider.Instance;
+}
 
-    public static class SqlServer2014Dialect
-    {
-        public static IOrmLiteDialectProvider Provider => SqlServer2014OrmLiteDialectProvider.Instance;
-        public static SqlServer2014OrmLiteDialectProvider Instance => SqlServer2014OrmLiteDialectProvider.Instance;
-    }
+public static class SqlServer2014Dialect
+{
+    public static IOrmLiteDialectProvider Provider => SqlServer2014OrmLiteDialectProvider.Instance;
+    public static SqlServer2014OrmLiteDialectProvider Instance => SqlServer2014OrmLiteDialectProvider.Instance;
+}
 
-    public static class SqlServer2016Dialect
-    {
-        public static IOrmLiteDialectProvider Provider => SqlServer2016OrmLiteDialectProvider.Instance;
-        public static SqlServer2016OrmLiteDialectProvider Instance => SqlServer2016OrmLiteDialectProvider.Instance;
-    }
+public static class SqlServer2016Dialect
+{
+    public static IOrmLiteDialectProvider Provider => SqlServerOrmLiteDialectProviderVersions.Instance;
+    public static SqlServerOrmLiteDialectProviderVersions Instance => SqlServerOrmLiteDialectProviderVersions.Instance;
+}
 
-    public static class SqlServer2017Dialect
-    {
-        public static IOrmLiteDialectProvider Provider => SqlServer2017OrmLiteDialectProvider.Instance;
-        public static SqlServer2017OrmLiteDialectProvider Instance => SqlServer2017OrmLiteDialectProvider.Instance;
-    }
+public static class SqlServer2017Dialect
+{
+    public static IOrmLiteDialectProvider Provider => SqlServer2017OrmLiteDialectProvider.Instance;
+    public static SqlServer2017OrmLiteDialectProvider Instance => SqlServer2017OrmLiteDialectProvider.Instance;
+}
 
-    public static class SqlServer2019Dialect
-    {
-        public static IOrmLiteDialectProvider Provider => SqlServer2019OrmLiteDialectProvider.Instance;
-        public static SqlServer2019OrmLiteDialectProvider Instance => SqlServer2019OrmLiteDialectProvider.Instance;
-    }
+public static class SqlServer2019Dialect
+{
+    public static IOrmLiteDialectProvider Provider => SqlServer2019OrmLiteDialectProvider.Instance;
+    public static SqlServer2019OrmLiteDialectProvider Instance => SqlServer2019OrmLiteDialectProvider.Instance;
+}
 
+public static class SqlServer2022Dialect
+{
+    public static IOrmLiteDialectProvider Provider => SqlServer2022OrmLiteDialectProvider.Instance;
+    public static SqlServer2022OrmLiteDialectProvider Instance => SqlServer2022OrmLiteDialectProvider.Instance;
 }
---
+++ ServiceStack.OrmLite/src/ServiceStack.OrmLite.SqlServer/SqlServerOrmLiteDialectProviderVersions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+using ServiceStack.OrmLite.SqlServer.Converters;
+
+namespace ServiceStack.OrmLite.SqlServer;
+
+public class SqlServerOrmLiteDialectProviderVersions : SqlServer2014OrmLiteDialectProvider
+{
+	public SqlServerOrmLiteDialectProviderVersions() : base()
+	{
+		base.RegisterConverter<String>(new SqlServerJsonStringConverter());
+	}
+
+	public new static SqlServerOrmLiteDialectProviderVersions Instance = new();
+
+	public override SqlExpression<T> SqlExpression<T>() => new SqlServer2016Expression<T>(this);
+}
+
+public class SqlServer2017OrmLiteDialectProvider : SqlServerOrmLiteDialectProviderVersions
+{
+	public new static SqlServer2017OrmLiteDialectProvider Instance = new();
+}
+
+public class SqlServer2019OrmLiteDialectProvider : SqlServer2017OrmLiteDialectProvider
+{
+	public new static SqlServer2019OrmLiteDialectProvider Instance = new();
+}
+
+public class SqlServer2022OrmLiteDialectProvider : SqlServer2019OrmLiteDialectProvider
+{
+	public new static SqlServer2022OrmLiteDialectProvider Instance = new();
+}
