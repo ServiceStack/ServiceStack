ea74eab8e91db01ab577e36815dbec0e6a87f54d
Demis Bellot <demis.bellot@gmail.com>
2023-09-17 23:02:42 +0800

    Add support for using VirtualFiles to upload voice requests

--- ServiceStack/src/ServiceStack.GoogleCloud/GoogleCloudSpeechToText.cs
+++ ServiceStack/src/ServiceStack.GoogleCloud/GoogleCloudSpeechToText.cs
@@ -1,5 +1,7 @@
 ﻿using Google.Cloud.Speech.V2;
+using Google.Protobuf;
 using ServiceStack.AI;
+using ServiceStack.IO;
 
 namespace ServiceStack.GoogleCloud;
 
@@ -8,14 +10,22 @@ public class GoogleCloudSpeechConfig
     public string Project { get; set; } 
     public string Location { get; set; }
     public string Bucket { get; set; }
-    public string PhraseSetId { get; set; }
-    public string RecognizerId { get; set; }
+    public string? PhraseSetId { get; set; }
+    public string? RecognizerId { get; set; }
+
+    public RecognitionConfig RecognitionConfig { get; set; } = new()
+    {
+        AutoDecodingConfig = new AutoDetectDecodingConfig(),
+        LanguageCodes = { "en-US", "en-AU" },
+        Model = "latest_short",
+    };
 }
 
-public class GoogleCloudSpeechToText : ISpeechToText
+public class GoogleCloudSpeechToText : ISpeechToText, IRequireVirtualFiles
 {
     GoogleCloudSpeechConfig Config { get; }
     SpeechClient SpeechClient { get; }
+    public IVirtualFiles? VirtualFiles { get; set; }
 
     public GoogleCloudSpeechToText(GoogleCloudSpeechConfig config, SpeechClient speechClient)
     {
@@ -59,40 +69,51 @@ public class GoogleCloudSpeechToText : ISpeechToText
         }
         catch (Exception ignoreNonExistingRecognizer) {}
 
-        await SpeechClient.CreateRecognizerAsync(new CreateRecognizerRequest
+        var request = new CreateRecognizerRequest
         {
             Parent = $"projects/{Config.Project}/locations/{Config.Location}",
             RecognizerId = Config.RecognizerId,
             Recognizer = new Recognizer
             {
-                DefaultRecognitionConfig = new RecognitionConfig
-                {
-                    AutoDecodingConfig = new AutoDetectDecodingConfig(),
-                    LanguageCodes = { "en-US", "en-AU" },
-                    Model = "latest_short",
-                    Adaptation = new SpeechAdaptation
-                    {
-                        PhraseSets =
-                        {
-                            new SpeechAdaptation.Types.AdaptationPhraseSet
-                            {
-                                PhraseSet = $"projects/{Config.Project}/locations/{Config.Location}/phraseSets/{Config.PhraseSetId}"
-                            }
-                        }
-                    }
-                },
+                DefaultRecognitionConfig = Config.RecognitionConfig.Clone(),
             },
-        });
+        };
+        if (Config.PhraseSetId != null)
+        {
+            request.Recognizer.DefaultRecognitionConfig.Adaptation ??= new();
+            if (request.Recognizer.DefaultRecognitionConfig.Adaptation.PhraseSets.Count == 0)
+            {
+                request.Recognizer.DefaultRecognitionConfig.Adaptation.PhraseSets.Add(new SpeechAdaptation.Types.AdaptationPhraseSet
+                {
+                    PhraseSet = $"projects/{Config.Project}/locations/{Config.Location}/phraseSets/{Config.PhraseSetId}"
+                });
+            }
+        }
+
+        await SpeechClient.CreateRecognizerAsync(request);
     }
 
     public async Task<TranscriptResult> TranscribeAsync(string recordingPath, CancellationToken token = default)
     {
-        var response = await SpeechClient.RecognizeAsync(new RecognizeRequest
-        {
-            Recognizer = $"projects/{Config.Project}/locations/{Config.Location}/recognizers/{Config.RecognizerId}",
-            Uri = $"gs://{Config.Bucket}".CombineWith(recordingPath)
-        });
+        var recognizer = Config.RecognizerId ?? "_";
 
+        var request = new RecognizeRequest {
+            Recognizer = $"projects/{Config.Project}/locations/{Config.Location}/recognizers/{recognizer}",
+        };
+        if (VirtualFiles is null or GoogleCloudVirtualFiles)
+        {
+            request.Uri = $"gs://{Config.Bucket}".CombineWith(recordingPath);
+        }
+        else
+        {
+            var file = VirtualFiles.GetFile(recordingPath);
+            if (file == null)
+                throw new FileNotFoundException($"File not found {VirtualFiles.GetType().Name}:{recordingPath}");
+            await using var fileStream = file.OpenRead();
+            request.Content = await ByteString.FromStreamAsync(fileStream, token);
+        }
+
+        var response = await SpeechClient.RecognizeAsync(request);
         var alt = response.Results.Count > 0 && response.Results[0].Alternatives.Count > 0
             ? response.Results[0].Alternatives[0]
             : null;
