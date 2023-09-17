f4fa1e79a4ae3843bb4a99e684f8b6902ebf4b89
Demis Bellot <demis.bellot@gmail.com>
2023-09-17 23:13:50 +0800

    Fix build .NET 6

--- ServiceStack/src/ServiceStack.GoogleCloud/GoogleCloudSpeechToText.cs
+++ ServiceStack/src/ServiceStack.GoogleCloud/GoogleCloudSpeechToText.cs
@@ -109,7 +109,11 @@ public class GoogleCloudSpeechToText : ISpeechToText, IRequireVirtualFiles
             var file = VirtualFiles.GetFile(recordingPath);
             if (file == null)
                 throw new FileNotFoundException($"File not found {VirtualFiles.GetType().Name}:{recordingPath}");
+#if NET6_0_OR_GREATER
             await using var fileStream = file.OpenRead();
+#else
+            using var fileStream = file.OpenRead();
+#endif
             request.Content = await ByteString.FromStreamAsync(fileStream, token);
         }
 
