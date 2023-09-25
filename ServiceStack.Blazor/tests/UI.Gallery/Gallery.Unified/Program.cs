3ca6daf7a2dd882437cd5cd863f22a0522eab8ec
Demis Bellot <demis.bellot@gmail.com>
2023-09-25 13:13:52 +0800

    Add Gallery.Unified

---
+++ ServiceStack.Blazor/tests/UI.Gallery/Gallery.Unified/Program.cs
@@ -0,0 +1,26 @@
+using Gallery.Unified.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddServerComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.MapRazorComponents<App>()
+    .AddServerRenderMode();
+
+app.Run();
