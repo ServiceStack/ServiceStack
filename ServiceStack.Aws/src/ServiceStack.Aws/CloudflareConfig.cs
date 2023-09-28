28561aa6a20645563bc16b4edd733e3c2bb5aca7
Demis Bellot <demis.bellot@gmail.com>
2023-09-28 12:27:30 +0800

    Rename R2Config to CloudflareConfig

--- ServiceStack.Aws/src/ServiceStack.Aws/CloudflareConfig.cs
+++ ServiceStack.Aws/src/ServiceStack.Aws/CloudflareConfig.cs
@@ -2,14 +2,14 @@
 
 namespace ServiceStack.Aws;
 
-public class R2Config
+public class CloudflareConfig
 {
     public string? AccountId { get; set; }
     public string? AccessKey { get; set; }
     public string? SecretKey { get; set; }
     public string? Bucket { get; set; }
+    public string? OpenAiBaseUrl { get; set; }
     public string? ToServiceUrl() => AccountId == null
         ? null
         : $"https://{AccountId}.r2.cloudflarestorage.com";
-
 }
