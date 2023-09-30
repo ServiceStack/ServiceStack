c4bc62b88928338e678a6a7cdf13ad3830b4c4ba
Demis Bellot <demis.bellot@gmail.com>
2023-09-30 13:13:08 +0800

    Cache DbConnection access, log when not exists when closing/disposing

--- ServiceStack.OrmLite/src/ServiceStack.OrmLite/OrmLiteConnection.cs
+++ ServiceStack.OrmLite/src/ServiceStack.OrmLite/OrmLiteConnection.cs
@@ -4,6 +4,7 @@ using System.Data.Common;
 using System.Threading;
 using System.Threading.Tasks;
 using ServiceStack.Data;
+using ServiceStack.Logging;
 using ServiceStack.Text;
 
 namespace ServiceStack.OrmLite
@@ -36,8 +37,14 @@ namespace ServiceStack.OrmLite
         {
             Factory.OnDispose?.Invoke(this);
             if (!Factory.AutoDisposeConnection) return;
+            
+            if (dbConnection == null)
+            {
+                LogManager.GetLogger(GetType()).WarnFormat("No dbConnection to Dispose()");
+                return;
+            }
 
-            DbConnection.Dispose();
+            dbConnection?.Dispose();
             dbConnection = null;
         }
 
@@ -59,12 +66,17 @@ namespace ServiceStack.OrmLite
 
         public void Close()
         {
-            var id = Diagnostics.OrmLite.WriteConnectionCloseBefore(DbConnection);
-            var connectionId = DbConnection.GetConnectionId();
+            if (dbConnection == null)
+            {
+                LogManager.GetLogger(GetType()).WarnFormat("No dbConnection to Close()");
+                return;
+            }
+            var id = Diagnostics.OrmLite.WriteConnectionCloseBefore(dbConnection);
+            var connectionId = dbConnection.GetConnectionId();
             Exception e = null;
             try
             {
-                DbConnection.Close();
+                dbConnection.Close();
             }
             catch (Exception ex)
             {
@@ -74,9 +86,9 @@ namespace ServiceStack.OrmLite
             finally
             {
                 if (e != null)
-                    Diagnostics.OrmLite.WriteConnectionCloseError(id, connectionId, DbConnection, e);
+                    Diagnostics.OrmLite.WriteConnectionCloseError(id, connectionId, dbConnection, e);
                 else
-                    Diagnostics.OrmLite.WriteConnectionCloseAfter(id, connectionId, DbConnection);
+                    Diagnostics.OrmLite.WriteConnectionCloseAfter(id, connectionId, dbConnection);
             }
         }
 
@@ -97,21 +109,22 @@ namespace ServiceStack.OrmLite
 
         public void Open()
         {
-            if (DbConnection.State == ConnectionState.Broken)
-                DbConnection.Close();
+            var dbConn = DbConnection;
+            if (dbConn.State == ConnectionState.Broken)
+                dbConn.Close();
 
-            if (DbConnection.State == ConnectionState.Closed)
+            if (dbConn.State == ConnectionState.Closed)
             {
-                var id = Diagnostics.OrmLite.WriteConnectionOpenBefore(DbConnection);
+                var id = Diagnostics.OrmLite.WriteConnectionOpenBefore(dbConn);
                 Exception e = null;
                 try
                 {
-                    DbConnection.Open();
+                    dbConn.Open();
                     //so the internal connection is wrapped for example by miniprofiler
                     if (Factory.ConnectionFilter != null)
-                        dbConnection = Factory.ConnectionFilter(dbConnection);
+                        dbConn = Factory.ConnectionFilter(dbConn);
 
-                    DialectProvider.InitConnection(dbConnection);
+                    DialectProvider.InitConnection(dbConn);
                 }
                 catch (Exception ex)
                 {
@@ -121,30 +134,31 @@ namespace ServiceStack.OrmLite
                 finally
                 {
                     if (e != null)
-                        Diagnostics.OrmLite.WriteConnectionOpenError(id, DbConnection, e);
+                        Diagnostics.OrmLite.WriteConnectionOpenError(id, dbConn, e);
                     else
-                        Diagnostics.OrmLite.WriteConnectionOpenAfter(id, DbConnection);
+                        Diagnostics.OrmLite.WriteConnectionOpenAfter(id, dbConn);
                 }
             }
         }
 
         public async Task OpenAsync(CancellationToken token = default)
         {
-            if (DbConnection.State == ConnectionState.Broken)
-                DbConnection.Close();
+            var dbConn = DbConnection;
+            if (dbConn.State == ConnectionState.Broken)
+                dbConn.Close();
 
-            if (DbConnection.State == ConnectionState.Closed)
+            if (dbConn.State == ConnectionState.Closed)
             {
-                var id = Diagnostics.OrmLite.WriteConnectionOpenBefore(DbConnection);
+                var id = Diagnostics.OrmLite.WriteConnectionOpenBefore(dbConn);
                 Exception e = null;
                 try
                 {
-                    await DialectProvider.OpenAsync(DbConnection, token).ConfigAwait();
+                    await DialectProvider.OpenAsync(dbConn, token).ConfigAwait();
                     //so the internal connection is wrapped for example by miniprofiler
                     if (Factory.ConnectionFilter != null)
-                        dbConnection = Factory.ConnectionFilter(dbConnection);
+                        dbConn = Factory.ConnectionFilter(dbConn);
 
-                    DialectProvider.InitConnection(dbConnection);
+                    DialectProvider.InitConnection(dbConn);
                 }
                 catch (Exception ex)
                 {
@@ -154,9 +168,9 @@ namespace ServiceStack.OrmLite
                 finally
                 {
                     if (e != null)
-                        Diagnostics.OrmLite.WriteConnectionOpenError(id, DbConnection, e);
+                        Diagnostics.OrmLite.WriteConnectionOpenError(id, dbConn, e);
                     else
-                        Diagnostics.OrmLite.WriteConnectionOpenAfter(id, DbConnection);
+                        Diagnostics.OrmLite.WriteConnectionOpenAfter(id, dbConn);
                 }
             }
         }
