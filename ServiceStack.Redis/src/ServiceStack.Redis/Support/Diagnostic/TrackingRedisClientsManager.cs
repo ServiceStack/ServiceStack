304a34a56aa9352023e24e9d140ade2b4930ee95
Demis Bellot <demis.bellot@gmail.com>
2023-11-07 10:50:38 +0800

    fix build errors

--- ServiceStack.Redis/src/ServiceStack.Redis/Support/Diagnostic/TrackingRedisClientsManager.cs
+++ ServiceStack.Redis/src/ServiceStack.Redis/Support/Diagnostic/TrackingRedisClientsManager.cs
@@ -1,4 +1,4 @@
-﻿#if !(NETSTANDARD2_0 || NETSTANDARD2_1 || NET6_0)
+﻿#if !(NETSTANDARD2_0 || NETSTANDARD2_1 || NET6_0_OR_GREATER)
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
