e6fed0bbf647eb359a0f9ef578d07495c038be31
Demis Bellot <demis.bellot@gmail.com>
2023-11-09 13:37:28 +0800

    Add DarkModeToggleLite

---
+++ ServiceStack.Blazor/src/ServiceStack.Blazor/Components/UiComponentLiteBase.cs
@@ -0,0 +1,24 @@
+﻿#pragma warning disable IDE1006 // Naming Styles
+
+using Microsoft.AspNetCore.Components;
+
+namespace ServiceStack.Blazor.Components;
+
+public abstract class UiComponentLiteBase : ComponentBase
+{
+    /// <summary>
+    /// Optional user defined classes for this component
+    /// </summary>
+    [Parameter] public string? @class { get; set; }
+    public string? Class => @class;
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object?>? AdditionalAttributes { get; set; }
+
+    /// <summary>
+    /// Helper to combine multiple css classes. Strings can contain multiple classes, empty strings are ignored.
+    /// </summary>
+    /// <param name="classes"></param>
+    /// <returns></returns>
+    protected virtual string ClassNames(params string?[] classes) => CssUtils.ClassNames(classes);
+}
