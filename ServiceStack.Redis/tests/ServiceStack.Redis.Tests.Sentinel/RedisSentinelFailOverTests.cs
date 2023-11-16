8b1c24367c0fded0a6d4deb41896554e89c899f9
Donald Hanson <dhanson@opentable.com>
2023-11-16 14:59:46 -0800

    test to validate sentinel host failover to next available host

---
+++ ServiceStack.Redis/tests/ServiceStack.Redis.Tests.Sentinel/RedisSentinelFailOverTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using ServiceStack.Logging;
+
+namespace ServiceStack.Redis.Tests.Sentinel
+{
+    [TestFixture, Category("Integration")]
+    public class RedisSentinelFailOverTests : RedisSentinelTestBase
+    {
+        [OneTimeSetUp]
+        public void OnBeforeTestFixture()
+        {
+            StartAllRedisServers();
+            StartAllRedisSentinels();
+            LogManager.LogFactory = new ConsoleLogFactory(debugEnabled:true);
+        }
+
+        [OneTimeTearDown]
+        public void OnAfterTestFixture()
+        {
+            ShutdownAllRedisSentinels();
+            ShutdownAllRedisServers();
+        }
+        
+        [Test]
+        public void Can_Handle_First_Sentinel_Down()
+        {
+            var sentinel = new RedisSentinel(SentinelHosts)
+            {
+                RedisManagerFactory = (masters, replicas) => new PooledRedisClientManager(masters, replicas)
+            };
+
+            var redisManager = sentinel.Start();
+
+            var cacheKey = Guid.NewGuid().ToString();
+
+            var client = new RedisClient("127.0.0.1", SentinelPorts[0]);
+            client.ShutdownNoSave();
+            
+            using var readOnlyClient = redisManager.GetReadOnlyClient();
+
+            readOnlyClient.Get<long>(cacheKey);
+        }
+    }
+}
