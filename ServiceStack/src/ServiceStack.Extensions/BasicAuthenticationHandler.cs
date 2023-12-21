3bc7e48de107bd6d837bc5dbb39c39c2387a1e2e
Demis Bellot <demis.bellot@gmail.com>
2023-12-22 03:23:21 +0800

    Create BasicAuthenticationHandler.cs

---
+++ ServiceStack/src/ServiceStack.Extensions/BasicAuthenticationHandler.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ServiceStack;
+
+public static class BasicAuthenticationHandler
+{
+    public const string Scheme = "basic";
+}
+
+public class BasicAuthenticationHandler<TUser>
+#if NET6_0
+    (SignInManager<TUser> signInManager,
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    ISystemClock clock)
+    : BasicAuthenticationHandler<TUser,string>(signInManager, options, logger, encoder, clock)
+#else
+    (SignInManager<TUser> signInManager,
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder)
+    : BasicAuthenticationHandler<TUser,string>(signInManager, options, logger, encoder)
+#endif
+    where TUser : IdentityUser<string>;
+
+public class BasicAuthenticationHandler<TUser, TKey>
+#if NET6_0
+    (SignInManager<TUser> signInManager,
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder,
+    ISystemClock clock)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder, clock)
+#else
+    (SignInManager<TUser> signInManager,
+    IOptionsMonitor<AuthenticationSchemeOptions> options,
+    ILoggerFactory logger,
+    UrlEncoder encoder)
+    : AuthenticationHandler<AuthenticationSchemeOptions>(options, logger, encoder)
+#endif
+    where TUser : IdentityUser<TKey> 
+    where TKey : IEquatable<TKey>
+{  
+    protected override async Task<AuthenticateResult> HandleAuthenticateAsync()  
+    {  
+        if (!Request.Headers.TryGetValue(HttpHeaders.Authorization, out var auth))
+            return AuthenticateResult.NoResult();
+                
+        var authHeader = AuthenticationHeaderValue.Parse(auth!);
+        var credentials = Encoding.UTF8.GetString(Convert.FromBase64String(authHeader.Parameter!));  
+        var username = credentials.LeftPart(':');  
+        var password = credentials.RightPart(':');
+
+        var result = await signInManager.PasswordSignInAsync(username, password, isPersistent: false, lockoutOnFailure: false);
+        if (!result.Succeeded)
+            return AuthenticateResult.Fail(result.ToString());
+  
+        var ticket = new AuthenticationTicket(Request.HttpContext.User, Scheme.Name);  
+        return AuthenticateResult.Success(ticket);  
+    }  
+}
