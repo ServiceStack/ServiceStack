fbfdf6382c08bebab96a8831a6e93a9aed612b09
Demis Bellot <demis.bellot@gmail.com>
2023-12-24 15:17:45 +0800

    Add endpoints mapping to CleanUrlsFeature

--- ServiceStack/src/ServiceStack/CleanUrlsFeature.cs
+++ ServiceStack/src/ServiceStack/CleanUrlsFeature.cs
@@ -1,12 +1,19 @@
 using ServiceStack.Host.Handlers;
 using ServiceStack.IO;
 
+#if NET8_0_OR_GREATER
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+#endif
+
 namespace ServiceStack;
 
 public class CleanUrlsFeature : IPlugin, Model.IHasStringId
 {
     public string Id { get; set; } = Plugins.CleanUrls;
-    public string[] Extensions { get; set; } = { "html" };
+    public string[] Extensions { get; set; } = ["html"];
     
     public void Register(IAppHost appHost)
     {
@@ -25,5 +32,26 @@ public class CleanUrlsFeature : IPlugin, Model.IHasStringId
             }
             return null;
         });
+        
+#if NET8_0_OR_GREATER
+        var host = (AppHostBase)appHost;
+        host.MapEndpoints(routeBuilder => {
+            routeBuilder.MapGet("{*path:nonfile}", httpContext => {
+                var path = httpContext.Request.Path.Value;
+                if (path.IndexOfAny('.') == -1)
+                {
+                    foreach (var ext in Extensions)
+                    {
+                        var relativePath = path + "." + ext;
+                        var file = fs.GetFile(relativePath);
+                        if (file != null)
+                            return httpContext.ProcessRequestAsync(new StaticFileHandler(file));
+                    }
+                }
+                return Task.CompletedTask;
+            })
+            .WithMetadata<string>(nameof(CleanUrlsFeature));
+        });
+#endif        
     }
 }
---
+++ ServiceStack/tests/NorthwindBlazor/wwwroot/docs/test.html
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test</title>
+</head>
+<body>
+    <h1>Test</h1>
+</body>
+</html>
