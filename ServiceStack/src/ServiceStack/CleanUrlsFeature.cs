a21e4ca7caafc9b8df5f12f27d056c0c7e3876bb
Demis Bellot <demis.bellot@gmail.com>
2023-12-24 16:18:46 +0800

    Refactor CleanUrlsFeature to only use FallbackHandler for both SS or endpoint routing

--- ServiceStack/src/ServiceStack/CleanUrlsFeature.cs
+++ ServiceStack/src/ServiceStack/CleanUrlsFeature.cs
@@ -1,12 +1,7 @@
+#nullable enable
+
 using ServiceStack.Host.Handlers;
 using ServiceStack.IO;
-
-#if NET8_0_OR_GREATER
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-#endif
 
 namespace ServiceStack;
 
@@ -15,43 +10,26 @@ public class CleanUrlsFeature : IPlugin, Model.IHasStringId
     public string Id { get; set; } = Plugins.CleanUrls;
     public string[] Extensions { get; set; } = ["html"];
     
+    public FileSystemVirtualFiles? VirtualFiles { get; set; }
+
     public void Register(IAppHost appHost)
     {
-        var fs = ((ServiceStackHost)appHost).GetVirtualFileSource<FileSystemVirtualFiles>();
-        appHost.CatchAllHandlers.Add((string httpMethod, string pathInfo, string filePath) =>
+        VirtualFiles = ((ServiceStackHost)appHost).GetVirtualFileSource<FileSystemVirtualFiles>();
+        appHost.FallbackHandlers.Add(req => GetHandler(req.Verb, req.PathInfo));
+    }
+
+    public HttpAsyncTaskHandler? GetHandler(string httpMethod, string pathInfo)
+    {
+        if (httpMethod == HttpMethods.Get && pathInfo.IndexOfAny('.') == -1)
         {
-            if (httpMethod == HttpMethods.Get && pathInfo.IndexOfAny('.') == -1)
+            foreach (var ext in Extensions)
             {
-                foreach (var ext in Extensions)
-                {
-                    var relativePath = pathInfo + "." + ext;
-                    var file = fs.GetFile(relativePath);
-                    if (file != null)
-                        return new StaticFileHandler(file);
-                }
+                var relativePath = pathInfo + "." + ext;
+                var file = VirtualFiles!.GetFile(relativePath);
+                if (file != null)
+                    return new StaticFileHandler(file);
             }
-            return null;
-        });
-        
-#if NET8_0_OR_GREATER
-        var host = (AppHostBase)appHost;
-        host.MapEndpoints(routeBuilder => {
-            routeBuilder.MapGet("{*path:nonfile}", httpContext => {
-                var path = httpContext.Request.Path.Value;
-                if (path.IndexOfAny('.') == -1)
-                {
-                    foreach (var ext in Extensions)
-                    {
-                        var relativePath = path + "." + ext;
-                        var file = fs.GetFile(relativePath);
-                        if (file != null)
-                            return httpContext.ProcessRequestAsync(new StaticFileHandler(file));
-                    }
-                }
-                return Task.CompletedTask;
-            })
-            .WithMetadata<string>(nameof(CleanUrlsFeature));
-        });
-#endif        
+        }
+        return null;
     }
 }
