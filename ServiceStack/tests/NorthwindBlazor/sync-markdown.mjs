ffb3f1a5909bd3ce8f328d337b3759cb80909061
Demis Bellot <demis.bellot@gmail.com>
2024-02-01 12:04:49 +0800

    Update sync-markdown.mjs

--- ServiceStack/tests/NorthwindBlazor/sync-markdown.mjs
+++ ServiceStack/tests/NorthwindBlazor/sync-markdown.mjs
@@ -1,16 +1,6 @@
 import { promises as fs } from 'fs'
 import path from 'path'
 
-const all = [
-    'Markdown.Blog.cs',
-    'Markdown.Meta.cs',
-    'Markdown.Pages.cs',
-    'Markdown.Videos.cs',
-    'Markdown.WhatsNew.cs',
-    'MarkdownPagesBase.cs',
-    'MarkdownTagHelper.cs',
-]
-
 function copyTo(all, dir) {
     all.forEach(async src => {
         const dest = path.join(dir,src)
@@ -19,5 +9,18 @@ function copyTo(all, dir) {
     })
 }
 
-await copyTo(all, '../../../../servicestack.net/MyApp')
+await copyTo([
+    'Markdown.Blog.cs',
+    'Markdown.Meta.cs',
+    'Markdown.Pages.cs',
+    'Markdown.Videos.cs',
+    'Markdown.WhatsNew.cs',
+    'MarkdownPagesBase.cs',
+    'MarkdownTagHelper.cs',
+], '../../../../servicestack.net/MyApp')
 
+await copyTo([
+    'Markdown.Pages.cs',
+    'Markdown.Videos.cs',
+    'MarkdownPagesBase.cs',
+], '../../../../../NetCoreTemplates/blazor/MyApp')
