00c640a8e5346f5134454e95f20926d6f46bcaa0
Demis Bellot <demis.bellot@gmail.com>
2024-02-19 17:39:42 +0800

    Refactor DisableRedirectsForApis to be more reusable

--- ServiceStack/src/ServiceStack.Extensions/AuthenticationBuilderExtensions.cs
+++ ServiceStack/src/ServiceStack.Extensions/AuthenticationBuilderExtensions.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 
@@ -9,37 +11,47 @@ public static class AuthenticationBuilderExtensions
 {
     public static IdentityCookiesBuilder DisableRedirectsForApis(this IdentityCookiesBuilder builder)
     {
-        builder.ApplicationCookie!.Configure(configure =>
-        {
-            configure.Events.OnRedirectToLogin += ctx =>
-            {
-                if ((ctx.Request.Path.StartsWithSegments("/api") || ctx.Request.Headers.Accept.ToString().Contains(MimeTypes.Json, StringComparison.OrdinalIgnoreCase)) 
-                    && ctx.Response.StatusCode is StatusCodes.Status200OK or StatusCodes.Status302Found)
-                {
-                    ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
-                    if (ctx.HttpContext.Items.TryGetValue(Keywords.ResponseStatus, out var oStatus) && oStatus is ResponseStatus status)
-                    {
-                        var dto = new ErrorResponse { ResponseStatus = status };
-                        ctx.Response.WriteAsJsonAsync(dto);
-                    }
-                }
-                return Task.CompletedTask;
-            };
-            configure.Events.OnRedirectToAccessDenied += ctx =>
-            {
-                if ((ctx.Request.Path.StartsWithSegments("/api") || ctx.Request.Headers.Accept.ToString().Contains(MimeTypes.Json, StringComparison.OrdinalIgnoreCase)) 
-                    && ctx.Response.StatusCode is StatusCodes.Status200OK or StatusCodes.Status302Found)
-                {
-                    ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
-                    if (ctx.HttpContext.Items.TryGetValue(Keywords.ResponseStatus, out var oStatus) && oStatus is ResponseStatus status)
-                    {
-                        var dto = new ErrorResponse { ResponseStatus = status };
-                        ctx.Response.WriteAsJsonAsync(dto);
-                    }
-                }
-                return Task.CompletedTask;
-            };
-        });
+        builder.ApplicationCookie!.Configure(options => options.DisableRedirectsForApis());
         return builder;
     }
+
+    public static void DisableRedirectsForApis(this CookieAuthenticationOptions options)
+    {
+        options.Events.OnRedirectToLogin += DisableApiRedirectsForUnauthorized();
+        options.Events.OnRedirectToAccessDenied += DisableApiRedirectsForForbidden();
+        
+    }
+
+    public static Func<RedirectContext<CookieAuthenticationOptions>, Task> DisableApiRedirectsForForbidden()
+    {
+        return ctx =>
+        {
+            if ((ctx.Request.Path.StartsWithSegments("/api") || ctx.Request.Headers.Accept.ToString().Contains(MimeTypes.Json, StringComparison.OrdinalIgnoreCase)) 
+                && ctx.Response.StatusCode is StatusCodes.Status200OK or StatusCodes.Status302Found)
+            {
+                ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
+                if (ctx.HttpContext.Items.TryGetValue(Keywords.ResponseStatus, out var oStatus) && oStatus is ResponseStatus status)
+                {
+                    var dto = new ErrorResponse { ResponseStatus = status };
+                    ctx.Response.WriteAsJsonAsync(dto);
+                }
+            }
+            return Task.CompletedTask;
+        };
+    }
+
+    public static Func<RedirectContext<CookieAuthenticationOptions>, Task> DisableApiRedirectsForUnauthorized() => ctx =>
+    {
+        if ((ctx.Request.Path.StartsWithSegments("/api") || ctx.Request.Headers.Accept.ToString().Contains(MimeTypes.Json, StringComparison.OrdinalIgnoreCase)) 
+            && ctx.Response.StatusCode is StatusCodes.Status200OK or StatusCodes.Status302Found)
+        {
+            ctx.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            if (ctx.HttpContext.Items.TryGetValue(Keywords.ResponseStatus, out var oStatus) && oStatus is ResponseStatus status)
+            {
+                var dto = new ErrorResponse { ResponseStatus = status };
+                ctx.Response.WriteAsJsonAsync(dto);
+            }
+        }
+        return Task.CompletedTask;
+    };
 }
