d85a9f04159cb53f844d253a7627f003c05644c2
Demis Bellot <demis.bellot@gmail.com>
2024-02-22 21:18:52 +0800

    Upgrade test projects to net8.0

--- ServiceStack/build/run-webhost-tests.sh
+++ ServiceStack/build/run-webhost-tests.sh
@@ -2,5 +2,5 @@
 docker-compose -f ./webhost-tests-environment.yml up -d
 export PGSQL_CONNECTION="Server=localhost;Port=48303;User Id=postgres;Password=test;Database=test;Pooling=true;MinPoolSize=0;MaxPoolSize=200"
 export MSSQL_CONNECTION="Server=localhost,48501;Database=master;User Id=sa;Password=Test!tesT;MultipleActiveResultSets=True;"
-dotnet clean --framework net6.0 ../tests/ServiceStack.WebHost.Endpoints.Tests/ServiceStack.WebHost.Endpoints.Tests.csproj
-dotnet test --framework net6.0 ../tests/ServiceStack.WebHost.Endpoints.Tests/ServiceStack.WebHost.Endpoints.Tests.csproj
+dotnet clean --framework net8.0 ../tests/ServiceStack.WebHost.Endpoints.Tests/ServiceStack.WebHost.Endpoints.Tests.csproj
+dotnet test --framework net8.0 ../tests/ServiceStack.WebHost.Endpoints.Tests/ServiceStack.WebHost.Endpoints.Tests.csproj
