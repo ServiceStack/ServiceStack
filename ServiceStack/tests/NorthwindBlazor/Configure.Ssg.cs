052fcc123518c2347cc009f8d4097fb615d8f7d0
Demis Bellot <demis.bellot@gmail.com>
2024-03-23 11:03:40 +0800

    Update Configure.Ssg.cs

--- ServiceStack/tests/NorthwindBlazor/Configure.Ssg.cs
+++ ServiceStack/tests/NorthwindBlazor/Configure.Ssg.cs
@@ -7,44 +7,40 @@ namespace MyApp;
 public class ConfigureSsg : IHostingStartup
 {
     public void Configure(IWebHostBuilder builder) => builder
-        .ConfigureServices(services =>
-        {
+        .ConfigureServices(services => {
             services.AddSingleton<MarkdownIncludes>();
             services.AddSingleton<MarkdownPages>();
             services.AddSingleton<MarkdownVideos>();
             services.AddSingleton<MarkdownBlog>();
             services.AddSingleton<MarkdownMeta>();
         })
-        .ConfigureAppHost(
-            appHost => appHost.Plugins.Add(new CleanUrlsFeature()),
-            afterPluginsLoaded: appHost =>
+        .ConfigureAppHost(afterPluginsLoaded: appHost => {
+            MarkdigConfig.Set(new MarkdigConfig
             {
-                MarkdigConfig.Set(new MarkdigConfig
+                ConfigurePipeline = pipeline =>
                 {
-                    ConfigurePipeline = pipeline =>
-                    {
-                        // Extend Markdig Pipeline
-                    },
-                    ConfigureContainers = config =>
-                    {
-                        config.AddBuiltInContainers();
-                        // Add Custom Block or Inline containers
-                    }
-                });
-
-                var includes = appHost.Resolve<MarkdownIncludes>();
-                var pages = appHost.Resolve<MarkdownPages>();
-                var videos = appHost.Resolve<MarkdownVideos>();
-                var blogPosts = appHost.Resolve<MarkdownBlog>();
-                var meta = appHost.Resolve<MarkdownMeta>();
-
-                meta.Features = [pages, videos, blogPosts];
-                
-                includes.LoadFrom("_includes");
-                pages.LoadFrom("_pages");
-                videos.LoadFrom("_videos");
-                blogPosts.LoadFrom("_posts");
+                    // Extend Markdig Pipeline
+                },
+                ConfigureContainers = config =>
+                {
+                    config.AddBuiltInContainers();
+                    // Add Custom Block or Inline containers
+                }
             });
+
+            var includes = appHost.Resolve<MarkdownIncludes>();
+            var pages = appHost.Resolve<MarkdownPages>();
+            var videos = appHost.Resolve<MarkdownVideos>();
+            var blogPosts = appHost.Resolve<MarkdownBlog>();
+            var meta = appHost.Resolve<MarkdownMeta>();
+
+            meta.Features = [pages, videos, blogPosts];
+            
+            includes.LoadFrom("_includes");
+            pages.LoadFrom("_pages");
+            videos.LoadFrom("_videos");
+            blogPosts.LoadFrom("_posts");
+        });
 }
 
 // Add additional frontmatter info to include
