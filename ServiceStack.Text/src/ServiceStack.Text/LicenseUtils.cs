4bd06cedce3656225ad8e94624195ee710b73440
Demis Bellot <demis.bellot@gmail.com>
2024-03-26 23:30:35 +0800

    Update LicenseUtils.cs

--- ServiceStack.Text/src/ServiceStack.Text/LicenseUtils.cs
+++ ServiceStack.Text/src/ServiceStack.Text/LicenseUtils.cs
@@ -221,22 +221,27 @@ public static class LicenseUtils
         Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture;
         try
         {
-            if (Env.IsAot())
-            {
-                __setActivatedLicense(new __ActivatedLicense(new LicenseKey { Type = LicenseType.Indie }));
-                return;
-            }
-
+            licenseKeyText = licenseKeyText?.Trim();
+            if (string.IsNullOrEmpty(licenseKeyText))
+                throw new ArgumentNullException(nameof(licenseKeyText));
+            
             if (IsFreeLicenseKey(licenseKeyText))
             {
                 ValidateFreeLicenseKey(licenseKeyText);
                 return;
             }
                 
-            var parts = licenseKeyText.SplitOnFirst('-');
-            subId = parts[0];
+            subId = licenseKeyText.LeftPart('-');
+            if (!int.TryParse(subId, out var subIdInt))
+                throw new LicenseException("This license is invalid." + ContactDetails);
+                
+            if (Env.IsAot())
+            {
+                __setActivatedLicense(new __ActivatedLicense(new LicenseKey { Type = LicenseType.Indie }));
+                return;
+            }
 
-            if (int.TryParse(subId, out var subIdInt) && revokedSubs.Contains(subIdInt))
+            if (revokedSubs.Contains(subIdInt))
                 throw new LicenseException("This subscription has been revoked. " + ContactDetails);
 
             var key = VerifyLicenseKeyText(licenseKeyText);
