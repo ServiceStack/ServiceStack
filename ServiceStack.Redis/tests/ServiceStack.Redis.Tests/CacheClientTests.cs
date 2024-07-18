62867b2fab2d05bd93e2506babcdaa6ee35b23d2
Demis Bellot <demis.bellot@gmail.com>
2024-07-18 10:04:43 +0800

    Support non-numeric value types values

--- ServiceStack.Redis/tests/ServiceStack.Redis.Tests/CacheClientTests.cs
+++ ServiceStack.Redis/tests/ServiceStack.Redis.Tests/CacheClientTests.cs
@@ -19,4 +19,19 @@ public class CacheClientTests
         var result = cache.Get<string>("key");
         Assert.That(result, Is.EqualTo("A"));
     }
+
+    [Test]
+    public void Can_add_bool_value()
+    {
+        var cache = redisManager.GetCacheClient();
+        cache.FlushAll();
+        var cacheKey = $"BoolValue";
+
+        cache.Add(cacheKey, false, DateTime.UtcNow.AddMinutes(1));
+        Assert.That(cache.Get<bool>(cacheKey), Is.False);
+
+        cache.FlushAll();
+        cache.Add(cacheKey, true, DateTime.UtcNow.AddMinutes(1));
+        Assert.That(cache.Get<bool>(cacheKey), Is.True);
+    }
 }
