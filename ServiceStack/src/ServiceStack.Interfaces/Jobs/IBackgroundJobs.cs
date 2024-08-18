35ee05e9bab468692853eb77aa95172ff30c46e2
Demis Bellot <demis.bellot@gmail.com>
2024-08-18 12:40:25 +0800

    Add default workers

--- ServiceStack/src/ServiceStack.Interfaces/Jobs/IBackgroundJobs.cs
+++ ServiceStack/src/ServiceStack.Interfaces/Jobs/IBackgroundJobs.cs
@@ -128,6 +128,19 @@ public class WorkerStats
 }
 
 /// <summary>
+/// Worker Thread names
+/// </summary>
+public static class Workers
+{
+    /// <summary>
+    /// Worker name for OrmLite default DB Connection
+    /// </summary>
+    public const string AppDb = "app.db";
+    public const string JobsDb = "jobs.db";
+    public const string RequestsDb = "requests.db";
+}
+
+/// <summary>
 /// Execute AutoQuery Create/Update/Delete Request DTO in a background thread
 /// </summary>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
