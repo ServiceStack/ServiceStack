ecad760956d8e6bdedc62f108169eaf40fa9b822
Demis Bellot <demis.bellot@gmail.com>
2024-08-23 11:25:00 +0800

    Update BackgroundJobsWorker.cs

--- ServiceStack/src/ServiceStack.Jobs/BackgroundJobsWorker.cs
+++ ServiceStack/src/ServiceStack.Jobs/BackgroundJobsWorker.cs
@@ -88,6 +88,10 @@ public class BackgroundJobsWorker : IDisposable
                         Interlocked.Increment(ref failed);
                         throw;
                     }
+                    finally
+                    {
+                        lastRunStarted = null;
+                    }
                 }
             }
         }
