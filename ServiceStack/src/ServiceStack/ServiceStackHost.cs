4639b4218b6f490e5295e4264ade5d67178336dc
Demis Bellot <demis.bellot@gmail.com>
2025-01-03 09:39:37 +0800

    Add AppHost.ThrowIfDisposed

--- ServiceStack/src/ServiceStack/ServiceStackHost.cs
+++ ServiceStack/src/ServiceStack/ServiceStackHost.cs
@@ -2116,6 +2116,11 @@ public abstract partial class ServiceStackHost
 
     protected bool isDisposed;
     public bool IsDisposed => isDisposed;
+    public static void ThrowIfDisposed()
+    {
+        if (Instance.isDisposed)
+            throw new ObjectDisposedException(Instance.GetType().Name);
+    }
 
     /// <summary>
     /// Executes OnDisposeCallbacks and Disposes IDisposable's dependencies in the IOC &amp; reset singleton states
