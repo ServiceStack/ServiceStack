7f61f09eb78634e33ee7cb4af6e7b099ddc57e83
Demis Bellot <demis.bellot@gmail.com>
2025-01-31 10:26:52 +0800

    Add cast as allowed file typed

--- ServiceStack/src/ServiceStack/HostConfig.cs
+++ ServiceStack/src/ServiceStack/HostConfig.cs
@@ -96,7 +96,7 @@ public class HostConfig
                 "avi", "divx", "m3u", "mov", "mp3", "mpeg", "mpg", "qt", "vob", "wav", "wma", "wmv",
                 "flv", "swf", "xap", "xaml", "ogg", "ogv", "mp4", "webm", "eot", "ttf", "woff", "woff2", "map",
                 "xls", "xla", "xlsx", "xltx", "doc", "dot", "docx", "dotx", "ppt", "pps", "ppa", "pptx", "potx", 
-                "wasm", "proto", "cer", "crt", "webmanifest", "mjs", "cjs", 
+                "wasm", "proto", "cer", "crt", "webmanifest", "mjs", "cjs", "cast",
             },
             CompressFilesWithExtensions = [],
             AllowFilePaths =
