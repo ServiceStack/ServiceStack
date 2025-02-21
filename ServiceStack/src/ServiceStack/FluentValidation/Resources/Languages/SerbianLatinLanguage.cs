13cc93a8bd1ba38971bc9a41ba67452ee01170a0
Demis Bellot <demis.bellot@gmail.com>
2025-02-21 11:09:10 +0800

    Update FluentValidation Langauges

--- ServiceStack/src/ServiceStack/FluentValidation/Resources/Languages/SerbianLatinLanguage.cs
+++ ServiceStack/src/ServiceStack/FluentValidation/Resources/Languages/SerbianLatinLanguage.cs
@@ -22,37 +22,37 @@
 
 namespace ServiceStack.FluentValidation.Resources;
 
-internal class BosnianLanguage {
-	public const string Culture = "bs";
+internal class SerbianLatinLanguage {
+	public const string Culture = "sr-Latn";
 
 	public static string GetTranslation(string key) => key switch {
 		"EmailValidator" => "'{PropertyName}' nije validna email adresa.",
-		"GreaterThanOrEqualValidator" => "'{PropertyName}' mora biti veće ili jednako '{ComparisonValue}'.",
+		"GreaterThanOrEqualValidator" => "'{PropertyName}' mora biti veće ili jednako od '{ComparisonValue}'.",
 		"GreaterThanValidator" => "'{PropertyName}' mora biti veće od '{ComparisonValue}'.",
-		"LengthValidator" => "'{PropertyName}' mora imati između {MinLength} i {MaxLength} karatkera. Uneseno je {TotalLength} karaktera.",
+		"LengthValidator" => "'{PropertyName}' mora imati između {MinLength} i {MaxLength} karaktera. Uneseno je {TotalLength} karaktera.",
 		"MinimumLengthValidator" => "'{PropertyName}' mora imati najmanje {MinLength} karaktera. Uneseno je {TotalLength} karaktera.",
-		"MaximumLengthValidator" => "'{PropertyName}' ne smije imati više od {MaxLength} karaktera. Uneseno je {TotalLength} karaktera.",
-		"LessThanOrEqualValidator" => "'{PropertyName}' mora biti manje ili jednako '{ComparisonValue}'.",
+		"MaximumLengthValidator" => "'{PropertyName}' ne sme imati više od {MaxLength} karaktera. Uneseno je {TotalLength} karaktera.",
+		"LessThanOrEqualValidator" => "'{PropertyName}' mora biti manje ili jednako od '{ComparisonValue}'.",
 		"LessThanValidator" => "'{PropertyName}' mora biti manje od '{ComparisonValue}'.",
-		"NotEmptyValidator" => "'{PropertyName}' ne smije biti prazan.",
-		"NotEqualValidator" => "'{PropertyName}' ne smije biti jednak '{ComparisonValue}'.",
-		"NotNullValidator" => "'{PropertyName}' ne smije biti prazan.",
-		"PredicateValidator" => "Zadan uslov nije ispunjen za '{PropertyName}'.",
-		"AsyncPredicateValidator" => "Zadan uslov nije ispunjen za '{PropertyName}'.",
+		"NotEmptyValidator" => "'{PropertyName}' ne sme biti prazno.",
+		"NotEqualValidator" => "'{PropertyName}' ne sme biti jednako '{ComparisonValue}'.",
+		"NotNullValidator" => "'{PropertyName}' ne sme biti prazno.",
+		"PredicateValidator" => "Zadati uslov nije ispunjen za '{PropertyName}'.",
+		"AsyncPredicateValidator" => "Zadati uslov nije ispunjen za '{PropertyName}'.",
 		"RegularExpressionValidator" => "'{PropertyName}' nije u odgovarajućem formatu.",
-		"EqualValidator" => "'{PropertyName}' mora biti jednak '{ComparisonValue}'.",
+		"EqualValidator" => "'{PropertyName}' mora biti jednako '{ComparisonValue}'.",
 		"ExactLengthValidator" => "'{PropertyName}' mora imati tačno {MaxLength} karaktera. Uneseno je {TotalLength} karaktera.",
 		"InclusiveBetweenValidator" => "'{PropertyName}' mora biti između {From} i {To}. Uneseno je {PropertyValue}.",
 		"ExclusiveBetweenValidator" => "'{PropertyName}' mora biti između {From} i {To} (ekskluzivno). Uneseno je {PropertyValue}.",
-		"CreditCardValidator" => "'{PropertyName}' nije validna kreditna kartica.",
-		"ScalePrecisionValidator" => "'{PropertyName}' ne smije imati više od {ExpectedPrecision} cifara, sa dozvoljenih {ExpectedScale} decimalnih mjesta. Uneseno je {Digits} cifara i {ActualScale} decimalnih mjesta.",
+		"CreditCardValidator" => "'{PropertyName}' nije validan broj kreditne kartice.",
+		"ScalePrecisionValidator" => "'{PropertyName}' ne sme imati više od {ExpectedPrecision} cifara, sa dozvoljenih {ExpectedScale} decimalnih mesta. Uneseno je {Digits} cifara i {ActualScale} decimalnih mesta.",
 		"EmptyValidator" => "'{PropertyName}' mora biti prazno.",
 		"NullValidator" => "'{PropertyName}' mora biti prazno.",
-		"EnumValidator" => "'{PropertyName}' ima raspon vrijednosti koji ne uključuje '{PropertyValue}'.",
+		"EnumValidator" => "'{PropertyName}' ima raspon vrednosti koji ne uključuje '{PropertyValue}'.",
 		// Additional fallback messages used by clientside validation integration.
 		"Length_Simple" => "'{PropertyName}' mora imati između {MinLength} i {MaxLength} karaktera.",
 		"MinimumLength_Simple" => "'{PropertyName}' mora imati najmanje {MinLength} karaktera.",
-		"MaximumLength_Simple" => "'{PropertyName}' ne smije imati više od {MaxLength} karaktera.",
+		"MaximumLength_Simple" => "'{PropertyName}' ne sme imati više od {MaxLength} karaktera.",
 		"ExactLength_Simple" => "'{PropertyName}' mora imati tačno {MaxLength} karaktera.",
 		"InclusiveBetween_Simple" => "'{PropertyName}' mora biti između {From} i {To}.",
 		_ => null,
