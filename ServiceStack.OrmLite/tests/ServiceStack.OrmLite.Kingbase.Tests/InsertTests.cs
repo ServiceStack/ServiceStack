bccc139946138a2f2de46bd722904679c3c13d24
pedoc <pedoc@qq.com>
2025-06-24 10:36:41 +0800

    fix: Remove unused usings and convert namespace and class to file-scoped style

--- ServiceStack.OrmLite/tests/ServiceStack.OrmLite.Kingbase.Tests/InsertTests.cs
+++ ServiceStack.OrmLite/tests/ServiceStack.OrmLite.Kingbase.Tests/InsertTests.cs
@@ -1,37 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using AutoFixture;
 using FluentAssertions;
 using NUnit.Framework;
 
-namespace ServiceStack.OrmLite.Kingbase.Tests
+namespace ServiceStack.OrmLite.Kingbase.Tests;
+
+public class InsertTests : KingbaseTestBase
 {
-    public class InsertTests : KingbaseTestBase
+    [Test]
+    public Task Kingbase_MySql_Model_Insert()
     {
-        [Test]
-        public Task Kingbase_MySql_Model_Insert()
-        {
-            var factory = BuildOrmLiteConnectionFactory(KingbaseDialect.MySql);
-            using var db = factory.OpenDbConnection();
+        var factory = BuildOrmLiteConnectionFactory(KingbaseDialect.MySql);
+        using var db = factory.OpenDbConnection();
 
-            db.DropAndCreateTable<Person>();
-            db.DropAndCreateTable<Address>();
-            db.TableExists<Person>().Should().BeTrue();
-            db.TableExists<Address>().Should().BeTrue();
-            var now = DateTime.Now;
-            var fixture = new Fixture();
+        db.DropAndCreateTable<Person>();
+        db.DropAndCreateTable<Address>();
+        db.TableExists<Person>().Should().BeTrue();
+        db.TableExists<Address>().Should().BeTrue();
+        var now = DateTime.Now;
+        var fixture = new Fixture();
 
-            var address = fixture.Create<Address>();
-            var id = db.Insert(address, selectIdentity: true);
-            address.Id = (int)id;
+        var address = fixture.Create<Address>();
+        var id = db.Insert(address, selectIdentity: true);
+        address.Id = (int)id;
 
-            var person = fixture.Create<Person>();
-            id = db.Insert(person, selectIdentity: true);
-            person.Id = (int)id;
-            return Task.CompletedTask;
-        }
+        var person = fixture.Create<Person>();
+        id = db.Insert(person, selectIdentity: true);
+        person.Id = (int)id;
+        return Task.CompletedTask;
     }
 }
