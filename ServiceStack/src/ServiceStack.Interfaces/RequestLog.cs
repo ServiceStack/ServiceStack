0a90f47d279597f0891e35da4b719a5f99f53cff
Demis Bellot <demis.bellot@gmail.com>
2025-09-30 14:44:31 +0800

    Move RequestLog data model to ServiceStack.Interfaces

--- ServiceStack/src/ServiceStack.Interfaces/RequestLog.cs
+++ ServiceStack/src/ServiceStack.Interfaces/RequestLog.cs
@@ -1,44 +1,43 @@
 #nullable enable
 
 using System;
-using System.Collections;
 using System.Collections.Generic;
+using ServiceStack;
 using ServiceStack.DataAnnotations;
 
-namespace ServiceStack;
-
 /// <summary>
-/// A log entry added by the IRequestLogger (In Memory, Redis, CSV, etc)
+/// Request Log Data Model for (SqliteRequestLogger, DbLogger)
 /// </summary>
-public class RequestLogEntry : IMeta
+public class RequestLog : IMeta
 {
     [AutoIncrement]
     public long Id { get; set; }
-    public string? TraceId { get; set; }
-    public string? OperationName { get; set; }
+    public string TraceId { get; set; }
+    public string OperationName { get; set; }
     public DateTime DateTime { get; set; }
     public int StatusCode { get; set; }
     public string? StatusDescription { get; set; }
     public string? HttpMethod { get; set; }
     public string? AbsoluteUri { get; set; }
     public string? PathInfo { get; set; }
+    public string? Request { get; set; }
     [StringLength(StringLengthAttribute.MaxText)]
     public string? RequestBody { get; set; }
-    public object? RequestDto { get; set; }
     public string? UserAuthId { get; set; }
     public string? SessionId { get; set; }
     public string? IpAddress { get; set; }
     public string? ForwardedFor { get; set; }
     public string? Referer { get; set; }
-    public Dictionary<string, string>? Headers { get; set; }
+    public Dictionary<string, string> Headers { get; set; } = [];
     public Dictionary<string, string>? FormData { get; set; }
-    public Dictionary<string, string>? Items { get; set; }
+    public Dictionary<string, string> Items { get; set; } = [];
     public Dictionary<string, string>? ResponseHeaders { get; set; }
-    public object? Session { get; set; }
-    public object? ResponseDto { get; set; }
-    public object? ErrorResponse { get; set; }
+    public string? Response { get; set; }
+    public string? ResponseBody { get; set; }
+    public string? SessionBody { get; set; }
+    public ResponseStatus? Error { get; set; }
     public string? ExceptionSource { get; set; }
-    public IDictionary? ExceptionData { get; set; }
+    public string? ExceptionDataBody { get; set; }
     public TimeSpan RequestDuration { get; set; }
     public Dictionary<string, string>? Meta { get; set; }
 }
