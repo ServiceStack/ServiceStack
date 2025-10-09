4f99aa091fa9ff55b42099a6e00af5b066955a94
Demis Bellot <demis.bellot@gmail.com>
2025-10-10 01:32:53 +0800

    Add C# example

---
+++ ServiceStack/tests/AdhocNew/clients/csharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using ServiceStack;
+using ServiceStack.AI;
+
+var client = new JsonApiClient("http://localhost:5166");
+client.BearerToken = "ak-87949de37e894627a9f6173154e7cafa";
+
+var api = client.Api(new ChatCompletion {
+    Model = "gemini-flash-latest",
+    Messages = [
+		new AiMessage {
+            Role = "user",
+            Content = [
+				new AiTextContent {
+                    Type = "text",
+                    Text = "Capital of France?"
+                }
+			]
+        }
+	]
+});
+
+ClientConfig.PrintDump(api.Response);
