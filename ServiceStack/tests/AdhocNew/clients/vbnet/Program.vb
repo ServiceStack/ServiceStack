37509ad4f1a1e36a350f71df749779844b09801a
Demis Bellot <demis.bellot@gmail.com>
2025-10-10 01:35:05 +0800

    Add VB.NET example

---
+++ ServiceStack/tests/AdhocNew/clients/vbnet/Program.vb
@@ -0,0 +1,27 @@
+Imports System
+Imports ServiceStack
+Imports ServiceStack.AI
+
+Module Program
+    Sub Main(args As String())
+        Dim client = New JsonApiClient("http://localhost:5166")
+        client.BearerToken = "ak-87949de37e894627a9f6173154e7cafa"
+
+        Dim api = client.Api(New ChatCompletion With {
+            .Model = "gemini-flash-latest",
+            .Messages = New List(Of AiMessage) From {
+                New AiMessage With {
+                    .Role = "user",
+                    .Content = New List(Of AiContent) From {
+                        New AiTextContent With {
+                            .Type = "text",
+                            .Text = "Capital of France?"
+                        }
+                    }
+                }
+            }
+        })
+
+        ClientConfig.PrintDump(api.Response)
+    End Sub
+End Module
