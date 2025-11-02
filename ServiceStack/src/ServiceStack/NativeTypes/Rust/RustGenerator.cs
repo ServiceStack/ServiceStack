15217835522041893fe9788e17381013b6c12be9
Demis Bellot <demis.bellot@gmail.com>
2025-11-03 03:15:13 +0800

    Update RustGenerator.cs

--- ServiceStack/src/ServiceStack/NativeTypes/Rust/RustGenerator.cs
+++ ServiceStack/src/ServiceStack/NativeTypes/Rust/RustGenerator.cs
@@ -261,7 +261,6 @@ public class RustGenerator : ILangGenerator
             sb.AppendLine("Version: {0}".Fmt(Env.VersionString));
             sb.AppendLine("Tip: {0}".Fmt(HelpMessages.NativeTypesDtoOptionsTip.Fmt("//")));
             sb.AppendLine("BaseUrl: {0}".Fmt(Config.BaseUrl));
-            sb.AppendLine("//RustPropertyStyle: ENABLED - Using snake_case with serde rename");
             sb.AppendLine();
             sb.AppendLine("{0}GlobalNamespace: {1}".Fmt(defaultValue("GlobalNamespace"), Config.GlobalNamespace));
             sb.AppendLine("{0}MakePropertiesOptional: {1}".Fmt(defaultValue("MakePropertiesOptional"), Config.MakePropertiesOptional));
