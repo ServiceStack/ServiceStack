3e515e87b97517c364eb2321cd2eae6f98428e55
Demis Bellot <demis.bellot@gmail.com>
2025-11-26 16:54:34 +0800

    Downgrade Microsoft.AspNetCore.OpenApi to use its Microsoft.OpenApi to avoid breaking changes

--- ServiceStack/tests/AdhocNew/Configure.OpenApi.cs
+++ ServiceStack/tests/AdhocNew/Configure.OpenApi.cs
@@ -1,5 +1,4 @@
-﻿using MyApp.Data;
-using Scalar.AspNetCore;
+﻿using Scalar.AspNetCore;
 
 [assembly: HostingStartup(typeof(MyApp.ConfigureOpenApi))]
 
@@ -12,22 +11,26 @@ public class ConfigureOpenApi : IHostingStartup
             if (context.HostingEnvironment.IsDevelopment())
             {
                 services.AddOpenApi();
-                services.AddServiceStackOpenApi(configure: metadata =>
-                {
-                    metadata.AddBasicAuth();
-                    //metadata.AddJwtBearer();
-                });
-            }
-        })
-        .Configure((context, app) => {
-            if (context.HostingEnvironment.IsDevelopment())
-            {
-                app.UseRouting();
-                app.UseEndpoints(endpoints =>
-                {
-                    endpoints.MapOpenApi();
-                    endpoints.MapScalarApiReference();
-                });
+                services.AddServiceStackOpenApi();
+                services.AddBasicAuth<Data.ApplicationUser>();
+                // services.AddJwtAuth();
+                services.AddApiKeys();
+
+                services.AddTransient<IStartupFilter,StartupFilter>();
             }
         });
+
+    public class StartupFilter : IStartupFilter
+    {
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next) => app =>
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapOpenApi();
+                endpoints.MapScalarApiReference();
+            });
+            next(app);
+        };
+    }
 }
