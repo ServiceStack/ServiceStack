31209dfc57f1911bc4057049119502f015401912
Demis Bellot <demis.bellot@gmail.com>
2026-02-09 19:45:47 +0800

    bump to v10.0.6

--- ServiceStack.Text/src/ServiceStack.Text/Env.cs
+++ ServiceStack.Text/src/ServiceStack.Text/Env.cs
@@ -136,7 +136,7 @@ public static class Env
 
     public static string VersionString { get; set; }
 
-    public static decimal ServiceStackVersion = 10.05m;
+    public static decimal ServiceStackVersion = 10.06m;
 
     public static bool IsLinux { get; set; }
 
