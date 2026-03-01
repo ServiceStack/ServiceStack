a3884060d8f39c2b28ac84dd8dfdaa54125d6c92
DeonHeyns <deonheyns@opt.local>
2026-03-01 11:08:17 -0400

    Remove protected from OnlyFields in SqlExpression

--- ServiceStack.OrmLite/src/ServiceStack.OrmLite/Expressions/SqlExpression.cs
+++ ServiceStack.OrmLite/src/ServiceStack.OrmLite/Expressions/SqlExpression.cs
@@ -26,7 +26,7 @@ namespace ServiceStack.OrmLite
         private string groupBy = string.Empty;
         private string havingExpression;
         private string orderBy = string.Empty;
-        public HashSet<string> OnlyFields { get; protected set; }
+        public HashSet<string> OnlyFields { get; set; }
 
         public List<string> UpdateFields { get; set; }
         public List<string> InsertFields { get; set; }
