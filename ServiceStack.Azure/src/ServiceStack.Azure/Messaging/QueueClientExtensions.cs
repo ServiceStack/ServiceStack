4fef49c74d60bfa13c2e4ab0cca51f4ed3d11bd7
Deon Heyns <heynsd@gmail.com>
2026-04-30 14:15:17 +0100

    Update FromMessageBody comment

--- ServiceStack.Azure/src/ServiceStack.Azure/Messaging/QueueClientExtensions.cs
+++ ServiceStack.Azure/src/ServiceStack.Azure/Messaging/QueueClientExtensions.cs
@@ -10,6 +10,7 @@ public static class QueueClientExtensions
     {
         // WindowsAzure.ServiceBus (net472) is not wire-compatible with Azure.Messaging.ServiceBus (netcore).
         // Messages from the Windows client have a 64-char header and 2-char footer that must be stripped.
+        // This has been left here for any legacy messages that have yet to be processed using Azure.Messaging.ServiceBus.
         // See https://github.com/Azure/azure-service-bus-dotnet/issues/239
         if (strMessage.StartsWith("@\u0006string", StringComparison.Ordinal))
         {
