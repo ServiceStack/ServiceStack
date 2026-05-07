9f9bc3a81bf272555117e8441a0d1eb5d93f55a2
Demis Bellot <demis.bellot@gmail.com>
2026-05-07 23:10:37 +0800

    Fix Ambiguous Routes in .NET for UpdateEventSubscriber

--- ServiceStack/src/ServiceStack.Client/AuthDtos.cs
+++ ServiceStack/src/ServiceStack.Client/AuthDtos.cs
@@ -194,7 +194,6 @@ public class CancelRequestResponse : IHasResponseStatus, IMeta
 
 [ExcludeMetadata]
 [DataContract]
-[Route("/event-subscribers/{Id}")]
 public class UpdateEventSubscriber : IPost, IReturn<UpdateEventSubscriberResponse>
 {
     [DataMember(Order = 1)]
