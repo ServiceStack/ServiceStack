d5631c47dea29673f4c748875eb6f1f330d8ca4e
Demis Bellot <demis.bellot@gmail.com>
2026-08-11 21:58:02 +0800

    test for schema dictionary

--- ServiceStack/tests/ServiceStack.Extensions.Tests/ApiToolSchemaTests.cs
+++ ServiceStack/tests/ServiceStack.Extensions.Tests/ApiToolSchemaTests.cs
@@ -41,4 +41,22 @@ public class ApiDescribeSchemaTests
         Assert.That(actual["properties"]!["Id"]!["type"]!.GetValue<string>(), Is.EqualTo("integer"));
         Assert.That(actual["properties"]!["Name"]!["type"]!.GetValue<string>(), Is.EqualTo("string"));
     }
+
+    class ModelWithDict
+    {
+        public int Id { get; set; }
+        public System.Collections.Generic.Dictionary<string, string>? Meta { get; set; }
+    }
+
+    [Test]
+    public void Metadata_schema_generator_creates_additionalProperties_for_dictionary()
+    {
+        var schema = MetadataSchemaGenerator.CreateModelSchema(typeof(ModelWithDict));
+        var metaProp = schema["properties"]?["Meta"] as System.Text.Json.Nodes.JsonObject;
+        Assert.That(metaProp, Is.Not.Null);
+        Assert.That(metaProp!["type"]?.ToString(), Is.EqualTo("object"));
+        var additionalProps = metaProp["additionalProperties"] as System.Text.Json.Nodes.JsonObject;
+        Assert.That(additionalProps, Is.Not.Null);
+        Assert.That(additionalProps!["type"]?.ToString(), Is.EqualTo("string"));
+    }
 }
