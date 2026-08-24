7b144c18840167ccb346390ac0eb09b338a43318
Demis Bellot <demis.bellot@gmail.com>
2026-08-24 14:56:16 +0800

    Add custom tooltip for doc filters

--- ServiceStack/src/ServiceStack.AI.Chat/chat/ext/gemini/index.mjs
+++ ServiceStack/src/ServiceStack.AI.Chat/chat/ext/gemini/index.mjs
@@ -2270,8 +2270,8 @@ const GeminiPage = {
 
 const GeminiHeader = {
     template: `
-        <div data-tag="GeminiHeader" v-if="fileSearch" class="flex space-x-1 items-center cursor-pointer text-xs rounded transition-colors border" :class="[$styles.tagLabel]"
-            :title="fileSearch.title"
+        <div data-tag="GeminiHeader" v-if="fileSearch" class="flex space-x-1 items-center cursor-pointer text-xs rounded transition-colors border custom-tooltip" :class="[$styles.tagLabel]"
+            :data-title="fileSearch.title"
             @click="fileSearch.url ? $ctx.to(fileSearch.url) : null" style="line-height: 20px;"
         >
             <svg class="ml-1 size-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>Gemini File Search</title><path fill="currentColor" d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5t1.888-4.612T9.5 3t4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5t-1.312-3.187T9.5 5T6.313 6.313T5 9.5t1.313 3.188T9.5 14"/></svg>
